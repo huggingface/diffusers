86da45bc66ee7ca782d5f498b0cacb490051d0f6
patil-suraj <surajp815@gmail.com>
2022-06-13 14:29:31 +0200

    add BDDMPipeline in init

--- src/diffusers/pipelines/__init__.py
+++ src/diffusers/pipelines/__init__.py
@@ -2,3 +2,4 @@ from .pipeline_ddim import DDIM
 from .pipeline_ddpm import DDPM
 from .pipeline_glide import GLIDE
 from .pipeline_latent_diffusion import LatentDiffusion
+from .pipeline_bddm import BDDMPipeline
