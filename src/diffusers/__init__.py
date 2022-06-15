611163405f354dae60f32b2685a66087473f44a7
Patrick von Platen <patrick.v.platen@gmail.com>
2022-06-15 16:21:11 +0200

    v0.0.4-release

--- src/diffusers/__init__.py
+++ src/diffusers/__init__.py
@@ -2,7 +2,7 @@
 # There's no way to ignore "F401 '...' imported but unused" warnings in this
 # module, but to preserve other warnings. So, don't check this module at all.
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 from .modeling_utils import ModelMixin
 from .models.unet import UNetModel
