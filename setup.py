57a70b809ec1ab14f152f37347a9ac8802e92f3f
Patrick von Platen <patrick.v.platen@gmail.com>
2022-06-15 15:53:48 +0200

    v0.0.3-release

--- setup.py
+++ setup.py
@@ -176,7 +176,7 @@ install_requires = [
 
 setup(
     name="diffusers",
-    version="0.0.2",
+    version="0.0.3",
     description="Diffusers",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
