070a60fdadc1d4eee2f3460df6868dfd24580787
Arthur <arthur.zucker@gmail.com>
2022-07-18 18:36:48 +0200

    Update

--- scripts/convert_ncsnpp_original_checkpoint_to_diffusers.py
+++ scripts/convert_ncsnpp_original_checkpoint_to_diffusers.py
@@ -15,6 +15,7 @@
 """ Conversion script for the NCSNPP checkpoints. """
 
 import argparse
+from distutils.command import check
 import json
 import torch
 from diffusers import UNetUnconditionalModel 
@@ -144,45 +145,93 @@ def convert_ncsnpp_checkpoint(checkpoint, config):
 
 
     new_model_architecture  = UNetUnconditionalModel(**config)
-    get_block = lambda index: [k for k in checkpoint.keys() if k.startswith('all_modules.{}.'.format(index))]
     # compared to the `set_weights`function, all_modules are not available. `
-    
+
     module_index = 4
+
+
+    def set_attention_weights(new_layer,old_checkpoint,index):
+        new_layer.query.weight.data = old_checkpoint[f"all_modules.{index}.NIN_0.W"].data.T
+        new_layer.key.weight.data = old_checkpoint[f"all_modules.{index}.NIN_1.W"].data.T
+        new_layer.value.weight.data = old_checkpoint[f"all_modules.{index}.NIN_2.W"].data.T
+        
+        new_layer.query.bias.data = old_checkpoint[f"all_modules.{index}.NIN_0.b"].data
+        new_layer.key.bias.data = old_checkpoint[f"all_modules.{index}.NIN_1.b"].data
+        new_layer.value.bias.data = old_checkpoint[f"all_modules.{index}.NIN_2.b"].data
+
+        new_layer.proj_attn.weight.data = old_checkpoint[f"all_modules.{index}.NIN_3.W"].data.T
+        new_layer.proj_attn.bias.data = old_checkpoint[f"all_modules.{index}.NIN_3.b"].data
+
+        new_layer.group_norm.weight.data = old_checkpoint[f"all_modules.{index}.GroupNorm_0.weight"].data
+        new_layer.group_norm.bias.data = old_checkpoint[f"all_modules.{index}.GroupNorm_0.bias"].data
+
+    def set_resnet_weights(new_layer,old_checkpoint,index):
+        new_layer.conv1.weight.data = old_checkpoint[f"all_modules.{index}.Conv_0.weight"].data
+        new_layer.conv1.bias.data = old_checkpoint[f"all_modules.{index}.Conv_0.bias"].data
+        new_layer.norm1.weight.data = old_checkpoint[f"all_modules.{index}.GroupNorm_0.weight"].data
+        new_layer.norm1.bias.data = old_checkpoint[f"all_modules.{index}.GroupNorm_0.bias"].data
+
+        new_layer.conv2.weight.data = old_checkpoint[f"all_modules.{index}.Conv_1.weight"].data
+        new_layer.conv2.bias.data = old_checkpoint[f"all_modules.{index}.Conv_1.bias"].data
+        new_layer.norm2.weight.data = old_checkpoint[f"all_modules.{index}.GroupNorm_1.weight"].data
+        new_layer.norm2.bias.data = old_checkpoint[f"all_modules.{index}.GroupNorm_1.bias"].data
+                        
+        new_layer.time_emb_proj.weight.data = old_checkpoint[f"all_modules.{index}.Dense_0.weight"].data
+        new_layer.time_emb_proj.bias.data = old_checkpoint[f"all_modules.{index}.Dense_0.bias"].data
+
+        if new_layer.in_channels != new_layer.out_channels or new_layer.up or new_layer.down:
+            new_layer.nin_shortcut.weight.data = old_checkpoint[f"all_modules.{index}.Conv_2.weight"].data
+            new_layer.nin_shortcut.bias.data = old_checkpoint[f"all_modules.{index}.Conv_2.bias"].data
+
     for i, block in enumerate(new_model_architecture.downsample_blocks):
-        blocks_to_convert = []
-        while len(blocks_to_convert) < len(block.state_dict()): 
-            blocks_to_convert += get_block(module_index)
+        has_attentions = hasattr(block, "attentions")
+        if hasattr(block, "downsamplers") and block.downsamplers is not None:
+            set_resnet_weights(block.resnets_down,checkpoint, module_index)
+            module_index += 1
+            block.skip_conv.weight.data = checkpoint[f"all_modules.{module_index}.Conv_0.weight"].data
+            block.skip_conv.bias.data = checkpoint[f"all_modules.{module_index}.Conv_0.bias"].data
+            module_index += 1
+        
+        for j in range(len(block.resnets)):
+            set_resnet_weights(block.resnets[j],checkpoint, module_index)
+            module_index += 1
+            if has_attentions:
+                set_attention_weights(block.attentions[j],checkpoint, module_index)
+                module_index += 1
+                    
+    set_resnet_weights(new_model_architecture.mid.resnets[0],checkpoint,module_index)
+    module_index += 1
+    set_attention_weights(new_model_architecture.mid.attentions[0],checkpoint, module_index)
+    module_index += 1
+    set_resnet_weights(new_model_architecture.mid.resnets[1],checkpoint,module_index)
+
+    module_index += 1
+
+    for i, block in enumerate(new_model_architecture.downsample_blocks):
+        has_attentions = hasattr(block, "attentions")
+        for j in range(len(block.resnets)):
+            set_resnet_weights(block.resnets[j],checkpoint, module_index)
+            module_index += 1
+        if has_attentions:
+            set_attention_weights(block.attentions[0],checkpoint, module_index) # why can there only be a single attention layer for up? 
             module_index += 1
             
-        for index in range(len(blocks_to_convert)):
-            new_checkpoint[list(block.state_dict().keys())[index]] = checkpoint[blocks_to_convert[index]]
-        
-    # TODO convert the attentions in the mid to the diffusers format. Overall just concatenate the 3 query weight matrices into one.
-    # if the block has am attention layer should look for it in the checkpoints? 
-    
-
-    for i, block in enumerate(new_model_architecture.upsample_blocks):
-        for j in range(len(block.resnets)):
-            block.resnets[j].set_weight(new_model_architecture.all_modules[module_index])
-            module_index += 1
-        if hasattr(block, "attentions") and block.attentions is not None:
-            block.attentions[0].set_weight(new_model_architecture.all_modules[module_index])
-            module_index += 1
         if hasattr(block, "resnet_up") and block.resnet_up is not None:
-            block.skip_norm.weight.data = new_model_architecture.all_modules[module_index].weight.data
-            block.skip_norm.bias.data = new_model_architecture.all_modules[module_index].bias.data
+            block.skip_norm.weight.data = checkpoint[f"all_modules.{module_index}.weight"].data
+            block.skip_norm.bias.data = checkpoint[f"all_modules.{module_index}"].data
             module_index += 1
-            block.skip_conv.weight.data = new_model_architecture.all_modules[module_index].weight.data
-            block.skip_conv.bias.data = new_model_architecture.all_modules[module_index].bias.data
+            block.skip_conv.weight.data = checkpoint[f"all_modules.{module_index}.weight"].data
+            block.skip_conv.bias.data = checkpoint[f"all_modules.{module_index}.bias"].data
             module_index += 1
-            block.resnet_up.set_weight(new_model_architecture.all_modules[module_index])
+            
+            set_resnet_weights(block.resnet_up,checkpoint, module_index)
             module_index += 1
-
-    new_model_architecture.conv_norm_out.weight.data = new_model_architecture.all_modules[module_index].weight.data
-    new_model_architecture.conv_norm_out.bias.data = new_model_architecture.all_modules[module_index].bias.data
+                    
+    new_model_architecture.conv_norm_out.weight.data = checkpoint[f"all_modules.{module_index}.weight"].data
+    new_model_architecture.conv_norm_out.bias.data = checkpoint[f"all_modules.{module_index}.bias"].data
     module_index += 1
-    new_model_architecture.conv_out.weight.data = new_model_architecture.all_modules[module_index].weight.data
-    new_model_architecture.conv_out.bias.data = new_model_architecture.all_modules[module_index].bias.data
+    new_model_architecture.conv_out.weight.data = checkpoint[f"all_modules.{module_index}.weight"].data
+    new_model_architecture.conv_out.bias.data = checkpoint[f"all_modules.{module_index}.bias"].data
     
     get_block = lambda index: {k for k in checkpoint.keys() if k.split('.')[1]==str(index)  }
 
