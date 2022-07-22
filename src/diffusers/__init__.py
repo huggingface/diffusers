570d3f1eb92c723653b9f07177fb10f4bbc15a8b
Omar Sanseviero <osanseviero@gmail.com>
2022-07-22 13:29:42 +0200

    Expose LR schedulers (#80)
    * Expose schedulers
    
    * Update __init__.py
    
    Co-authored-by: Anton Lozhkov <anton@huggingface.co>

--- src/diffusers/__init__.py
+++ src/diffusers/__init__.py
@@ -8,9 +8,19 @@ __version__ = "0.1.2"
 
 from .modeling_utils import ModelMixin
 from .models import AutoencoderKL, UNet2DConditionModel, UNet2DModel, VQModel
+from .optimization import (
+    get_constant_schedule,
+    get_constant_schedule_with_warmup,
+    get_linear_schedule_with_warmup,
+    get_cosine_schedule_with_warmup,
+    get_cosine_with_hard_restarts_schedule_with_warmup,
+    get_polynomial_decay_schedule_with_warmup,
+    get_scheduler,
+)
 from .pipeline_utils import DiffusionPipeline
 from .pipelines import DDIMPipeline, DDPMPipeline, LDMPipeline, PNDMPipeline, ScoreSdeVePipeline
 from .schedulers import DDIMScheduler, DDPMScheduler, PNDMScheduler, SchedulerMixin, ScoreSdeVeScheduler
+from .training_utils import EMAModel
 
 
 if is_transformers_available():
