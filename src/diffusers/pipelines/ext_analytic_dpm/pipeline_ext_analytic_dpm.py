d397dfe83494414757b34902a0aa165dd3967899
Kashif Rasul <kashif.rasul@gmail.com>
2022-08-05 19:23:28 -0400

    updated api

--- src/diffusers/pipelines/ext_analytic_dpm/pipeline_ext_analytic_dpm.py
+++ src/diffusers/pipelines/ext_analytic_dpm/pipeline_ext_analytic_dpm.py
@@ -10,6 +10,8 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
+
+
 import torch
 
 import tqdm
@@ -18,11 +20,12 @@ from ...pipeline_utils import DiffusionPipeline
 
 
 class SNDPMPipeline(DiffusionPipeline):
-    def __init__(self, unet, sn_net, noise_scheduler):
+    def __init__(self, unet, sn_net, scheduler):
         super().__init__()
-        self.register_modules(unet=unet, noise_scheduler=noise_scheduler, sn_net=sn_net)
+        scheduler = scheduler.set_format("pt")
+        self.register_modules(unet=unet, noise_scheduler=scheduler, sn_net=sn_net)
 
-    def __call__(self, batch_size=1, generator=None, torch_device=None):
+    def __call__(self, batch_size=1, generator=None, torch_device=None, output_type="pil"):
         if torch_device is None:
             torch_device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -31,48 +34,53 @@ class SNDPMPipeline(DiffusionPipeline):
 
         # Sample gaussian noise to begin loop
         image = torch.randn(
-            (batch_size, self.unet.in_channels, self.unet.resolution, self.unet.resolution),
+            (batch_size, self.unet.in_channels, self.unet.sample_size, self.unet.sample_size),
             generator=generator,
         )
         image = image.to(torch_device)
 
-        num_prediction_steps = len(self.noise_scheduler)
-        for t in tqdm.tqdm(reversed(range(num_prediction_steps)), total=num_prediction_steps):
+        # set step values
+        self.scheduler.set_timesteps(1000)
+
+        for t in tqdm(self.scheduler.timesteps):
             # 1. predict noise residual
-            with torch.no_grad():
-                residual = self.unet(image, t)
-                noise_square = self.sn_net(image, t)
-                noise_residual_square = noise_square - residual**2
+            model_output = self.unet(image, t)["sample"]
+            noise_square = self.sn_net(image, t)["sample"]
+            noise_residual_square = noise_square - model_output**2
 
             # 2. predict previous mean of image x_t-1
-            pred_prev_image = self.noise_scheduler.step(residual, image, t)
+            pred_prev_image = self.scheduler.step(model_output, t, image)["prev_sample"]
 
             # 3. optionally sample variance via equation (10)
             variance = 0
             if t > 0:
-                gamma = (torch.sqrt(self.noise_scheduler.alphas_cumprod[t - 1]) * self.noise_scheduler.betas[t]) / (
-                    1 - self.noise_scheduler.alphas_cumprod[t]
+                gamma = (torch.sqrt(self.scheduler.alphas_cumprod[t - 1]) * self.noise_scheduler.betas[t]) / (
+                    1 - self.scheduler.alphas_cumprod[t]
                 )
-                beta_bar_div_alpha_bar = (
-                    1 - self.noise_scheduler.alphas_cumprod[t]
-                ) / self.noise_scheduler.alphas_cumprod[t]
+                beta_bar_div_alpha_bar = (1 - self.scheduler.alphas_cumprod[t]) / self.scheduler.alphas_cumprod[t]
                 noise = torch.randn(image.shape, generator=generator).to(image.device)
                 variance = (
-                    self.noise_scheduler.get_variance(t) + gamma**2 * beta_bar_div_alpha_bar * noise_residual_square
+                    self.scheduler.get_variance(t) + gamma**2 * beta_bar_div_alpha_bar * noise_residual_square
                 ).sqrt() * noise
 
             # 4. set current image to prev_image: x_t -> x_t-1
             image = pred_prev_image + variance
 
-        return image
+        image = (image / 2 + 0.5).clamp(0, 1)
+        image = image.cpu().permute(0, 2, 3, 1).numpy()
+        if output_type == "pil":
+            image = self.numpy_to_pil(image)
+
+        return {"sample": image}
 
 
 class NPRDPMPipeline(DiffusionPipeline):
-    def __init__(self, unet, npr_net, noise_scheduler):
+    def __init__(self, unet, npr_net, scheduler):
         super().__init__()
-        self.register_modules(unet=unet, noise_scheduler=noise_scheduler, npr_net=npr_net)
+        scheduler = scheduler.set_format("pt")
+        self.register_modules(unet=unet, scheduler=scheduler, npr_net=npr_net)
 
-    def __call__(self, batch_size=1, generator=None, torch_device=None):
+    def __call__(self, batch_size=1, generator=None, torch_device=None, output_type="pil"):
         if torch_device is None:
             torch_device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -81,37 +89,40 @@ class NPRDPMPipeline(DiffusionPipeline):
 
         # Sample gaussian noise to begin loop
         image = torch.randn(
-            (batch_size, self.unet.in_channels, self.unet.resolution, self.unet.resolution),
+            (batch_size, self.unet.in_channels, self.unet.sample_size, self.unet.sample_size),
             generator=generator,
         )
         image = image.to(torch_device)
 
-        num_prediction_steps = len(self.noise_scheduler)
-        for t in tqdm.tqdm(reversed(range(num_prediction_steps)), total=num_prediction_steps):
-            # 1. predict noise residual
-            with torch.no_grad():
-                residual = self.unet(image, t)
-                noise_pred_residual_square = self.npr_net(image, t)
+        # set step values
+        self.scheduler.set_timesteps(1000)
+
+        for t in tqdm(self.scheduler.timesteps):
+            # 1. predict noise and residual
+            model_output = self.unet(image, t)["sample"]
+            noise_pred_residual_square = self.npr_net(image, t)["sample"]
 
             # 2. predict previous mean of image x_t-1
-            pred_prev_image = self.noise_scheduler.step(residual, image, t)
+            pred_prev_image = self.noise_scheduler.step(model_output, image, t)["prev_sample"]
 
             # 3. optionally sample variance via equation (16)
             variance = 0
             if t > 0:
-                gamma = (torch.sqrt(self.noise_scheduler.alphas_cumprod[t - 1]) * self.noise_scheduler.betas[t]) / (
-                    1 - self.noise_scheduler.alphas_cumprod[t]
+                gamma = (torch.sqrt(self.scheduler.alphas_cumprod[t - 1]) * self.noise_scheduler.betas[t]) / (
+                    1 - self.scheduler.alphas_cumprod[t]
                 )
-                beta_bar_div_alpha_bar = (
-                    1 - self.noise_scheduler.alphas_cumprod[t]
-                ) / self.noise_scheduler.alphas_cumprod[t]
-                noise = torch.randn(image.shape, generator=generator).to(image.device)
+                beta_bar_div_alpha_bar = (1 - self.scheduler.alphas_cumprod[t]) / self.scheduler.alphas_cumprod[t]
+                noise = torch.randn(model_output.shape, generator=generator).to(model_output.device)
                 variance = (
-                    self.noise_scheduler.get_variance(t)
-                    + gamma**2 * beta_bar_div_alpha_bar * noise_pred_residual_square
+                    self.scheduler.get_variance(t) + gamma**2 * beta_bar_div_alpha_bar * noise_pred_residual_square
                 ).sqrt() * noise
 
             # 4. set current image to prev_image: x_t -> x_t-1
             image = pred_prev_image + variance
 
-        return image
+        image = (image / 2 + 0.5).clamp(0, 1)
+        image = image.cpu().permute(0, 2, 3, 1).numpy()
+        if output_type == "pil":
+            image = self.numpy_to_pil(image)
+
+        return {"sample": image}
