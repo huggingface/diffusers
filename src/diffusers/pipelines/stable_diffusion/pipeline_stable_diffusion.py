Fix derivatives

src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion.py
@@ -110,19 +110,14 @@ class StableDiffusionPipeline(DiffusionPipeline):
             latents = latents * self.scheduler.sigmas[0]
 
         for i, t in tqdm(enumerate(self.scheduler.timesteps)):
-            print()
             # expand the latents if we are doing classifier free guidance
             latent_model_input = torch.cat([latents] * 2) if do_classifier_free_guidance else latents
             if isinstance(self.scheduler, LmsDiscreteScheduler):
                 sigma = self.scheduler.sigmas[i]
-                print("c_in", (1 / (sigma**2 + 1) ** 0.5).item())
-                print("sigma_to_t", t)
-                latent_model_input = latent_model_input / (sigma**2 + 1) ** 0.5
+                latent_model_input = latent_model_input / ((sigma**2 + 1) ** 0.5)
 
             # predict the noise residual
-            noise_pred = self.unet(
-                latent_model_input, t, encoder_hidden_states=text_embeddings
-            )["sample"]
+            noise_pred = self.unet(latent_model_input, t, encoder_hidden_states=text_embeddings)["sample"]
 
             # perform guidance
             if do_classifier_free_guidance:

tests/test_modeling_utils.py
@@ -978,32 +978,20 @@ class PipelineTesterMixin(unittest.TestCase):
         expected_slice = np.array([0.26815, 0.1581, 0.2658, 0.23248, 0.1550, 0.2539, 0.1131, 0.1024, 0.0837])
         assert np.abs(image_slice.flatten() - expected_slice).max() < 1e-2
 
-    #@slow
+    # @slow
     def test_lms_stable_diffusion_pipeline(self):
-        from PIL import Image
-
         model_id = "CompVis/stable-diffusion-v1-1-diffusers"
         pipe = StableDiffusionPipeline.from_pretrained(model_id, use_auth_token=True)
         scheduler = LmsDiscreteScheduler.from_config(model_id, subfolder="scheduler", use_auth_token=True)
         pipe.scheduler = scheduler
 
         prompt = "a photograph of an astronaut riding a horse"
-        generator = torch.Generator(device='cuda')
-        generator.manual_seed(0)
-        image = pipe([prompt], generator=generator, guidance_scale=7.5, num_inference_steps=50, output_type="numpy")[
+        generator = torch.Generator(device=torch_device).manual_seed(0)
+        image = pipe([prompt], generator=generator, guidance_scale=7.5, num_inference_steps=10, output_type="numpy")[
             "sample"
         ]
 
-        # def make_grid(images, rows, cols):
-        #     w, h = images[0].size
-        #     grid = Image.new('RGB', size=(cols * w, rows * h))
-        #     for i, image in enumerate(images):
-        #         grid.paste(image, box=(i % cols * w, i // cols * h))
-        #     return grid
-        #
-        # image = make_grid(image, 2, 2)
-
         image_slice = image[0, -3:, -3:, -1]
         assert image.shape == (1, 512, 512, 3)
-        expected_slice = np.array([0.0926, 0.0537, 0.0521, 0.0740, 0.0619, 0.0390, 0.0363, 0.0316, 0.0603])
+        expected_slice = np.array([0.9077, 0.9254, 0.9181, 0.9227, 0.9213, 0.9367, 0.9399, 0.9406, 0.9024])
         assert np.abs(image_slice.flatten() - expected_slice).max() < 1e-2