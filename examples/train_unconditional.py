a452dc9a1b9b280da26daffd8bf51c8d176c1083
vgvm <luckybit4755@gmail.com>
2022-08-18 16:35:25 -0400

    load existing model if found locally, options to periodically checkpoint the model and timestamp the test_samples

--- examples/train_unconditional.py
+++ examples/train_unconditional.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 
+from datetime import datetime
+
 import torch
 import torch.nn.functional as F
 
@@ -34,29 +36,37 @@ def main(args):
         logging_dir=logging_dir,
     )
 
-    model = UNet2DModel(
-        sample_size=args.resolution,
-        in_channels=3,
-        out_channels=3,
-        layers_per_block=2,
-        block_out_channels=(128, 128, 256, 256, 512, 512),
-        down_block_types=(
-            "DownBlock2D",
-            "DownBlock2D",
-            "DownBlock2D",
-            "DownBlock2D",
-            "AttnDownBlock2D",
-            "DownBlock2D",
-        ),
-        up_block_types=(
-            "UpBlock2D",
-            "AttnUpBlock2D",
-            "UpBlock2D",
-            "UpBlock2D",
-            "UpBlock2D",
-            "UpBlock2D",
-        ),
-    )
+
+    model_dir = f"{args.output_dir}/unet"
+    if os.path.exists(model_dir):
+        print(f"reloading model from {model_dir}")
+        model = UNet2DModel.from_pretrained(model_dir)
+        print(f"reloaded model from {model_dir}")
+    else:
+        print(f"creating fresh model")
+        model = UNet2DModel(
+            sample_size=args.resolution,
+            in_channels=3,
+            out_channels=3,
+            layers_per_block=2,
+            block_out_channels=(128, 128, 256, 256, 512, 512),
+            down_block_types=(
+                "DownBlock2D",
+                "DownBlock2D",
+                "DownBlock2D",
+                "DownBlock2D",
+                "AttnDownBlock2D",
+                "DownBlock2D",
+            ),
+            up_block_types=(
+                "UpBlock2D",
+                "AttnUpBlock2D",
+                "UpBlock2D",
+                "UpBlock2D",
+                "UpBlock2D",
+                "UpBlock2D",
+            ),
+        )
     noise_scheduler = DDPMScheduler(num_train_timesteps=1000, tensor_format="pt")
     optimizer = torch.optim.AdamW(
         model.parameters(),
@@ -171,8 +181,13 @@ def main(args):
 
                 # denormalize the images and save to tensorboard
                 images_processed = (images * 255).round().astype("uint8")
+                epoch06 = str(epoch).zfill( 6 )
+                if args.timestamp_test_samples:
+                    test_samples = datetime.now().strftime(f"test_samples-%Y-%m-%d+%H-%M-%S+{epoch06}")
+                else:
+                    test_samples = "test_samples"
                 accelerator.trackers[0].writer.add_images(
-                    "test_samples", images_processed.transpose(0, 3, 1, 2), epoch
+                    test_samples, images_processed.transpose(0, 3, 1, 2), epoch
                 )
 
             if epoch % args.save_model_epochs == 0 or epoch == args.num_epochs - 1:
@@ -180,6 +195,12 @@ def main(args):
                 if args.push_to_hub:
                     push_to_hub(args, pipeline, repo, commit_message=f"Epoch {epoch}", blocking=False)
                 else:
+                    if not 0 == args.checkpoint_model_epochs and epoch % args.checkpoint_model_epochs == 0 and os.path.exists(model_dir):
+                        checkpoints_dir = f"{args.output_dir}/checkpoints"
+                        os.makedirs(checkpoints_dir, exist_ok=True)
+                        checkpoint_dir = datetime.now().strftime(f"{checkpoints_dir}/checkpoint-%Y-%m-%d+%H-%M-%S")
+                        os.rename(model_dir,checkpoint_dir)
+                        print(f"checkpointed {model_dir} to {checkpoint_dir}")
                     pipeline.save_pretrained(args.output_dir)
         accelerator.wait_for_everyone()
 
@@ -219,14 +240,26 @@ if __name__ == "__main__":
     parser.add_argument("--hub_model_id", type=str, default=None)
     parser.add_argument("--hub_private_repo", action="store_true")
     parser.add_argument("--logging_dir", type=str, default="logs")
+    parser.add_argument("--timestamp_test_samples", action="store_true")
+    parser.add_argument(
+        "--checkpoint_model_epochs",
+        type=int,
+        default=0,
+        help=(
+            "Make periodic backups of the model on save. "
+            "The value should be n * save_model_epochs value, "
+            "where n is a positive integer. "
+            "A value of 0 disables the behavior and is the default."
+        ),
+    )
     parser.add_argument(
         "--mixed_precision",
         type=str,
         default="no",
         choices=["no", "fp16", "bf16"],
         help=(
-            "Whether to use mixed precision. Choose"
-            "between fp16 and bf16 (bfloat16). Bf16 requires PyTorch >= 1.10."
+            "Whether to use mixed precision. Choose "
+            "between fp16 and bf16 (bfloat16). Bf16 requires PyTorch >= 1.10. "
             "and an Nvidia Ampere GPU."
         ),
     )
