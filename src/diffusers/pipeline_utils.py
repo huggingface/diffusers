71881d4231fe4a20d6989953bfd69dfa847521e0
Patrick von Platen <patrick.v.platen@gmail.com>
2022-08-30 11:49:21 +0200

    add some tests

--- src/diffusers/pipeline_utils.py
+++ src/diffusers/pipeline_utils.py
@@ -271,6 +271,10 @@ class DiffusionPipeline(ConfigMixin):
     def progress_bar(self, iterable):
         if not hasattr(self, "_progress_bar_config"):
             self._progress_bar_config = {}
+        elif not isinstance(self._progress_bar_config, dict):
+            raise ValueError(
+                f"`self._progress_bar_config` should be of type `dict`, but is {type(self._progress_bar_config)}."
+            )
 
         return tqdm(iterable, **self._progress_bar_config)
 
