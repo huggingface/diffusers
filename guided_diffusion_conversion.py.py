a619dc1efc04edfcf1170c9b45685073f7927d0a
ydshieh <ydshieh@users.noreply.github.com>
2022-09-05 20:52:23 +0200

    A script for conversion

---
+++ guided_diffusion_conversion.py.py
@@ -0,0 +1,61 @@
+# --------------------------------------------------------------------------------
+# diffusers/src
+
+from diffusers import UNet2DModel
+
+repo_id = "google/ddpm-church-256"
+model = UNet2DModel.from_pretrained(repo_id)
+print(type(model.config))
+
+config = dict(model.config)
+config["block_out_channels"] = [
+    256,
+    256,
+    512,
+    512,
+    1024,
+    1024,
+  ]
+model = UNet2DModel(**config)
+
+print(model)
+model.save_pretrained("ddpm-church-256-custom")
+
+# --------------------------------------------------------------------------------
+# diffusers/src
+
+import torch
+
+ckpt = "ddpm-church-256-custom/diffusion_pytorch_model.bin"
+model = torch.load(ckpt)
+
+# print(model)
+# print(model.keys())
+
+# for k in model.keys():
+#     if k.startswith("time_"):
+#         print(k)
+
+info = {k: str(list(v.shape)) for k, v in model.items()}
+import json
+with open("target.json", "w") as fp:
+    json.dump(info, fp, indent=4)
+
+# --------------------------------------------------------------------------------
+# diffusers/src
+
+import torch
+
+ckpt = "checkpoints/256x256_diffusion_uncond.pt"
+model = torch.load(ckpt)
+# print(model)
+# print(model.keys())
+
+# for k in model.keys():
+#    if k.startswith("time_embed"):
+#        print(k)
+
+info = {k: str(list(v.shape)) for k, v in model.items()}
+import json
+with open("guided-diffusion.json", "w") as fp:
+    json.dump(info, fp, indent=4)
