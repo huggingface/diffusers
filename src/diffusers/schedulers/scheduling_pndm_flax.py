359ec4e2d7503a66bd7c222b1ae639d0df5e19f4
Kashif Rasul <kashif.rasul@gmail.com>
2022-09-13 18:02:05 +0200

    round instead of cast

--- src/diffusers/schedulers/scheduling_pndm_flax.py
+++ src/diffusers/schedulers/scheduling_pndm_flax.py
@@ -166,7 +166,7 @@ class FlaxPNDMScheduler(SchedulerMixin, ConfigMixin):
         step_ratio = self.config.num_train_timesteps // num_inference_steps
         # creates integer timesteps by multiplying by ratio
         # casting to int to avoid issues when num_inference_step is power of 3
-        _timesteps = (jnp.arange(0, num_inference_steps) * step_ratio).astype(jnp.int64)[::-1].copy()
+        _timesteps = (jnp.arange(0, num_inference_steps) * step_ratio).round()[::-1].copy()
         _timesteps = _timesteps + offset
 
         state = state.replace(num_inference_steps=num_inference_steps, _offset=offset, _timesteps=_timesteps)
