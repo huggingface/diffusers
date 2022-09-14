91559f3107c7d31763094dae4076837d81cbcea1
Mishig Davaadorj <dmishig@gmail.com>
2022-09-14 08:23:27 +0000

    Rm unused method `framework`

--- src/diffusers/modeling_flax_utils.py
+++ src/diffusers/modeling_flax_utils.py
@@ -55,13 +55,6 @@ class FlaxModelMixin:
         """
         return cls(config, **kwargs)
 
-    @property
-    def framework(self) -> str:
-        """
-        :str: Identifies that this is a Flax model.
-        """
-        return "flax"
-
     def _cast_floating_to(self, params: Union[Dict, FrozenDict], dtype: jnp.dtype, mask: Any = None) -> Any:
         """
         Helper method to cast floating-point values of given parameter `PyTree` to given `dtype`.
