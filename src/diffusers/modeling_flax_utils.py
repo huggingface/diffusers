6842d29e30a497f52331c90d277241bde9fad3e9
Patrick von Platen <patrick.v.platen@gmail.com>
2022-09-14 15:07:55 +0200

    correct ignore kwargs

--- src/diffusers/modeling_flax_utils.py
+++ src/diffusers/modeling_flax_utils.py
@@ -45,6 +45,7 @@ class FlaxModelMixin:
     """
     config_name = CONFIG_NAME
     _automatically_saved_args = ["_diffusers_version", "_class_name", "_name_or_path"]
+    _flax_internal_args = ["name", "parent"]
 
     @classmethod
     def _from_config(cls, config, **kwargs):
