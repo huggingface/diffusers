869014ca901aabd82b7e053feb251caed74f9484
Mishig Davaadorj <dmishig@gmail.com>
2022-09-15 14:54:57 +0000

    docstring diffusers

--- src/diffusers/modeling_flax_utils.py
+++ src/diffusers/modeling_flax_utils.py
@@ -427,10 +427,7 @@ class FlaxModelMixin:
                 f"Some weights of the model checkpoint at {pretrained_model_name_or_path} were not used when"
                 f" initializing {model.__class__.__name__}: {unexpected_keys}\n- This IS expected if you are"
                 f" initializing {model.__class__.__name__} from the checkpoint of a model trained on another task or"
-                " with another architecture (e.g. initializing a BertForSequenceClassification model from a"
-                " BertForPreTraining model).\n- This IS NOT expected if you are initializing"
-                f" {model.__class__.__name__} from the checkpoint of a model that you expect to be exactly identical"
-                " (initializing a BertForSequenceClassification model from a BertForSequenceClassification model)."
+                " with another architecture."
             )
         else:
             logger.info(f"All model checkpoint weights were used when initializing {model.__class__.__name__}.\n")
