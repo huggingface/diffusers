91dbe7828e166307b46930e8db6a5f27e9ef46f5
Patrick von Platen <patrick.v.platen@gmail.com>
2022-09-15 16:47:01 +0200

    update

--- src/diffusers/pipeline_utils.py
+++ src/diffusers/pipeline_utils.py
@@ -31,8 +31,9 @@ from tqdm.auto import tqdm
 
 from .configuration_utils import ConfigMixin
 from .modeling_utils import WEIGHTS_NAME
+from .onnx_utils import ONNX_WEIGHTS_NAME
 from .schedulers.scheduling_utils import SCHEDULER_CONFIG_NAME
-from .utils import DIFFUSERS_CACHE, BaseOutput, logging, CONFIG_NAME
+from .utils import CONFIG_NAME, DIFFUSERS_CACHE, BaseOutput, logging
 
 
 INDEX_FILE = "diffusion_pytorch_model.bin"
@@ -299,7 +300,7 @@ class DiffusionPipeline(ConfigMixin):
             # make sure we only download sub-folders and `diffusers` filenames
             folder_names = [k for k in config_dict.keys() if not k.startswith("_")]
             allow_patterns = [os.path.join(k, "*") for k in folder_names]
-            allow_patterns += [WEIGHTS_NAME, SCHEDULER_CONFIG_NAME, CONFIG_NAME, cls.config_name]
+            allow_patterns += [WEIGHTS_NAME, SCHEDULER_CONFIG_NAME, CONFIG_NAME, ONNX_WEIGHTS_NAME, cls.config_name]
 
             # download all allow_patterns
             cached_folder = snapshot_download(
