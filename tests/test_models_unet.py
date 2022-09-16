76b8e4a0a21957a275eefa3863ee39215f0f5f6c
Pi Esposito <piero.skywalker@gmail.com>
2022-09-16 12:44:31 -0300

    add test to ensure results are the same when loading with accelerate

--- tests/test_models_unet.py
+++ tests/test_models_unet.py
@@ -15,6 +15,7 @@
 
 import math
 import unittest
+import gc
 
 import torch
 
@@ -139,6 +140,35 @@ class UNetLDMModelTests(ModelTesterMixin, unittest.TestCase):
         image = model(**self.dummy_input).sample
 
         assert image is not None, "Make sure output is not None"
+
+    def test_from_pretrained_accelerate_wont_change_results(self):
+        model_accelerate, _ = UNet2DModel.from_pretrained("fusing/unet-ldm-dummy-update", output_loading_info=True, device_map="auto")
+        model_accelerate.to(torch_device)
+        model_accelerate.eval()
+
+        noise = torch.randn(
+            1,
+            model_accelerate.config.in_channels,
+            model_accelerate.config.sample_size,
+            model_accelerate.config.sample_size,
+            generator=torch.manual_seed(0),
+        )
+        noise = noise.to(torch_device)
+        time_step = torch.tensor([10] * noise.shape[0]).to(torch_device)
+
+        arr_accelerate = model_accelerate(noise, time_step)["sample"]
+
+        # two models don't need to stay in the device at the same time
+        del model_accelerate
+        torch.cuda.empty_cache()
+        gc.collect()
+
+        model_normal_load, _ = UNet2DModel.from_pretrained("fusing/unet-ldm-dummy-update", output_loading_info=True)
+        model_normal_load.to(torch_device)
+        model_normal_load.eval()
+        arr_normal_load = model_normal_load(noise, time_step)["sample"]
+        
+        assert torch.allclose(arr_accelerate["sample"], arr_normal_load, rtol=1e-3)
 
     def test_output_pretrained(self):
         model = UNet2DModel.from_pretrained("fusing/unet-ldm-dummy-update")
