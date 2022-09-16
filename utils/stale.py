c3f13ac9e44e23978f0ba789b5202542da057b3d
anton-l <anton@huggingface.co>
2022-09-16 12:03:23 +0200

    Make sure not to spam

--- utils/stale.py
+++ utils/stale.py
@@ -38,8 +38,12 @@ def main():
     open_issues = repo.get_issues(state="open")
 
     for issue in open_issues:
+        comments = sorted([comment for comment in issue.get_comments()], key=lambda i: i.created_at, reverse=True)
+        last_comment = comments[0] if len(comments) > 0 else None
         if (
-            (dt.utcnow() - issue.updated_at).days > 23
+            last_comment is not None
+            and last_comment.user.login != "github-actions[bot]"
+            and (dt.utcnow() - issue.updated_at).days > 23
             and (dt.utcnow() - issue.created_at).days >= 30
             and not any(label.name.lower() in LABELS_TO_EXEMPT for label in issue.get_labels())
         ):
