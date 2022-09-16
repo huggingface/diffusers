b404452b6bb3966385296b2777167ee003c0c10c
Anton Lozhkov <aglozhkov@gmail.com>
2022-09-16 18:56:30 +0200

    Update tests/test_scheduler.py

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -902,11 +902,7 @@ class LMSDiscreteSchedulerTest(SchedulerCommonTest):
 
             scheduler.set_timesteps(self.num_inference_steps)
             scheduler_pt.set_timesteps(self.num_inference_steps)
-
-            # copy over dummy past residuals (must be done after set_timesteps)
-            scheduler.ets = dummy_past_residuals[:]
-            scheduler_pt.ets = dummy_past_residuals_pt[:]
-
+            
             output = scheduler.step(residual, 1, sample).prev_sample
             output_pt = scheduler_pt.step(residual_pt, 1, sample_pt).prev_sample
             assert np.sum(np.abs(output - output_pt.numpy())) < 1e-4, "Scheduler outputs are not identical"
