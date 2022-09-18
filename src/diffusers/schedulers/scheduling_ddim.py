c1bb9577a8dd4b874d154358d1f33b191815f473
Kashif Rasul <kashif.rasul@gmail.com>
2022-09-18 18:34:04 +0200

    remove match_shape

--- src/diffusers/schedulers/scheduling_ddim.py
+++ src/diffusers/schedulers/scheduling_ddim.py
@@ -265,9 +265,15 @@ class DDIMScheduler(ConfigMixin):
     ) -> torch.FloatTensor:
         timesteps = timesteps.to(self.alphas_cumprod.device)
         sqrt_alpha_prod = self.alphas_cumprod[timesteps] ** 0.5
+        sqrt_alpha_prod = sqrt_alpha_prod.flatten()
+        while len(sqrt_alpha_prod.shape) < len(original_samples.shape):
+            sqrt_alpha_prod = sqrt_alpha_prod.unsqueeze(-1)
         sqrt_alpha_prod = self.match_shape(sqrt_alpha_prod, original_samples)
+
         sqrt_one_minus_alpha_prod = (1 - self.alphas_cumprod[timesteps]) ** 0.5
-        sqrt_one_minus_alpha_prod = self.match_shape(sqrt_one_minus_alpha_prod, original_samples)
+        sqrt_one_minus_alpha_prod = sqrt_one_minus_alpha_prod.flatten()
+        while len(sqrt_one_minus_alpha_prod.shape) < len(original_samples.shape):
+            sqrt_one_minus_alpha_prod = sqrt_one_minus_alpha_prod.unsqueeze(-1)
 
         noisy_samples = sqrt_alpha_prod * original_samples + sqrt_one_minus_alpha_prod * noise
         return noisy_samples
