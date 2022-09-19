882542c5821e14e682b9751c096f18ea82380ad0
Pedro Cuenca <pedro@huggingface.co>
2022-09-19 15:24:15 +0200

    Pass dtype to modules
    Co-authored-by: Suraj Patil <surajp815@gmail.com>

--- src/diffusers/pipelines/stable_diffusion/safety_checker_flax.py
+++ src/diffusers/pipelines/stable_diffusion/safety_checker_flax.py
@@ -42,7 +42,7 @@ class FlaxStableDiffusionSafetyChecker(nn.Module, FlaxModelMixin, ConfigMixin):
 
     def setup(self):
         clip_vision_config = CLIPVisionConfig(**self.vision_config)
-        self.vision_model = FlaxCLIPVisionModule(clip_vision_config)
+        self.vision_model = FlaxCLIPVisionModule(clip_vision_config, dtype=self.dtype)
         self.visual_projection = nn.Dense(self.projection_dim, use_bias=False)
 
         self.concept_embeds = self.param("concept_embeds", jax.nn.initializers.ones, (17, self.projection_dim))
