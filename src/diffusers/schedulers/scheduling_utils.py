4c91e362dbcf2ed7638bcb4aff96466b4a3991e6
Kashif Rasul <kashif.rasul@gmail.com>
2022-09-19 11:40:25 +0200

    removed unsed imports

--- src/diffusers/schedulers/scheduling_utils.py
+++ src/diffusers/schedulers/scheduling_utils.py
@@ -12,9 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from dataclasses import dataclass
-from typing import Union
 
-import numpy as np
 import torch
 
 from ..utils import BaseOutput
