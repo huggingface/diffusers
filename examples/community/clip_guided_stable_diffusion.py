e33b49c4ca4b22eb4cb931d3451603d38420264a
patil-suraj <surajp815@gmail.com>
2022-09-19 13:28:49 +0200

    add credits

--- examples/community/clip_guided_stable_diffusion.py
+++ examples/community/clip_guided_stable_diffusion.py
@@ -44,6 +44,10 @@ def set_requires_grad(model, value):
 
 
 class CLIPGuidedStableDiffusion(DiffusionPipeline):
+    """CLIP guided stable diffusion based on the amazing repo by @crowsonkb and @Jack000
+    - https://github.com/Jack000/glid-3-xl
+    - https://github.dev/crowsonkb/k-diffusion
+    """
     def __init__(
         self,
         vae: AutoencoderKL,
