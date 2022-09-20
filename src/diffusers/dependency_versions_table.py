26821eb566182c400c4a4b42990ed896502244d8
anton-l <anton@huggingface.co>
2022-09-20 18:30:07 +0200

    style

--- src/diffusers/dependency_versions_table.py
+++ src/diffusers/dependency_versions_table.py
@@ -2,7 +2,7 @@
 # 1. modify the `_deps` dict in setup.py
 # 2. run `make deps_table_update``
 deps = {
-    "Pillow": "Pillow",
+    "Pillow": "Pillow<10.0",
     "accelerate": "accelerate>=0.11.0",
     "black": "black==22.8",
     "datasets": "datasets",
