a9fdb3de9ee4f247cf95b7ceba48236cb1abc36a
Pedro Cuenca <pedro@huggingface.co>
2022-09-21 22:25:27 +0200

    Return Flax scheduler state (#601)
    * Optionally return state in from_config.
    
    Useful for Flax schedulers.
    
    * has_state is now a property, make check more strict.
    
    I don't check the class is `SchedulerMixin` to prevent circular
    dependencies. It should be enough that the class name starts with "Flax"
    the object declares it "has_state" and the "create_state" exists too.
    
    * Use state in pipeline from_pretrained.
    
    * Make style

--- src/diffusers/configuration_utils.py
+++ src/diffusers/configuration_utils.py
@@ -160,12 +160,19 @@ class ConfigMixin:
         if "dtype" in unused_kwargs:
             init_dict["dtype"] = unused_kwargs.pop("dtype")
 
+        # Return model and optionally state and/or unused_kwargs
         model = cls(**init_dict)
+        return_tuple = (model,)
+
+        # Flax schedulers have a state, so return it.
+        if cls.__name__.startswith("Flax") and hasattr(model, "create_state") and getattr(model, "has_state", False):
+            state = model.create_state()
+            return_tuple += (state,)
 
         if return_unused_kwargs:
-            return model, unused_kwargs
+            return return_tuple + (unused_kwargs,)
         else:
-            return model
+            return return_tuple if len(return_tuple) > 1 else model
 
     @classmethod
     def get_config_dict(
