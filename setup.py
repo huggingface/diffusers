e9dff3482c0396ae9b4481d3aeb454f9bf022edb
anton-l <anton@huggingface.co>
2022-09-21 13:53:10 +0200

    Add torchvision to training deps

--- setup.py
+++ setup.py
@@ -174,7 +174,7 @@ extras = {}
 extras["quality"] = ["black==22.8", "isort>=5.5.4", "flake8>=3.8.3", "hf-doc-builder"]
 extras["docs"] = ["hf-doc-builder"]
 extras["training"] = ["accelerate", "datasets", "tensorboard", "modelcards"]
-extras["test"] = ["datasets", "onnxruntime", "pytest", "pytest-timeout", "pytest-xdist", "scipy", "transformers"]
+extras["test"] = ["datasets", "onnxruntime", "pytest", "pytest-timeout", "pytest-xdist", "scipy", "torchvision", "transformers"]
 extras["torch"] = deps_list("torch")
 
 if os.name == "nt":  # windows
