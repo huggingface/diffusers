2d4cfd4cc4bc95e4c8996775b851f08e498fd5c3
Pedro Cuenca <pedro@latenitesoft.com>
2022-09-21 05:13:56 +0000

    Fix typo in docstring.

--- src/diffusers/pipelines/stable_diffusion/pipeline_flax_stable_diffusion.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_flax_stable_diffusion.py
@@ -30,7 +30,7 @@ class FlaxStableDiffusionPipeline(FlaxDiffusionPipeline):
             Tokenizer of class
             [CLIPTokenizer](https://huggingface.co/docs/transformers/v4.21.0/en/model_doc/clip#transformers.CLIPTokenizer).
         unet ([`FlaxUNet2DConditionModel`]): Conditional U-Net architecture to denoise the encoded image latents.
-        scheduler ([`FlaxSchedulerMixin`]):
+        scheduler ([`SchedulerMixin`]):
             A scheduler to be used in combination with `unet` to denoise the encoded image latens. Can be one of
             [`FlaxDDIMScheduler`], [`FlaxLMSDiscreteScheduler`], or [`FlaxPNDMScheduler`].
         safety_checker ([`FlaxStableDiffusionSafetyChecker`]):
