8b0be9359667c794e77b4378f2c4736af9e2eda8
Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
2022-09-23 13:24:16 +0200

    Flax documentation (#589)
    * documenting `attention_flax.py` file
    
    * documenting `embeddings_flax.py`
    
    * documenting `unet_blocks_flax.py`
    
    * Add new objs to doc page
    
    * document `vae_flax.py`
    
    * Apply suggestions from code review
    
    * modify `unet_2d_condition_flax.py`
    
    * make style
    
    * Apply suggestions from code review
    
    * make style
    
    * Apply suggestions from code review
    
    * fix indent
    
    * fix typo
    
    * fix indent unet
    
    * Update src/diffusers/models/vae_flax.py
    
    * Apply suggestions from code review
    
    Co-authored-by: Pedro Cuenca <pedro@huggingface.co>
    
    Co-authored-by: Mishig Davaadorj <dmishig@gmail.com>
    Co-authored-by: Pedro Cuenca <pedro@huggingface.co>

--- src/diffusers/models/embeddings_flax.py
+++ src/diffusers/models/embeddings_flax.py
@@ -37,6 +37,15 @@ def get_sinusoidal_embeddings(timesteps, embedding_dim, freq_shift: float = 1):
 
 
 class FlaxTimestepEmbedding(nn.Module):
+    r"""
+    Time step Embedding Module. Learns embeddings for input time steps.
+
+    Args:
+        time_embed_dim (`int`, *optional*, defaults to `32`):
+                Time step embedding dimension
+        dtype (:obj:`jnp.dtype`, *optional*, defaults to jnp.float32):
+                Parameters `dtype`
+    """
     time_embed_dim: int = 32
     dtype: jnp.dtype = jnp.float32
 
@@ -49,6 +58,13 @@ class FlaxTimestepEmbedding(nn.Module):
 
 
 class FlaxTimesteps(nn.Module):
+    r"""
+    Wrapper Module for sinusoidal Time step Embeddings as described in https://arxiv.org/abs/2006.11239
+
+    Args:
+        dim (`int`, *optional*, defaults to `32`):
+                Time step embedding dimension
+    """
     dim: int = 32
     freq_shift: float = 1
 
