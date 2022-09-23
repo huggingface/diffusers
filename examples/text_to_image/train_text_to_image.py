5c0540130accccfd5a8dd54c736bdf27d2203158
patil-suraj <surajp815@gmail.com>
2022-09-23 21:29:39 +0200

    contiguous_format

--- examples/text_to_image/train_text_to_image.py
+++ examples/text_to_image/train_text_to_image.py
@@ -287,7 +287,7 @@ class EMAModel:
                 ema_param = param.float().clone() if param.ndim == 1 else copy.deepcopy(param)
                 ema_state_dict[key] = ema_param
 
-            param = param.clone().detach().to(ema_param.dtype)
+            param = param.clone().detach().to(ema_param.dtype).to(ema_param.device)
 
             if param.requires_grad:
                 ema_state_dict[key].sub_(self.decay * (ema_param - param))
@@ -494,6 +494,7 @@ def main():
 
     def collate_fn(examples):
         pixel_values = torch.stack([example["pixel_values"] for example in examples])
+        pixel_values = pixel_values.to(memory_format=torch.contiguous_format).float()
         input_ids = [example["input_ids"] for example in examples]
         padded_tokens = tokenizer.pad(
             {"input_ids": input_ids},
