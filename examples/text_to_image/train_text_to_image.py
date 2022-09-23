95d78360b4d2c89c4b0b0c73f5bcc4a5da7f85c7
patil-suraj <surajp815@gmail.com>
2022-09-23 12:18:29 +0200

    fix stupid bug

--- examples/text_to_image/train_text_to_image.py
+++ examples/text_to_image/train_text_to_image.py
@@ -506,7 +506,7 @@ def main():
                 # Convert images to latent space
                 with torch.no_grad():
                     if accelerator.num_processes > 1:
-                        vae.module.encode(batch["pixel_values"]).latent_dist.sample().detach()
+                        latents = vae.module.encode(batch["pixel_values"]).latent_dist.sample().detach()
                     else:
                         latents = vae.encode(batch["pixel_values"]).latent_dist.sample().detach()
                 latents = latents * 0.18215
