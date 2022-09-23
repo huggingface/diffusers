ce31f83d8c44c977dcf8413c99b8a6cb7f189c07
Ryan Russell <ryanrussell@users.noreply.github.com>
2022-09-23 08:02:12 -0500

    refactor: pipelines readability improvements (#622)
    * refactor: pipelines readability improvements
    
    Signed-off-by: Ryan Russell <git@ryanrussell.org>
    
    * docs: remove todo comment from flax pipeline
    
    Signed-off-by: Ryan Russell <git@ryanrussell.org>
    
    Signed-off-by: Ryan Russell <git@ryanrussell.org>

--- src/diffusers/pipelines/stable_diffusion/safety_checker.py
+++ src/diffusers/pipelines/stable_diffusion/safety_checker.py
@@ -48,20 +48,20 @@ class StableDiffusionSafetyChecker(PreTrainedModel):
             # at the cost of increasing the possibility of filtering benign images
             adjustment = 0.0
 
-            for concet_idx in range(len(special_cos_dist[0])):
-                concept_cos = special_cos_dist[i][concet_idx]
-                concept_threshold = self.special_care_embeds_weights[concet_idx].item()
-                result_img["special_scores"][concet_idx] = round(concept_cos - concept_threshold + adjustment, 3)
-                if result_img["special_scores"][concet_idx] > 0:
-                    result_img["special_care"].append({concet_idx, result_img["special_scores"][concet_idx]})
+            for concept_idx in range(len(special_cos_dist[0])):
+                concept_cos = special_cos_dist[i][concept_idx]
+                concept_threshold = self.special_care_embeds_weights[concept_idx].item()
+                result_img["special_scores"][concept_idx] = round(concept_cos - concept_threshold + adjustment, 3)
+                if result_img["special_scores"][concept_idx] > 0:
+                    result_img["special_care"].append({concept_idx, result_img["special_scores"][concept_idx]})
                     adjustment = 0.01
 
-            for concet_idx in range(len(cos_dist[0])):
-                concept_cos = cos_dist[i][concet_idx]
-                concept_threshold = self.concept_embeds_weights[concet_idx].item()
-                result_img["concept_scores"][concet_idx] = round(concept_cos - concept_threshold + adjustment, 3)
-                if result_img["concept_scores"][concet_idx] > 0:
-                    result_img["bad_concepts"].append(concet_idx)
+            for concept_idx in range(len(cos_dist[0])):
+                concept_cos = cos_dist[i][concept_idx]
+                concept_threshold = self.concept_embeds_weights[concept_idx].item()
+                result_img["concept_scores"][concept_idx] = round(concept_cos - concept_threshold + adjustment, 3)
+                if result_img["concept_scores"][concept_idx] > 0:
+                    result_img["bad_concepts"].append(concept_idx)
 
             result.append(result_img)
 
