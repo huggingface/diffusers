7baec94a898ca795250e65e84fb317e6788d102f
Ryan Russell <git@ryanrussell.org>
2022-09-24 10:19:32 -0500

    refactor(scripts): `params` and `conv_shortcut`
    Signed-off-by: Ryan Russell <git@ryanrussell.org>

--- scripts/convert_ddpm_original_checkpoint_to_diffusers.py
+++ scripts/convert_ddpm_original_checkpoint_to_diffusers.py
@@ -21,7 +21,7 @@ def renew_resnet_paths(old_list, n_shave_prefix_segments=0):
     for old_item in old_list:
         new_item = old_item
         new_item = new_item.replace("block.", "resnets.")
-        new_item = new_item.replace("conv_shorcut", "conv1")
+        new_item = new_item.replace("conv_shortcut", "conv1")
         new_item = new_item.replace("in_shortcut", "conv_shortcut")
         new_item = new_item.replace("temb_proj", "time_emb_proj")
 
