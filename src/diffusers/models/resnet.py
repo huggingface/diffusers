fdc649ffc076fbd4cbb0cc2d8fb2d9a221d226fd
isamu-isozaki <isamu.website@gmail.com>
2022-09-26 16:44:48 -0400

    Ran style

--- src/diffusers/models/resnet.py
+++ src/diffusers/models/resnet.py
@@ -110,8 +110,9 @@ class FirUpsample2D(nn.Module):
         """Fused `upsample_2d()` followed by `Conv2d()`.
 
         Args:
-        Padding is performed only once at the beginning, not between the operations. The fused op is considerably more
+        Padding is performed only once at the beginning, not between the operations. The fused op is considerably more:
         efficient than performing the same calculation using standard TensorFlow ops. It supports gradients of arbitrary:
+            
         order.
         x: Input tensor of the shape `[N, C, H, W]` or `[N, H, W,
             C]`.
@@ -198,8 +199,9 @@ class FirDownsample2D(nn.Module):
         """Fused `Conv2d()` followed by `downsample_2d()`.
 
         Args:
-        Padding is performed only once at the beginning, not between the operations. The fused op is considerably more
+        Padding is performed only once at the beginning, not between the operations. The fused op is considerably more:
         efficient than performing the same calculation using standard TensorFlow ops. It supports gradients of arbitrary:
+            
         order.
             x: Input tensor of the shape `[N, C, H, W]` or `[N, H, W, C]`. w: Weight tensor of the shape `[filterH,
             filterW, inChannels, outChannels]`. Grouped convolution can be performed by `inChannels = x.shape[0] //
@@ -372,9 +374,10 @@ def upsample_2d(x, kernel=None, factor=2, gain=1):
     r"""Upsample2D a batch of 2D images with the given filter.
 
     Args:
-    Accepts a batch of 2D images of the shape `[N, C, H, W]` or `[N, H, W, C]` and upsamples each image with the given
+    Accepts a batch of 2D images of the shape `[N, C, H, W]` or `[N, H, W, C]` and upsamples each image with the given:
     filter. The filter is normalized so that if the input pixels are constant, they will be scaled by the specified
     `gain`. Pixels outside the image are assumed to be zero, and the filter is padded with zeros so that its shape is a:
+        
     multiple of the upsampling factor.
         x: Input tensor of the shape `[N, C, H, W]` or `[N, H, W,
           C]`.
@@ -405,7 +408,7 @@ def downsample_2d(x, kernel=None, factor=2, gain=1):
     Args:
     Accepts a batch of 2D images of the shape `[N, C, H, W]` or `[N, H, W, C]` and downsamples each image with the
     given filter. The filter is normalized so that if the input pixels are constant, they will be scaled by the
-    specified `gain`. Pixels outside the image are assumed to be zero, and the filter is padded with zeros so that its
+    specified `gain`. Pixels outside the image are assumed to be zero, and the filter is padded with zeros so that its:
     shape is a multiple of the downsampling factor.
         x: Input tensor of the shape `[N, C, H, W]` or `[N, H, W,
           C]`.
