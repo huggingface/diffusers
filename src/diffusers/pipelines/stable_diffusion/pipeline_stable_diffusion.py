0fb42d4c6515fd4080503e4a48c9035f66061f4f
Nouamane Tazi <nouamane98@gmail.com>
2022-09-29 09:38:10 +0000

    styling

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion.py
@@ -278,9 +278,9 @@ class StableDiffusionPipeline(DiffusionPipeline):
         image = image.cpu().permute(0, 2, 3, 1).numpy()
 
         # run safety checker
-        safety_cheker_input = self.feature_extractor(self.numpy_to_pil(image), return_tensors="pt").to(self.device)
+        safety_checker_input = self.feature_extractor(self.numpy_to_pil(image), return_tensors="pt").to(self.device)
         image, has_nsfw_concept = self.safety_checker(
-            images=image, clip_input=safety_cheker_input.pixel_values.to(text_embeddings.dtype)
+            images=image, clip_input=safety_checker_input.pixel_values.to(text_embeddings.dtype)
         )
 
         if output_type == "pil":
