a56ec4b1c27a4664facd92e192ae2db5f6401d25
Josh <jachiam@berkeley.edu>
2022-10-02 17:11:04 -0700

    ran black again

--- scripts/convert_diffusers_to_original_stable_diffusion.py
+++ scripts/convert_diffusers_to_original_stable_diffusion.py
@@ -199,6 +199,7 @@ if __name__ == "__main__":
 
     parser.add_argument("--model_path", default=None, type=str, required=True, help="Path to the model to convert.")
     parser.add_argument("--checkpoint_path", default=None, type=str, required=True, help="Path to the output model.")
+    parser.add_argument("--half", action="store_true", help="Save weights in half precision.")
 
     args = parser.parse_args()
 
@@ -211,21 +212,23 @@ if __name__ == "__main__":
     text_enc_path = osp.join(args.model_path, "text_encoder", "pytorch_model.bin")
 
     # Convert the UNet model
-    unet_state_dict = torch.load(unet_path, map_location='cpu')
+    unet_state_dict = torch.load(unet_path, map_location="cpu")
     unet_state_dict = convert_unet_state_dict(unet_state_dict)
     unet_state_dict = {"model.diffusion_model." + k: v for k, v in unet_state_dict.items()}
 
     # Convert the VAE model
-    vae_state_dict = torch.load(vae_path, map_location='cpu')
+    vae_state_dict = torch.load(vae_path, map_location="cpu")
     vae_state_dict = convert_vae_state_dict(vae_state_dict)
     vae_state_dict = {"first_stage_model." + k: v for k, v in vae_state_dict.items()}
 
     # Convert the text encoder model
-    text_enc_dict = torch.load(text_enc_path, map_location='cpu')
+    text_enc_dict = torch.load(text_enc_path, map_location="cpu")
     text_enc_dict = convert_text_enc_state_dict(text_enc_dict)
     text_enc_dict = {"cond_stage_model.transformer." + k: v for k, v in text_enc_dict.items()}
 
     # Put together new checkpoint
     state_dict = {**unet_state_dict, **vae_state_dict, **text_enc_dict}
+    if args.half:
+        state_dict = {k: v.half() for k, v in state_dict.items()}
     state_dict = {"state_dict": state_dict}
     torch.save(state_dict, args.checkpoint_path)
