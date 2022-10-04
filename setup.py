4ff4d4db1273d0f813814337a35918212497f18c
Josh Achiam <jachiam@berkeley.edu>
2022-10-04 04:33:38 -0700

    Checkpoint conversion script from Diffusers => Stable Diffusion (CompVis) (#701)
    * Conversion script
    
    * ran black
    
    * ran isort
    
    * remove unused import
    
    * map location so everything gets loaded onto CPU before conversion
    
    * ran black again
    
    * Update setup.py
    
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- setup.py
+++ setup.py
@@ -67,11 +67,12 @@ To create the package for pypi.
     you need to go back to main before executing this.
 """
 
-import re
 import os
+import re
 from distutils.core import Command
 
 from setuptools import find_packages, setup
+
 
 # IMPORTANT:
 # 1. all dependencies should be listed here with their version requirements if any
@@ -177,14 +178,7 @@ extras["quality"] = deps_list("black", "isort", "flake8", "hf-doc-builder")
 extras["docs"] = deps_list("hf-doc-builder")
 extras["training"] = deps_list("accelerate", "datasets", "tensorboard", "modelcards")
 extras["test"] = deps_list(
-    "datasets",
-    "onnxruntime",
-    "pytest",
-    "pytest-timeout",
-    "pytest-xdist",
-    "scipy",
-    "torchvision",
-    "transformers"
+    "datasets", "onnxruntime", "pytest", "pytest-timeout", "pytest-xdist", "scipy", "torchvision", "transformers"
 )
 extras["torch"] = deps_list("torch")
 
