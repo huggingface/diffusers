8132cfd4ab9c4154d4422335ed138dc3328d6fad
Pi Esposito <piero.skywalker@gmail.com>
2022-10-05 09:27:58 -0300

    use low_cpu_mem_usage in transformers if device_map is not available

--- src/diffusers/pipeline_utils.py
+++ src/diffusers/pipeline_utils.py
@@ -406,9 +406,12 @@ class DiffusionPipeline(ConfigMixin):
                     loading_kwargs["device_map"] = device_map
 
                 # if using transformers and class obj has no _no_split modules, using device map will break loading
+                # so we just use low cpu memory usage to reduce ram usage on cpu
                 elif library_name == "transformers":
                     if getattr(class_obj, "_no_split_modules", None) is not None:
                         loading_kwargs["device_map"] = device_map
+                    else:
+                        loading_kwargs["low_cpu_mem_usage"] = True
 
                 # check if the module is in a subdirectory
                 if os.path.isdir(os.path.join(cached_folder, name)):
