7057b07e95b4d1291becd24a64526da85632369f
anton-l <anton@huggingface.co>
2022-10-06 17:56:35 +0200

    Temporarily remove Flax modules from the public API

--- src/diffusers/models/__init__.py
+++ src/diffusers/models/__init__.py
@@ -12,14 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from ..utils import is_flax_available, is_torch_available
+from ..utils import is_torch_available
 
 
 if is_torch_available():
     from .unet_2d import UNet2DModel
     from .unet_2d_condition import UNet2DConditionModel
     from .vae import AutoencoderKL, VQModel
-
-if is_flax_available():
-    from .unet_2d_condition_flax import FlaxUNet2DConditionModel
-    from .vae_flax import FlaxAutoencoderKL
