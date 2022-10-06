ff75b7744d17ba8db09b42f936a3e77a498a0603
anton-l <anton@huggingface.co>
2022-10-06 18:35:40 +0200

    Revert "[v0.4.0] Temporarily remove Flax modules from the public API (#755)"
    This reverts commit 2e209c30cf6f2ba42001d0629dc6b7ce354b9a9d.

--- src/diffusers/pipelines/__init__.py
+++ src/diffusers/pipelines/__init__.py
@@ -21,3 +21,6 @@ if is_torch_available() and is_transformers_available():
 
 if is_transformers_available() and is_onnx_available():
     from .stable_diffusion import StableDiffusionOnnxPipeline
+
+if is_transformers_available() and is_flax_available():
+    from .stable_diffusion import FlaxStableDiffusionPipeline
