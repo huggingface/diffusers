9418ce4277f72486615e3d78943812b50051b936
Suraj Patil <surajp815@gmail.com>
2022-10-06 18:12:54 +0200

    Update clip_guided_stable_diffusion.py

--- examples/community/clip_guided_stable_diffusion.py
+++ examples/community/clip_guided_stable_diffusion.py
@@ -148,7 +148,7 @@ class CLIPGuidedStableDiffusion(DiffusionPipeline):
             image = transforms.Resize(self.feature_extractor.size)(image)
         image = self.normalize(image)
 
-        image_embeddings_clip = self.clip_model.get_image_features(image).float()
+        image_embeddings_clip = self.clip_model.get_image_features(image)
         image_embeddings_clip = image_embeddings_clip / image_embeddings_clip.norm(p=2, dim=-1, keepdim=True)
 
         if use_cutouts:
