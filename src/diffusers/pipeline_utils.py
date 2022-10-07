e17e7fa5dce683a65d57fb41f16846e97982591c
apolinario <joaopaulo.passos@gmail.com>
2022-10-07 10:18:39 +0200

    make style

--- src/diffusers/pipeline_utils.py
+++ src/diffusers/pipeline_utils.py
@@ -170,9 +170,11 @@ class DiffusionPipeline(ConfigMixin):
             if isinstance(module, torch.nn.Module):
                 if module.dtype == torch.float16 and str(torch_device) in ["cpu", "mps"]:
                     logger.warning(
-                        "Pipelines loaded with `torch_dtype=torch.float16` cannot run with `cpu` or `mps` device. It is not recommended to move them to `cpu` or `mps` as running them will fail. Please make sure to use a `cuda` device to run the pipeline in inference. "
-                        "due to the lack of support for `float16` operations on those devices in PyTorch. "
-                        "Please remove the `torch_dtype=torch.float16` argument, or use a `cuda` device to run inference."
+                        "Pipelines loaded with `torch_dtype=torch.float16` cannot run with `cpu` or `mps` device. It"
+                        " is not recommended to move them to `cpu` or `mps` as running them will fail. Please make"
+                        " sure to use a `cuda` device to run the pipeline in inference. due to the lack of support for"
+                        " `float16` operations on those devices in PyTorch. Please remove the"
+                        " `torch_dtype=torch.float16` argument, or use a `cuda` device to run inference."
                     )
                 module.to(torch_device)
         return self
