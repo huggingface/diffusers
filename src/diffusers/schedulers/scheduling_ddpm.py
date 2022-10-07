77de2d18cf3787f930d3f2dc794ccdc99612c7d3
Nathan Lambert <nol@berkeley.edu>
2022-10-07 14:30:02 -0700

    convert to torch

--- src/diffusers/schedulers/scheduling_ddpm.py
+++ src/diffusers/schedulers/scheduling_ddpm.py
@@ -134,12 +134,8 @@ class DDPMScheduler(SchedulerMixin, ConfigMixin):
             # Glide cosine schedule
             self.betas = betas_for_alpha_bar(num_train_timesteps)
         elif beta_schedule == "sigmoid":
-
-            def sigmoid(x):
-                return 1 / (np.exp(-x) + 1)
-
-            betas = np.linspace(-6, 6, num_train_timesteps)
-            self.betas = sigmoid(betas) * (beta_end - beta_start) + beta_start
+            betas = torch.linspace(-6, 6, num_train_timesteps)
+            self.betas = torch.sigmoid(betas) * (beta_end - beta_start) + beta_start
         else:
             raise NotImplementedError(f"{beta_schedule} does is not implemented for {self.__class__}")
 
