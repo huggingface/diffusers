f29ace484b6950daecc1e75578117676f23e0e4e
Ben Glickenhaus <benglickenhaus@gmail.com>
2022-10-08 12:15:04 -0400

    valuefunction code

--- src/diffusers/models/unet_rl.py
+++ src/diffusers/models/unet_rl.py
@@ -175,3 +175,120 @@ class TemporalUNet(ModelMixin, ConfigMixin):
             return (sample,)
 
         return TemporalUNetOutput(sample=sample)
+
+
+class ValueFunction(ModelMixin, ConfigMixin):
+    @register_to_config
+    def __init__(
+        self,
+        training_horizon=128,
+        transition_dim=14,
+        cond_dim=3,
+        predict_epsilon=False,
+        clip_denoised=True,
+        dim=32,
+        dim_mults=(1, 4, 8),
+        out_dim=1,
+    ):
+        super().__init__()
+
+        self.transition_dim = transition_dim
+        self.cond_dim = cond_dim
+        self.predict_epsilon = predict_epsilon
+        self.clip_denoised = clip_denoised
+
+        self.time_proj = Timesteps(num_channels=dim, flip_sin_to_cos=False, downscale_freq_shift=1)
+        self.time_mlp = TimestepEmbedding(channel=dim, time_embed_dim=4 * dim, act_fn="mish", out_dim=dim)
+
+        dims = [transition_dim, *map(lambda m: dim * m, dim_mults)]
+        in_out = list(zip(dims[:-1], dims[1:]))
+
+        self.blocks = nn.ModuleList([])
+        num_resolutions = len(in_out)
+
+        for ind, (dim_in, dim_out) in enumerate(in_out):
+            is_last = ind >= (num_resolutions - 1)
+
+            self.blocks.append(
+                nn.ModuleList(
+                    [
+                        ResidualTemporalBlock(dim_in, dim_out, embed_dim=dim, horizon=training_horizon),
+                        ResidualTemporalBlock(dim_out, dim_out, embed_dim=dim, horizon=training_horizon),
+                        Downsample1D(dim_out, use_conv=True),
+                    ]
+                )
+            )
+
+            if not is_last:
+                training_horizon = training_horizon // 2
+    
+        mid_dim = dims[-1]
+        mid_dim_2 = mid_dim // 2
+        mid_dim_3 = mid_dim // 4
+        ##
+        self.mid_block1 = ResidualTemporalBlock(mid_dim, mid_dim_2, embed_dim=dim, horizon=training_horizon)
+        self.mid_down1 = Downsample1D(mid_dim_2, use_conv=True)
+        training_horizon = training_horizon // 2
+        ##
+        self.mid_block2 = ResidualTemporalBlock(mid_dim_2, mid_dim_3, embed_dim=dim, horizon=training_horizon)
+        self.mid_down2 = Downsample1D(mid_dim_3, use_conv=True)
+        training_horizon = training_horizon // 2
+        ##
+        fc_dim = mid_dim_3 * max(training_horizon, 1)
+        self.final_block = nn.ModuleList(
+            nn.Linear(fc_dim + dim, fc_dim // 2),
+            nn.Mish(),
+            nn.Linear(fc_dim // 2, out_dim),
+        )
+
+    def forward(
+        self,
+        sample: torch.FloatTensor,
+        timestep: Union[torch.Tensor, float, int],
+        return_dict: bool = True,
+    ) -> Union[TemporalUNetOutput, Tuple]:
+        """r
+        Args:
+            sample (`torch.FloatTensor`): (batch, horizon, obs_dimension + action_dimension) noisy inputs tensor
+            timestep (`torch.FloatTensor` or `float` or `int): batch (batch) timesteps
+            return_dict (`bool`, *optional*, defaults to `True`):
+                Whether or not to return a [`~models.unet_2d.UNet2DOutput`] instead of a plain tuple.
+
+        Returns:
+            [`~models.unet_2d.UNet2DOutput`] or `tuple`: [`~models.unet_2d.UNet2DOutput`] if `return_dict` is True,
+            otherwise a `tuple`. When returning a tuple, the first element is the sample tensor.
+        """
+        sample = sample.permute(0, 2, 1)
+
+        # 1. time
+        timesteps = timestep
+        if not torch.is_tensor(timesteps):
+            timesteps = torch.tensor([timesteps], dtype=torch.long, device=sample.device)
+        elif torch.is_tensor(timesteps) and len(timesteps.shape) == 0:
+            timesteps = timesteps[None].to(sample.device)
+
+        t = self.time_proj(timesteps)
+        t = self.time_mlp(t)
+        h = []
+
+        # 2. down
+        for resnet, resnet2, downsample in self.blocks:
+            sample = resnet(sample, t)
+            sample = resnet2(sample, t)
+            h.append(sample)
+            sample = downsample(sample)
+
+        # 3. mid
+        sample = self.mid_block1(sample, t)
+        sample = self.mid_down1(sample)
+        sample = self.mid_block2(sample, t)
+        sample = self.mid_down2(sample)
+
+        sample = sample.view(sample.shape[0], -1)
+        sample = torch.cat((sample, t), dim=1)
+        sample = self.final_block(sample)
+
+        if not return_dict:
+            return (sample,)
+
+        return TemporalUNetOutput(sample=sample)
