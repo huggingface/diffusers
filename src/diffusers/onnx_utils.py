277602b9dc35e7c5595ed3cf216a77901a5acce4
Anton Lozhkov <aglozhkov@gmail.com>
2022-10-10 12:58:32 +0200

    Update src/diffusers/onnx_utils.py

--- src/diffusers/onnx_utils.py
+++ src/diffusers/onnx_utils.py
@@ -79,10 +79,10 @@ class OnnxRuntimeModel:
 
         src_path = self.model_save_dir.joinpath(self.latest_model_name)
         dst_path = Path(save_directory).joinpath(model_file_name)
-        if dst_path.is_file() and not src_path.samefile(dst_path):
+        try:
             shutil.copyfile(src_path, dst_path)
-        elif not dst_path.is_file():
-            shutil.copyfile(src_path, dst_path)
+        except shutil.SameFileError:
+            pass
 
     def save_pretrained(
         self,
