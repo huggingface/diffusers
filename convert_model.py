wrong normalization function

convert_model.py
@@ -18,7 +18,7 @@ def unet(hor):
         down_block_types = ("DownResnetBlock1D", "DownResnetBlock1D", "DownResnetBlock1D", "DownResnetBlock1D")
         block_out_channels = (32, 64, 128, 256)
         up_block_types = ("UpResnetBlock1D", "UpResnetBlock1D", "UpResnetBlock1D")
-    model = torch.load(f"/Users/bglickenhaus/Documents/diffuser/temporal_unet-hopper-hor{hor}.torch")
+    model = torch.load(f"/Users/bglickenhaus/Documents/diffuser/temporal_unet-hopper-mediumv2-hor{hor}.torch")
     state_dict = model.state_dict()
     config = dict(down_block_types=down_block_types, block_out_channels=block_out_channels, up_block_types=up_block_types, layers_per_block=1)
     hf_value_function = UNet1DModel(**config)
@@ -36,13 +36,13 @@ def unet(hor):
 def value_function():
     config = dict(in_channels=14, down_block_types=("DownResnetBlock1D", "DownResnetBlock1D", "DownResnetBlock1D", "DownResnetBlock1D"), block_out_channels=(32, 64, 128, 256), layers_per_block=1)
 
-    model = torch.load("/Users/bglickenhaus/Documents/diffuser/value_function-hopper-hor32.torch")
-    state_dict = model.state_dict()
+    model = torch.load("/Users/bglickenhaus/Documents/diffuser/value_function-hopper-mediumv2-hor32.torch")
+    state_dict = model
     hf_value_function = ValueFunction(**config)
     print(f"length of state dict: {len(state_dict.keys())}")
     print(f"length of value function dict: {len(hf_value_function.state_dict().keys())}")
 
-    mapping = dict((k, hfk) for k, hfk in zip(model.state_dict().keys(), hf_value_function.state_dict().keys()))
+    mapping = dict((k, hfk) for k, hfk in zip(state_dict.keys(), hf_value_function.state_dict().keys()))
     for k, v in mapping.items():
         state_dict[v] = state_dict.pop(k)
 

examples/diffuser/helpers.py
@@ -9,16 +9,14 @@ import tqdm
 
 DTYPE = torch.float
 def normalize(x_in, data, key):
-  upper = np.max(data[key], axis=0)
-  lower = np.min(data[key], axis=0)
-  x_out = 2*(x_in - lower)/(upper-lower) - 1
-  return x_out
+    means = data[key].mean(axis=0)
+    stds = data[key].std(axis=0)
+    return (x_in - means) / stds
 
 def de_normalize(x_in, data, key):
-	upper = np.max(data[key], axis=0)
-	lower = np.min(data[key], axis=0)
-	x_out = lower + (upper - lower)*(1 + x_in) /2
-	return x_out
+    means = data[key].mean(axis=0)
+    stds = data[key].std(axis=0)
+    return x_in * stds + means
 	
 def to_torch(x_in, dtype=None, device='cuda'):
 	dtype = dtype or DTYPE
@@ -61,7 +59,7 @@ def run_diffusion(x, scheduler, generator, network, unet, conditions, action_dim
         
         # 3. [optional] add posterior noise to the sample
         if config['eta'] > 0:
-            noise = torch.randn(x.shape, generator=generator).to(x.device)
+            noise = torch.randn(x.shape).to(x.device)
             posterior_variance = scheduler._get_variance(i) # * noise
             # no noise when t == 0
             # NOTE: original implementation missing sqrt on posterior_variance

examples/diffuser/run_diffuser_value_guided.py
@@ -10,6 +10,9 @@ import helpers
 import wandb
 import modal
 import os
+from pytorch_lightning import seed_everything
+
+seed_everything(0)
 
 stub = modal.Stub("diffusers-value-guided")
 image = modal.Image.debian_slim().apt_install([
@@ -34,7 +37,8 @@ image = modal.Image.debian_slim().apt_install([
     "mediapy",
     "Pillow==9.0.0",
     "moviepy",
-    "imageio"
+    "imageio",
+    "pytorch-lightning",
     ])
 
 config = dict(
@@ -46,7 +50,7 @@ config = dict(
     scale=0.1,
     eta=0.0,
     t_grad_cutoff=2,
-    device='cpu'
+    device='cuda'
 )
 
 def _run():
@@ -83,6 +87,7 @@ def _run():
 
     ## add a batch dimension and repeat for multiple samples
     ## [ observation_dim ] --> [ n_samples x observation_dim ]
+    env.seed(0)
     obs = env.reset()
     total_reward = 0
     total_score = 0
@@ -109,7 +114,7 @@ def _run():
             shape = (batch_size, config['horizon'], state_dim+action_dim)
 
             # sample random initial noise vector
-            x1 = torch.randn(shape, device=DEVICE, generator=generator)
+            x1 = torch.randn(shape, device=DEVICE)
 
             # this model is conditioned from an initial state, so you will see this function
             #  multiple times to change the initial state of generated data to the state 
@@ -169,6 +174,6 @@ def run():
 
 
 if __name__ == "__main__":
-    _run()
-    # with stub.run():
-        # run()
+    # _run()
+    with stub.run():
+        run()