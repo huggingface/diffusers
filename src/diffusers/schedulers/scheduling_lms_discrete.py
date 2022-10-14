e4f7410394601066b6e19721a2c48e829c1b2137
Anton Lozhkov <anton@huggingface.co>
2022-10-14 13:24:38 +0200

    Update src/diffusers/schedulers/scheduling_lms_discrete.py
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- src/diffusers/schedulers/scheduling_lms_discrete.py
+++ src/diffusers/schedulers/scheduling_lms_discrete.py
@@ -74,7 +74,6 @@ class LMSDiscreteScheduler(SchedulerMixin, ConfigMixin):
         beta_end: float = 0.02,
         beta_schedule: str = "linear",
         trained_betas: Optional[np.ndarray] = None,
-        **kwargs,
     ):
         if trained_betas is not None:
             self.betas = torch.from_numpy(trained_betas)
