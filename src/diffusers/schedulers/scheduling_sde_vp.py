9bf4c188140bdb158614e3e6f1f7a6e610f4edc0
Anton Lozhkov <anton@huggingface.co>
2022-10-14 13:25:02 +0200

    Update src/diffusers/schedulers/scheduling_sde_vp.py
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- src/diffusers/schedulers/scheduling_sde_vp.py
+++ src/diffusers/schedulers/scheduling_sde_vp.py
@@ -39,7 +39,7 @@ class ScoreSdeVpScheduler(SchedulerMixin, ConfigMixin):
     """
 
     @register_to_config
-    def __init__(self, num_train_timesteps=2000, beta_min=0.1, beta_max=20, sampling_eps=1e-3, **kwargs):
+    def __init__(self, num_train_timesteps=2000, beta_min=0.1, beta_max=20, sampling_eps=1e-3):
         self.sigmas = None
         self.discrete_sigmas = None
         self.timesteps = None
