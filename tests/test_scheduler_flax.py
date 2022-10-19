b00f754b1fcb60d1f1fe877ec88d1d8da676b6b7
Kashif Rasul <kashif.rasul@gmail.com>
2022-10-19 14:13:45 +0200

    shape required for pndm

--- tests/test_scheduler_flax.py
+++ tests/test_scheduler_flax.py
@@ -778,7 +778,7 @@ class FlaxPNDMSchedulerTest(FlaxSchedulerCommonTest):
         scheduler_config = self.get_scheduler_config(steps_offset=1)
         scheduler = scheduler_class(**scheduler_config)
         state = scheduler.create_state()
-        state = scheduler.set_timesteps(state, 10)
+        state = scheduler.set_timesteps(state, 10, shape=())
         assert jnp.equal(
             state.timesteps,
             jnp.array([901, 851, 851, 801, 801, 751, 751, 701, 701, 651, 651, 601, 601, 501, 401, 301, 201, 101, 1]),
