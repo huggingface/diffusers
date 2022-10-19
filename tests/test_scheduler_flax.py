3f2f4f975d4d6c5eec84003d1b9b50a3058d4fca
Kashif Rasul <kashif.rasul@gmail.com>
2022-10-19 14:53:35 +0200

    fix style

--- tests/test_scheduler_flax.py
+++ tests/test_scheduler_flax.py
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import tempfile
-from typing import Dict, List, Tuple
 import unittest
+from typing import Dict, List, Tuple
 
 from diffusers.utils import is_flax_available
 from diffusers.utils.testing_utils import require_flax
