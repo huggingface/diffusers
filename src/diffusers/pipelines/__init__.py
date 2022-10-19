3c84784ac7ee7d7e83051b1e1775206d4a7c2f9e
Patrick von Platen <patrick.v.platen@gmail.com>
2022-10-19 12:23:10 +0200

    finish

--- src/diffusers/pipelines/__init__.py
+++ src/diffusers/pipelines/__init__.py
@@ -16,6 +16,7 @@ if is_torch_available() and is_transformers_available():
     from .stable_diffusion import (
         StableDiffusionImg2ImgPipeline,
         StableDiffusionInpaintPipeline,
+        StableDiffusionInpaintPipelineLegacy,
         StableDiffusionPipeline,
     )
 
