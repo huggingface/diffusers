079b6b730bc047fd856cde114e587674b55f9a2b
Shivam Shrirao <shivamshrirao@gmail.com>
2022-10-19 18:34:54 +0530

    dreambooth much better results

--- examples/dreambooth/launch.sh
+++ examples/dreambooth/launch.sh
@@ -18,9 +18,9 @@ accelerate launch train_dreambooth.py \
   --mixed_precision="fp16" \
   --use_8bit_adam \
   --gradient_accumulation_steps=1 \
-  --learning_rate=2e-6 \
+  --learning_rate=1e-6 \
   --lr_scheduler="constant" \
   --lr_warmup_steps=0 \
-  --num_class_images=40 \
+  --num_class_images=50 \
   --sample_batch_size=4 \
-  --max_train_steps=1000
+  --max_train_steps=800
