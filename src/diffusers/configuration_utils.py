f35301e0965b14ea66b94eb96058e7f77572b6d5
Nathan Lambert <nol@berkeley.edu>
2022-10-20 16:27:30 -0700

    clean up loading docs

--- src/diffusers/configuration_utils.py
+++ src/diffusers/configuration_utils.py
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" ConfigMixinuration base class and utilities."""
+""" ConfigMixin base class and utilities."""
 import dataclasses
 import functools
 import inspect
