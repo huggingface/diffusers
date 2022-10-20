5b1e29268ba21d08d7f3f54444bc01f9e6743c69
Patrick von Platen <patrick.v.platen@gmail.com>
2022-10-20 17:51:39 +0200

    up

--- scripts/convert_dance_diffusion_to_diffusers.py
+++ scripts/convert_dance_diffusion_to_diffusers.py
@@ -242,9 +242,9 @@ def transform_conv_attns(new_state_dict, new_k, v):
         single_shape = trippled_shape // 3
         for i in range(3):
             if len(v.shape) == 3:
-                new_state_dict[new_k[i]] = v[i * single_shape: (i + 1) * single_shape, :, 0]
+                new_state_dict[new_k[i]] = v[i * single_shape : (i + 1) * single_shape, :, 0]
             else:
-                new_state_dict[new_k[i]] = v[i * single_shape: (i + 1) * single_shape]
+                new_state_dict[new_k[i]] = v[i * single_shape : (i + 1) * single_shape]
     return new_state_dict
 
 
--- src/diffusers/models/unet_1d_blocks.py
+++ src/diffusers/models/unet_1d_blocks.py
@@ -11,9 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import math
+
 import torch
 import torch.nn.functional as F
-import math
 from torch import nn
 
 
