09090bcd1f57369b0bf952776444d157a3feee5c
anton-l <anton@huggingface.co>
2022-10-25 13:45:42 +0200

    revert num_steps

--- tests/pipelines/ddim/test_ddim.py
+++ tests/pipelines/ddim/test_ddim.py
@@ -52,8 +52,7 @@ class DDIMPipelineFastTests(PipelineTesterMixin, unittest.TestCase):
 
         # Warmup pass when using mps (see #372)
         if torch_device == "mps":
-            _ = ddpm(num_inference_steps=5)
-            _ = ddpm(num_inference_steps=5)
+            _ = ddpm(num_inference_steps=1)
 
         generator = torch.manual_seed(0)
         image = ddpm(generator=generator, num_inference_steps=2, output_type="numpy").images
@@ -82,7 +81,7 @@ class DDIMPipelineFastTests(PipelineTesterMixin, unittest.TestCase):
 
         # Warmup pass when using mps (see #372)
         if torch_device == "mps":
-            _ = ddpm(num_inference_steps=5)
+            _ = ddpm(num_inference_steps=1)
 
         generator = torch.manual_seed(0)
         image = ddpm(generator=generator, num_inference_steps=2, output_type="numpy").images
