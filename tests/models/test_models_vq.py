8a70c5b694046cd0be11562339c285ed2e91f6f7
Patrick von Platen <patrick.v.platen@gmail.com>
2022-10-27 13:25:14 +0200

    finish

--- tests/models/test_models_vq.py
+++ tests/models/test_models_vq.py
@@ -17,7 +17,7 @@ import unittest
 
 import torch
 
-from diffusers import VQModel, AutoencoderKL
+from diffusers import AutoencoderKL, VQModel
 from diffusers.utils import floats_tensor, torch_device
 
 from .test_modeling_common import ModelTesterMixin
