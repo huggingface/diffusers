f7a4c29df10f9101fe85f6a30ed29ce2c17ba583
lukovnikov <lukovnikov@users.noreply.github.com>
2022-10-27 19:55:02 +0200

    changed training example to add option to train model that predicts x0 (instead of eps), changed DDPM pipeline accordingly

--- examples/unconditional_image_generation/train_unconditional.py
+++ examples/unconditional_image_generation/train_unconditional.py
@@ -30,6 +30,24 @@ from tqdm.auto import tqdm
 logger = get_logger(__name__)
 
 
+def _extract_into_tensor(arr, timesteps, broadcast_shape):
+    """
+    Extract values from a 1-D numpy array for a batch of indices.
+
+    :param arr: the 1-D numpy array.
+    :param timesteps: a tensor of indices into the array to extract.
+    :param broadcast_shape: a larger shape of K dimensions with the batch
+                            dimension equal to the length of timesteps.
+    :return: a tensor of shape [batch_size, 1, ...] where the shape has K dims.
+    """
+    if not isinstance(arr, torch.Tensor):
+        arr = torch.from_numpy(arr)
+    res = arr[timesteps].float().to(timesteps.device)
+    while len(res.shape) < len(broadcast_shape):
+        res = res[..., None]
+    return res.expand(broadcast_shape)
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Simple example of a training script.")
     parser.add_argument(
