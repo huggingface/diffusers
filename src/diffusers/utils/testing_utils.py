fae97880eab58d9b1f896ab91b8f885013394a88
Patrick von Platen <patrick.v.platen@gmail.com>
2022-10-28 15:37:45 +0000

    [Tests] Better prints

--- src/diffusers/utils/testing_utils.py
+++ src/diffusers/utils/testing_utils.py
@@ -34,6 +34,14 @@ if is_torch_available():
         torch_device = "mps" if (mps_backend_registered and torch.backends.mps.is_available()) else torch_device
 
 
+def torch_all_close(a, b, *args, **kwargs):
+    if not is_torch_available():
+        raise ValueError("PyTorch needs to be installed to use this function.")
+    if not torch.allclose(a, b, *args, **kwargs):
+        assert False, f"Max diff is absolute {(a - b).abs().max()}. Diff tensor is {(a - b).abs()}."
+    return True
+
+
 def get_tests_dir(append_path=None):
     """
     Args:
