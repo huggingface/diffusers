ca7cd9fd441668e0eedc3f87a06b3520160389ce
Juan Carlos Piñeros <juancopi_81@hotmail.com>
2022-10-30 10:32:02 -0500

    Add helper function to translate prompt to English

--- examples/community/multilingual_stable_diffusion.py
+++ examples/community/multilingual_stable_diffusion.py
@@ -31,6 +31,16 @@ def detect_language(pipe, prompt: Union[str, List[str]], batch_size):
         return detected_languages
 
 
+def translate_prompt(prompt, translation_tokenizer, translation_model, device):
+    """helper function to translate prompt to English"""
+
+    encoded_prompt = translation_tokenizer(prompt, return_tensors="pt").to(device)
+    generated_tokens = translation_model.generate(**encoded_prompt, max_new_tokens=1000)
+    en_trans = translation_tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+
+    return en_trans[0]
+
+
 class MultilingualStableDiffusion(DiffusionPipeline):
     r"""
     Pipeline for text-to-image generation using Stable Diffusion.
