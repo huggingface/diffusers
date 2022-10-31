c6ac28436b8bab624c800394483aa115dcb56024
Ben Glickenhaus <benglickenhaus@gmail.com>
2022-10-31 12:39:18 -0400

    make fix copies

--- src/diffusers/utils/dummy_pt_objects.py
+++ src/diffusers/utils/dummy_pt_objects.py
@@ -182,6 +182,21 @@ class DDPMPipeline(metaclass=DummyObject):
         requires_backends(cls, ["torch"])
 
 
+class DistillationPipeline(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+    @classmethod
+    def from_config(cls, *args, **kwargs):
+        requires_backends(cls, ["torch"])
+
+    @classmethod
+    def from_pretrained(cls, *args, **kwargs):
+        requires_backends(cls, ["torch"])
+
+
 class KarrasVePipeline(metaclass=DummyObject):
     _backends = ["torch"]
 
