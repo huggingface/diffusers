3e9b7535ad9443bcff64baab8bffd6da0d996ef9
Pedro Cuenca <pedro@latenitesoft.com>
2022-11-01 16:15:20 +0000

    make style

--- src/diffusers/pipelines/stable_diffusion/pipeline_flax_stable_diffusion.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_flax_stable_diffusion.py
@@ -301,12 +301,8 @@ class FlaxStableDiffusionPipeline(FlaxDiffusionPipeline):
 
 
 @partial(jax.pmap, in_axes=(None, 0, 0, 0, None, None, None, None, 0), static_broadcasted_argnums=(0, 4, 5, 6))
-def _p_generate(
-    pipe, prompt_ids, params, prng_seed, num_inference_steps, height, width, guidance_scale, latents
-):
-    return pipe._generate(
-        prompt_ids, params, prng_seed, num_inference_steps, height, width, guidance_scale, latents
-    )
+def _p_generate(pipe, prompt_ids, params, prng_seed, num_inference_steps, height, width, guidance_scale, latents):
+    return pipe._generate(prompt_ids, params, prng_seed, num_inference_steps, height, width, guidance_scale, latents)
 
 
 @partial(jax.pmap, static_broadcasted_argnums=(0,))
