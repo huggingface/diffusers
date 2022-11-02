fea9ffe0bd1f0bf020cdc18872974353f8eccf96
Yuta Hayashibe <yuta@hayashibe.jp>
2022-11-03 01:15:18 +0900

    Use logger.warning instead of raise errors

--- examples/dreambooth/train_dreambooth.py
+++ examples/dreambooth/train_dreambooth.py
@@ -213,9 +213,9 @@ def parse_args(input_args=None):
             raise ValueError("You must specify prompt for class images.")
     else:
         if args.class_data_dir is not None:
-            raise ValueError("You need not use --class_data_dir without --with_prior_preservation.")
+            logger.warning("You need not use --class_data_dir without --with_prior_preservation.")
         if args.class_prompt is not None:
-            raise ValueError("You need not use --class_prompt without --with_prior_preservation.")
+            logger.warning("You need not use --class_prompt without --with_prior_preservation.")
 
     return args
 
