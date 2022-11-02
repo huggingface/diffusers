3d9e0b59a26552bf18805a9c4a77461bbd6779a5
Yuta Hayashibe <yuta@hayashibe.jp>
2022-11-03 00:01:08 +0900

    Updated messages

--- examples/dreambooth/train_dreambooth.py
+++ examples/dreambooth/train_dreambooth.py
@@ -213,9 +213,9 @@ def parse_args(input_args=None):
             raise ValueError("You must specify prompt for class images.")
     else:
         if args.class_data_dir is not None:
-            raise ValueError("You need not specify a data directory for class images.")
+            raise ValueError("You need not use --class_data_dir without --with_prior_preservation.")
         if args.class_prompt is not None:
-            raise ValueError("You need not specify prompt for class images.")
+            raise ValueError("You need not use --class_prompt without --with_prior_preservation.")
 
     return args
 
