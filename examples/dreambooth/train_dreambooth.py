5e323b466cc3b3083fb1eb1d48e6f30223db7081
Shivam Shrirao <shivamshrirao@gmail.com>
2022-11-04 01:41:38 +0530

    fix autoencoder subfolder

--- examples/dreambooth/train_dreambooth.py
+++ examples/dreambooth/train_dreambooth.py
@@ -446,6 +446,7 @@ def main(args):
                         args.pretrained_model_name_or_path,
                         vae=AutoencoderKL.from_pretrained(
                             args.pretrained_vae_name_or_path or args.pretrained_model_name_or_path,
+                            subfolder=None if args.pretrained_vae_name_or_path else "vae",
                             revision=None if args.pretrained_vae_name_or_path else args.revision
                         ),
                         torch_dtype=torch_dtype,
