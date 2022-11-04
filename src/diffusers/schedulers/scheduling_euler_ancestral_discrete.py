2fcae69f2a181960c37be8e3081b9da0f2dc3b21
Anton Lozhkov <anton@huggingface.co>
2022-11-04 19:06:24 +0100

    Bump to 0.8.0.dev0 (#1131)
    * Bump to 0.8.0.dev0
    
    * deprecate int timesteps
    
    * style

--- src/diffusers/schedulers/scheduling_euler_ancestral_discrete.py
+++ src/diffusers/schedulers/scheduling_euler_ancestral_discrete.py
@@ -19,7 +19,7 @@ import numpy as np
 import torch
 
 from ..configuration_utils import ConfigMixin, register_to_config
-from ..utils import BaseOutput, deprecate, logging
+from ..utils import BaseOutput, logging
 from .scheduling_utils import SchedulerMixin
 
 
@@ -253,19 +253,7 @@ class EulerAncestralDiscreteScheduler(SchedulerMixin, ConfigMixin):
             timesteps = timesteps.to(original_samples.device)
 
         schedule_timesteps = self.timesteps
-
-        if isinstance(timesteps, torch.IntTensor) or isinstance(timesteps, torch.LongTensor):
-            deprecate(
-                "timesteps as indices",
-                "0.8.0",
-                "Passing integer indices  (e.g. from `enumerate(timesteps)`) as timesteps to"
-                " `EulerAncestralDiscreteScheduler.add_noise()` will not be supported in future versions. Make sure to"
-                " pass values from `scheduler.timesteps` as timesteps.",
-                standard_warn=False,
-            )
-            step_indices = timesteps
-        else:
-            step_indices = [(schedule_timesteps == t).nonzero().item() for t in timesteps]
+        step_indices = [(schedule_timesteps == t).nonzero().item() for t in timesteps]
 
         sigma = self.sigmas[step_indices].flatten()
         while len(sigma.shape) < len(original_samples.shape):
