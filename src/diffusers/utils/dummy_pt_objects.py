1d0f3c211ebb13897859bd457c218c64b5e8a805
Patrick von Platen <patrick.v.platen@gmail.com>
2022-11-04 14:58:52 +0100

    Move accelerate to a soft-dependency (#1134)
    * finish
    
    * finish
    
    * Update src/diffusers/modeling_utils.py
    
    * Update src/diffusers/pipeline_utils.py
    
    Co-authored-by: Anton Lozhkov <anton@huggingface.co>
    
    * more fixes
    
    * fix
    
    Co-authored-by: Anton Lozhkov <anton@huggingface.co>

--- src/diffusers/utils/dummy_pt_objects.py
+++ src/diffusers/utils/dummy_pt_objects.py
@@ -272,21 +272,6 @@ class ScoreSdeVePipeline(metaclass=DummyObject):
         requires_backends(cls, ["torch"])
 
 
-class VQDiffusionPipeline(metaclass=DummyObject):
-    _backends = ["torch"]
-
-    def __init__(self, *args, **kwargs):
-        requires_backends(self, ["torch"])
-
-    @classmethod
-    def from_config(cls, *args, **kwargs):
-        requires_backends(cls, ["torch"])
-
-    @classmethod
-    def from_pretrained(cls, *args, **kwargs):
-        requires_backends(cls, ["torch"])
-
-
 class DDIMScheduler(metaclass=DummyObject):
     _backends = ["torch"]
 
