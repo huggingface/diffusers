7bfd0d9bc879aca20fb203e8315d5892576f50cc
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2022-11-06 04:02:41 +0800

    StableDiffusion: Decode latents separately to run larger batches

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_inpaint_legacy.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_inpaint_legacy.py
@@ -405,7 +405,7 @@ class StableDiffusionInpaintPipelineLegacy(DiffusionPipeline):
                 callback(i, t, latents)
 
         latents = 1 / 0.18215 * latents
-        image = self.vae.decode(latents).sample
+        image = torch.cat([self.vae.decode(latent).sample for latent in latents.split(1)])
 
         image = (image / 2 + 0.5).clamp(0, 1)
         image = image.cpu().permute(0, 2, 3, 1).numpy()
