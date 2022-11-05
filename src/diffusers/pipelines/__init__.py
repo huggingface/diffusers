d189eeaa273ea77c6ebdbadb44e2b1c7d4568390
duongna21 <lablaugrana01@gmail.com>
2022-11-05 15:47:41 +0700

    minor

--- src/diffusers/pipelines/__init__.py
+++ src/diffusers/pipelines/__init__.py
@@ -5,6 +5,7 @@ if is_torch_available():
     from .dance_diffusion import DanceDiffusionPipeline
     from .ddim import DDIMPipeline
     from .ddpm import DDPMPipeline
+    from .latent_diffusion import LDMSuperResolutionPipeline
     from .latent_diffusion_uncond import LDMPipeline
     from .pndm import PNDMPipeline
     from .score_sde_ve import ScoreSdeVePipeline
@@ -13,7 +14,7 @@ else:
     from ..utils.dummy_pt_objects import *  # noqa F403
 
 if is_torch_available() and is_transformers_available():
-    from .latent_diffusion import LDMSuperResolutionPipeline, LDMTextToImagePipeline
+    from .latent_diffusion import LDMTextToImagePipeline
     from .stable_diffusion import (
         StableDiffusionImg2ImgPipeline,
         StableDiffusionInpaintPipeline,
