a5028234677a59e45199bc89cc3581031e40c10a
patil-suraj <surajp815@gmail.com>
2022-11-07 18:20:56 +0100

    remove negative prompt

--- src/diffusers/pipelines/rdm/pipeline_rdm.py
+++ src/diffusers/pipelines/rdm/pipeline_rdm.py
@@ -191,7 +191,6 @@ class RDMPipeline(DiffusionPipeline):
         width: int = 768,
         num_inference_steps: int = 50,
         guidance_scale: float = 7.5,
-        negative_prompt: Optional[Union[str, List[str]]] = None,
         num_images_per_prompt: Optional[int] = 1,
         eta: float = 0.0,
         generator: Optional[torch.Generator] = None,
@@ -314,40 +313,6 @@ class RDMPipeline(DiffusionPipeline):
         do_classifier_free_guidance = guidance_scale > 1.0
         # get unconditional embeddings for classifier free guidance
         if do_classifier_free_guidance:
-            # uncond_tokens: List[str]
-            # if negative_prompt is None:
-            #     uncond_tokens = [""] * batch_size
-            # elif type(prompt) is not type(negative_prompt):
-            #     raise TypeError(
-            #         f"`negative_prompt` should be the same type to `prompt`, but got {type(negative_prompt)} !="
-            #         f" {type(prompt)}."
-            #     )
-            # elif isinstance(negative_prompt, str):
-            #     uncond_tokens = [negative_prompt]
-            # elif batch_size != len(negative_prompt):
-            #     raise ValueError(
-            #         f"`negative_prompt`: {negative_prompt} has batch size {len(negative_prompt)}, but `prompt`:"
-            #         f" {prompt} has batch size {batch_size}. Please make sure that passed `negative_prompt` matches"
-            #         " the batch size of `prompt`."
-            #     )
-            # else:
-            #     uncond_tokens = negative_prompt
-
-            # max_length = text_input_ids.shape[-1]
-            # uncond_input = self.tokenizer(
-            #     uncond_tokens,
-            #     padding="max_length",
-            #     max_length=max_length,
-            #     truncation=True,
-            #     return_tensors="pt",
-            # )
-            # uncond_embeddings = self.text_encoder(uncond_input.input_ids.to(self.device))[0]
-
-            # duplicate unconditional embeddings for each generation per prompt, using mps friendly method
-            # seq_len = uncond_embeddings.shape[1]
-            # uncond_embeddings = uncond_embeddings.repeat(1, num_images_per_prompt, 1)
-            # uncond_embeddings = uncond_embeddings.view(batch_size * num_images_per_prompt, seq_len, -1)
-
             uncond_embeddings = torch.zeros_like(text_embeddings)
 
             # For classifier free guidance, we need to do two forward passes.
