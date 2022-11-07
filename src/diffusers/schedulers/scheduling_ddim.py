f4ebecab8e1bb7a37dfc8755d326e76c0a340c7c
lukovnikov <lukovnikov@users.noreply.github.com>
2022-11-07 13:38:48 +0100

    fix for when the number of inference steps in ddim is not dividing the number of training steps cleanly

--- src/diffusers/schedulers/scheduling_ddim.py
+++ src/diffusers/schedulers/scheduling_ddim.py
@@ -217,7 +217,7 @@ class DDIMScheduler(SchedulerMixin, ConfigMixin):
         if substeps_mode.startswith("l"):
             # using linearly spaced steps
             self.num_inference_steps = num_inference_steps
-            step_ratio = self.config.num_train_timesteps // self.num_inference_steps
+            step_ratio = self.config.num_train_timesteps / self.num_inference_steps
             # creates integer timesteps by multiplying by ratio
             # casting to int to avoid issues when num_inference_step is power of 3
             timesteps = (np.arange(0, num_inference_steps) * step_ratio).round()[::-1].copy().astype(np.int64)
