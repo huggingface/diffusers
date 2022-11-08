e7d9fc6df566645bf75b047a65f6c40cf9ddf340
patil-suraj <surajp815@gmail.com>
2022-11-08 12:58:54 +0100

    don't use str for device

--- src/diffusers/schedulers/scheduling_ddim.py
+++ src/diffusers/schedulers/scheduling_ddim.py
@@ -288,7 +288,7 @@ class DDIMScheduler(SchedulerMixin, ConfigMixin):
 
         if eta > 0:
             # randn_like does not support generator https://github.com/pytorch/pytorch/issues/27072
-            device = model_output.device if torch.is_tensor(model_output) else "cpu"
+            device = model_output.device if torch.is_tensor(model_output) else torch.device("cpu")
             if variance_noise is not None and generator is not None:
                 raise ValueError(
                     "Cannot pass both generator and variance_noise. Please make sure that either `generator` or"
@@ -296,7 +296,7 @@ class DDIMScheduler(SchedulerMixin, ConfigMixin):
                 )
 
             if variance_noise is None:
-                if str(device) == "mps":
+                if device.type == "mps":
                     # randn does not work reproducibly on mps
                     variance_noise = torch.randn(model_output.shape, dtype=model_output.dtype, generator=generator)
                     variance_noise = variance_noise.to(device)
