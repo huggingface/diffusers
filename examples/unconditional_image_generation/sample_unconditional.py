d777cdf8daa7debfcdf1f8bae43ca756a7236744
lukovnikov <lukovnikov@users.noreply.github.com>
2022-11-10 00:34:02 +0100

    removed predict_mode from pipeline call

--- examples/unconditional_image_generation/sample_unconditional.py
+++ examples/unconditional_image_generation/sample_unconditional.py
@@ -8,13 +8,23 @@ from diffusers import DDIMPipeline, DDIMScheduler, DDPMPipeline
 from PIL import Image
 
 
+def _ddim_scheduler_from_ddpm_scheduler(sched):
+    ret = DDIMScheduler(
+        num_train_timesteps=sched.num_train_timesteps,
+        trained_betas=sched.betas,
+        clip_sample=sched.clip_sample,
+    )
+    assert torch.allclose(sched.alphas_cumprod, ret.alphas_cumprod)
+    return ret
+
+
 def main(args):
     pipeline = DDPMPipeline.from_pretrained(args.load_dir).to(
         torch.device(f"cuda:{args.gpu}" if args.gpu >= 0 else "cpu")
     )
 
     if args.sampler == "ddim":
-        ddimsched = DDIMScheduler.from_ddpm_scheduler(pipeline.scheduler)
+        ddimsched = _ddim_scheduler_from_ddpm_scheduler(pipeline.scheduler)
         pipeline = DDIMPipeline(pipeline.unet, ddimsched)
 
     num_steps = args.num_steps if args.num_steps != -1 else pipeline.scheduler.num_train_timesteps
@@ -37,14 +47,12 @@ def main(args):
                 generator=generator,
                 batch_size=args.batch_size,
                 output_type="numpy",
-                predict_epsilon=args.predict_mode == "eps",
             ).images
         elif args.sampler == "ddim":
             images = pipeline(
                 generator=generator,
                 batch_size=args.batch_size,
                 output_type="numpy",
-                predict_epsilon=args.predict_mode == "eps",
                 num_inference_steps=args.num_steps,
                 use_clipped_model_output=True,
                 eta=args.ddim_eta,
@@ -76,8 +84,8 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size", type=int, default=16)
     parser.add_argument("--output_subdir", type=str, default="samples")
     parser.add_argument("--num_samples", type=int, default=10)
-    parser.add_argument("--predict_mode", type=str, default="eps")  # "eps" or "x0"
     parser.add_argument("--gpu", type=int, default=-1)
 
     args = parser.parse_args()
     main(args)
+
