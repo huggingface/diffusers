b424987099db6e41008b6fa295e93432d9a87953
anton-l <anton@huggingface.co>
2022-11-10 00:03:17 +0100

    Revert "fix reproducible initial noise"
    This reverts commit f300d05cb9782ed320064a0c58577a32d4139e6d.

--- src/diffusers/pipelines/ddpm/pipeline_ddpm.py
+++ src/diffusers/pipelines/ddpm/pipeline_ddpm.py
@@ -94,13 +94,11 @@ class DDPMPipeline(DiffusionPipeline):
             generator = None
 
         # Sample gaussian noise to begin loop
-        image_shape = (batch_size, self.unet.in_channels, self.unet.sample_size, self.unet.sample_size)
-        if self.device.type == "mps":
-            # randn does not work reproducibly on mps
-            image = torch.randn(image_shape, generator=generator)
-            image = image.to(device)
-        else:
-            image = torch.randn(image_shape, generator=generator, device=self.device)
+        image = torch.randn(
+            (batch_size, self.unet.in_channels, self.unet.sample_size, self.unet.sample_size),
+            generator=generator,
+            device=self.device,
+        )
 
         # set step values
         self.scheduler.set_timesteps(num_inference_steps)
