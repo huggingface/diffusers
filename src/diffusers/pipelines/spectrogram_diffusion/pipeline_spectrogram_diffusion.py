b6373b896eb228c4b18c76d7ac2dd1d29add800a
Kashif Rasul <kashif.rasul@gmail.com>
2022-11-10 14:53:15 +0100

    added scale_to_features

--- src/diffusers/pipelines/spectrogram_diffusion/pipeline_spectrogram_diffusion.py
+++ src/diffusers/pipelines/spectrogram_diffusion/pipeline_spectrogram_diffusion.py
@@ -560,6 +560,15 @@ class SpectrogramDiffusionPipeline(DiffusionPipeline):
         # Scale to [min_out, max_out].
         return zero_one * (max_out - min_out) + min_out
 
+    def scale_to_features(self, outputs, input_range=(-1.0, 1.0), clip=False):
+        """Invert by linearly scaling network outputs to features range."""
+        min_out, max_out = input_range
+        outputs = torch.clip(outputs, min_out, max_out) if clip else outputs
+        # Scale to [0, 1].
+        zero_one = (outputs - min_out) / (max_out - min_out)
+        # Scale to [self.min_value, self.max_value].
+        return zero_one * (self.max_value - self.min_value) + self.min_value
+
     @torch.no_grad()
     def __call__(
         self,
