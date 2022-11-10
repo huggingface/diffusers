9d33ef4b526a716c07f337e3ac381a7af030b694
lukovnikov <lukovnikov@users.noreply.github.com>
2022-11-10 01:13:14 +0100

    missing import

--- src/diffusers/pipelines/ddim/pipeline_ddim.py
+++ src/diffusers/pipelines/ddim/pipeline_ddim.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import inspect
 from typing import Optional, Tuple, Union
 
 import torch
