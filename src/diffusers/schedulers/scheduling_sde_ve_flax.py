864d1d9d7ef15ec6ae5c339915f96e2bdf65caa1
Patrick von Platen <patrick.v.platen@gmail.com>
2022-11-14 21:17:19 +0000

    uP

--- src/diffusers/schedulers/scheduling_sde_ve_flax.py
+++ src/diffusers/schedulers/scheduling_sde_ve_flax.py
@@ -64,8 +64,8 @@ class FlaxScoreSdeVeScheduler(FlaxSchedulerMixin, ConfigMixin):
 
     [`~ConfigMixin`] takes care of storing all config attributes that are passed in the scheduler's `__init__`
     function, such as `num_train_timesteps`. They can be accessed via `scheduler.config.num_train_timesteps`.
-    [`~ConfigMixin`] also provides general loading and saving functionality via the [`~ConfigMixin.save_config`] and
-    [`~ConfigMixin.from_config`] functions.
+    [`SchedulerMixin`] provides general loading and saving functionality via the [`SchedulerMixin.save_pretrained`] and
+    [`~SchedulerMixin.from_pretrained`] functions.
 
     Args:
         num_train_timesteps (`int`): number of diffusion steps used to train the model.
