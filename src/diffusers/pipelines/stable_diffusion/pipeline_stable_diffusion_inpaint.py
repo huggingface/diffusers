8bb5b3549e756583b0284136452181c2e32e592b
vict0rsch <vsch@pm.me>
2022-11-16 17:32:19 -0500

    handle mask as `np.array`

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_inpaint.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_inpaint.py
@@ -66,8 +66,8 @@ def prepare_mask_and_masked_image(image, mask):
             image = np.array(image.convert("RGB"))
         image = image[None].transpose(0, 3, 1, 2)
         image = torch.from_numpy(image).to(dtype=torch.float32) / 127.5 - 1.0
-
-        mask = np.array(mask.convert("L"))
+        if isinstance(mask, PIL.Image.Image):
+            mask = np.array(mask.convert("L"))
         mask = mask.astype(np.float32) / 255.0
         mask = mask[None, None]
         mask[mask < 0.5] = 0
