847921bae8a6f47aba515e973a70ac57fba9f591
vict0rsch <vsch@pm.me>
2022-11-16 18:37:40 -0500

    fix: wrong indentation

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_inpaint.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_inpaint.py
@@ -102,7 +102,7 @@ def prepare_mask_and_masked_image(image, mask):
         image = torch.from_numpy(image).to(dtype=torch.float32) / 127.5 - 1.0
         if isinstance(mask, PIL.Image.Image):
             mask = np.array(mask.convert("L"))
-        mask = mask.astype(np.float32) / 255.0
+            mask = mask.astype(np.float32) / 255.0
         mask = mask[None, None]
         mask[mask < 0.5] = 0
         mask[mask >= 0.5] = 1
