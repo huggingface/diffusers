f1fcfdeec5ae98b30b0939baf9e64f47c813da99
Will Berman <wlbberman@gmail.com>
2022-11-16 08:51:43 -0800

    vq diffusion classifier free sampling (#1294)
    * vq diffusion classifier free sampling
    
    * correct
    
    * uP
    
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- src/diffusers/pipelines/vq_diffusion/__init__.py
+++ src/diffusers/pipelines/vq_diffusion/__init__.py
@@ -1,1 +1,5 @@
-from .pipeline_vq_diffusion import VQDiffusionPipeline
+from ...utils import is_torch_available, is_transformers_available
+
+
+if is_transformers_available() and is_torch_available():
+    from .pipeline_vq_diffusion import LearnedClassifierFreeSamplingEmbeddings, VQDiffusionPipeline
