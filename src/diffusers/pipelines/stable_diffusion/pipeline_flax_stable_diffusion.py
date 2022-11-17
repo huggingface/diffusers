5412f4b942ca0df9a8fe5fbfba062ceec60c999c
entrpn <juancevedo@gmail.com>
2022-11-17 08:15:44 -0800

    support negative prompts in sd jax pipeline

--- src/diffusers/pipelines/stable_diffusion/pipeline_flax_stable_diffusion.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_flax_stable_diffusion.py
@@ -165,6 +165,7 @@ class FlaxStableDiffusionPipeline(FlaxDiffusionPipeline):
         guidance_scale: float = 7.5,
         latents: Optional[jnp.array] = None,
         debug: bool = False,
+        neg_prompt_ids: jnp.array = [""]
     ):
         if height % 8 != 0 or width % 8 != 0:
             raise ValueError(f"`height` and `width` have to be divisible by 8 but are {height} and {width}.")
@@ -178,7 +179,7 @@ class FlaxStableDiffusionPipeline(FlaxDiffusionPipeline):
 
         max_length = prompt_ids.shape[-1]
         uncond_input = self.tokenizer(
-            [""] * batch_size, padding="max_length", max_length=max_length, return_tensors="np"
+            neg_prompt_ids * batch_size, padding="max_length", max_length=max_length, return_tensors="np"
         )
         uncond_embeddings = self.text_encoder(uncond_input.input_ids, params=params["text_encoder"])[0]
         context = jnp.concatenate([uncond_embeddings, text_embeddings])
@@ -251,6 +252,7 @@ class FlaxStableDiffusionPipeline(FlaxDiffusionPipeline):
         return_dict: bool = True,
         jit: bool = False,
         debug: bool = False,
+        neg_prompt_ids: jnp.array = [""],
         **kwargs,
     ):
         r"""
@@ -298,11 +300,11 @@ class FlaxStableDiffusionPipeline(FlaxDiffusionPipeline):
         """
         if jit:
             images = _p_generate(
-                self, prompt_ids, params, prng_seed, num_inference_steps, height, width, guidance_scale, latents, debug
+                self, prompt_ids, params, prng_seed, num_inference_steps, height, width, guidance_scale, latents, debug, neg_prompt_ids
             )
         else:
             images = self._generate(
-                prompt_ids, params, prng_seed, num_inference_steps, height, width, guidance_scale, latents, debug
+                prompt_ids, params, prng_seed, num_inference_steps, height, width, guidance_scale, latents, debug, neg_prompt_ids
             )
 
         if self.safety_checker is not None:
@@ -333,10 +335,10 @@ class FlaxStableDiffusionPipeline(FlaxDiffusionPipeline):
 # TODO: maybe use a config dict instead of so many static argnums
 @partial(jax.pmap, static_broadcasted_argnums=(0, 4, 5, 6, 7, 9))
 def _p_generate(
-    pipe, prompt_ids, params, prng_seed, num_inference_steps, height, width, guidance_scale, latents, debug
+    pipe, prompt_ids, params, prng_seed, num_inference_steps, height, width, guidance_scale, latents, debug, neg_prompt_ids
 ):
     return pipe._generate(
-        prompt_ids, params, prng_seed, num_inference_steps, height, width, guidance_scale, latents, debug
+        prompt_ids, params, prng_seed, num_inference_steps, height, width, guidance_scale, latents, debug, neg_prompt_ids
     )
 
 
