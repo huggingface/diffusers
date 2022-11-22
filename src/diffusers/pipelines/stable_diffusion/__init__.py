c928758c16fc5d882b253a157b1e622fadf2d1a6
patil-suraj <surajp815@gmail.com>
2022-11-22 12:02:30 +0100

    add ini init

--- src/diffusers/pipelines/stable_diffusion/__init__.py
+++ src/diffusers/pipelines/stable_diffusion/__init__.py
@@ -28,6 +28,7 @@ class StableDiffusionPipelineOutput(BaseOutput):
 
 
 if is_transformers_available() and is_torch_available():
+    from .pipelie_stabel_diffusion_image_variation import StableDiffusionImageVariationPipeline
     from .pipeline_cycle_diffusion import CycleDiffusionPipeline
     from .pipeline_stable_diffusion import StableDiffusionPipeline
     from .pipeline_stable_diffusion_img2img import StableDiffusionImg2ImgPipeline
