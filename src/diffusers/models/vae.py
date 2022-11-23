51ef9396a3a8acce7d9b280b3fd9ed83fd5fcd98
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2022-11-23 22:15:53 +0800

    Skip VAE slicing for one-image batches

--- src/diffusers/models/vae.py
+++ src/diffusers/models/vae.py
@@ -603,7 +603,7 @@ class AutoencoderKL(ModelMixin, ConfigMixin):
         self.use_slicing = False
 
     def decode(self, z: torch.FloatTensor, return_dict: bool = True) -> Union[DecoderOutput, torch.FloatTensor]:
-        if self.use_slicing:
+        if self.use_slicing and z.shape[0] > 1:
             decoded_slices = [self._decode(z_slice).sample for z_slice in z.split(1)]
             decoded = torch.cat(decoded_slices)
         else:
