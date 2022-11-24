966cbb897a165d7af46bbf919f846f23b7eb8f08
Patrick von Platen <patrick.v.platen@gmail.com>
2022-11-24 20:15:15 +0000

    uP

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_img2img.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_img2img.py
@@ -146,15 +146,15 @@ class StableDiffusionImg2ImgPipeline(DiffusionPipeline):
 
         if hasattr(unet.config, "sample_size") and unet.config.sample_size == 32:
             deprecation_message = (
-                f"The configuration file of the unet: {unet.config} has set the default `sample_size` to smaller than"
-                " 64 which seems highly unlikely .If you're checkpoint is a fine-tuned version of any of the"
-                " following: \n- CompVis/stable-diffusion-v1-4 \n- CompVis/stable-diffusion-v1-3 \n-"
-                " CompVis/stable-diffusion-v1-2 \n- CompVis/stable-diffusion-v1-1 \n- runwayml/stable-diffusion-v1-5"
-                " \n- runwayml/stable-diffusion-inpainting \n you should change 'sample_size' to 64 in the"
-                " configuration file. Please make sure to update the config accordingly as leaving `sample_size=32`"
-                " in the config might lead to incorrect results in future versions. If you have downloaded this"
-                " checkpoint from the Hugging Face Hub, it would be very nice if you could open a Pull request for"
-                " the `unet/config.json` file"
+                f"The configuration file of the unet has set the default `sample_size` to smaller than"
+                f" 64 which seems highly unlikely .If you're checkpoint is a fine-tuned version of any of the"
+                f" following: \n- CompVis/stable-diffusion-v1-4 \n- CompVis/stable-diffusion-v1-3 \n-"
+                f" CompVis/stable-diffusion-v1-2 \n- CompVis/stable-diffusion-v1-1 \n- runwayml/stable-diffusion-v1-5"
+                f" \n- runwayml/stable-diffusion-inpainting \n you should change 'sample_size' to 64 in the"
+                f" configuration file. Please make sure to update the config accordingly as leaving `sample_size=32`"
+                f" in the config might lead to incorrect results in future versions. If you have downloaded this"
+                f" checkpoint from the Hugging Face Hub, it would be very nice if you could open a Pull request for"
+                f" the `unet/config.json` file"
             )
             deprecate("sample_size<64", "1.0.0", deprecation_message, standard_warn=False)
             new_config = dict(unet.config)
