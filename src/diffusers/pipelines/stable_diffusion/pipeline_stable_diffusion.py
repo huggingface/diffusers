c28d3c82ce6f56c4b373a8260c56357d13db900a
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2022-11-29 20:28:14 +0800

    StableDiffusion: Decode latents separately to run larger batches (#1150)
    * StableDiffusion: Decode latents separately to run larger batches
    
    * Move VAE sliced decode under enable_vae_sliced_decode and vae.enable_sliced_decode
    
    * Rename sliced_decode to slicing
    
    * fix whitespace
    
    * fix quality check and repository consistency
    
    * VAE slicing tests and documentation
    
    * API doc hooks for VAE slicing
    
    * reformat vae slicing tests
    
    * Skip VAE slicing for one-image batches
    
    * Documentation tweaks for VAE slicing
    
    Co-authored-by: Ilmari Heikkinen <ilmari@fhtr.org>

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion.py
@@ -215,6 +215,22 @@ class StableDiffusionPipeline(DiffusionPipeline):
         # set slice_size = `None` to disable `attention slicing`
         self.enable_attention_slicing(None)
 
+    def enable_vae_slicing(self):
+        r"""
+        Enable sliced VAE decoding.
+
+        When this option is enabled, the VAE will split the input tensor in slices to compute decoding in several
+        steps. This is useful to save some memory and allow larger batch sizes.
+        """
+        self.vae.enable_slicing()
+
+    def disable_vae_slicing(self):
+        r"""
+        Disable sliced VAE decoding. If `enable_vae_slicing` was previously invoked, this method will go back to
+        computing decoding in one step.
+        """
+        self.vae.disable_slicing()
+
     def enable_sequential_cpu_offload(self, gpu_id=0):
         r"""
         Offloads all models to CPU using accelerate, significantly reducing memory usage. When called, unet,
