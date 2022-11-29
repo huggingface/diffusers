6a0a312370538d8cd0562337e76696774978d02a
Rohan Taori <rtaori13@gmail.com>
2022-11-29 04:29:23 -0800

    Fix bug in half precision for DPMSolverMultistepScheduler (#1349)
    * cast to float for quantile method
    
    * add fp16 test for DPMSolverMultistepScheduler fix
    
    * formatting update

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -991,6 +991,22 @@ class DPMSolverMultistepSchedulerTest(SchedulerCommonTest):
 
         assert abs(result_mean.item() - 0.3301) < 1e-3
 
+    def test_fp16_support(self):
+        scheduler_class = self.scheduler_classes[0]
+        scheduler_config = self.get_scheduler_config(thresholding=True, dynamic_thresholding_ratio=0)
+        scheduler = scheduler_class(**scheduler_config)
+
+        num_inference_steps = 10
+        model = self.dummy_model()
+        sample = self.dummy_sample_deter.half()
+        scheduler.set_timesteps(num_inference_steps)
+
+        for i, t in enumerate(scheduler.timesteps):
+            residual = model(sample, t)
+            sample = scheduler.step(residual, t, sample).prev_sample
+
+        assert sample.dtype == torch.float16
+
 
 class PNDMSchedulerTest(SchedulerCommonTest):
     scheduler_classes = (PNDMScheduler,)
