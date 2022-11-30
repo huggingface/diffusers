4d02ebd23f8772d137924d620912cd33a2355fbc
Patrick von Platen <patrick.v.platen@gmail.com>
2022-11-30 10:30:42 +0000

    Correct stacklevel

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -162,3 +162,9 @@ class DeprecateTester(unittest.TestCase):
             deprecate(("deprecated_arg", self.higher_version, "This message is better!!!"), standard_warn=False)
 
         assert str(warning.warning) == "This message is better!!!"
+
+    def test_deprecate_stacklevel(self):
+        with self.assertWarns(FutureWarning) as warning:
+            deprecate(("deprecated_arg", self.higher_version, "This message is better!!!"), standard_warn=False)
+        assert str(warning.warning) == "This message is better!!!"
+        assert warning.filename == "/home/patrick_huggingface_co/diffusers/tests/test_utils.py"
