ea7ac3bd890fc573c475a25b596cabd2d7f04ffa
Nathan Lambert <nol@berkeley.edu>
2022-11-29 16:25:10 -0800

    reset dance diff test

--- tests/pipelines/dance_diffusion/test_dance_diffusion.py
+++ tests/pipelines/dance_diffusion/test_dance_diffusion.py
@@ -73,7 +73,7 @@ class PipelineFastTests(unittest.TestCase):
         audio_from_tuple_slice = audio_from_tuple[0, -3:, -3:]
 
         assert audio.shape == (1, 2, self.dummy_unet.sample_size)
-        expected_slice = np.array([-0.8682, 1.0000, -1.0000, -0.3130, 0.5133, -1.0000])
+        expected_slice = np.array([-0.7265, 1.0000, -0.8388, 0.1175, 0.9498, -1.0000])
         assert np.abs(audio_slice.flatten() - expected_slice).max() < 1e-2
         assert np.abs(audio_from_tuple_slice.flatten() - expected_slice).max() < 1e-2
 
@@ -101,7 +101,7 @@ class PipelineIntegrationTests(unittest.TestCase):
         audio_slice = audio[0, -3:, -3:]
 
         assert audio.shape == (1, 2, pipe.unet.sample_size)
-        expected_slice = np.array([-0.7265, 1.0000, -0.8388, 0.1175, 0.9498, -1.0000])
+        expected_slice = np.array([-0.1576, -0.1526, -0.127, -0.2699, -0.2762, -0.2487])
         assert np.abs(audio_slice.flatten() - expected_slice).max() < 1e-2
 
     def test_dance_diffusion_fp16(self):
@@ -118,5 +118,5 @@ class PipelineIntegrationTests(unittest.TestCase):
         audio_slice = audio[0, -3:, -3:]
 
         assert audio.shape == (1, 2, pipe.unet.sample_size)
-        expected_slice = np.array([-0.1576, -0.1526, -0.127, -0.2699, -0.2762, -0.2487])
+        expected_slice = np.array([-0.1693, -0.1698, -0.1447, -0.3044, -0.3203, -0.2937])
         assert np.abs(audio_slice.flatten() - expected_slice).max() < 1e-2
