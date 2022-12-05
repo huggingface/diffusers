67c31019634ca80dd4c74ca5fd8a07723453847c
patil-suraj <surajp815@gmail.com>
2022-12-05 17:50:15 +0100

    fix error_message

--- src/diffusers/utils/__init__.py
+++ src/diffusers/utils/__init__.py
@@ -96,14 +96,10 @@ def check_min_version(min_version):
     if version.parse(__version__) < version.parse(min_version):
         if "dev" in min_version:
             error_message = (
-                "This example requires a source install from HuggingFace Transformers (see "
-                "`https://huggingface.co/transformers/installation.html#installing-from-source`),"
+                "This example requires a source install from HuggingFace diffusers (see "
+                "`https://huggingface.co/docs/diffusers/installation#install-from-source`),"
             )
         else:
             error_message = f"This example requires a minimum version of {min_version},"
         error_message += f" but the version found is {__version__}.\n"
-        raise ImportError(
-            error_message
-            + "Check out https://huggingface.co/transformers/examples.html for the examples corresponding to other "
-            "versions of HuggingFace Transformers."
-        )
+        raise ImportError(error_message)
