be38a23f39817093d7e770aacea959c55deddb98
skytnt <SkyTNT@outlook.com>
2022-12-06 22:46:10 +0800

    rollback preprocess_mask resample

--- examples/community/lpw_stable_diffusion_onnx.py
+++ examples/community/lpw_stable_diffusion_onnx.py
@@ -374,7 +374,7 @@ def preprocess_mask(mask, scale_factor=8):
     mask = mask.convert("L")
     w, h = mask.size
     w, h = map(lambda x: x - x % 32, (w, h))  # resize to integer multiple of 32
-    mask = mask.resize((w // scale_factor, h // scale_factor), resample=PIL.Image.NEAREST)
+    mask = mask.resize((w // scale_factor, h // scale_factor), resample=PIL_INTERPOLATION["nearest"])
     mask = np.array(mask).astype(np.float32) / 255.0
     mask = np.tile(mask, (4, 1, 1))
     mask = mask[None].transpose(0, 1, 2, 3)  # what does this step do?
