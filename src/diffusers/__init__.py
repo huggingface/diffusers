657a534f510099b1e8ec5d4b7f898e07ce6b169e
Anton Lozhkov <anton@huggingface.co>
2022-12-08 13:49:41 +0100

    Update src/diffusers/__init__.py
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- src/diffusers/__init__.py
+++ src/diffusers/__init__.py
@@ -105,7 +105,7 @@ try:
     if not (is_torch_available() and is_transformers_available() and is_k_diffusion_available()):
         raise OptionalDependencyNotAvailable()
 except OptionalDependencyNotAvailable:
-    from .utils.dummy_torch_and_transformers_and_onnx_objects import *  # noqa F403
+    from .utils.dummy_torch_and_transformers_and_k_diffusion_objects import *  # noqa F403
 else:
     from .pipelines import StableDiffusionKDiffusionPipeline
 
