1abcd21d905e59b8ec2380c75dcf1d5210135c8f
Patrick von Platen <patrick.v.platen@gmail.com>
2022-12-10 18:05:20 +0100

    Apply suggestions from code review

--- src/diffusers/pipelines/alt_diffusion/pipeline_alt_diffusion_img2img.py
+++ src/diffusers/pipelines/alt_diffusion/pipeline_alt_diffusion_img2img.py
@@ -428,7 +428,7 @@ class AltDiffusionImg2ImgPipeline(DiffusionPipeline):
     def __call__(
         self,
         prompt: Union[str, List[str]],
-        image: Union[torch.FloatTensor, PIL.Image.Image] = None,
+        image: Union[torch.FloatTensor, PIL.Image.Image],
         strength: float = 0.8,
         num_inference_steps: Optional[int] = 50,
         guidance_scale: Optional[float] = 7.5,
