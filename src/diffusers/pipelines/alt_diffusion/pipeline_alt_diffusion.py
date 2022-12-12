ded3299d68ccb5ccf890a2c0ef06bf0aefa97ad1
Kangfu Mei <mikumkf@gmail.com>
2022-12-12 09:02:13 -0500

    fix bug if we don't do_classifier_free_guidance (#1601)
    * fix bug if we don't do_classifier_free_guidance
    
    * update for copied diffusers.pipelines..alt_diffusion..pipeline_alt_diffusion.AltDiffusionPipeline

--- src/diffusers/pipelines/alt_diffusion/pipeline_alt_diffusion.py
+++ src/diffusers/pipelines/alt_diffusion/pipeline_alt_diffusion.py
@@ -522,8 +522,8 @@ class AltDiffusionPipeline(DiffusionPipeline):
                     noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
                     noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
 
-                    # compute the previous noisy sample x_t -> x_t-1
-                    latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs).prev_sample
+                # compute the previous noisy sample x_t -> x_t-1
+                latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs).prev_sample
 
                 # call the callback, if provided
                 if i == len(timesteps) - 1 or ((i + 1) > num_warmup_steps and (i + 1) % self.scheduler.order == 0):
