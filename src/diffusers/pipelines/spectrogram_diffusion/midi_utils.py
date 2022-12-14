5472ef576664e36cefe06311bcb632faa753f590
Kashif Rasul <kashif.rasul@gmail.com>
2022-12-14 16:50:13 +0100

    use np.where

--- src/diffusers/pipelines/spectrogram_diffusion/midi_utils.py
+++ src/diffusers/pipelines/spectrogram_diffusion/midi_utils.py
@@ -228,7 +228,7 @@ def programs_to_midi_classes(tokens, codec):
     """Modifies program events to be the first program in the MIDI class."""
     min_program_id, max_program_id = codec.event_type_range("program")
     is_program = (tokens >= min_program_id) & (tokens <= max_program_id)
-    return tf.where(is_program, min_program_id + 8 * ((tokens - min_program_id) // 8), tokens)
+    return np.where(is_program, min_program_id + 8 * ((tokens - min_program_id) // 8), tokens)
 
 
 PROGRAM_GRANULARITIES = {
