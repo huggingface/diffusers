63105dbf8593db0559c55deafa8d91584dd34c66
Anton Lozhkov <anton@huggingface.co>
2022-12-16 17:06:54 +0100

    Update src/diffusers/utils/testing_utils.py
    Co-authored-by: Pedro Cuenca <pedro@huggingface.co>

--- src/diffusers/utils/testing_utils.py
+++ src/diffusers/utils/testing_utils.py
@@ -116,7 +116,7 @@ def nightly(test_case):
     """
     Decorator marking a test that runs nightly in the diffusers CI.
 
-    Slow tests are skipped by default. Set the RUN_SLOW environment variable to a truthy value to run them.
+    Slow tests are skipped by default. Set the RUN_NIGHTLY environment variable to a truthy value to run them.
 
     """
     return unittest.skipUnless(_run_nightly_tests, "test is nightly")(test_case)
