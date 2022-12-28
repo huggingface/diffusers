1f127e0d04bf8e740493e10630b63442775329ae
Kashif Rasul <kashif.rasul@gmail.com>
2022-12-28 20:02:13 +0100

    flip_sin_to_cos and better var names

--- src/diffusers/pipelines/dit/pipeline_dit.py
+++ src/diffusers/pipelines/dit/pipeline_dit.py
@@ -29,25 +29,43 @@ class DiTPipeline(DiffusionPipeline):
         latent_size = self.dit.config.input_size
 
         latents = torch.randn(batch_size, 4, latent_size, latent_size, device=self.device)
-        y = torch.tensor(class_labels, device=self.device)
+        latent_model_input = torch.cat([latents] * 2) if guidance_scale > 1 else latents
 
-        # Setup classifier-free guidance:
-        latents = torch.cat([latents, latents], 0)
-        y_null = torch.tensor([1000] * batch_size, device=self.device)
-        y = torch.cat([y, y_null], 0)
+        class_labels = torch.tensor(class_labels, device=self.device)
+        class_null = torch.tensor([1000] * batch_size, device=self.device)
+        class_labels_input = torch.cat([class_labels, class_null], 0) if guidance_scale > 1 else class_labels
 
         # set step values
         self.scheduler.set_timesteps(num_inference_steps)
 
         for t in self.progress_bar(self.scheduler.timesteps):
-            # 1. predict noise model_output
-            latents_model_output = self.forward_with_cfg(latents, t, y, guidance_scale)
+            half = latent_model_input[: len(latent_model_input) // 2]
+            latent_model_input = torch.cat([half, half], dim=0)
+            latent_model_input = self.scheduler.scale_model_input(latent_model_input, t)
 
-            # 2. compute previous image: x_t -> x_t-1
-            latents = self.scheduler.step(latents_model_output[:, :4], t, latents, generator=generator).prev_sample
+            # predict noise model_output
+            noise_pred = self.dit(latent_model_input, t, class_labels_input)
 
-        latents, _ = latents.chunk(2, dim=0)
-        samples = self.vae.decode(latents / 0.18215).sample
+            # perform guidance
+            if guidance_scale > 1:
+                eps, rest = noise_pred[:, :3], noise_pred[:, 3:]
+                cond_eps, uncond_eps = torch.split(eps, len(eps) // 2, dim=0)
+
+                half_eps = uncond_eps + guidance_scale * (cond_eps - uncond_eps)
+                eps = torch.cat([half_eps, half_eps], dim=0)
+                
+                noise_pred = torch.cat([eps, rest], dim=1)
+
+            # learned sigma
+            _, C = noise_pred.shape[:2]
+            model_output, _ = torch.split(noise_pred, C, dim=1)
+
+            # compute previous image: x_t -> x_t-1
+            latent_model_input = self.scheduler.step(model_output, t, latent_model_input, generator=generator).prev_sample
+
+        latents, _ = latent_model_input.chunk(2, dim=0)
+        latents = 1 / 0.18215 * latents
+        samples = self.vae.decode(latents).sample
 
         samples = (samples / 2 + 0.5).clamp(0, 1)
 
@@ -62,16 +80,4 @@ class DiTPipeline(DiffusionPipeline):
 
         return ImagePipelineOutput(images=samples)
 
-    def forward_with_cfg(self, x, t, y, guidance_scale):
-        """
-        Forward pass of DiT, but also batches the unconditional forward pass for classifier-free guidance.
-        """
-        # https://github.com/openai/glide-text2im/blob/main/notebooks/text2im.ipynb
-        half = x[: len(x) // 2]
-        combined = torch.cat([half, half], dim=0)
-        model_out = self.dit(combined, t, y)
-        eps, rest = model_out[:, :3], model_out[:, 3:]
-        cond_eps, uncond_eps = torch.split(eps, len(eps) // 2, dim=0)
-        half_eps = uncond_eps + guidance_scale * (cond_eps - uncond_eps)
-        eps = torch.cat([half_eps, half_eps], dim=0)
-        return torch.cat([eps, rest], dim=1)
+  
