18f1056f8a7bf03c6dced5e220574d642ae75bd9
YiYi Xu <yixu310@gmail.com>
2023-01-01 19:07:02 -1000

    Update src/diffusers/pipelines/alt_diffusion/pipeline_alt_diffusion.py
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- src/diffusers/pipelines/alt_diffusion/pipeline_alt_diffusion.py
+++ src/diffusers/pipelines/alt_diffusion/pipeline_alt_diffusion.py
@@ -479,7 +479,8 @@ class AltDiffusionPipeline(DiffusionPipeline):
         >>> pipe = AltDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16)
         >>> pipe = pipe.to("cuda")
 
-        >>> prompt = "a photo of an astronaut riding a horse on mars"
+        >>> # "dark elf princess, highly detailed, d & d, fantasy, highly detailed, digital painting, trending on artstation, concept art, sharp focus, illustration, art by artgerm and greg rutkowski and fuji choko and viktoria gavrilenko and hoang lap"
+        >>> prompt = "黑暗精灵公主，非常详细，幻想，非常详细，数字绘画，概念艺术，敏锐的焦点，插图"
         >>> image = pipe(prompt).images[0]
         ```
 
