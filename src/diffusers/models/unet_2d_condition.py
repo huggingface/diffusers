c673e4f2aab3acf6dc4cd3d2b15258662c9ecf46
Patrick von Platen <patrick.v.platen@gmail.com>
2023-01-04 12:13:47 +0100

    Apply suggestions from code review

--- src/diffusers/models/unet_2d_condition.py
+++ src/diffusers/models/unet_2d_condition.py
@@ -387,7 +387,6 @@ class UNet2DConditionModel(ModelMixin, ConfigMixin):
         # prepare attention_mask
         if attention_mask is not None:
             attention_mask = (1 - attention_mask.to(sample.dtype)) * -10000.0
-            attention_mask = attention_mask.to(sample.dtype)
             attention_mask = attention_mask.unsqueeze(1)
 
         # 0. center input if necessary
