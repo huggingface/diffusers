72ae22f1fc6fd955c2db459e95694ac5f96d8687
Patrick von Platen <patrick.v.platen@gmail.com>
2023-01-04 23:01:07 +0000

    fix

--- src/diffusers/schedulers/scheduling_deis_multistep.py
+++ src/diffusers/schedulers/scheduling_deis_multistep.py
@@ -153,13 +153,13 @@ class DEISMultistepScheduler(SchedulerMixin, ConfigMixin):
 
         # settings for DEIS
         if algorithm_type not in ["deis"]:
-            if algorithm_type not in ["dpmsolver", "dpmsolver++"]:
+            if algorithm_type in ["dpmsolver", "dpmsolver++"]:
                 algorithm_type = "deis"
             else:
                 raise NotImplementedError(f"{algorithm_type} does is not implemented for {self.__class__}")
 
         if solver_type not in ["logrho"]:
-            if solver_type not in ["midpoint", "heun"]:
+            if solver_type in ["midpoint", "heun"]:
                 solver_type = "logrho"
             else:
                 raise NotImplementedError(f"solver type {solver_type} does is not implemented for {self.__class__}")
