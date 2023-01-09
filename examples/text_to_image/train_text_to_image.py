0f6259987b670e19ceba9decc1367913c3665557
Julia Turc <turc.raluca@gmail.com>
2023-01-09 16:23:18 -0500

    Bug fix: Reorder input image transformations.
    Currently, the input image is resized, and *then* cropped. This changes the aspect ratio of the input image.
    
    The intent of first cropping and then resizing is expressed in the --center_crop flag description ("Whether to center crop images *before* resizing to resolution"), but the code doesn't currently reflect that.

--- examples/text_to_image/train_text_to_image.py
+++ examples/text_to_image/train_text_to_image.py
@@ -580,8 +580,8 @@ def main():
     # Preprocessing the datasets.
     train_transforms = transforms.Compose(
         [
-            transforms.Resize(args.resolution, interpolation=transforms.InterpolationMode.BILINEAR),
             transforms.CenterCrop(args.resolution) if args.center_crop else transforms.RandomCrop(args.resolution),
+            transforms.Resize(args.resolution, interpolation=transforms.InterpolationMode.BILINEAR), 
             transforms.RandomHorizontalFlip() if args.random_flip else transforms.Lambda(lambda x: x),
             transforms.ToTensor(),
             transforms.Normalize([0.5], [0.5]),
