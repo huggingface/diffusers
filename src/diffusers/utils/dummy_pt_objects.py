5d2ef24d9f0c2ccccc54faa918989e25207c777b
Pi Esposito <piero.skywalker@gmail.com>
2023-01-17 10:02:22 -0300

    add dummy TextualInversionMixin

--- src/diffusers/utils/dummy_pt_objects.py
+++ src/diffusers/utils/dummy_pt_objects.py
@@ -617,6 +617,21 @@ class VQDiffusionScheduler(metaclass=DummyObject):
         requires_backends(cls, ["torch"])
 
 
+class TextualInversionMixin(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+    @classmethod
+    def from_config(cls, *args, **kwargs):
+        requires_backends(cls, ["torch"])
+
+    @classmethod
+    def from_pretrained(cls, *args, **kwargs):
+        requires_backends(cls, ["torch"])
+
+
 class EMAModel(metaclass=DummyObject):
     _backends = ["torch"]
 
