f2b5d40684f948abe9a9191227012c60f6cd8835
patil-suraj <surajp815@gmail.com>
2023-01-18 14:21:52 +0100

    default to init values for some args

--- src/diffusers/training_utils.py
+++ src/diffusers/training_utils.py
@@ -228,31 +228,31 @@ class EMAModel:
         # deepcopy, to be consistent with module API
         state_dict = copy.deepcopy(state_dict)
 
-        self.decay = state_dict["decay"]
+        self.decay = state_dict.get("decay", self.decay)
         if self.decay < 0.0 or self.decay > 1.0:
             raise ValueError("Decay must be between 0 and 1")
 
-        self.min_decay = state_dict["min_decay"]
+        self.min_decay = state_dict.get("min_decay", self.min_decay)
         if not isinstance(self.min_decay, float):
             raise ValueError("Invalid min_decay")
 
-        self.optimization_step = state_dict["optimization_step"]
+        self.optimization_step = state_dict.get("optimization_step", self.optimization_step)
         if not isinstance(self.optimization_step, int):
             raise ValueError("Invalid optimization_step")
 
-        self.update_after_step = state_dict["update_after_step"]
+        self.update_after_step = state_dict.get("update_after_step", self.update_after_step)
         if not isinstance(self.update_after_step, int):
             raise ValueError("Invalid update_after_step")
 
-        self.use_ema_warmup = state_dict["use_ema_warmup"]
+        self.use_ema_warmup = state_dict.get("use_ema_warmup", self.use_ema_warmup)
         if not isinstance(self.use_ema_warmup, bool):
             raise ValueError("Invalid use_ema_warmup")
 
-        self.inv_gamma = state_dict["inv_gamma"]
+        self.inv_gamma = state_dict.get("inv_gamma", self.inv_gamma)
         if not isinstance(self.inv_gamma, (float, int)):
             raise ValueError("Invalid inv_gamma")
 
-        self.power = state_dict["power"]
+        self.power = state_dict["power"].get("power", self.power)
         if not isinstance(self.power, (float, int)):
             raise ValueError("Invalid power")
 
