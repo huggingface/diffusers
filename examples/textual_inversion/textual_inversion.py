5bef9d132437fc32a6bf088357d6f27e959475d0
isamu-isozaki <isamu.website@gmail.com>
2023-01-20 20:24:01 -0500

    Added method to load automatic1111 tokens

--- examples/textual_inversion/textual_inversion.py
+++ examples/textual_inversion/textual_inversion.py
@@ -91,7 +91,18 @@ def load_multitoken_tokenizer(tokenizer, text_encoder, learned_embeds_dict):
         token_embeds = text_encoder.get_input_embeddings().weight.data
         for i, placeholder_token_id in enumerate(placeholder_token_ids):
             token_embeds[placeholder_token_id] =token_embeds[i]        
-
+def load_multitoken_tokenizer_from_automatic(tokenizer, text_encoder, automatic_dict, placeholder_token):
+    """
+    Automatic1111's tokens have format
+    {'string_to_token': {'*': 265}, 'string_to_param': {'*': tensor([[ 0.0833,  0.0030,  0.0057,  ..., -0.0264, -0.0616, -0.0529],
+        [ 0.0058, -0.0190, -0.0584,  ..., -0.0025, -0.0945, -0.0490],
+        [ 0.0916,  0.0025,  0.0365,  ..., -0.0685, -0.0124,  0.0728],
+        [ 0.0812, -0.0199, -0.0100,  ..., -0.0581, -0.0780,  0.0254]],
+       requires_grad=True)}, 'name': 'FloralMarble-400', 'step': 399, 'sd_checkpoint': '4bdfc29c', 'sd_checkpoint_name': 'SD2.1-768'}
+    """ 
+    learned_embeds_dict = {}
+    learned_embeds_dict[placeholder_token] = automatic_dict['string_to_param']['*']
+    load_multitoken_tokenizer(tokenizer, text_encoder, learned_embeds_dict)
 def get_mask(tokenizer, accelerator):
     # Get the mask of the weights that won't change
     mask = torch.ones(len(tokenizer)).to(accelerator.device, dtype=torch.bool)
