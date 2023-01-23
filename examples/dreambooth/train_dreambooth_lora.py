5d138ffca4aca9da161cebdbf6d604e625ab7833
Pedro Cuenca <pedro@huggingface.co>
2023-01-23 17:07:39 +0100

    Specify image format instead of transposing.
    As discussed with @sayakpaul.

--- examples/dreambooth/train_dreambooth_lora.py
+++ examples/dreambooth/train_dreambooth_lora.py
@@ -933,8 +933,7 @@ def main(args):
             for tracker in accelerator.trackers:
                 if tracker.name == "tensorboard":
                     np_images = np.stack([np.asarray(img) for img in images])
-                    np_images = np_images.transpose(0, 3, 1, 2)
-                    tracker.writer.add_images("validation", np_images, epoch)
+                    tracker.writer.add_images("validation", np_images, epoch, dataformats='NHWC')
                 if tracker.name == "wandb":
                     tracker.log(
                         {
@@ -973,8 +972,7 @@ def main(args):
         for tracker in accelerator.trackers:
             if tracker.name == "tensorboard":
                 np_images = np.stack([np.asarray(img) for img in images])
-                np_images = np_images.transpose(0, 3, 1, 2)
-                tracker.writer.add_images("test", np_images, epoch)
+                tracker.writer.add_images("test", np_images, epoch, dataformats='NHWC')
             if tracker.name == "wandb":
                 tracker.log(
                     {
