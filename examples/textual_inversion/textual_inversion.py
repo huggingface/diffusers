3c39b173ce92ddbaeca7f9fab981c9fadd76622b
isamu-isozaki <isamu.website@gmail.com>
2023-01-23 11:01:40 -0500

    Cast embeddings to fp16 when loading

--- examples/textual_inversion/textual_inversion.py
+++ examples/textual_inversion/textual_inversion.py
@@ -105,12 +105,14 @@ def save_progress(tokenizer, text_encoder, accelerator, save_path):
 
 def load_multitoken_tokenizer(tokenizer, text_encoder, learned_embeds_dict):
     for placeholder_token in learned_embeds_dict:
-        num_vec_per_token = learned_embeds_dict[placeholder_token].shape[0]
+        placeholder_embeds = learned_embeds_dict[placeholder_token]
+        num_vec_per_token = placeholder_embeds.shape[0]
+        placeholder_embeds = placeholder_embeds.to(dtype=text_encoder.dtype)
         add_tokens(tokenizer, text_encoder, placeholder_token, num_vec_per_token=num_vec_per_token)
         placeholder_token_ids = tokenizer.encode(placeholder_token, add_special_tokens=False)
         token_embeds = text_encoder.get_input_embeddings().weight.data
         for i, placeholder_token_id in enumerate(placeholder_token_ids):
-            token_embeds[placeholder_token_id] = learned_embeds_dict[placeholder_token][i]
+            token_embeds[placeholder_token_id] = placeholder_embeds[i]
 
 
 def load_multitoken_tokenizer_from_automatic(tokenizer, text_encoder, automatic_dict, placeholder_token):
