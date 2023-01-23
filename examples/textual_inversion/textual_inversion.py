fe6d990030810359eae8d42e3599dfcd6ab286b3
Pedro Cuenca <pedro@huggingface.co>
2023-01-23 17:09:29 +0100

    Style

--- examples/textual_inversion/textual_inversion.py
+++ examples/textual_inversion/textual_inversion.py
@@ -793,7 +793,7 @@ def main():
             for tracker in accelerator.trackers:
                 if tracker.name == "tensorboard":
                     np_images = np.stack([np.asarray(img) for img in images])
-                    tracker.writer.add_images("validation", np_images, epoch, dataformats='NHWC')
+                    tracker.writer.add_images("validation", np_images, epoch, dataformats="NHWC")
                 if tracker.name == "wandb":
                     tracker.log(
                         {
