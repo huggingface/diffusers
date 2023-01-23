0a6b2a8fde8e41d423c0b76424f6862ffcf719ee
Patrick von Platen <patrick.v.platen@gmail.com>
2023-01-23 15:07:34 +0200

    make style

--- src/diffusers/pipelines/alt_diffusion/pipeline_alt_diffusion_img2img.py
+++ src/diffusers/pipelines/alt_diffusion/pipeline_alt_diffusion_img2img.py
@@ -248,7 +248,7 @@ class AltDiffusionImg2ImgPipeline(DiffusionPipeline):
         device,
         num_images_per_prompt,
         do_classifier_free_guidance,
-        negative_prompt,
+        negative_prompt=None,
         text_embeddings: Optional[torch.FloatTensor] = None,
         uncond_embeddings: Optional[torch.FloatTensor] = None,
     ):
