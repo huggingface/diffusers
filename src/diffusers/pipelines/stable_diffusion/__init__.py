f818945be8f70d138e9be219a80bacf7e35c9314
Patrick von Platen <patrick.v.platen@gmail.com>
2023-01-25 13:24:23 +0200

    Improve transformers versions handling

--- src/diffusers/pipelines/stable_diffusion/__init__.py
+++ src/diffusers/pipelines/stable_diffusion/__init__.py
@@ -57,7 +57,7 @@ else:
 
 
 try:
-    if not (is_transformers_available() and is_torch_available() and is_transformers_version(">=", "4.26.0.dev0")):
+    if not (is_transformers_available() and is_torch_available() and is_transformers_version(">=", "4.26.0")):
         raise OptionalDependencyNotAvailable()
 except OptionalDependencyNotAvailable:
     from ...utils.dummy_torch_and_transformers_objects import StableDiffusionDepth2ImgPipeline
--- src/diffusers/utils/import_utils.py
+++ src/diffusers/utils/import_utils.py
@@ -392,10 +392,10 @@ def requires_backends(obj, backends):
 
     if name in [
         "StableDiffusionDepth2ImgPipeline",
-    ] and is_transformers_version("<", "4.26.0.dev0"):
+    ] and is_transformers_version("<", "4.26.0"):
         raise ImportError(
-            f"You need to install `transformers` from 'main' in order to use {name}: \n```\n pip install"
-            " git+https://github.com/huggingface/transformers \n```"
+            f"You need to install `transformers>=4.26` in order to use {name}: \n```\n pip install"
+            " --upgrade transformers \n```"
         )
 
 
