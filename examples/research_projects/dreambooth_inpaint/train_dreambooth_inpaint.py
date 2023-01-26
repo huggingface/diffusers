fafd4ff0cb2e086b07e489cc1fed8616c198848e
patil-suraj <surajp815@gmail.com>
2023-01-26 11:22:26 +0100

    examples min version

--- examples/research_projects/dreambooth_inpaint/train_dreambooth_inpaint.py
+++ examples/research_projects/dreambooth_inpaint/train_dreambooth_inpaint.py
@@ -33,7 +33,7 @@ from transformers import CLIPTextModel, CLIPTokenizer
 
 
 # Will error if the minimal version of diffusers is not installed. Remove at your own risks.
-check_min_version("0.10.0.dev0")
+check_min_version("0.13.0.dev0")
 
 logger = get_logger(__name__)
 
