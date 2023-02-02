b4f5836f58d55f21adec597e1785f32642819a3e
Patrick von Platen <patrick.v.platen@gmail.com>
2023-02-02 16:01:37 +0000

    more corrections of conversion state

--- scripts/convert_k_upscaler_to_diffusers.py
+++ scripts/convert_k_upscaler_to_diffusers.py
@@ -46,16 +46,16 @@ def self_attn_to_diffusers_checkpoint(checkpoint, *, diffusers_attention_prefix,
         f"{diffusers_attention_prefix}.norm1.linear.weight": checkpoint[f"{attention_prefix}.norm_in.mapper.weight"],
         f"{diffusers_attention_prefix}.norm1.linear.bias": checkpoint[f"{attention_prefix}.norm_in.mapper.bias"],
         # to_q
-        f"{diffusers_attention_prefix}.attn1.to_q.weight": weight_q,
+        f"{diffusers_attention_prefix}.attn1.to_q.weight": weight_q.squeeze(-1).squeeze(-1),
         f"{diffusers_attention_prefix}.attn1.to_q.bias": bias_q,
         # to_k
-        f"{diffusers_attention_prefix}.attn1.to_k.weight": weight_k,
+        f"{diffusers_attention_prefix}.attn1.to_k.weight": weight_k.squeeze(-1).squeeze(-1),
         f"{diffusers_attention_prefix}.attn1.to_k.bias": bias_k,
         # to_v
-        f"{diffusers_attention_prefix}.attn1.to_v.weight": weight_v,
+        f"{diffusers_attention_prefix}.attn1.to_v.weight": weight_v.squeeze(-1).squeeze(-1),
         f"{diffusers_attention_prefix}.attn1.to_v.bias": bias_v,
         # to_out
-        f"{diffusers_attention_prefix}.attn1.to_out.0.weight": checkpoint[f"{attention_prefix}.out_proj.weight"],
+        f"{diffusers_attention_prefix}.attn1.to_out.0.weight": checkpoint[f"{attention_prefix}.out_proj.weight"].squeeze(-1).squeeze(-1),
         f"{diffusers_attention_prefix}.attn1.to_out.0.bias": checkpoint[f"{attention_prefix}.out_proj.bias"],
     }
 
@@ -86,23 +86,23 @@ def cross_attn_to_diffusers_checkpoint(
         # to_q
         f"{diffusers_attention_prefix}.attn{diffusers_attention_index}.to_q.weight": checkpoint[
             f"{attention_prefix}.q_proj.weight"
-        ],
+        ].squeeze(-1).squeeze(-1),
         f"{diffusers_attention_prefix}.attn{diffusers_attention_index}.to_q.bias": checkpoint[
             f"{attention_prefix}.q_proj.bias"
         ],
         # to_k
-        f"{diffusers_attention_prefix}.attn{diffusers_attention_index}.to_k.weight": weight_k,
+        f"{diffusers_attention_prefix}.attn{diffusers_attention_index}.to_k.weight": weight_k.squeeze(-1).squeeze(-1),
         f"{diffusers_attention_prefix}.attn{diffusers_attention_index}.to_k.bias": bias_k,
         # to_v
-        f"{diffusers_attention_prefix}.attn{diffusers_attention_index}.to_v.weight": weight_v,
+        f"{diffusers_attention_prefix}.attn{diffusers_attention_index}.to_v.weight": weight_v.squeeze(-1).squeeze(-1),
         f"{diffusers_attention_prefix}.attn{diffusers_attention_index}.to_v.bias": bias_v,
         # to_out
         f"{diffusers_attention_prefix}.attn{diffusers_attention_index}.to_out.0.weight": checkpoint[
             f"{attention_prefix}.out_proj.weight"
-        ],
+        ].squeeze(-1).squeeze(-1),
         f"{diffusers_attention_prefix}.attn{diffusers_attention_index}.to_out.0.bias": checkpoint[
             f"{attention_prefix}.out_proj.bias"
-        ],
+        ]
     }
 
     return rv
