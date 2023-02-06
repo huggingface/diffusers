acce6efd996f8ada2246fcd9f0dddbf690a85661
yiyixuxu <yixu@yis-macbook-pro.lan>
2023-02-06 06:14:38 -1000

    add missing #copied from for preprocess

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_latent_upscale.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_latent_upscale.py
@@ -31,6 +31,7 @@ from ..pipeline_utils import DiffusionPipeline, ImagePipelineOutput
 logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
 
 
+# Copied from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion_upscale.preprocess
 def preprocess(image):
     if isinstance(image, torch.Tensor):
         return image
