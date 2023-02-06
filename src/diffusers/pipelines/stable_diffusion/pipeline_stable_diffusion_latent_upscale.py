810d6479870e0c6836048296878f054a1ac2d6c1
yiyixuxu <yixu@yis-macbook-pro.lan>
2023-02-06 13:22:53 -1000

    fix

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_latent_upscale.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_latent_upscale.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import inspect
 from typing import Callable, List, Optional, Union
 
 import numpy as np
