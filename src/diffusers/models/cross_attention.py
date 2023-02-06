97958bcdc808649d13b0fa3b0f7fad686caf3866
Pedro Cuenca <pedro@huggingface.co>
2023-02-06 11:20:28 +0100

    mps cross-attention hack: don't crash on fp16

--- src/diffusers/models/cross_attention.py
+++ src/diffusers/models/cross_attention.py
@@ -234,7 +234,7 @@ class CrossAttention(nn.Module):
                 # HACK: MPS: Does not support padding by greater than dimension of input tensor.
                 # Instead, we can manually construct the padding tensor.
                 padding_shape = (attention_mask.shape[0], attention_mask.shape[1], target_length)
-                padding = torch.zeros(padding_shape, device=attention_mask.device)
+                padding = torch.zeros(padding_shape).to(attention_mask)
                 attention_mask = torch.concat([attention_mask, padding], dim=2)
             else:
                 attention_mask = F.pad(attention_mask, (0, target_length), value=0.0)
