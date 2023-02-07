0f04e799dc915fbbcb1ccd0e8fe5ce30633f0214
Patrick von Platen <patrick.v.platen@gmail.com>
2023-02-07 08:34:19 +0000

    fix vae pt script

--- scripts/convert_vae_pt_to_diffusers.py
+++ scripts/convert_vae_pt_to_diffusers.py
@@ -146,4 +146,6 @@ if __name__ == "__main__":
     parser.add_argument("--vae_pt_path", default=None, type=str, required=True, help="Path to the VAE.pt to convert.")
     parser.add_argument("--dump_path", default=None, type=str, required=True, help="Path to the VAE.pt to convert.")
 
+    args = parser.parse_args()
+
     vae_pt_to_vae_diffuser(args.vae_pt_path, args.dump_path)
