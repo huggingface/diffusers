b25971d0c1523abcbb6aadc3bb7988c7dfa495fe
Will Berman <wlbberman@gmail.com>
2023-02-09 12:32:04 -0800

    Update src/diffusers/pipelines/stable_diffusion/pipeline_stable_unclip.py
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_unclip.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_unclip.py
@@ -636,7 +636,7 @@ class StableUnCLIPPipeline(DiffusionPipeline):
         Examples:
 
         Returns:
-            [`~pipelines.ImagePipelineOutput`] or `tuple`: [`~ pipeline_utils.ImagePipelineOutput `] if `return_dict`
+            [`~pipelines.ImagePipelineOutput`] or `tuple`: [`~ pipeline_utils.ImagePipelineOutput`] if `return_dict`
             is True, otherwise a `tuple. When returning a tuple, the first element is a list with the generated images.
         """
         # 0. Default height and width to unet
