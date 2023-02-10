c6e66212cc7564b533941dfeb110876e271d5038
Patrick von Platen <patrick.v.platen@gmail.com>
2023-02-10 13:37:56 +0100

    Update tests/pipelines/altdiffusion/test_alt_diffusion_img2img.py

--- tests/pipelines/altdiffusion/test_alt_diffusion_img2img.py
+++ tests/pipelines/altdiffusion/test_alt_diffusion_img2img.py
@@ -241,7 +241,7 @@ class AltDiffusionImg2ImgPipelineFastTests(unittest.TestCase):
         image_slice = image[255:258, 383:386, -1]
 
         assert image.shape == (504, 760, 3)
-        expected_slice = np.array([0.4115, 0.3870, 0.4089, 0.4807, 0.4668, 0.4144, 0.4151, 0.4721, 0.4569])
+        expected_slice = np.array([0.9358, 0.9397, 0.9599, 0.9901, 1.0000, 1.0000, 0.9882, 1.0000, 1.0000])
 
         assert np.abs(image_slice.flatten() - expected_slice).max() < 1e-3
 
