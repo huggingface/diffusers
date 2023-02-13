710480d71d809477675659b1c656202318704d1a
Patrick von Platen <patrick.v.platen@gmail.com>
2023-02-13 17:11:50 +0000

    use path splittetx

--- src/diffusers/models/modeling_utils.py
+++ src/diffusers/models/modeling_utils.py
@@ -143,7 +143,7 @@ def _load_state_dict_into_model(model_to_load, state_dict):
 
 def _add_variant(weights_name: str, variant: Optional[str] = None) -> str:
     if variant is not None:
-        splits = weights_name.split(".")
+        splits = os.path.splittext(weights_name)
         splits = splits[:-1] + [variant] + splits[-1:]
         weights_name = ".".join(splits)
 
