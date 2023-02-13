ec6eb488fd7b5c5f9a802f2ca317f3e5ff17e660
Patrick von Platen <patrick.v.platen@gmail.com>
2023-02-13 12:31:40 +0200

    prep test file for changes

--- tests/pipelines/stable_diffusion_2/test_stable_diffusion_depth.py
+++ tests/pipelines/stable_diffusion_2/test_stable_diffusion_depth.py
@@ -41,7 +41,7 @@ from diffusers import (
 )
 from diffusers.utils import floats_tensor, load_image, load_numpy, nightly, slow, torch_device
 from diffusers.utils.import_utils import is_accelerate_available
-from diffusers.utils.testing_utils import require_torch_gpu
+from diffusers.utils.testing_utils import require_torch_gpu, print_tensor_test
 
 from ...test_pipelines_common import PipelineTesterMixin
 
@@ -284,11 +284,12 @@ class StableDiffusionDepth2ImgPipelineFastTests(PipelineTesterMixin, unittest.Te
         image = pipe(**inputs).images
         image_slice = image[0, -3:, -3:, -1]
 
-        assert image.shape == (1, 32, 32, 3)
-        if torch_device == "mps":
-            expected_slice = np.array([0.6071, 0.5035, 0.4378, 0.5776, 0.5753, 0.4316, 0.4513, 0.5263, 0.4546])
-        else:
-            expected_slice = np.array([0.6374, 0.5039, 0.4199, 0.4819, 0.5563, 0.4617, 0.4028, 0.5381, 0.4711])
+#        assert image.shape == (1, 32, 32, 3)
+#        if torch_device == "mps":
+#            expected_slice = np.array([0.6071, 0.5035, 0.4378, 0.5776, 0.5753, 0.4316, 0.4513, 0.5263, 0.4546])
+#        else:
+        print_tensor_test(image_slice)
+        expected_slice = np.array([0.6374, 0.5039, 0.4199, 0.4819, 0.5563, 0.4617, 0.4028, 0.5381, 0.4711])
 
         assert np.abs(image_slice.flatten() - expected_slice).max() < 1e-3
 
@@ -306,10 +307,11 @@ class StableDiffusionDepth2ImgPipelineFastTests(PipelineTesterMixin, unittest.Te
         image_slice = image[0, -3:, -3:, -1]
 
         assert image.shape == (1, 32, 32, 3)
-        if torch_device == "mps":
-            expected_slice = np.array([0.5825, 0.5135, 0.4095, 0.5452, 0.6059, 0.4211, 0.3994, 0.5177, 0.4335])
-        else:
-            expected_slice = np.array([0.6332, 0.5167, 0.3911, 0.4446, 0.5971, 0.4619, 0.3821, 0.5323, 0.4621])
+#        if torch_device == "mps":
+#            expected_slice = np.array([0.5825, 0.5135, 0.4095, 0.5452, 0.6059, 0.4211, 0.3994, 0.5177, 0.4335])
+#        else:
+        print_tensor_test(image_slice)
+        expected_slice = np.array([0.6332, 0.5167, 0.3911, 0.4446, 0.5971, 0.4619, 0.3821, 0.5323, 0.4621])
 
         assert np.abs(image_slice.flatten() - expected_slice).max() < 1e-3
 
@@ -328,10 +330,11 @@ class StableDiffusionDepth2ImgPipelineFastTests(PipelineTesterMixin, unittest.Te
 
         assert image.shape == (2, 32, 32, 3)
 
-        if torch_device == "mps":
-            expected_slice = np.array([0.6501, 0.5150, 0.4939, 0.6688, 0.5437, 0.5758, 0.5115, 0.4406, 0.4551])
-        else:
-            expected_slice = np.array([0.6248, 0.5206, 0.6007, 0.6749, 0.5022, 0.6442, 0.5352, 0.4140, 0.4681])
+#        if torch_device == "mps":
+#            expected_slice = np.array([0.6501, 0.5150, 0.4939, 0.6688, 0.5437, 0.5758, 0.5115, 0.4406, 0.4551])
+#        else:
+        print_tensor_test(image_slice)
+        expected_slice = np.array([0.6248, 0.5206, 0.6007, 0.6749, 0.5022, 0.6442, 0.5352, 0.4140, 0.4681])
 
         assert np.abs(image_slice.flatten() - expected_slice).max() < 1e-3
 
@@ -383,10 +386,11 @@ class StableDiffusionDepth2ImgPipelineFastTests(PipelineTesterMixin, unittest.Te
         image = pipe(**inputs).images
         image_slice = image[0, -3:, -3:, -1]
 
-        if torch_device == "mps":
-            expected_slice = np.array([0.53232, 0.47015, 0.40868, 0.45651, 0.4891, 0.4668, 0.4287, 0.48822, 0.47439])
-        else:
-            expected_slice = np.array([0.6374, 0.5039, 0.4199, 0.4819, 0.5563, 0.4617, 0.4028, 0.5381, 0.4711])
+#        if torch_device == "mps":
+#            expected_slice = np.array([0.53232, 0.47015, 0.40868, 0.45651, 0.4891, 0.4668, 0.4287, 0.48822, 0.47439])
+#        else:
+        expected_slice = np.array([0.6374, 0.5039, 0.4199, 0.4819, 0.5563, 0.4617, 0.4028, 0.5381, 0.4711])
+        print_tensor_test(image_slice)
 
         assert np.abs(image_slice.flatten() - expected_slice).max() < 1e-3
 
