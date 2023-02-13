d44d6992c8cc32b83c41bd981fa936ab3565375e
Sayak Paul <spsayakpaul@gmail.com>
2023-02-13 16:18:18 +0530

    fix: prepare_attention_mask() call.

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_pix2pix_zero.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_pix2pix_zero.py
@@ -75,8 +75,8 @@ def costruct_direction(source_embedding_path: str, target_embedding_path: str):
 
 class Pix2PixZeroCrossAttnProcessor:
     def __call__(self, attn: CrossAttention, hidden_states, encoder_hidden_states=None, attention_mask=None):
-        _, sequence_length, _ = hidden_states.shape
-        attention_mask = attn.prepare_attention_mask(attention_mask, sequence_length)
+        batch_size, sequence_length, _ = hidden_states.shape
+        attention_mask = attn.prepare_attention_mask(attention_mask, sequence_length, batch_size)
         query = attn.to_q(hidden_states)
 
         if encoder_hidden_states is None:
