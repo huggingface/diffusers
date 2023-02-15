bab362f883b6f872e642c83e9784120d66124180
Sean Sube <seansube@gmail.com>
2023-02-15 17:36:09 -0600

    remove LMS from fast tests

--- tests/pipelines/stable_diffusion/test_onnx_stable_diffusion_upscale.py
+++ tests/pipelines/stable_diffusion/test_onnx_stable_diffusion_upscale.py
@@ -106,24 +106,6 @@ class OnnxStableDiffusionUpscalePipelineFastTests(OnnxPipelineTesterMixin, unitt
 
         assert np.abs(image_slice.flatten() - expected_slice).max() < 1e-1
 
-    def test_pipeline_lms(self):
-        pipe = OnnxStableDiffusionUpscalePipeline.from_pretrained(self.hub_checkpoint, provider="CPUExecutionProvider")
-        pipe.scheduler = LMSDiscreteScheduler.from_config(pipe.scheduler.config)
-        pipe.set_progress_bar_config(disable=None)
-
-        # warmup pass to apply optimizations
-        _ = pipe(**self.get_dummy_inputs())
-
-        inputs = self.get_dummy_inputs()
-        image = pipe(**inputs).images
-        image_slice = image[0, -3:, -3:, -1]
-
-        assert image.shape == (1, 512, 512, 3)
-        expected_slice = np.array(
-            [0.6974782, 0.68902093, 0.70135885, 0.7583618, 0.7804545, 0.7854912, 0.78667426, 0.78743863, 0.78070223]
-        )
-        assert np.abs(image_slice.flatten() - expected_slice).max() < 1e-1
-
     def test_pipeline_euler(self):
         pipe = OnnxStableDiffusionUpscalePipeline.from_pretrained(self.hub_checkpoint, provider="CPUExecutionProvider")
         pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
