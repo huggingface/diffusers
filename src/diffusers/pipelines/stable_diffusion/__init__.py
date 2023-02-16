98b52998455389b7e0979c37500b57de87dbb7c6
Pedro Cuenca <pedro@huggingface.co>
2023-02-16 20:05:00 +0100

    Make diffusers importable with transformers < 4.26

--- src/diffusers/pipelines/stable_diffusion/__init__.py
+++ src/diffusers/pipelines/stable_diffusion/__init__.py
@@ -64,7 +64,10 @@ try:
     if not (is_transformers_available() and is_torch_available() and is_transformers_version(">=", "4.26.0")):
         raise OptionalDependencyNotAvailable()
 except OptionalDependencyNotAvailable:
-    from ...utils.dummy_torch_and_transformers_objects import StableDiffusionDepth2ImgPipeline
+    from ...utils.dummy_torch_and_transformers_objects import (
+        StableDiffusionDepth2ImgPipeline,
+        StableDiffusionPix2PixZeroPipeline,
+    )
 else:
     from .pipeline_stable_diffusion_depth2img import StableDiffusionDepth2ImgPipeline
     from .pipeline_stable_diffusion_pix2pix_zero import StableDiffusionPix2PixZeroPipeline
