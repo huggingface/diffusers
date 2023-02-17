55de50921f89ec06ed51381514c2710cabab1d8e
Patrick von Platen <patrick.v.platen@gmail.com>
2023-02-18 00:00:01 +0200

    make style

--- src/diffusers/pipelines/alt_diffusion/pipeline_alt_diffusion_img2img.py
+++ src/diffusers/pipelines/alt_diffusion/pipeline_alt_diffusion_img2img.py
@@ -561,7 +561,6 @@ class AltDiffusionImg2ImgPipeline(DiffusionPipeline):
         return_dict: bool = True,
         callback: Optional[Callable[[int, int, torch.FloatTensor], None]] = None,
         callback_steps: int = 1,
-        **kwargs,
     ):
         r"""
         Function invoked when calling the pipeline for generation.
@@ -628,10 +627,6 @@ class AltDiffusionImg2ImgPipeline(DiffusionPipeline):
             list of `bool`s denoting whether the corresponding generated image likely represents "not-safe-for-work"
             (nsfw) content, according to the `safety_checker`.
         """
-        message = "Please use `image` instead of `init_image`."
-        init_image = deprecate("init_image", "0.14.0", message, take_from=kwargs)
-        image = init_image or image
-
         # 1. Check inputs. Raise error if not correct
         self.check_inputs(prompt, strength, callback_steps, negative_prompt, prompt_embeds, negative_prompt_embeds)
 
