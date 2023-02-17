653448311cbe93f95fd58a9d6d0bbb2f3d5dba50
Haofan Wang <haofanwang.ai@gmail.com>
2023-02-18 01:53:01 +0800

    Update convert_lora_safetensor_to_diffusers.py

--- scripts/convert_lora_safetensor_to_diffusers.py
+++ scripts/convert_lora_safetensor_to_diffusers.py
@@ -15,12 +15,12 @@
 
 """ Conversion script for the LoRA's safetensors checkpoints. """
 
+import argparse
+
 import torch
 from safetensors.torch import load_file
-from diffusers import StableDiffusionPipeline
-from diffusers import DPMSolverMultistepScheduler
 
-import argparse
+from diffusers import StableDiffusionPipeline
 
 
 def convert(base_model_path, checkpoint_path, LORA_PREFIX_UNET, LORA_PREFIX_TEXT_ENCODER, alpha):
@@ -35,19 +35,19 @@ def convert(base_model_path, checkpoint_path, LORA_PREFIX_UNET, LORA_PREFIX_TEXT
 
     # directly update weight in diffusers model
     for key in state_dict:
-        
+
         # it is suggested to print out the key, it usually will be something like below
         # "lora_te_text_model_encoder_layers_0_self_attn_k_proj.lora_down.weight"
-        
+
         # as we have set the alpha beforehand, so just skip
-        if '.alpha' in key or key in visited:
+        if ".alpha" in key or key in visited:
             continue
-            
-        if 'text' in key:
-            layer_infos = key.split('.')[0].split(LORA_PREFIX_TEXT_ENCODER+'_')[-1].split('_')
+
+        if "text" in key:
+            layer_infos = key.split(".")[0].split(LORA_PREFIX_TEXT_ENCODER + "_")[-1].split("_")
             curr_layer = pipeline.text_encoder
         else:
-            layer_infos = key.split('.')[0].split(LORA_PREFIX_UNET+'_')[-1].split('_')
+            layer_infos = key.split(".")[0].split(LORA_PREFIX_UNET + "_")[-1].split("_")
             curr_layer = pipeline.unet
 
         # find the target layer
@@ -61,18 +61,18 @@ def convert(base_model_path, checkpoint_path, LORA_PREFIX_UNET, LORA_PREFIX_TEXT
                     break
             except Exception:
                 if len(temp_name) > 0:
-                    temp_name += '_'+layer_infos.pop(0)
+                    temp_name += "_" + layer_infos.pop(0)
                 else:
                     temp_name = layer_infos.pop(0)
-        
+
         pair_keys = []
-        if 'lora_down' in key:
-            pair_keys.append(key.replace('lora_down', 'lora_up'))
+        if "lora_down" in key:
+            pair_keys.append(key.replace("lora_down", "lora_up"))
             pair_keys.append(key)
         else:
             pair_keys.append(key)
-            pair_keys.append(key.replace('lora_up', 'lora_down'))
-        
+            pair_keys.append(key.replace("lora_up", "lora_down"))
+
         # update weight
         if len(state_dict[pair_keys[0]].shape) == 4:
             weight_up = state_dict[pair_keys[0]].squeeze(3).squeeze(2).to(torch.float32)
@@ -82,7 +82,7 @@ def convert(base_model_path, checkpoint_path, LORA_PREFIX_UNET, LORA_PREFIX_TEXT
             weight_up = state_dict[pair_keys[0]].to(torch.float32)
             weight_down = state_dict[pair_keys[1]].to(torch.float32)
             curr_layer.weight.data += alpha * torch.mm(weight_up, weight_down)
-            
+
         # update visited list
         for item in pair_keys:
             visited.append(item)
@@ -99,25 +99,22 @@ if __name__ == "__main__":
     parser.add_argument(
         "--checkpoint_path", default=None, type=str, required=True, help="Path to the checkpoint to convert."
     )
-    parser.add_argument(
-        "--dump_path", default=None, type=str, required=True, help="Path to the output model."
-    )
+    parser.add_argument("--dump_path", default=None, type=str, required=True, help="Path to the output model.")
     parser.add_argument(
         "--lora_prefix_unet", default="lora_unet", type=str, help="The prefix of UNet weight in safetensors"
     )
     parser.add_argument(
-        "--lora_prefix_text_encoder", default="lora_te", type=str, help="The prefix of text encoder weight in safetensors"
+        "--lora_prefix_text_encoder",
+        default="lora_te",
+        type=str,
+        help="The prefix of text encoder weight in safetensors",
     )
-    parser.add_argument(
-        "--alpha", default=0.75, type=int, help="The merging ratio in W = W0 + alpha * deltaW"
-    )
+    parser.add_argument("--alpha", default=0.75, type=int, help="The merging ratio in W = W0 + alpha * deltaW")
     parser.add_argument(
         "--to_safetensors", action="store_true", help="Whether to store pipeline in safetensors format or not."
     )
-    parser.add_argument(
-        "--device", type=str, help="Device to use (e.g. cpu, cuda:0, cuda:1, etc.)"
-    )
-    
+    parser.add_argument("--device", type=str, help="Device to use (e.g. cpu, cuda:0, cuda:1, etc.)")
+
     args = parser.parse_args()
 
     base_model_path = args.base_model_path
