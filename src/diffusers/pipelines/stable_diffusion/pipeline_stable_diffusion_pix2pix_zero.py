56515d19256eac0c8e37e87d8b85a6185b49695d
Patrick von Platen <patrick.v.platen@gmail.com>
2023-02-17 15:16:07 +0000

    up

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_pix2pix_zero.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_pix2pix_zero.py
@@ -76,9 +76,10 @@ EXAMPLE_DOC_STRING = """
 
 
         >>> def download(embedding_url, local_filepath):
-        r = requests.get(embedding_url)
-            with open(local_filepath, "wb") as f:
-                f.write(r.content)
+        ...     r = requests.get(embedding_url)
+        ...     with open(local_filepath, "wb") as f:
+        ...         f.write(r.content)
+
 
         >>> model_ckpt = "CompVis/stable-diffusion-v1-4"
         >>> pipeline = StableDiffusionPix2PixZeroPipeline.from_pretrained(model_ckpt, torch_dtype=torch.float16)
@@ -111,41 +112,58 @@ EXAMPLE_INVERT_DOC_STRING = """
         ```py
         >>> import torch
         >>> from transformers import BlipForConditionalGeneration, BlipProcessor
-        >>> from diffusers import DDIMScheduler, DDIMInverseScheduler, StableDiffusionPix2PixZeroPipeline-
+        >>> from diffusers import DDIMScheduler, DDIMInverseScheduler, StableDiffusionPix2PixZeroPipeline
 
-        >>> import requests 
-        >>> from PIL import Image 
+        >>> import requests
+        >>> from PIL import Image
 
-        >>> captioner_id = "Salesforce/blip-image-captioning-base" 
-        >>> processor = BlipProcessor.from_pretrained(captioner_id) 
-        >>> model = BlipForConditionalGeneration.from_pretrained(captioner_id, torch_dtype=torch.float16, low_cpu_mem_usage=True)
+        >>> captioner_id = "Salesforce/blip-image-captioning-base"
+        >>> processor = BlipProcessor.from_pretrained(captioner_id)
+        >>> model = BlipForConditionalGeneration.from_pretrained(
+        ...     captioner_id, torch_dtype=torch.float16, low_cpu_mem_usage=True
+        ... )
 
-        >>> sd_model_ckpt = "CompVis/stable-diffusion-v1-4" 
-        >>> pipeline = StableDiffusionPix2PixZeroPipeline.from_pretrained(sd_model_ckpt, caption_generator=model, caption_processor=processor, torch_dtype=torch.float16, safety_checker=None) 
+        >>> sd_model_ckpt = "CompVis/stable-diffusion-v1-4"
+        >>> pipeline = StableDiffusionPix2PixZeroPipeline.from_pretrained(
+        ...     sd_model_ckpt,
+        ...     caption_generator=model,
+        ...     caption_processor=processor,
+        ...     torch_dtype=torch.float16,
+        ...     safety_checker=None,
+        ... )
 
-        >>> pipeline.scheduler = DDIMScheduler.from_config(pipeline.scheduler.config) >>> pipeline.inverse_scheduler = DDIMInverseScheduler.from_config(pipeline.scheduler.config)
+        >>> pipeline.scheduler = DDIMScheduler.from_config(pipeline.scheduler.config)
+        >>> pipeline.inverse_scheduler = DDIMInverseScheduler.from_config(pipeline.scheduler.config)
         >>> pipeline.enable_model_cpu_offload()
 
-        >>> img_url = "https://github.com/pix2pixzero/pix2pix-zero/raw/main/assets/test_images/cats/cat_6.png" 
+        >>> img_url = "https://github.com/pix2pixzero/pix2pix-zero/raw/main/assets/test_images/cats/cat_6.png"
 
         >>> raw_image = Image.open(requests.get(img_url, stream=True).raw).convert("RGB").resize((512, 512))
-        >>> # generate caption 
-        >>> caption = pipeline.generate_caption(raw_image) 
+        >>> # generate caption
+        >>> caption = pipeline.generate_caption(raw_image)
 
-        >>> # "a photography of a cat with flowers and dai dai daie - daie - daie kasaii" 
+        >>> # "a photography of a cat with flowers and dai dai daie - daie - daie kasaii"
         >>> inv_latents = pipeline.invert(caption, image=raw_image).latents
         >>> # we need to generate source and target embeds
 
-        >>> source_prompts = ["a cat sitting on the street", "a cat playing in the
-        field", "a face of a cat"]
+        >>> source_prompts = ["a cat sitting on the street", "a cat playing in the field", "a face of a cat"]
 
-        >>> target_prompts = ["a dog sitting on the street", "a dog playing in the field", "a face of a dog"] 
+        >>> target_prompts = ["a dog sitting on the street", "a dog playing in the field", "a face of a dog"]
 
         >>> source_embeds = pipeline.get_embeds(source_prompts)
         >>> target_embeds = pipeline.get_embeds(target_prompts)
         >>> # the latents can then be used to edit a real image
 
-        >>> image = pipeline(caption, source_embeds=source_embeds, target_embeds=target_embeds, num_inference_steps=50, cross_attention_guidance_amount=0.15, generator=generator, latents=inv_latents, negative_prompt=caption, ).images[0] 
+        >>> image = pipeline(
+        ...     caption,
+        ...     source_embeds=source_embeds,
+        ...     target_embeds=target_embeds,
+        ...     num_inference_steps=50,
+        ...     cross_attention_guidance_amount=0.15,
+        ...     generator=generator,
+        ...     latents=inv_latents,
+        ...     negative_prompt=caption,
+        ... ).images[0]
         >>> image.save("edited_image.png")
         ```
 """
