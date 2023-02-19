bf0bd174e8c6bb6b3c49b76bcd9792d2c131c911
yiyixuxu <yixu310@gmail,com>
2023-02-19 00:34:37 +0000

    fix get_indices function

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_attend_and_excite.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_attend_and_excite.py
@@ -662,7 +662,7 @@ class StableDiffusionAttendAndExcitePipeline(DiffusionPipeline):
     def get_indices(self, prompt: str) -> Dict[str, int]:
         """Utility function to list the indices of the tokens you wish to alte"""
         ids = self.tokenizer(prompt).input_ids
-        indices = {tok: i for tok, i in zip(self.tokenizer.convert_ids_to_tokens(ids), range(len(ids)))}
+        indices = {i: tok for tok, i in zip(self.tokenizer.convert_ids_to_tokens(ids), range(len(ids)))}
         return indices
 
     @torch.no_grad()
