bf3964c5652dfd7ae4147b0ed55b97f723ba641c
sanchit-gandhi <sanchit@huggingface.co>
2023-02-20 18:43:05 +0100

    clean-up: cpu offload vocoder instead of safety checker

--- src/diffusers/pipelines/audioldm/pipeline_audioldm.py
+++ src/diffusers/pipelines/audioldm/pipeline_audioldm.py
@@ -159,7 +159,7 @@ class AudioLDMPipeline(DiffusionPipeline):
     def enable_sequential_cpu_offload(self, gpu_id=0):
         r"""
         Offloads all models to CPU using accelerate, significantly reducing memory usage. When called, unet,
-        text_encoder, vae and safety checker have their state dicts saved to CPU and then are moved to a
+        text_encoder, vae and vocoder have their state dicts saved to CPU and then are moved to a
         `torch.device('meta') and loaded to GPU only when their specific submodule has its `forward` method called.
         """
         if is_accelerate_available():
@@ -169,7 +169,7 @@ class AudioLDMPipeline(DiffusionPipeline):
 
         device = torch.device(f"cuda:{gpu_id}")
 
-        for cpu_offloaded_model in [self.unet, self.text_encoder, self.vae]:
+        for cpu_offloaded_model in [self.unet, self.text_encoder, self.vae, self.vocoder]:
             cpu_offload(cpu_offloaded_model, device)
 
     @property
