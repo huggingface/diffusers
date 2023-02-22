33a6a4032dc65d771da0f18d9ab9b56f3c6688ac
Mitterand Ekole <mittrandekole@gmail.com>
2023-02-22 15:48:24 +0200

    test memory efficient

---
+++ tests/tests_mem_efficient.py
@@ -0,0 +1,19 @@
+''' Run a simple benchmark to test
+
+#  To Run without the Memory Efficient Attention
+python test.py
+
+# To Run with the Memory Efficient Attention
+USE_MEMORY_EFFICIENT_ATTENTION=1 python test.py
+
+'''
+
+import torch
+from diffusers import StableDiffusionPipeline
+
+pipe = StableDiffusionPipeline.from_pretrained(
+   "CompVis/stable-diffusion-v1-4", revision="fp16", torch_dtype=torch.float16, use_auth_token=True
+).to("cuda")
+
+with torch.inference_mode(), torch.autocast("cuda"):
+   image = pipe("a small cat")
