851eafa9c6690d970b7d77cf13c5a8b88616c374
Abhinay1997 <devarintinagasaiabhinay@gmail.com>
2023-02-27 13:11:52 +0530

    Add reshape_heads_to_batch_dim & reshape_batch_dim_to_heads

--- src/diffusers/models/attention.py
+++ src/diffusers/models/attention.py
@@ -856,3 +856,17 @@ class SparseCausalAttention(CrossAttention):
         # dropout
         hidden_states = self.to_out[1](hidden_states)
         return hidden_states
+    
+    def reshape_heads_to_batch_dim(self, tensor):
+        batch_size, seq_len, dim = tensor.shape
+        head_size = self.num_heads
+        tensor = tensor.reshape(batch_size, seq_len, head_size, dim // head_size)
+        tensor = tensor.permute(0, 2, 1, 3).reshape(batch_size * head_size, seq_len, dim // head_size)
+        return tensor
+
+    def reshape_batch_dim_to_heads(self, tensor):
+        batch_size, seq_len, dim = tensor.shape
+        head_size = self.num_heads
+        tensor = tensor.reshape(batch_size // head_size, head_size, seq_len, dim)
+        tensor = tensor.permute(0, 2, 1, 3).reshape(batch_size // head_size, seq_len, dim * head_size)
+        return tensor
