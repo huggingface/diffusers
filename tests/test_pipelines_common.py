86ec20922d7c740c0e463bb1384b3b8513a3b5e2
William Berman <WLBberman@gmail.com>
2023-02-27 15:33:48 -0800

    error message text re: @yiyixuxu

--- tests/test_pipelines_common.py
+++ tests/test_pipelines_common.py
@@ -85,10 +85,10 @@ class PipelineTesterMixin:
     def params(self) -> frozenset:
         raise NotImplementedError(
             "You need to set the attribute `params` in the child test class. "
-            "`params` are checked if they can be passed to `__call__`. "
-            "`params` are used for families of pipelines that have common parameters such as text to "
-            "image pipelines for prompts and prompt embedding overrides. `pipeline_params.py` provides "
-            "some common sets of parameters such as `TEXT_TO_IMAGE_PARAMS`, `IMAGE_VARIATION_PARAMS`, etc... "
+            "`params` are checked for if all values are present in `__call__`'s signature."
+            " You can set `params` using one of the common set of parameters defined in`pipeline_params.py`"
+            " e.g., `TEXT_TO_IMAGE_PARAMS` defines the common parameters used in text to  "
+            "image pipelines, including prompts and prompt embedding overrides."
             "If your pipeline's set of arguments has minor changes from one of the common sets of arguments, "
             "do not make modifications to the existing common sets of arguments. I.e. a text to image pipeline "
             "with non-configurable height and width arguments should set the attribute as "
