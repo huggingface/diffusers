19a0fdfa9685109cb271f7863f5dc73c0ef13338
Patrick von Platen <patrick.v.platen@gmail.com>
2023-02-28 14:46:35 +0100

    Fix more

--- src/diffusers/utils/hub_utils.py
+++ src/diffusers/utils/hub_utils.py
@@ -155,6 +155,7 @@ def try_cache_hub_download(
     filename: str,
     *args,
     cache_dir: Union[str, Path, None] = None,
+    subfolder: Union[str, Path, None] = None,
     _commit_hash: Optional[str] = None,
     **kwargs,
 ) -> Union[os.PathLike, str]:
@@ -163,14 +164,16 @@ def try_cache_hub_download(
     that first tries to load from cache before pinging the Hub"""
     if _commit_hash is not None:
         # If the file is cached under that commit hash, we return it directly.
-        resolved_file = try_to_load_from_cache(repo_id, filename, cache_dir=cache_dir, revision=_commit_hash)
+        resolved_file = try_to_load_from_cache(
+            repo_id, filename, cache_dir=cache_dir, subfolder=subfolder, revision=_commit_hash
+        )
         if resolved_file is not None:
             if resolved_file is not _CACHED_NO_EXIST:
                 return resolved_file
             else:
                 raise EnvironmentError(f"Could not locate {filename} inside {repo_id}.")
 
-    return hf_hub_download(repo_id, filename, *args, cache_dir=cache_dir, **kwargs)
+    return hf_hub_download(repo_id, filename, *args, cache_dir=cache_dir, subfolder=subfolder, **kwargs)
 
 
 def try_to_load_from_cache(
@@ -178,6 +181,7 @@ def try_to_load_from_cache(
     filename: Union[str, Path, None] = None,
     cache_dir: Union[str, Path, None] = None,
     revision: Optional[str] = None,
+    subfolder: Optional[str] = None,
 ) -> Optional[str]:
     """
     Explores the cache to return the latest cached folder or file for a given revision if found.
@@ -205,6 +209,9 @@ def try_to_load_from_cache(
     if revision is None:
         revision = "main"
 
+    if subfolder is None:
+        subfolder = ""
+
     if cache_dir is None:
         cache_dir = DIFFUSERS_CACHE
 
@@ -213,8 +220,8 @@ def try_to_load_from_cache(
     if not os.path.isdir(repo_cache):
         # No cache for this model
         return None
-    for subfolder in ["refs", "snapshots"]:
-        if not os.path.isdir(os.path.join(repo_cache, subfolder)):
+    for folder in ["refs", "snapshots"]:
+        if not os.path.isdir(os.path.join(repo_cache, folder)):
             return None
 
     # Resolve refs (for instance to convert main to the associated commit sha)
@@ -228,7 +235,7 @@ def try_to_load_from_cache(
         # No cache for this revision and we won't try to return a random revision
         return None
 
-    cached_folder = os.path.join(repo_cache, "snapshots", revision)
+    cached_folder = os.path.join(repo_cache, "snapshots", revision, subfolder)
     cached_folder = cached_folder if os.path.isdir(cached_folder) else None
 
     if filename is None:
