d49296ca7cfa573abb555604dceba5532bea2dad
Will Berman <WLBberman@gmail.com>
2023-03-01 08:07:15 +0000

    [integration tests] test canny

--- tests/pipelines/stable_diffusion/test_stable_diffusion_controlnet.py
+++ tests/pipelines/stable_diffusion/test_stable_diffusion_controlnet.py
@@ -14,8 +14,6 @@
 # limitations under the License.
 
 import gc
-import tempfile
-import time
 import unittest
 
 import numpy as np
@@ -26,12 +24,10 @@ from diffusers import (
     AutoencoderKL,
     ControlNetModel,
     DDIMScheduler,
-    DPMSolverMultistepScheduler,
-    LMSDiscreteScheduler,
     StableDiffusionControlNetPipeline,
     UNet2DConditionModel,
 )
-from diffusers.utils import randn_tensor, slow, torch_device
+from diffusers.utils import load_image, load_numpy, randn_tensor, slow, torch_device
 from diffusers.utils.import_utils import is_xformers_available
 from diffusers.utils.testing_utils import require_torch_gpu
 
@@ -146,300 +142,57 @@ class StableDiffusionControlNetPipelineFastTests(PipelineTesterMixin, unittest.T
 @slow
 @require_torch_gpu
 class StableDiffusionControlNetPipelineSlowTests(unittest.TestCase):
-    model_id = "takuma104/control_sd15_canny"
-    controlnet_memsize = 1451078656  # in float32, https://gist.github.com/takuma104/ce954bde6511a1f0b031a87a646b1f7d
-
     def tearDown(self):
         super().tearDown()
         gc.collect()
         torch.cuda.empty_cache()
 
-    def get_inputs(self, device, generator_device="cpu", dtype=torch.float32, seed=0):
-        generator = torch.Generator(device=generator_device).manual_seed(seed)
-        latents = torch.randn((1, 4, 64, 64), generator=generator, dtype=dtype)
-        vae_scale_factor = 8
-        image = torch.randn((1, 3, 64 * vae_scale_factor, 64 * vae_scale_factor), generator=generator, dtype=dtype)
-        inputs = {
-            "prompt": "a photograph of an astronaut riding a horse",
-            "latents": latents,
-            "generator": generator,
-            "num_inference_steps": 50,
-            "guidance_scale": 7.5,
-            "output_type": "numpy",
-            "image": image,
-        }
-        return inputs
-
-    def test_stable_diffusion_controlnet_ddim(self):
-        sd_pipe = StableDiffusionControlNetPipeline.from_pretrained(self.model_id, safety_checker=None)
-        sd_pipe.scheduler = DDIMScheduler.from_config(sd_pipe.scheduler.config)
-        sd_pipe = sd_pipe.to(torch_device)
-        sd_pipe.set_progress_bar_config(disable=None)
-
-        inputs = self.get_inputs(torch_device)
-        image = sd_pipe(**inputs).images
-        image_slice = image[0, -3:, -3:, -1].flatten()
-
-        # print(image_slice)
-
-        assert image.shape == (1, 512, 512, 3)
-        expected_slice = np.array(
-            [1.0, 0.9598756, 0.8430315, 0.9999685, 0.9130426, 0.8025453, 0.87997377, 0.8080752, 0.7180274]
+    def test_canny(self):
+        controlnet = ControlNetModel.from_pretrained(
+            "fusing/stable-diffusion-v1-5-controlnet-seg", torch_dtype=torch.float16
         )
-        assert np.abs(image_slice - expected_slice).max() < 1e-4
-
-    def test_stable_diffusion_controlnet_lms(self):
-        sd_pipe = StableDiffusionControlNetPipeline.from_pretrained(self.model_id, safety_checker=None)
-        sd_pipe.scheduler = LMSDiscreteScheduler.from_config(sd_pipe.scheduler.config)
-        sd_pipe = sd_pipe.to(torch_device)
-        sd_pipe.set_progress_bar_config(disable=None)
-
-        inputs = self.get_inputs(torch_device)
-        image = sd_pipe(**inputs).images
-        image_slice = image[0, -3:, -3:, -1].flatten()
-
-        # print(image_slice)
-
-        assert image.shape == (1, 512, 512, 3)
-        expected_slice = np.array(
-            [1.0, 0.9631732, 0.84487236, 1.0, 0.914418, 0.8033508, 0.88200307, 0.809505, 0.7186936]
-        )
-        assert np.abs(image_slice - expected_slice).max() < 1e-4
-
-    def test_stable_diffusion_controlnet_dpm(self):
-        sd_pipe = StableDiffusionControlNetPipeline.from_pretrained(self.model_id, safety_checker=None)
-        sd_pipe.scheduler = DPMSolverMultistepScheduler.from_config(sd_pipe.scheduler.config)
-        sd_pipe = sd_pipe.to(torch_device)
-        sd_pipe.set_progress_bar_config(disable=None)
-
-        inputs = self.get_inputs(torch_device)
-        image = sd_pipe(**inputs).images
-        image_slice = image[0, -3:, -3:, -1].flatten()
-
-        # print(image_slice)
-
-        assert image.shape == (1, 512, 512, 3)
-        expected_slice = np.array(
-            [1.0, 0.9627134, 0.8445909, 1.0, 0.9132767, 0.8025819, 0.88159156, 0.8089917, 0.71824443]
-        )
-        assert np.abs(image_slice - expected_slice).max() < 1e-4
-
-    def test_stable_diffusion_controlnet_attention_slicing(self):
-        torch.cuda.reset_peak_memory_stats()
-        pipe = StableDiffusionControlNetPipeline.from_pretrained(self.model_id, torch_dtype=torch.float16)
-        pipe = pipe.to(torch_device)
-        pipe.set_progress_bar_config(disable=None)
-
-        # enable attention slicing
-        pipe.enable_attention_slicing()
-        inputs = self.get_inputs(torch_device, dtype=torch.float16)
-        image_sliced = pipe(**inputs).images
-
-        mem_bytes = torch.cuda.max_memory_allocated()
-        torch.cuda.reset_peak_memory_stats()
-        # make sure that less than 3.75 GB is allocated
-        assert mem_bytes < 3.75 * 10**9 + self.controlnet_memsize / 2
-
-        # disable slicing
-        pipe.disable_attention_slicing()
-        inputs = self.get_inputs(torch_device, dtype=torch.float16)
-        image = pipe(**inputs).images
-
-        # make sure that more than 3.75 GB is allocated
-        mem_bytes = torch.cuda.max_memory_allocated()
-        assert mem_bytes > 3.75 * 10**9 + self.controlnet_memsize / 2
-        assert np.abs(image_sliced - image).max() < 1e-3
-
-    def test_stable_diffusion_vae_slicing(self):
-        torch.cuda.reset_peak_memory_stats()
-        pipe = StableDiffusionControlNetPipeline.from_pretrained(self.model_id, torch_dtype=torch.float16)
-        pipe = pipe.to(torch_device)
-        pipe.set_progress_bar_config(disable=None)
-        pipe.enable_attention_slicing()
-
-        # enable vae slicing
-        pipe.enable_vae_slicing()
-        inputs = self.get_inputs(torch_device, dtype=torch.float16)
-        inputs["prompt"] = [inputs["prompt"]] * 4
-        inputs["latents"] = torch.cat([inputs["latents"]] * 4)
-        image_sliced = pipe(**inputs).images
-
-        mem_bytes = torch.cuda.max_memory_allocated()
-        torch.cuda.reset_peak_memory_stats()
-        # make sure that less than 4 GB is allocated
-        assert mem_bytes < 4e9 + self.controlnet_memsize / 2
-
-        # disable vae slicing
-        pipe.disable_vae_slicing()
-        inputs = self.get_inputs(torch_device, dtype=torch.float16)
-        inputs["prompt"] = [inputs["prompt"]] * 4
-        inputs["latents"] = torch.cat([inputs["latents"]] * 4)
-        image = pipe(**inputs).images
-
-        # make sure that more than 4 GB is allocated
-        mem_bytes = torch.cuda.max_memory_allocated()
-        assert mem_bytes > 4e9 + self.controlnet_memsize / 2
-        # There is a small discrepancy at the image borders vs. a fully batched version.
-        assert np.abs(image_sliced - image).max() < 1e-2
-
-    def test_stable_diffusion_fp16_vs_autocast(self):
-        # this test makes sure that the original model with autocast
-        # and the new model with fp16 yield the same result
-        pipe = StableDiffusionControlNetPipeline.from_pretrained(self.model_id, torch_dtype=torch.float16)
-        pipe = pipe.to(torch_device)
-        pipe.set_progress_bar_config(disable=None)
-
-        inputs = self.get_inputs(torch_device, dtype=torch.float16)
-        image_fp16 = pipe(**inputs).images
-
-        with torch.autocast(torch_device):
-            inputs = self.get_inputs(torch_device)
-            image_autocast = pipe(**inputs).images
-
-        # Make sure results are close enough
-        diff = np.abs(image_fp16.flatten() - image_autocast.flatten())
-        # They ARE different since ops are not run always at the same precision
-        # however, they should be extremely close.
-        assert diff.mean() < 2e-2
-
-    def test_stable_diffusion_controlnet_intermediate_state(self):
-        number_of_steps = 0
-
-        def callback_fn(step: int, timestep: int, latents: torch.FloatTensor) -> None:
-            callback_fn.has_been_called = True
-            nonlocal number_of_steps
-            number_of_steps += 1
-            if step == 1:
-                latents = latents.detach().cpu().numpy()
-                assert latents.shape == (1, 4, 64, 64)
-                latents_slice = latents[0, -3:, -3:, -1]
-                expected_slice = np.array([-1.981, 1.052, -1.0625, -0.01709, -1.138, -0.592, -0.372, 0.332, 0.845])
-                # print(latents_slice.flatten())
-                assert np.abs(latents_slice.flatten() - expected_slice).max() < 5e-2
-            elif step == 2:
-                latents = latents.detach().cpu().numpy()
-                assert latents.shape == (1, 4, 64, 64)
-                latents_slice = latents[0, -3:, -3:, -1]
-                expected_slice = np.array([-2.043, 1.113, -1.138, 0.062, -1.133, -0.614, -0.3901, 0.352, 0.8667])
-                # print(latents_slice.flatten())
-                assert np.abs(latents_slice.flatten() - expected_slice).max() < 5e-2
-
-        callback_fn.has_been_called = False
-
-        pipe = StableDiffusionControlNetPipeline.from_pretrained(self.model_id, torch_dtype=torch.float16)
-        pipe = pipe.to(torch_device)
-        pipe.set_progress_bar_config(disable=None)
-        pipe.enable_attention_slicing()
-
-        inputs = self.get_inputs(torch_device, dtype=torch.float16)
-        pipe(**inputs, callback=callback_fn, callback_steps=1)
-        assert callback_fn.has_been_called
-        assert number_of_steps == inputs["num_inference_steps"]
-
-    def test_stable_diffusion_low_cpu_mem_usage(self):
-        pipeline_id = self.model_id
-
-        start_time = time.time()
-        pipeline_low_cpu_mem_usage = StableDiffusionControlNetPipeline.from_pretrained(
-            pipeline_id, torch_dtype=torch.float16
-        )
-        pipeline_low_cpu_mem_usage.to(torch_device)
-        low_cpu_mem_usage_time = time.time() - start_time
-
-        start_time = time.time()
-        _ = StableDiffusionControlNetPipeline.from_pretrained(
-            pipeline_id, torch_dtype=torch.float16, low_cpu_mem_usage=False
-        )
-        normal_load_time = time.time() - start_time
-
-        assert 2 * low_cpu_mem_usage_time < normal_load_time
-
-    def test_stable_diffusion_pipeline_with_sequential_cpu_offloading(self):
-        torch.cuda.empty_cache()
-        torch.cuda.reset_max_memory_allocated()
-        torch.cuda.reset_peak_memory_stats()
-
-        pipe = StableDiffusionControlNetPipeline.from_pretrained(self.model_id, torch_dtype=torch.float16)
-        pipe = pipe.to(torch_device)
-        pipe.set_progress_bar_config(disable=None)
-        pipe.enable_attention_slicing(1)
-        pipe.enable_sequential_cpu_offload()
-
-        inputs = self.get_inputs(torch_device, dtype=torch.float16)
-        _ = pipe(**inputs)
-
-        mem_bytes = torch.cuda.max_memory_allocated()
-        # make sure that less than 2.8 GB is allocated
-        assert mem_bytes < 2.8 * 10**9 + self.controlnet_memsize / 2
-
-    def test_stable_diffusion_pipeline_with_model_offloading(self):
-        torch.cuda.empty_cache()
-        torch.cuda.reset_max_memory_allocated()
-        torch.cuda.reset_peak_memory_stats()
-
-        inputs = self.get_inputs(torch_device, dtype=torch.float16)
-
-        # Normal inference
 
         pipe = StableDiffusionControlNetPipeline.from_pretrained(
-            self.model_id,
-            torch_dtype=torch.float16,
+            "runwayml/stable-diffusion-v1-5", torch_dtype=torch.float16, safety_checker=None, controlnet=controlnet
         )
-        pipe.to(torch_device)
+        pipe = pipe.to(torch_device)
         pipe.set_progress_bar_config(disable=None)
-        outputs = pipe(**inputs)
-        mem_bytes = torch.cuda.max_memory_allocated()
 
-        # With model offloading
-
-        # Reload but don't move to cuda
-        pipe = StableDiffusionControlNetPipeline.from_pretrained(
-            self.model_id,
-            torch_dtype=torch.float16,
+        generator = torch.Generator(device="cpu").manual_seed(0)
+        prompt = "pig with skyscrapers in background"
+        image = load_image(
+            "https://huggingface.co/datasets/hf-internal-testing/diffusers-images/resolve/main/sd_controlnet/pig_canny.png"
         )
 
-        torch.cuda.empty_cache()
-        torch.cuda.reset_max_memory_allocated()
-        torch.cuda.reset_peak_memory_stats()
+        output = pipe(prompt, image, generator=generator, output_type="np")
 
-        pipe.enable_model_cpu_offload()
-        pipe.set_progress_bar_config(disable=None)
-        outputs_offloaded = pipe(**inputs)
-        mem_bytes_offloaded = torch.cuda.max_memory_allocated()
+        image = output.images[0]
 
-        assert np.abs(outputs.images - outputs_offloaded.images).max() < 1e-3
-        assert mem_bytes_offloaded < mem_bytes
-        assert mem_bytes_offloaded < 3.5 * 10**9 + self.controlnet_memsize / 2
-        for module in pipe.text_encoder, pipe.unet, pipe.vae, pipe.safety_checker:
-            assert module.device == torch.device("cpu")
+        assert image.shape == (512, 512, 3)
 
-        # With attention slicing
-        torch.cuda.empty_cache()
-        torch.cuda.reset_max_memory_allocated()
-        torch.cuda.reset_peak_memory_stats()
+        expected_image = load_numpy(
+            "https://huggingface.co/datasets/hf-internal-testing/diffusers-images/resolve/main/sd_controlnet/pig_canny_out.npy"
+        )
 
-        pipe.enable_attention_slicing()
-        _ = pipe(**inputs)
-        mem_bytes_slicing = torch.cuda.max_memory_allocated()
+        assert np.abs(expected_image - image).max() < 1e-4
 
-        assert mem_bytes_slicing < mem_bytes_offloaded
-        assert mem_bytes_slicing < 3 * 10**9 + self.controlnet_memsize / 2
+    def test_depth(self):
+        ...
 
-    def test_stable_diffusion_no_safety_checker(self):
-        pipe = StableDiffusionControlNetPipeline.from_pretrained(self.model_id, safety_checker=None)
-        assert isinstance(pipe, StableDiffusionControlNetPipeline)
-        assert isinstance(pipe.scheduler, DDIMScheduler)
-        assert pipe.safety_checker is None
+    def test_hed(self):
+        ...
 
-        image = pipe("example prompt", num_inference_steps=2).images[0]
-        assert image is not None
+    def test_mlsd(self):
+        ...
 
-        # check that there's no error when saving a pipeline with one of the models being None
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            pipe.save_pretrained(tmpdirname)
-            pipe = StableDiffusionControlNetPipeline.from_pretrained(tmpdirname)
+    def test_normal(self):
+        ...
 
-        # sanity check that the pipeline still works
-        assert pipe.safety_checker is None
-        image = pipe("example prompt", num_inference_steps=2).images[0]
-        assert image is not None
+    def test_openpose(self):
+        ...
+
+    def test_scribble(self):
+        ...
+
+    def test_seg(self):
+        ...
