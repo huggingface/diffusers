0ba19dac4765a8c07dc104c2948f79ec846d70ec
Will Berman <WLBberman@gmail.com>
2023-03-01 11:49:20 +0000

    [integration tests] cpu offloading

--- tests/pipelines/stable_diffusion/test_stable_diffusion_controlnet.py
+++ tests/pipelines/stable_diffusion/test_stable_diffusion_controlnet.py
@@ -362,3 +362,34 @@ class StableDiffusionControlNetPipelineSlowTests(unittest.TestCase):
         )
 
         assert np.abs(expected_image - image).max() < 1e-4
+
+    def test_sequential_cpu_offloading(self):
+        torch.cuda.empty_cache()
+        torch.cuda.reset_max_memory_allocated()
+        torch.cuda.reset_peak_memory_stats()
+
+        controlnet = ControlNetModel.from_pretrained("fusing/stable-diffusion-v1-5-controlnet-seg")
+
+        pipe = StableDiffusionControlNetPipeline.from_pretrained(
+            "runwayml/stable-diffusion-v1-5", safety_checker=None, controlnet=controlnet
+        )
+        pipe = pipe.to(torch_device)
+        pipe.set_progress_bar_config(disable=None)
+        pipe.enable_attention_slicing()
+        pipe.enable_sequential_cpu_offload()
+
+        prompt = "house"
+        image = load_image(
+            "https://huggingface.co/datasets/hf-internal-testing/diffusers-images/resolve/main/sd_controlnet/house_seg.png"
+        )
+
+        _ = pipe(
+            prompt,
+            image,
+            num_inference_steps=2,
+            output_type="np",
+        )
+
+        mem_bytes = torch.cuda.max_memory_allocated()
+        # make sure that less than 7 GB is allocated
+        assert mem_bytes < 7 * 10**9
