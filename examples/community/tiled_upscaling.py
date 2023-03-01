eadf0e2555cfa19b033e02de53553f71ac33536f
Patrick von Platen <patrick.v.platen@gmail.com>
2023-03-01 10:31:00 +0100

    [Copyright] 2023 (#2524)

--- examples/community/tiled_upscaling.py
+++ examples/community/tiled_upscaling.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Peter Willemsen <peter@codebuffet.co>. All rights reserved.
+# Copyright 2023 Peter Willemsen <peter@codebuffet.co>. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_sag.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_sag.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Susung Hong and The HuggingFace Team. All rights reserved.
+# Copyright 2023 Susung Hong and The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
