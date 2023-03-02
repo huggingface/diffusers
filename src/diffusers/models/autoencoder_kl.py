907788a34b677862d50fbb175d7167d294314b2c
Patrick von Platen <patrick.v.platen@gmail.com>
2023-03-02 17:26:23 +0100

    Apply suggestions from code review
    Co-authored-by: Pedro Cuenca <pedro@huggingface.co>

--- src/diffusers/models/autoencoder_kl.py
+++ src/diffusers/models/autoencoder_kl.py
@@ -203,7 +203,7 @@ class AutoencoderKL(ModelMixin, ConfigMixin):
         r"""Encode a batch of images using a tiled encoder.
         Args:
         When this option is enabled, the VAE will split the input tensor into tiles to compute encoding in several
-        steps. This is useful to keep memory use constant regardless of image size. The end result of tiled encoding is:
+        steps. This is useful to keep memory use constant regardless of image size. The end result of tiled encoding is
         different from non-tiled encoding due to each tile using a different encoder. To avoid tiling artifacts, the
         tiles overlap and are blended together to form a smooth output. You may still see tile-sized changes in the
         look of the output, but they should be much less noticeable.
