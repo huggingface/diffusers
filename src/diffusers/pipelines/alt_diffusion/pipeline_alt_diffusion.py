b9ed57c2369859e48b869faa62803f214ec02cb0
Patrick von Platen <patrick.v.platen@gmail.com>
2023-03-03 16:34:24 +0100

    make fix-copies

--- src/diffusers/pipelines/alt_diffusion/pipeline_alt_diffusion.py
+++ src/diffusers/pipelines/alt_diffusion/pipeline_alt_diffusion.py
@@ -214,6 +214,10 @@ class AltDiffusionPipeline(DiffusionPipeline):
 
         device = torch.device(f"cuda:{gpu_id}")
 
+        if self.device.type != "cpu":
+            self.to("cpu", silence_dtype_warnings=True)
+            torch.cuda.empty_cache()  # otherwise we don't see the memory savings (but they probably exist)
+
         for cpu_offloaded_model in [self.unet, self.text_encoder, self.vae]:
             cpu_offload(cpu_offloaded_model, device)
 
@@ -233,6 +237,10 @@ class AltDiffusionPipeline(DiffusionPipeline):
             raise ImportError("`enable_model_offload` requires `accelerate v0.17.0` or higher.")
 
         device = torch.device(f"cuda:{gpu_id}")
+
+        if self.device.type != "cpu":
+            self.to("cpu", silence_dtype_warnings=True)
+            torch.cuda.empty_cache()  # otherwise we don't see the memory savings (but they probably exist)
 
         hook = None
         for cpu_offloaded_model in [self.text_encoder, self.unet, self.vae]:
