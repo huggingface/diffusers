9f04578ceafc41c0ec99c109231421894e3ea079
Takuma Mori <takuma104@gmail.com>
2023-03-11 02:19:21 +0900

    make style error correct

--- tests/pipelines/stable_diffusion/test_stable_diffusion_controlnet.py
+++ tests/pipelines/stable_diffusion/test_stable_diffusion_controlnet.py
@@ -14,7 +14,6 @@
 # limitations under the License.
 
 import gc
-import tempfile
 import unittest
 
 import numpy as np
@@ -265,8 +264,11 @@ class StableDiffusionMultiControlNetPipelineFastTests(PipelineTesterMixin, unitt
     def test_xformers_attention_forwardGenerator_pass(self):
         self._test_xformers_attention_forwardGenerator_pass(expected_max_diff=2e-3)
 
+    @unittest.skip(reason="Not implemented")
     def test_inference_batch_single_identical(self):
-        self._test_inference_batch_single_identical(expected_max_diff=2e-3)
+        # TODO: implementation
+        # self._test_inference_batch_single_identical(expected_max_diff=2e-3)
+        pass
 
     # override PipelineTesterMixin
     @unittest.skipIf(torch_device != "cuda", reason="float16 requires CUDA")
@@ -291,40 +293,10 @@ class StableDiffusionMultiControlNetPipelineFastTests(PipelineTesterMixin, unitt
         max_diff = np.abs(output - output_fp16).max()
         self.assertLess(max_diff, 1e-2, "The outputs of the fp16 and fp32 pipelines are too different.")
 
-    # override PipelineTesterMixin
-    @unittest.skipIf(torch_device != "cuda", reason="float16 requires CUDA")
+    # # override PipelineTesterMixin
+    @unittest.skip(reason="Not implemented")
     def test_save_load_float16(self):
-        components = self.get_dummy_components()
-        for name, module in components.items():
-            if isinstance(module, list):
-                components[name] = [m.half() for m in module if hasattr(m, "half")]
-            elif hasattr(module, "half"):
-                components[name] = module.to(torch_device).half()
-        pipe = self.pipeline_class(**components)
-        pipe.to(torch_device)
-        pipe.set_progress_bar_config(disable=None)
-
-        inputs = self.get_dummy_inputs(torch_device)
-        output = pipe(**inputs)[0]
-
-        with tempfile.TemporaryDirectory() as tmpdir:
-            pipe.save_pretrained(tmpdir)
-            pipe_loaded = self.pipeline_class.from_pretrained(tmpdir, torch_dtype=torch.float16)
-            pipe_loaded.to(torch_device)
-            pipe_loaded.set_progress_bar_config(disable=None)
-
-        for name, component in pipe_loaded.components.items():
-            if hasattr(component, "dtype"):
-                self.assertTrue(
-                    component.dtype == torch.float16,
-                    f"`{name}.dtype` switched from `float16` to {component.dtype} after loading.",
-                )
-
-        inputs = self.get_dummy_inputs(torch_device)
-        output_loaded = pipe_loaded(**inputs)[0]
-
-        max_diff = np.abs(output - output_loaded).max()
-        self.assertLess(max_diff, 3e-3, "The output of the fp16 pipeline changed after saving and loading.")
+        pass
 
     # override PipelineTesterMixin
     @unittest.skip(reason="Not implemented")
@@ -334,18 +306,7 @@ class StableDiffusionMultiControlNetPipelineFastTests(PipelineTesterMixin, unitt
 
     # override PipelineTesterMixin
     @unittest.skip(reason="Not implemented")
-    def test_inference_batch_single_identical(self):
-        # TODO: implementation
-        pass
-
-    # override PipelineTesterMixin
-    @unittest.skip(reason="Not implemented")
     def test_save_load_local(self):
-        pass
-
-    # override PipelineTesterMixin
-    @unittest.skip(reason="Not implemented")
-    def test_save_load_float16(self):
         pass
 
     # override PipelineTesterMixin
