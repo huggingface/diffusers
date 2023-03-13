de4f5cd00aa0cd7b042f03415f3088f976f071c8
Pie31415 <andy999coc@gmail.com>
2023-03-13 19:01:58 -0400

    update code formatting

--- src/diffusers/models/autoencoder_kl.py
+++ src/diffusers/models/autoencoder_kl.py
@@ -223,12 +223,7 @@ class AutoencoderKL(ModelMixin, ConfigMixin):
         for i in range(0, x.shape[2], overlap_size):
             row = []
             for j in range(0, x.shape[3], overlap_size):
-                tile = x[
-                    :,
-                    :,
-                    i : i + self.tile_sample_min_size,
-                    j : j + self.tile_sample_min_size,
-                ]
+                tile = x[:, :, i : i + self.tile_sample_min_size, j : j + self.tile_sample_min_size]
                 tile = self.encoder(tile)
                 tile = self.quant_conv(tile)
                 row.append(tile)
@@ -276,12 +271,7 @@ class AutoencoderKL(ModelMixin, ConfigMixin):
         for i in range(0, z.shape[2], overlap_size):
             row = []
             for j in range(0, z.shape[3], overlap_size):
-                tile = z[
-                    :,
-                    :,
-                    i : i + self.tile_latent_min_size,
-                    j : j + self.tile_latent_min_size,
-                ]
+                tile = z[:, :, i : i + self.tile_latent_min_size, j : j + self.tile_latent_min_size]
                 tile = self.post_quant_conv(tile)
                 decoded = self.decoder(tile)
                 row.append(decoded)
