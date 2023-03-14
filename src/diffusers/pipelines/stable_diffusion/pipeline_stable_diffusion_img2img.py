f009e9781fd048819cd645724301020b4d53483e
yiyixuxu <yixu310@gmail,com>
2023-03-14 00:54:16 +0000

    add back preprocess function

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_img2img.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_img2img.py
@@ -68,6 +68,27 @@ EXAMPLE_DOC_STRING = """
 """
 
 
+def preprocess(image):
+    if isinstance(image, torch.Tensor):
+        return image
+    elif isinstance(image, PIL.Image.Image):
+        image = [image]
+
+    if isinstance(image[0], PIL.Image.Image):
+        w, h = image[0].size
+        w, h = map(lambda x: x - x % 8, (w, h))  # resize to integer multiple of 8
+
+        image = [np.array(i.resize((w, h), resample=PIL_INTERPOLATION["lanczos"]))[None, :] for i in image]
+        image = np.concatenate(image, axis=0)
+        image = np.array(image).astype(np.float32) / 255.0
+        image = image.transpose(0, 3, 1, 2)
+        image = 2.0 * image - 1.0
+        image = torch.from_numpy(image)
+    elif isinstance(image[0], torch.Tensor):
+        image = torch.cat(image, dim=0)
+    return image
+
+    
 class StableDiffusionImg2ImgPipeline(DiffusionPipeline):
     r"""
     Pipeline for text-guided image to image generation using Stable Diffusion.
