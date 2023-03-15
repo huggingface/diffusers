daaff4279065948f3e016ea5f5b9bda66ac05480
Sayak Paul <spsayakpaul@gmail.com>
2023-03-15 08:03:00 +0530

    Apply suggestions from code review
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- examples/instruct_pix2pix/train_instruct_pix2pix.py
+++ examples/instruct_pix2pix/train_instruct_pix2pix.py
@@ -517,7 +517,7 @@ def main():
                 logger.warn(
                     "xFormers 0.0.16 cannot be used for training in some GPUs. If you observe problems during training, please update xFormers to at least 0.0.17. See https://huggingface.co/docs/diffusers/main/en/optimization/xformers for more details."
                 )
-            instruct_pix2pix_unet.enable_xformers_memory_efficient_attention()
+            unet.enable_xformers_memory_efficient_attention()
         else:
             raise ValueError("xformers is not available. Make sure it is installed correctly")
 
@@ -851,7 +851,7 @@ def main():
                     prompt_mask = random_p < 2 * args.conditioning_dropout_prob
                     prompt_mask = prompt_mask.reshape(bsz, 1, 1)
                     # Final text conditioning.
-                    null_conditioning = text_encoder(tokenize_captions([NULL_PROMPT]).to(accelerator.device))[0]
+                    null_conditioning = text_encoder(tokenize_captions([""]).to(accelerator.device))[0]
                     encoder_hidden_states = torch.where(prompt_mask, null_conditioning, encoder_hidden_states)
 
                     # Sample masks for the original images.
