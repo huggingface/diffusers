12bc5f76242c62de2152cde74591cb640c933a18
Pedro Cuenca <pedro@huggingface.co>
2023-03-21 18:23:50 +0000

    Remove warmup passes in mps tests.

--- tests/models/test_models_vae.py
+++ tests/models/test_models_vae.py
@@ -124,12 +124,7 @@ class AutoencoderKLTests(ModelTesterMixin, unittest.TestCase):
         model = model.to(torch_device)
         model.eval()
 
-        # One-time warmup pass (see #372)
-        if torch_device == "mps" and isinstance(model, ModelMixin):
-            image = torch.randn(1, model.config.in_channels, model.config.sample_size, model.config.sample_size)
-            image = image.to(torch_device)
-            with torch.no_grad():
-                _ = model(image, sample_posterior=True).sample
+        if torch_device == "mps":
             generator = torch.manual_seed(0)
         else:
             generator = torch.Generator(device=torch_device).manual_seed(0)
