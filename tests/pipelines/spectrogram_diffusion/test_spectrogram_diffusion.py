ebb8e9a77a95777da6bf9eabe8161ea28b473448
Kashif Rasul <kashif.rasul@gmail.com>
2023-03-21 14:06:08 +0100

    Update tests/pipelines/spectrogram_diffusion/test_spectrogram_diffusion.py
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- tests/pipelines/spectrogram_diffusion/test_spectrogram_diffusion.py
+++ tests/pipelines/spectrogram_diffusion/test_spectrogram_diffusion.py
@@ -198,7 +198,7 @@ class PipelineIntegrationTests(unittest.TestCase):
     def test_spectrogram_fast(self):
         device = torch_device
 
-        pipe = SpectrogramDiffusionPipeline.from_pretrained("kashif/music-spectrogram-diffusion")
+        pipe = SpectrogramDiffusionPipeline.from_pretrained("google/music-spectrogram-diffusion")
         pipe = pipe.to(device)
         pipe.set_progress_bar_config(disable=None)
         processor = MidiProcessor()
