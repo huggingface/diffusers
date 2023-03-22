92e1164e2e4e96b8fa9ebb5ea8344f2188ea7b86
Pedro Cuenca <pedro@huggingface.co>
2023-03-22 18:29:27 +0000

    `mps`: remove warmup passes (#2771)
    * Remove warmup passes in mps tests.
    
    * Update mps docs: no warmup pass in PyTorch 2
    
    * Update imports.

--- tests/models/test_models_vq.py
+++ tests/models/test_models_vq.py
@@ -85,9 +85,6 @@ class VQModelTests(ModelTesterMixin, unittest.TestCase):
         image = torch.randn(1, model.config.in_channels, model.config.sample_size, model.config.sample_size)
         image = image.to(torch_device)
         with torch.no_grad():
-            # Warmup pass when using mps (see #372)
-            if torch_device == "mps":
-                _ = model(image)
             output = model(image).sample
 
         output_slice = output[0, -1, -3:, -3:].flatten().cpu()
