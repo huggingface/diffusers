387181c398a6d1dbed9702a6e2b37a66c2d8db6d
Patrick von Platen <patrick.v.platen@gmail.com>
2023-03-22 11:54:07 +0000

    up

--- src/diffusers/models/unet_3d_condition.py
+++ src/diffusers/models/unet_3d_condition.py
@@ -1,4 +1,5 @@
-# Copyright 2023 The HuggingFace Team. All rights reserved.
+# Copyright 2023 Alibaba DAMO-VILAB and The HuggingFace Team. All rights reserved.
+# Copyright 2023 The ModelScope Team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
