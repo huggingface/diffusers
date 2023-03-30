fix: loss calculation.

examples/text_to_image/train_text_to_image.py
@@ -866,7 +866,7 @@ def main():
                     # Finally, we take the mean of the rebalanced loss.
                     loss = F.mse_loss(model_pred.float(), target.float(), reduction="none")
                     loss = loss.mean(dim=list(range(1, len(loss.shape)))) * mse_loss_weights
-                    loss = (mse_loss_weights * loss).mean()
+                    loss = loss.mean()
 
                 # Gather the losses across all processes for logging (if we use distributed training).
                 avg_loss = accelerator.gather(loss.repeat(args.train_batch_size)).mean()