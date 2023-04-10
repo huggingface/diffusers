ba49272db8c21e4b728c50447969c74005a93b9f
Andranik Movsisyan <48154088+19and99@users.noreply.github.com>
2023-04-11 00:09:53 +0400

    [Pipeline] Add TextToVideoZeroPipeline (#2954)
    * add TextToVideoZeroPipeline and CrossFrameAttnProcessor
    
    * add docs for text-to-video zero
    
    * add teaser image for text-to-video zero docs
    
    * Fix review changes. Add Documentation. Add test
    
    * clean up the codes in pipeline_text_to_video.py. Add descriptive comments and docstrings
    
    * make style && make quality
    
    * make fix-copies
    
    * make requested changes to docs. use huggingface server links for resources, delete res folder
    
    * make style && make quality && make fix-copies
    
    * make style && make quality
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Sayak Paul <spsayakpaul@gmail.com>

--- src/diffusers/utils/dummy_torch_and_transformers_objects.py
+++ src/diffusers/utils/dummy_torch_and_transformers_objects.py
@@ -407,6 +407,21 @@ class TextToVideoSDPipeline(metaclass=DummyObject):
         requires_backends(cls, ["torch", "transformers"])
 
 
+class TextToVideoZeroPipeline(metaclass=DummyObject):
+    _backends = ["torch", "transformers"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch", "transformers"])
+
+    @classmethod
+    def from_config(cls, *args, **kwargs):
+        requires_backends(cls, ["torch", "transformers"])
+
+    @classmethod
+    def from_pretrained(cls, *args, **kwargs):
+        requires_backends(cls, ["torch", "transformers"])
+
+
 class UnCLIPImageVariationPipeline(metaclass=DummyObject):
     _backends = ["torch", "transformers"]
 
