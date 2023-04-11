a7187e665f901d3e04b1f9cd156b1345ef843917
Paul Truitt <ptruitt7@gmail.com>
2023-04-11 18:58:19 -0400

    Adding cpu

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_instruct_pix2pix.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_instruct_pix2pix.py
@@ -299,7 +299,7 @@ class StableDiffusionInstructPix2PixPipeline(DiffusionPipeline, TextualInversion
             do_classifier_free_guidance,
             generator,
         )
-        original_image = self.decode_latents_inter(image_latents[:1]).numpy()
+        original_image = self.decode_latents_inter(image_latents[:1]).cpu().numpy()
         im = PIL.Image.fromarray(original_image[0])
         im.save('/home/ptruitt7/testtttt.jpg')
 
