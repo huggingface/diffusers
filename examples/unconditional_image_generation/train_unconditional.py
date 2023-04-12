0a73b4d3cd1dd58e6470cfd7f1e10b7b81c63511
Patrick von Platen <patrick.v.platen@gmail.com>
2023-04-12 18:18:30 +0200

    [Post release] v0.16.0dev (#3072)

--- examples/unconditional_image_generation/train_unconditional.py
+++ examples/unconditional_image_generation/train_unconditional.py
@@ -28,7 +28,7 @@ from diffusers.utils.import_utils import is_xformers_available
 
 
 # Will error if the minimal version of diffusers is not installed. Remove at your own risks.
-check_min_version("0.15.0")
+check_min_version("0.16.0.dev0")
 
 logger = get_logger(__name__, log_level="INFO")
 
