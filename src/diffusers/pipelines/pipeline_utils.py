74907ee49a2aa05601bd112a6f9788074f7746a9
Patrick von Platen <patrick.v.platen@gmail.com>
2023-04-13 14:33:11 +0200

    Throw deprecation warning for return_cached_folder (#3092)
    Throw deprecation warning

--- src/diffusers/pipelines/pipeline_utils.py
+++ src/diffusers/pipelines/pipeline_utils.py
@@ -1059,7 +1059,7 @@ class DiffusionPipeline(ConfigMixin):
         return_cached_folder = kwargs.pop("return_cached_folder", False)
         if return_cached_folder:
             message = f"Passing `return_cached_folder=True` is deprecated and will be removed in `diffusers=0.17.0`. Please do the following instead: \n 1. Load the cached_folder via `cached_folder={cls}.download({pretrained_model_name_or_path})`. \n 2. Load the pipeline by loading from the cached folder: `pipeline={cls}.from_pretrained(cached_folder)`."
-            deprecate("return_cached_folder", "0.17.0", message, take_from=kwargs)
+            deprecate("return_cached_folder", "0.17.0", message)
             return model, cached_folder
 
         return model
