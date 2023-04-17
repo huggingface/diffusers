c6cc8244fa97531263ea8525842e188457508b95
Patrick von Platen <patrick.v.platen@gmail.com>
2023-04-17 11:42:50 +0000

    fix more

--- src/diffusers/configuration_utils.py
+++ src/diffusers/configuration_utils.py
@@ -126,8 +126,7 @@ class ConfigMixin:
         except AttributeError as e:
             try:
                 is_in_config = self._safe_hasattr("_internal_dict") and hasattr(self._internal_dict, name)
-                is_attribute = self._safe_hasattr(name)
-                if is_in_config and not is_attribute:
+                if is_in_config:
                     deprecation_message = f"Accessing config attribute `{name}` directly via '{self.__class__.__name__}' object attribute is deprecated. Please access '{name}' over '{self.__class__.__name__}'s config object instead, e.g. 'unet.config.{name}' or 'scheduler.config.{name}'."
                     deprecate("direct config name access", "1.0.0", deprecation_message, standard_warn=False)
                     return self._internal_dict[name]
