04072b49beb8cf6e613f9e13d78cf7ca89a2bdf5
Sayak Paul <spsayakpaul@gmail.com>
2023-04-18 11:49:46 +0530

    fix: save_model_card.

--- examples/custom_diffusion/train_custom_diffusion.py
+++ examples/custom_diffusion/train_custom_diffusion.py
@@ -70,27 +70,29 @@ def save_model_card(repo_id: str, images=None, base_model=str, prompt=str, repo_
     img_str = ""
     for i, image in enumerate(images):
         image.save(os.path.join(repo_folder, f"image_{i}.png"))
-        img_str += f"./image_{i}.png\n"
+        img_str += f"![img_{i}](./image_{i}.png)\n"
 
     yaml = f"""
-        ---
-        license: creativeml-openrail-m
-        base_model: {base_model}
-        instance_prompt: {prompt}
-        tags:
-        - stable-diffusion
-        - stable-diffusion-diffusers
-        - text-to-image
-        - diffusers
-        - custom-diffusion
-        inference: true
-        ---
-            """
+---
+license: creativeml-openrail-m
+base_model: {base_model}
+instance_prompt: {prompt}
+tags:
+- stable-diffusion
+- stable-diffusion-diffusers
+- text-to-image
+- diffusers
+- custom-diffusion
+inference: true
+---
+    """
     model_card = f"""
         # Custom Diffusion - {repo_id}
 
         These are Custom Diffusion adaption weights for {base_model}. The weights were trained on {prompt} using [Custom Diffusion](https://www.cs.cmu.edu/~custom-diffusion). You can find some example images in the following. \n
-        {img_str[0]}
+        {img_str}
+        
+        For more details on the training, please follow [this link](https://github.com/huggingface/diffusers/blob/main/examples/custom_diffusion). 
         """
     with open(os.path.join(repo_folder, "README.md"), "w") as f:
         f.write(yaml + model_card)
