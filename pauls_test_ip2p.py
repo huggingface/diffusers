4e48b86ebc059b4a850650f24f795fe3534b89c8
Paul Truitt <ptruitt7@gmail.com>
2023-04-19 15:14:45 -0400

    Adding more args

--- pauls_test_ip2p.py
+++ pauls_test_ip2p.py
@@ -15,10 +15,14 @@ pipe.to('cuda')
 pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
 img = Image.open(sys.argv[1])
 mask_init = Image.open(sys.argv[2])
+mask_scale = float(sys.argv[5])
+mask_guidance_scale = float(sys.argv[6])
+mask_frequency = int(sys.argv[7])
 mask_numpy = np.array(mask_init)
 mask_int = mask_numpy.astype(int)
 mask = mask_int / mask_int.max()
-images = pipe(sys.argv[3], image=img, mask=mask, mask_guidance_scale=0.2).images
+images = pipe(sys.argv[3], image=img, mask=mask, mask_guidance_scale=mask_scale,
+              guidance_scale=mask_guidance_scale, mask_enforcement_frequency=mask_frequency).images
 result = images[0]
 
 result.save(sys.argv[4])
