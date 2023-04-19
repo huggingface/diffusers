e947c194e241fb9f2c6c443b0350e4f117f73b46
Sayak Paul <spsayakpaul@gmail.com>
2023-04-19 10:25:26 +0530

    remove unnecessary print.

--- examples/custom_diffusion/train_custom_diffusion.py
+++ examples/custom_diffusion/train_custom_diffusion.py
@@ -667,7 +667,6 @@ def main(args):
     # We need to initialize the trackers we use, and also store our configuration.
     # The trackers initializes automatically on the main process.
     if accelerator.is_main_process:
-        print(vars(args))
         accelerator.init_trackers("custom-diffusion", config=vars(args))
 
     # If passed along, set the training seed now.
