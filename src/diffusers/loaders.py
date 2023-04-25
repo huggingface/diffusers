711119ae7a9d202c562aa71e4506bb91f3675dc9
Patrick von Platen <patrick.v.platen@gmail.com>
2023-04-25 15:18:40 +0200

    Fix docs text inversion (#3166)
    * Fix docs text inversion
    
    * Apply suggestions from code review

--- src/diffusers/loaders.py
+++ src/diffusers/loaders.py
@@ -511,7 +511,7 @@ class TextualInversionLoaderMixin:
         model_id = "runwayml/stable-diffusion-v1-5"
         pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to("cuda")
 
-        pipe.load_textual_inversion("./charturnerv2.pt")
+        pipe.load_textual_inversion("./charturnerv2.pt", token="charturnerv2")
 
         prompt = "charturnerv2, multiple views of the same character in the same outfit, a character turnaround of a woman wearing a black jacket and red shirt, best quality, intricate details."
 
