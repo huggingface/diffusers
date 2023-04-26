e55832d6fbea6b06cdde8324066b0427875a712f
njindal <njindal@adobe.com>
2023-04-26 18:03:06 +0530

    [Review comment]: Add is_torchsde_available()

--- src/diffusers/utils/dummy_pt_objects.py
+++ src/diffusers/utils/dummy_pt_objects.py
@@ -465,21 +465,6 @@ class DPMSolverMultistepScheduler(metaclass=DummyObject):
         requires_backends(cls, ["torch"])
 
 
-class DPMSolverSDEScheduler(metaclass=DummyObject):
-    _backends = ["torch"]
-
-    def __init__(self, *args, **kwargs):
-        requires_backends(self, ["torch"])
-
-    @classmethod
-    def from_config(cls, *args, **kwargs):
-        requires_backends(cls, ["torch"])
-
-    @classmethod
-    def from_pretrained(cls, *args, **kwargs):
-        requires_backends(cls, ["torch"])
-
-
 class DPMSolverSinglestepScheduler(metaclass=DummyObject):
     _backends = ["torch"]
 
