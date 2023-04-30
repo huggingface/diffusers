29f78bb8eddfd47eeef5ef2cc22667155357e6a7
showxu <10173746+showxu@users.noreply.github.com>
2023-04-30 22:56:58 +0800

    fix lint

--- src/diffusers/loaders.py
+++ src/diffusers/loaders.py
@@ -909,8 +909,8 @@ class LoraLoaderMixin:
                     else:
                         temp_name = layer_infos.pop(0)
 
-            layer = key.split(".", 1)[0]
-            layer_keys = list(map(lambda e: layer + e, [".lora_up.weight", ".lora_down.weight", ".alpha"]))
+            layers = map(lambda v: key.split(".", 1)[0] + v, [".lora_up.weight", ".lora_down.weight", ".alpha"])
+            layer_keys = list(layers)
 
             weight_up = state_dict[layer_keys[0]].to(dtype)
             weight_down = state_dict[layer_keys[1]].to(dtype)
