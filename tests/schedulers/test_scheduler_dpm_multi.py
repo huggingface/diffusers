393a8d8e621a143f65194a762e795a434c9c1a81
LuChengTHU <lucheng.lc15@gmail.com>
2023-05-05 22:09:09 +0800

    fix typo

--- tests/schedulers/test_scheduler_dpm_multi.py
+++ tests/schedulers/test_scheduler_dpm_multi.py
@@ -170,15 +170,17 @@ class DPMSolverMultistepSchedulerTest(SchedulerCommonTest):
         for algorithm_type in ["dpmsolver", "dpmsolver++", "sde-dpmsolver", "sde-dpmsolver++"]:
             for solver_type in ["midpoint", "heun"]:
                 for order in [1, 2, 3]:
-                    if order == 3 and algorithm_type in ["sde-dpmsolver", "sde-dpmsolver++"]:
-                        continue
                     for prediction_type in ["epsilon", "sample"]:
-                        self.check_over_configs(
-                            solver_order=order,
-                            solver_type=solver_type,
-                            prediction_type=prediction_type,
-                            algorithm_type=algorithm_type,
-                        )
+                        if algorithm_type in ["sde-dpmsolver", "sde-dpmsolver++"]:
+                            if order == 3:
+                                continue
+                        else:
+                            self.check_over_configs(
+                                solver_order=order,
+                                solver_type=solver_type,
+                                prediction_type=prediction_type,
+                                algorithm_type=algorithm_type,
+                            )
                         sample = self.full_loop(
                             solver_order=order,
                             solver_type=solver_type,
