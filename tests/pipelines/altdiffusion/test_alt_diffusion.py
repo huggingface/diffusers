1760fbc4990f08783aa7bc925d676e55f870d447
Sayak Paul <spsayakpaul@gmail.com>
2023-05-10 11:20:53 +0530

    add: determinism to alt_diffusion.

--- tests/pipelines/altdiffusion/test_alt_diffusion.py
+++ tests/pipelines/altdiffusion/test_alt_diffusion.py
@@ -33,6 +33,7 @@ from ..test_pipelines_common import PipelineLatentTesterMixin, PipelineTesterMix
 
 
 torch.backends.cuda.matmul.allow_tf32 = False
+torch.use_deterministic_algorithms(True)
 
 
 class AltDiffusionPipelineFastTests(PipelineLatentTesterMixin, PipelineTesterMixin, unittest.TestCase):
