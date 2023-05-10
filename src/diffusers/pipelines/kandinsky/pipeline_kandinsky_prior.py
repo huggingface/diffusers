5a57f2398b4719a21daf36d5b96f1e02c4ce0525
yiyixuxu <yixu310@gmail,com>
2023-05-10 21:51:01 +0000

    fix _execution_device for prior pipeline

--- src/diffusers/pipelines/kandinsky/pipeline_kandinsky_prior.py
+++ src/diffusers/pipelines/kandinsky/pipeline_kandinsky_prior.py
@@ -153,6 +153,15 @@ class KandinskyPriorPipeline(DiffusionPipeline):
         `pipeline.enable_sequential_cpu_offload()` the execution device can only be inferred from Accelerate's module
         hooks.
         """
+        if self.device != torch.device("meta") or not hasattr(self.decoder, "_hf_hook"):
+            return self.device
+        for module in self.decoder.modules():
+            if (
+                hasattr(module, "_hf_hook")
+                and hasattr(module._hf_hook, "execution_device")
+                and module._hf_hook.execution_device is not None
+            ):
+                return torch.device(module._hf_hook.execution_device)
         return self.device
 
     def _encode_prompt(
