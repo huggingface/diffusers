74d5bae075b813ffe3474526e3833b8324ec66f1
Sayak Paul <spsayakpaul@gmail.com>
2023-05-11 16:10:06 +0530

    make style and determinism to sd 2 depth

--- tests/pipelines/test_pipelines_common.py
+++ tests/pipelines/test_pipelines_common.py
@@ -76,7 +76,9 @@ class PipelineLatentTesterMixin:
         output_pil = pipe(**self.get_dummy_inputs_by_type(torch_device, output_type="pil"))[0]
 
         max_diff = np.abs(output_pt.cpu().numpy().transpose(0, 2, 3, 1) - output_np).max()
-        self.assertLess(max_diff, expected_max_diff, "`output_type=='pt'` generate different results from `output_type=='np'`")
+        self.assertLess(
+            max_diff, expected_max_diff, "`output_type=='pt'` generate different results from `output_type=='np'`"
+        )
 
         max_diff = np.abs(np.array(output_pil[0]) - (output_np * 255).round()).max()
         self.assertLess(max_diff, 2.0, "`output_type=='pil'` generate different results from `output_type=='np'`")
