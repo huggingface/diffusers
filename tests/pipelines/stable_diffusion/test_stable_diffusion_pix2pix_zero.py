edd083773c9c08a26b44893ac562607fe7ffa084
Sayak Paul <spsayakpaul@gmail.com>
2023-05-11 15:18:28 +0530

    determinism to pix2pix zero.

--- tests/pipelines/stable_diffusion/test_stable_diffusion_pix2pix_zero.py
+++ tests/pipelines/stable_diffusion/test_stable_diffusion_pix2pix_zero.py
@@ -40,6 +40,7 @@ from ..test_pipelines_common import PipelineLatentTesterMixin, PipelineTesterMix
 
 
 torch.backends.cuda.matmul.allow_tf32 = False
+torch.use_deterministic_algorithms(True)
 
 
 @skip_mps
