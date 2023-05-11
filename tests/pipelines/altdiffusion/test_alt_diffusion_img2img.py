37fb81b59388d7ef55f7df7861929aea3246cb22
Sayak Paul <spsayakpaul@gmail.com>
2023-05-11 11:24:20 +0530

    determinism to alt diffusion i2i

--- tests/pipelines/altdiffusion/test_alt_diffusion_img2img.py
+++ tests/pipelines/altdiffusion/test_alt_diffusion_img2img.py
@@ -37,6 +37,7 @@ from diffusers.utils.testing_utils import require_torch_gpu
 
 
 torch.backends.cuda.matmul.allow_tf32 = False
+torch.use_deterministic_algorithms(True)
 
 
 class AltDiffusionImg2ImgPipelineFastTests(unittest.TestCase):
