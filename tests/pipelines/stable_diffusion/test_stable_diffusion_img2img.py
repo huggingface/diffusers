0a863bc2362006dfd6fa10a03b3fa5a0d5cc1d47
Sayak Paul <spsayakpaul@gmail.com>
2023-05-11 14:08:46 +0530

    tolerance to i2i sd

--- tests/pipelines/stable_diffusion/test_stable_diffusion_img2img.py
+++ tests/pipelines/stable_diffusion/test_stable_diffusion_img2img.py
@@ -45,6 +45,7 @@ from ..test_pipelines_common import PipelineLatentTesterMixin, PipelineTesterMix
 
 
 torch.backends.cuda.matmul.allow_tf32 = False
+torch.use_deterministic_algorithms(True)
 
 
 class StableDiffusionImg2ImgPipelineFastTests(PipelineLatentTesterMixin, PipelineTesterMixin, unittest.TestCase):
