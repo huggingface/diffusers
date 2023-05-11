440f2aed737e726787b0c69e0a135c79d2a6eb39
Sayak Paul <spsayakpaul@gmail.com>
2023-05-11 12:41:21 +0530

    increase tolerance for paint by paint.

--- tests/pipelines/paint_by_example/test_paint_by_example.py
+++ tests/pipelines/paint_by_example/test_paint_by_example.py
@@ -32,6 +32,7 @@ from ..test_pipelines_common import PipelineTesterMixin
 
 
 torch.backends.cuda.matmul.allow_tf32 = False
+torch.use_deterministic_algorithms(True)
 
 
 class PaintByExamplePipelineFastTests(PipelineTesterMixin, unittest.TestCase):
