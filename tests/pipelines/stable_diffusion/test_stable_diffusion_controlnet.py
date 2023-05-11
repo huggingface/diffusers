01c056f09441a8670d0a88f24e2d4fb4a2956ae8
Takuma Mori <takuma104@gmail.com>
2023-05-11 22:58:07 +0900

    Support ControlNet v1.1 shuffle properly (#3340)
    * add inferring_controlnet_cond_batch
    
    * Revert "add inferring_controlnet_cond_batch"
    
    This reverts commit abe8d6311d4b7f5b9409ca709c7fabf80d06c1a9.
    
    * set guess_mode to True
    whenever global_pool_conditions is True
    
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>
    
    * nit
    
    * add integration test
    
    ---------
    
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- tests/pipelines/stable_diffusion/test_stable_diffusion_controlnet.py
+++ tests/pipelines/stable_diffusion/test_stable_diffusion_controlnet.py
@@ -623,6 +623,37 @@ class StableDiffusionControlNetPipelineSlowTests(unittest.TestCase):
 
         assert np.abs(expected_image - image).max() < 1e-1
 
+    def test_v11_shuffle_global_pool_conditions(self):
+        controlnet = ControlNetModel.from_pretrained("lllyasviel/control_v11e_sd15_shuffle")
+
+        pipe = StableDiffusionControlNetPipeline.from_pretrained(
+            "runwayml/stable-diffusion-v1-5", safety_checker=None, controlnet=controlnet
+        )
+        pipe.enable_model_cpu_offload()
+        pipe.set_progress_bar_config(disable=None)
+
+        generator = torch.Generator(device="cpu").manual_seed(0)
+        prompt = "New York"
+        image = load_image(
+            "https://huggingface.co/lllyasviel/control_v11e_sd15_shuffle/resolve/main/images/control.png"
+        )
+
+        output = pipe(
+            prompt,
+            image,
+            generator=generator,
+            output_type="np",
+            num_inference_steps=3,
+            guidance_scale=7.0,
+        )
+
+        image = output.images[0]
+        assert image.shape == (512, 640, 3)
+
+        image_slice = image[-3:, -3:, -1]
+        expected_slice = np.array([0.1338, 0.1597, 0.1202, 0.1687, 0.1377, 0.1017, 0.2070, 0.1574, 0.1348])
+        assert np.abs(image_slice.flatten() - expected_slice).max() < 1e-2
+
 
 @slow
 @require_torch_gpu
