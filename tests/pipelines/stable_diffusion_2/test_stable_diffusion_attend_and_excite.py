19fce174bc53cfd49935b68a65ed0cb667f5002f
Sayak Paul <spsayakpaul@gmail.com>
2023-05-11 15:57:29 +0530

    disallow tf32 matmul.

--- tests/pipelines/stable_diffusion_2/test_stable_diffusion_attend_and_excite.py
+++ tests/pipelines/stable_diffusion_2/test_stable_diffusion_attend_and_excite.py
@@ -32,7 +32,7 @@ from diffusers.utils.testing_utils import require_torch_gpu
 from ..pipeline_params import TEXT_TO_IMAGE_BATCH_PARAMS, TEXT_TO_IMAGE_IMAGE_PARAMS, TEXT_TO_IMAGE_PARAMS
 from ..test_pipelines_common import PipelineLatentTesterMixin, PipelineTesterMixin
 
-
+torch.backends.cuda.matmul.allow_tf32 = False
 torch.use_deterministic_algorithms(False)
 
 
