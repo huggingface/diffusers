8858ebb74624a1d929dfe08997a6deac288a27f4
Takuma Mori <takuma104@gmail.com>
2023-05-16 01:14:42 +0900

    match result: unet attention only

--- tests/test_kohya_loras_scaffold.py
+++ tests/test_kohya_loras_scaffold.py
@@ -9,8 +9,9 @@ import math
 
 import safetensors
 import torch
+from PIL import Image
 
-from diffusers import DiffusionPipeline
+from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler, StableDiffusionPipeline
 
 
 # modified from https://github.com/kohya-ss/sd-scripts/blob/ad5f318d066c52e5b27306b399bc87e41f2eef2b/networks/lora.py#L17
@@ -70,18 +71,20 @@ class LoRAModuleContainer(torch.nn.Module):
                 alpha = None
                 if lora_name_alpha in state_dict:
                     alpha = state_dict[lora_name_alpha].item()
-                hook = hooks[lora_name]
-                lora_module = LoRAModule(hook.orig_module, lora_dim=lora_dim, alpha=alpha, multiplier=multiplier)
-                self.register_module(lora_name, lora_module)
+                if lora_name in hooks:
+                    hook = hooks[lora_name]
+                    lora_module = LoRAModule(hook.orig_module, lora_dim=lora_dim, alpha=alpha, multiplier=multiplier)
+                    self.register_module(lora_name, lora_module)
 
         # Load whole LoRA weights
-        self.load_state_dict(state_dict)
+        self.load_state_dict(state_dict, strict=False)
 
         # Register LoRAModule to LoRAHook
         for name, module in self.named_modules():
             if module.__class__.__name__ == "LoRAModule":
-                hook = hooks[name]
-                hook.append_lora(module)
+                if name in hooks:
+                    hook = hooks[name]
+                    hook.append_lora(module)
 
     @property
     def alpha(self):
@@ -153,7 +156,8 @@ class LoRAHookInjector(object):
                 for child_name, child_module in module.named_modules():
                     is_linear = child_module.__class__.__name__ == "Linear"
                     is_conv2d = child_module.__class__.__name__ == "Conv2d"
-                    if is_linear or is_conv2d:
+                    # if is_linear or is_conv2d:
+                    if is_linear and not is_conv2d and "ff.net" not in child_name:
                         lora_name = prefix + "." + name + "." + child_name
                         lora_name = lora_name.replace(".", "_")
                         target_modules.append((lora_name, child_module))
@@ -162,12 +166,16 @@ class LoRAHookInjector(object):
     def install_hooks(self, pipe):
         """Install LoRAHook to the pipe."""
         assert len(self.hooks) == 0
-        text_encoder_targets = self._get_target_modules(pipe.text_encoder, "lora_te", ["CLIPAttention", "CLIPMLP"])
-        unet_targets = self._get_target_modules(pipe.unet, "lora_unet", ["Transformer2DModel", "Attention"])
+        # text_encoder_targets = self._get_target_modules(pipe.text_encoder, "lora_te", ["CLIPAttention", "CLIPMLP"])
+        # unet_targets = self._get_target_modules(pipe.unet, "lora_unet", ["Transformer2DModel", "Attention"])
+        text_encoder_targets = []
+        unet_targets = self._get_target_modules(pipe.unet, "lora_unet", ["Transformer2DModel"])
+
         for name, target_module in text_encoder_targets + unet_targets:
             hook = LoRAHook()
             hook.install(target_module)
             self.hooks[name] = hook
+            print(name)
 
         self.device = pipe.device
         self.dtype = pipe.unet.dtype
@@ -211,9 +219,6 @@ def uninstall_lora_hook(pipe: DiffusionPipeline):
     del pipe.remove_lora
 
 
-from PIL import Image
-
-
 def image_grid(imgs, rows, cols):
     assert len(imgs) == rows * cols
 
@@ -226,11 +231,68 @@ def image_grid(imgs, rows, cols):
     return grid
 
 
+TEXT_ENCODER_NAME = "text_encoder"
+UNET_NAME = "unet"
+
+
+def convert_kohya_lora_to_diffusers(state_dict):
+    unet_state_dict = {}
+    te_state_dict = {}
+
+    for key, value in state_dict.items():
+        if "lora_down" in key:
+            lora_name = key.split(".")[0]
+            value.size()[0]
+            lora_name_up = lora_name + ".lora_up.weight"
+            lora_name_alpha = lora_name + ".alpha"
+            if lora_name_alpha in state_dict:
+                state_dict[lora_name_alpha].item()
+                # print(lora_name_alpha, alpha, lora_dim, alpha / lora_dim)
+
+            if lora_name.startswith("lora_unet_"):
+                diffusers_name = key.replace("lora_unet_", "").replace("_", ".")
+                diffusers_name = diffusers_name.replace("down.blocks", "down_blocks")
+                diffusers_name = diffusers_name.replace("mid.block", "mid_block")
+                diffusers_name = diffusers_name.replace("up.blocks", "up_blocks")
+                diffusers_name = diffusers_name.replace("transformer.blocks", "transformer_blocks")
+                diffusers_name = diffusers_name.replace("to.q.lora", "to_q_lora")
+                diffusers_name = diffusers_name.replace("to.k.lora", "to_k_lora")
+                diffusers_name = diffusers_name.replace("to.v.lora", "to_v_lora")
+                diffusers_name = diffusers_name.replace("to.out.0.lora", "to_out_lora")
+                if "transformer_blocks" in diffusers_name:
+                    if "attn1" in diffusers_name or "attn2" in diffusers_name:
+                        diffusers_name = diffusers_name.replace("attn1", "attn1.processor")
+                        diffusers_name = diffusers_name.replace("attn2", "attn2.processor")
+                        unet_state_dict[diffusers_name] = value
+                        unet_state_dict[diffusers_name.replace(".down.", ".up.")] = state_dict[lora_name_up]
+            # elif lora_name.startswith("lora_te_"):
+            #     diffusers_name = key.replace("lora_te_", "").replace("_", ".")
+            #     diffusers_name = diffusers_name.replace("text.model", "text_model")
+            #     diffusers_name = diffusers_name.replace("self.attn", "self_attn")
+            #     diffusers_name = diffusers_name.replace("q.proj.lora", "to_q_lora")
+            #     diffusers_name = diffusers_name.replace("k.proj.lora", "to_k_lora")
+            #     diffusers_name = diffusers_name.replace("v.proj.lora", "to_v_lora")
+            #     diffusers_name = diffusers_name.replace("out.proj.lora", "to_out_lora")
+            #     if "self_attn" in diffusers_name:
+            #         prefix = ".".join(
+            #             diffusers_name.split(".")[:-3]
+            #         )  # e.g.: text_model.encoder.layers.0.self_attn
+            #         suffix = ".".join(diffusers_name.split(".")[-3:])  # e.g.: to_k_lora.down.weight
+            #         for module_name in TEXT_ENCODER_TARGET_MODULES:
+            #             diffusers_name = f"{prefix}.{module_name}.{suffix}"
+            #             te_state_dict[diffusers_name] = value
+            #             te_state_dict[diffusers_name.replace(".down.", ".up.")] = state_dict[lora_name_up]
+
+    unet_state_dict = {f"{UNET_NAME}.{module_name}": params for module_name, params in unet_state_dict.items()}
+    te_state_dict = {f"{TEXT_ENCODER_NAME}.{module_name}": params for module_name, params in te_state_dict.items()}
+    new_state_dict = {**unet_state_dict, **te_state_dict}
+    print("converted", len(new_state_dict), "keys")
+    for k in sorted(new_state_dict.keys()):
+        print(k)
+    return new_state_dict
+
+
 if __name__ == "__main__":
-    import torch
-
-    from diffusers import DPMSolverMultistepScheduler, StableDiffusionPipeline
-
     pipe = StableDiffusionPipeline.from_pretrained(
         "gsdf/Counterfeit-V2.5", torch_dtype=torch.float16, safety_checker=None
     ).to("cuda")
@@ -251,13 +313,12 @@ if __name__ == "__main__":
         height=768,
         num_inference_steps=15,
         num_images_per_prompt=4,
-        generator=torch.Generator(device="cuda").manual_seed(0),
+        generator=torch.manual_seed(0),
     ).images
     image_grid(images, 1, 4).save("test_orig.png")
 
     install_lora_hook(pipe)
     pipe.apply_lora(lora_fn)
-
     images = pipe(
         prompt=prompt,
         negative_prompt=negative_prompt,
@@ -265,14 +326,14 @@ if __name__ == "__main__":
         height=768,
         num_inference_steps=15,
         num_images_per_prompt=4,
-        generator=torch.Generator(device="cuda").manual_seed(0),
+        generator=torch.manual_seed(0),
     ).images
     image_grid(images, 1, 4).save("test_lora_hook.png")
-
     uninstall_lora_hook(pipe)
 
-    pipe.load_lora_weights(lora_fn)
-
+    state_dict = safetensors.torch.load_file(lora_fn)
+    pipe.load_lora_weights(convert_kohya_lora_to_diffusers(state_dict))
+    # pipe.load_lora_weights(lora_fn)
     images = pipe(
         prompt=prompt,
         negative_prompt=negative_prompt,
@@ -280,6 +341,7 @@ if __name__ == "__main__":
         height=768,
         num_inference_steps=15,
         num_images_per_prompt=4,
-        generator=torch.Generator(device="cuda").manual_seed(0),
+        generator=torch.manual_seed(0),
+        cross_attention_kwargs={"scale": 0.5},  # lora scale
     ).images
     image_grid(images, 1, 4).save("test_lora_dev.png")
