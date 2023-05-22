9fb47b5091980e3be2e5d2a630d17343ae607f0b
Pedro Cuenca <pedro@huggingface.co>
2023-05-22 16:05:09 +0200

    Use require_torch_2 decorator.

--- tests/pipelines/controlnet/test_controlnet.py
+++ tests/pipelines/controlnet/test_controlnet.py
@@ -33,7 +33,7 @@ from diffusers import (
 from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion_controlnet import MultiControlNetModel
 from diffusers.utils import load_image, load_numpy, randn_tensor, slow, torch_device
 from diffusers.utils.import_utils import is_xformers_available
-from diffusers.utils.testing_utils import enable_full_determinism, require_torch_gpu, run_test_in_subprocess
+from diffusers.utils.testing_utils import enable_full_determinism, require_torch_2, require_torch_gpu, run_test_in_subprocess
 
 from ..pipeline_params import (
     TEXT_TO_IMAGE_BATCH_PARAMS,
@@ -640,10 +640,8 @@ class ControlNetPipelineSlowTests(unittest.TestCase):
         expected_slice = np.array([0.2724, 0.2846, 0.2724, 0.3843, 0.3682, 0.2736, 0.4675, 0.3862, 0.2887])
         assert np.abs(image_slice.flatten() - expected_slice).max() < 1e-2
 
+    @require_torch_2
     def test_stable_diffusion_compile(self):
-        if version.parse(torch.__version__) < version.parse("2.0"):
-            print(f"Test `test_stable_diffusion_compile` is skipped because {torch.__version__} is < 2.0")
-            return
         run_test_in_subprocess(test_case=self, target_func=_test_stable_diffusion_compile, inputs=None)
 
     def test_v11_shuffle_global_pool_conditions(self):
