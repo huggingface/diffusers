47447830bb52b5a6cc2947fee9aefb153c61419d
Isotr0py <2037008807@qq.com>
2023-05-24 19:31:10 +0800

    reformat pndm test case

--- tests/pipelines/stable_diffusion/test_stable_diffusion_panorama.py
+++ tests/pipelines/stable_diffusion/test_stable_diffusion_panorama.py
@@ -175,7 +175,7 @@ class StableDiffusionPanoramaPipelineFastTests(PipelineLatentTesterMixin, Pipeli
         device = "cpu"  # ensure determinism for the device-dependent torch.Generator
         components = self.get_dummy_components()
         components["scheduler"] = PNDMScheduler(
-            beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", skip_prk_steps = True
+            beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", skip_prk_steps=True
         )
         sd_pipe = StableDiffusionPanoramaPipeline(**components)
         sd_pipe = sd_pipe.to(device)
@@ -187,7 +187,7 @@ class StableDiffusionPanoramaPipelineFastTests(PipelineLatentTesterMixin, Pipeli
 
         assert image.shape == (1, 64, 64, 3)
 
-        expected_slice =  np.array([0.6391, 0.6291, 0.4861, 0.5134, 0.5552, 0.4578, 0.5032, 0.5023, 0.4539])
+        expected_slice = np.array([0.6391, 0.6291, 0.4861, 0.5134, 0.5552, 0.4578, 0.5032, 0.5023, 0.4539])
 
         assert np.abs(image_slice.flatten() - expected_slice).max() < 1e-2
 
