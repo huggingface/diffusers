00df0a41391b7b57cf40cc143f517863b56b9d5f
Sayak Paul <spsayakpaul@gmail.com>
2023-05-31 09:13:04 +0530

    switch to flash attention

--- src/diffusers/models/attention_processor.py
+++ src/diffusers/models/attention_processor.py
@@ -1204,7 +1204,7 @@ class LoRAAttnProcessor2_0(nn.Module):
         value = attn.head_to_batch_dim(value)
 
         # TODO: add support for attn.scale when we move to Torch 2.1
-        with sdp_kernel(**BACKEND_MAP[SDPBackend.EFFICIENT_ATTENTION]):
+        with sdp_kernel(**BACKEND_MAP[SDPBackend.FLASH_ATTENTION]):
             hidden_states = F.scaled_dot_product_attention(
                 query, key, value, attn_mask=attention_mask, dropout_p=0.0, is_causal=False
             )
