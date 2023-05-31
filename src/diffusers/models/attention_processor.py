e5fad840283120fc0d553cb12a5e447a37385377
Sayak Paul <spsayakpaul@gmail.com>
2023-05-31 10:47:05 +0530

    explicitly specify the flash attention kernel in sdpa

--- src/diffusers/models/attention_processor.py
+++ src/diffusers/models/attention_processor.py
@@ -17,7 +17,7 @@ from typing import Callable, Optional, Union
 import torch
 import torch.nn.functional as F
 from torch import nn
-from torch.backends.cuda import SDPBackend
+from torch.backends.cuda import SDPBackend, sdp_kernel
 
 from ..utils import deprecate, logging, maybe_allow_in_graph
 from ..utils.import_utils import is_xformers_available
@@ -1301,9 +1301,10 @@ class LoRAAttnProcessor2_0(nn.Module):
         value = value.view(batch_size, -1, attn.heads, head_dim).transpose(1, 2)
 
         # TODO: add support for attn.scale when we move to Torch 2.1
-        hidden_states = F.scaled_dot_product_attention(
-            query, key, value, attn_mask=attention_mask, dropout_p=0.0, is_causal=False
-        )
+        with sdp_kernel(**BACKEND_MAP[SDPBackend.FLASH_ATTENTION]):
+            hidden_states = F.scaled_dot_product_attention(
+                query, key, value, attn_mask=attention_mask, dropout_p=0.0, is_causal=False
+            )
         hidden_states = hidden_states.transpose(1, 2).reshape(batch_size, -1, attn.heads * head_dim)
         hidden_states = hidden_states.to(query.dtype)
 
