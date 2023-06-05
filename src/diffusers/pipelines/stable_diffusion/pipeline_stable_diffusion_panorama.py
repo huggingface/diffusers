6c1faab1a94d41cf7160397e7a0c7012c6b637ad
Isotr0py <2037008807@qq.com>
2023-06-05 16:17:46 +0800

    format entry for the new argument

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_panorama.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_panorama.py
@@ -510,8 +510,8 @@ class StableDiffusionPanoramaPipeline(DiffusionPipeline, TextualInversionLoaderM
                 1`. Higher guidance scale encourages to generate images that are closely linked to the text `prompt`,
                 usually at the expense of lower image quality.
             view_batch_size (`int`, *optional*, defaults to 1):
-                The batch size to denoise splited views. For some GPUs with high performance, higher view batch size can contribute
-                to faster generation speed.
+                The batch size to denoise splited views. For some GPUs with high performance, higher view batch size
+                can speedup the generation.
             negative_prompt (`str` or `List[str]`, *optional*):
                 The prompt or prompts not to guide the image generation. If not defined, one has to pass
                 `negative_prompt_embeds` instead. Ignored when not using guidance (i.e., ignored if `guidance_scale` is
