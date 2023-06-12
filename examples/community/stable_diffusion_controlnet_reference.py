ef41671249dc79651b9b020140eef390f8faa683
kadirnar <kadir.nar@hotmail.com>
2023-06-12 18:17:22 +0300

    added the _default_height_width function.

--- examples/community/stable_diffusion_controlnet_reference.py
+++ examples/community/stable_diffusion_controlnet_reference.py
@@ -63,6 +63,31 @@ def torch_dfs(model: torch.nn.Module):
 
 
 class StableDiffusionControlNetReferencePipeline(StableDiffusionControlNetPipeline):
+    def _default_height_width(self, height, width, image):
+        # NOTE: It is possible that a list of images have different
+        # dimensions for each image, so just checking the first image
+        # is not _exactly_ correct, but it is simple.
+        while isinstance(image, list):
+            image = image[0]
+
+        if height is None:
+            if isinstance(image, PIL.Image.Image):
+                height = image.height
+            elif isinstance(image, torch.Tensor):
+                height = image.shape[2]
+
+            height = (height // 8) * 8  # round down to nearest multiple of 8
+
+        if width is None:
+            if isinstance(image, PIL.Image.Image):
+                width = image.width
+            elif isinstance(image, torch.Tensor):
+                width = image.shape[3]
+
+            width = (width // 8) * 8  # round down to nearest multiple of 8
+
+        return height, width
+    
     def prepare_ref_latents(self, refimage, batch_size, dtype, device, generator, do_classifier_free_guidance):
         refimage = refimage.to(device=device, dtype=dtype)
 
