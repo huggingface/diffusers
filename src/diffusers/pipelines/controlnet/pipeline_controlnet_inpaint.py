aa8e39d4584409bacbe03757e0448364abea0b7c
Patrick von Platen <patrick.v.platen@gmail.com>
2023-06-12 11:47:53 +0000

    Fix

--- src/diffusers/pipelines/controlnet/pipeline_controlnet_inpaint.py
+++ src/diffusers/pipelines/controlnet/pipeline_controlnet_inpaint.py
@@ -1306,15 +1306,12 @@ class StableDiffusionControlNetInpaintPipeline(DiffusionPipeline, TextualInversi
                     mask[:1]
 
                     if i < len(timesteps) - 1:
-                        # noise_timestep = timesteps[i + 1]
-                        noise_timestep = timesteps[i]
-                        print(noise_timestep)
+                        noise_timestep = timesteps[i + 1]
                         init_latents_proper = self.scheduler.add_noise(
                             init_latents_proper, noise, torch.tensor([noise_timestep])
                         )
 
-                    # latents = (1 - init_mask) * init_latents_proper + init_mask * latents
-                    latents = init_latents_proper
+                    latents = (1 - init_mask) * init_latents_proper + init_mask * latents
 
                 # call the callback, if provided
                 if i == len(timesteps) - 1 or ((i + 1) > num_warmup_steps and (i + 1) % self.scheduler.order == 0):
