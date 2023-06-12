2eb6e0a12e3683a428d72192b6220ed47cf452b1
Patrick von Platen <patrick.v.platen@gmail.com>
2023-06-12 10:02:50 +0000

    [MultiControlNet] Allow save and load

--- src/diffusers/pipelines/controlnet/pipeline_controlnet_img2img.py
+++ src/diffusers/pipelines/controlnet/pipeline_controlnet_img2img.py
@@ -757,18 +757,6 @@ class StableDiffusionControlNetImg2ImgPipeline(DiffusionPipeline, TextualInversi
 
         return latents
 
-    # override DiffusionPipeline
-    def save_pretrained(
-        self,
-        save_directory: Union[str, os.PathLike],
-        safe_serialization: bool = False,
-        variant: Optional[str] = None,
-    ):
-        if isinstance(self.controlnet, ControlNetModel):
-            super().save_pretrained(save_directory, safe_serialization, variant)
-        else:
-            raise NotImplementedError("Currently, the `save_pretrained()` is not implemented for Multi-ControlNet.")
-
     @torch.no_grad()
     @replace_example_docstring(EXAMPLE_DOC_STRING)
     def __call__(
