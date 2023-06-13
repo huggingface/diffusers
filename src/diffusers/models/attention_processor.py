99e35de79180d63cfd85263ceb766617d00d5386
William Berman <WLBberman@gmail.com>
2023-06-13 14:22:14 -0700

    refactor to support patching LoRA into T5
    instantiate the lora linear layer on the same device as the regular linear layer
    
    get lora rank from state dict
    
    tests
    
    fmt
    
    can create lora layer in float32 even when rest of model is float16
    
    fix loading model hook
    
    remove load_lora_weights_ and T5 dispatching
    
    remove Unet#attn_processors_state_dict
    
    docstrings

--- src/diffusers/models/attention_processor.py
+++ src/diffusers/models/attention_processor.py
@@ -506,14 +506,14 @@ class AttnProcessor:
 
 
 class LoRALinearLayer(nn.Module):
-    def __init__(self, in_features, out_features, rank=4, network_alpha=None):
+    def __init__(self, in_features, out_features, rank=4, network_alpha=None, device=None, dtype=None):
         super().__init__()
 
         if rank > min(in_features, out_features):
             raise ValueError(f"LoRA rank {rank} must be less or equal than {min(in_features, out_features)}")
 
-        self.down = nn.Linear(in_features, rank, bias=False)
-        self.up = nn.Linear(rank, out_features, bias=False)
+        self.down = nn.Linear(in_features, rank, bias=False, device=device, dtype=dtype)
+        self.up = nn.Linear(rank, out_features, bias=False, device=device, dtype=dtype)
         # This value has the same meaning as the `--network_alpha` option in the kohya-ss trainer script.
         # See https://github.com/darkstorm2150/sd-scripts/blob/main/docs/train_network_README-en.md#execute-learning
         self.network_alpha = network_alpha
