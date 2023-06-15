562ea3519e7e2c5cb7f603166b884d2106cb3b70
estelleafl <estelle.aflalo@intel.com>
2023-06-15 16:31:29 +0300

    Update src/diffusers/pipelines/stable_diffusion/__init__.py
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- src/diffusers/pipelines/stable_diffusion/__init__.py
+++ src/diffusers/pipelines/stable_diffusion/__init__.py
@@ -36,23 +36,6 @@ class StableDiffusionPipelineOutput(BaseOutput):
     nsfw_content_detected: Optional[List[bool]]
 
 
-@dataclass
-class LDM3DPipelineOutput(BaseOutput):
-    """
-    Output class for Stable Diffusion pipelines.
-
-    Args:
-        images (`List[PIL.Image.Image]` or `np.ndarray`)
-            List of denoised PIL images of length `batch_size` or numpy array of shape `(batch_size, height, width,
-            num_channels)`. PIL images or numpy array present the denoised images of the diffusion pipeline.
-        nsfw_content_detected (`List[bool]`)
-            List of flags denoting whether the corresponding generated image likely represents "not-safe-for-work"
-            (nsfw) content, or `None` if safety checking could not be performed.
-    """
-
-    rgb: Union[List[PIL.Image.Image], np.ndarray]
-    depth: Union[List[PIL.Image.Image], np.ndarray]
-    nsfw_content_detected: Optional[List[bool]]
 
 
 try:
