f44681af461f3c30ed0265034629adecbde3b217
Uranus <whlisikuang@outlook.com>
2023-06-21 20:52:33 +0800

    fix code style

--- src/diffusers/schedulers/scheduling_sde_ve.py
+++ src/diffusers/schedulers/scheduling_sde_ve.py
@@ -276,7 +276,11 @@ class ScoreSdeVeScheduler(SchedulerMixin, ConfigMixin):
         # Make sure sigmas and timesteps have the same device and dtype as original_samples
         timesteps = timesteps.to(original_samples.device)
         sigmas = self.discrete_sigmas.to(original_samples.device)[timesteps]
-        noise = noise * sigmas[:, None, None, None] if noise is not None else torch.randn_like(original_samples) * sigmas[:, None, None, None]
+        noise = (
+            noise * sigmas[:, None, None, None]
+            if noise is not None
+            else torch.randn_like(original_samples) * sigmas[:, None, None, None]
+        )
         noisy_samples = noise + original_samples
         return noisy_samples
 
