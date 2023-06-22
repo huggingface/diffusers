b6f1a7f6c6e5edf7f604126d2a2d15a05603f649
Aflalo <estellea@isl-gpu33.rr.intel.com>
2023-06-22 06:55:00 -0700

    make style

--- src/diffusers/image_processor.py
+++ src/diffusers/image_processor.py
@@ -317,7 +317,9 @@ class VaeImageProcessorLDM3D(VaeImageProcessor):
         images_depth = images[:, :, :, 3:]
         if images.shape[-1] == 6:
             images_depth = (images_depth * 255).round().astype("uint8")
-            pil_images = [Image.fromarray(self.rgblike_to_depthmap(image_depth), mode="I;16") for image_depth in images_depth]
+            pil_images = [
+                Image.fromarray(self.rgblike_to_depthmap(image_depth), mode="I;16") for image_depth in images_depth
+            ]
         elif images.shape[-1] == 4:
             images_depth = (images_depth * 65535.0).astype(np.uint16)
             pil_images = [Image.fromarray(image_depth, mode="I;16") for image_depth in images_depth]
