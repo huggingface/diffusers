ec18756f97835bb4ac407afae28be4b7a5ff5bde
Aflalo <estellea@isl-gpu33.rr.intel.com>
2023-06-22 06:54:09 -0700

    preprocessing for 4 channels and not 6

--- src/diffusers/image_processor.py
+++ src/diffusers/image_processor.py
@@ -314,12 +314,15 @@ class VaeImageProcessorLDM3D(VaeImageProcessor):
         """
         if images.ndim == 3:
             images = images[None, ...]
-        images = (images * 255).round().astype("uint8")
-        if images.shape[-1] == 1:
-            # special case for grayscale (single channel) images
-            raise Exception("Not supported")
+        images_depth = images[:, :, :, 3:]
+        if images.shape[-1] == 6:
+            images_depth = (images_depth * 255).round().astype("uint8")
+            pil_images = [Image.fromarray(self.rgblike_to_depthmap(image_depth), mode="I;16") for image_depth in images_depth]
+        elif images.shape[-1] == 4:
+            images_depth = (images_depth * 65535.0).astype(np.uint16)
+            pil_images = [Image.fromarray(image_depth, mode="I;16") for image_depth in images_depth]
         else:
-            pil_images = [Image.fromarray(self.rgblike_to_depthmap(image[:, :, 3:]), mode="I;16") for image in images]
+            raise Exception("Not supported")
 
         return pil_images
 
