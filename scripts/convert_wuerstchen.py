560da3b993394f72f1eab2e8c3163650adcb1605
Kashif Rasul <kashif.rasul@gmail.com>
2023-06-23 10:19:27 +0200

    use model_v2

--- scripts/convert_wuerstchen.py
+++ scripts/convert_wuerstchen.py
@@ -33,7 +33,7 @@ text_encoder = CLIPTextModel.from_pretrained("laion/CLIP-ViT-H-14-laion2B-s32B-b
 clip_tokenizer = AutoTokenizer.from_pretrained("laion/CLIP-ViT-H-14-laion2B-s32B-b79K")
 
 # EfficientNet
-state_dict = torch.load(os.path.join(model_path, "model_stage_b.pt"), map_location=device)["effnet_state_dict"]
+state_dict = torch.load(os.path.join(model_path, "model_v2_stage_b.pt"), map_location=device)["effnet_state_dict"]
 
 # Paella
 state_dict = torch.load(os.path.join(model_path, "model_stage_b.pt"), map_location=device)["state_dict"]
@@ -41,6 +41,55 @@ paella_model = Paella(byt5_embd=1024).to(device)
 paella_model.load_state_dict(state_dict)
 
 # Prior
-state_dict = torch.load(os.path.join(model_path, "model_stage_c.pt"), map_location=device)
+state_dict = torch.load(os.path.join(model_path, "model_v2__stage_c.pt"), map_location=device)
 prior_model = Prior(c_in=16, c=1536, c_cond=1024, c_r=64, depth=32, nhead=24).to(device)
 prior_model.load_state_dict(state_dict["state_dict"])
+
+
+# scheduler
+scheduler = DDPMScheduler(
+    beta_schedule="linear",
+)
+
+# WuerstchenPipeline(
+#     vae=VQGan()
+#     text_encoder=ClipTextEncoder(),
+#     prior=prior,
+#     (image_encoder)=efficient_net,
+# )
+# stage C = prior
+# stage B = unet
+# stage A = vae
+# WuerstchenPipeline(
+#     vae=VQGan()
+#     text_encoder=ClipTextEncoder(),
+#     unet = UNet2DConditionModel(),
+#     prior=prior,
+#     (image_encoder)=efficient_net,
+# )
+# Patrick von Platen4:17 PM
+# WuerstchenPipeline(
+#     vae=VQGan()
+#     text_encoder=ClipTextEncoder(),
+#     unet = UNet2DConditionModel(),
+#     prior=prior,
+#     tokenizer=CLIPTokenizer,
+#     (image_encoder)=efficient_net,
+# )
+# WuerstchenPipeline(
+#     vae=VQGan()
+#     text_encoder=ClipTextEncoder(),
+#     unet = UNet2DConditionModel(),
+#     prior=PriorTransformer(),
+#     tokenizer=CLIPTokenizer,
+#     (image_encoder)=efficient_net,
+# )
+# Patrick von Platen4:20 PM
+# WuerstchenPipeline(
+#     vae=VQGan()
+#     text_encoder=ClipTextEncoder(),
+#     unet = NewUNet(),  # Paella Style
+#     prior=NewPrior(),  # find good name
+#     tokenizer=CLIPTokenizer,
+#     (image_encoder)=efficient_net,
+# )
