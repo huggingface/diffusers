6bd46da964def53cdea4595fec5fe1a4a529d2db
yiyixuxu <yixu310@gmail,com>
2023-06-26 03:37:39 +0000

    Revert "move the rescale prompt_embeds from prior_transformer to pipeline"
    This reverts commit 5b5a8e6be918fefd114a2945ed89d8e8fa8be21b.

--- src/diffusers/pipelines/shap_e/pipeline_shap_e.py
+++ src/diffusers/pipelines/shap_e/pipeline_shap_e.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import math
 from dataclasses import dataclass
 from typing import List, Optional, Union
 
@@ -243,9 +242,6 @@ class ShapEPipeline(DiffusionPipeline):
             # Here we concatenate the unconditional and text embeddings into a single batch
             # to avoid doing two forward passes
             prompt_embeds = torch.cat([negative_prompt_embeds, prompt_embeds])
-        
-        # Rescale the features to have unit variance (this step is taken from the original repo)
-        prompt_embeds = math.sqrt(prompt_embeds.shape[1]) * prompt_embeds
 
         return prompt_embeds
 
