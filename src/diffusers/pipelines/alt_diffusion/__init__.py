d5577387d9b555461a068ce3897738cc98d36d52
Steven Liu <steven.liu@huggingface.co>
2023-06-29 13:45:42 -0700

    altdiffusion

--- src/diffusers/pipelines/alt_diffusion/__init__.py
+++ src/diffusers/pipelines/alt_diffusion/__init__.py
@@ -12,15 +12,15 @@ from ...utils import BaseOutput, is_torch_available, is_transformers_available
 # Copied from diffusers.pipelines.stable_diffusion.__init__.StableDiffusionPipelineOutput with Stable->Alt
 class AltDiffusionPipelineOutput(BaseOutput):
     """
-    Output class for Alt Diffusion pipelines.
+    Output class for [`AltDiffusionPipeline`].
 
     Args:
         images (`List[PIL.Image.Image]` or `np.ndarray`)
-            List of denoised PIL images of length `batch_size` or numpy array of shape `(batch_size, height, width,
-            num_channels)`. PIL images or numpy array present the denoised images of the diffusion pipeline.
+            List of denoised PIL images of length batch_size or NumPy array of shape (batch_size, height, width,
+            num_channels).
         nsfw_content_detected (`List[bool]`)
-            List of flags denoting whether the corresponding generated image likely represents "not-safe-for-work"
-            (nsfw) content, or `None` if safety checking could not be performed.
+            List indicating whether the corresponding generated image contains "not-safe-for-work" (nsfw) content, or
+            `None` if safety checking could not be performed.
     """
 
     images: Union[List[PIL.Image.Image], np.ndarray]
