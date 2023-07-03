fcb43b7443e594cffd013946d73cc0757f6b317f
Patrick von Platen <patrick.v.platen@gmail.com>
2023-07-03 14:25:45 +0200

    Fix more

--- tests/pipelines/test_pipelines_common.py
+++ tests/pipelines/test_pipelines_common.py
@@ -181,12 +181,22 @@ class PipelineKarrasSchedulerTesterMixin:
         inputs = self.get_dummy_inputs(torch_device)
         inputs["num_inference_steps"] = 2
 
+        if "strength" in inputs:
+            inputs["num_inference_steps"] = 4
+            inputs["strength"] = 0.5
+
         outputs = []
         for scheduler_enum in KarrasDiffusionSchedulers:
+            if "KDPM2" in scheduler_enum.name:
+                inputs["num_inference_steps"] = 5
+
             scheduler_cls = getattr(diffusers, scheduler_enum.name)
             pipe.scheduler = scheduler_cls.from_config(pipe.scheduler.config)
             output = pipe(**inputs)[0]
             outputs.append(output)
+
+            if "KDPM2" in scheduler_enum.name:
+                inputs["num_inference_steps"] = 2
 
         assert check_same_shape(outputs)
 
