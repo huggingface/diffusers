552ce7baeb7d8f5d64426980194d789b452fdea1
yiyixuxu <yixu310@gmail,com>
2023-07-06 02:52:23 +0000

    fix the #copied from

--- src/diffusers/pipelines/kandinsky2_2/pipeline_kandinsky2_2_decoder_inpainting.py
+++ src/diffusers/pipelines/kandinsky2_2/pipeline_kandinsky2_2_decoder_inpainting.py
@@ -81,7 +81,7 @@ EXAMPLE_DOC_STRING = """
 """
 
 
-# Copied from diffusers.pipelines.kandinsky2_2.pipelines_kandinsky_2_2_decoder.KandinskyV22Pipeline.downscale_height_and_width
+# Copied from diffusers.pipelines.kandinsky2_2.pipeline_kandinsky2_2_decoder.downscale_height_and_width
 def downscale_height_and_width(height, width, scale_factor=8):
     new_height = height // scale_factor**2
     if height % scale_factor**2 != 0:
