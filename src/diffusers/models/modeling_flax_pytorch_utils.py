bc267e4b2b04f5ad7223b54109fe88fde25d6232
Martin Müller <martin.muller.me@gmail.com>
2023-07-13 20:34:36 +0000

    add shape asserts when renaming attention layers

--- src/diffusers/models/modeling_flax_pytorch_utils.py
+++ src/diffusers/models/modeling_flax_pytorch_utils.py
@@ -49,9 +49,11 @@ def rename_key_and_reshape_tensor(pt_tuple_key, pt_tensor, random_flax_state_dic
     if len(pt_tuple_key) > 1:
         for rename_from, rename_to in (('to_out_0', 'proj_attn'), ('to_k', 'key'), ('to_v', 'value'), ('to_q', 'query')):
             if pt_tuple_key[-2] == rename_from:
-                weight_name = 'kernel' if pt_tuple_key[-1] == 'weight' else 'bias'
+                weight_name = pt_tuple_key[-1]
+                weight_name = 'kernel' if weight_name == 'weight' else weight_name
                 renamed_pt_tuple_key = pt_tuple_key[:-2] + (rename_to, weight_name)
                 if renamed_pt_tuple_key in random_flax_state_dict:
+                    assert random_flax_state_dict[renamed_pt_tuple_key].shape == pt_tensor.shape
                     return renamed_pt_tuple_key, pt_tensor
 
     if (
