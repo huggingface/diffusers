10600a3a33ad115035f4fcd2bb8a3217127df67f
Patrick von Platen <patrick.v.platen@gmail.com>
2023-07-15 06:53:58 +0200

    create general cpu offload & execution device

--- src/diffusers/pipelines/pipeline_utils.py
+++ src/diffusers/pipelines/pipeline_utils.py
@@ -1086,6 +1086,57 @@ class DiffusionPipeline(ConfigMixin):
     def name_or_path(self) -> str:
         return getattr(self.config, "_name_or_path", None)
 
+    @property
+    def _execution_device(self):
+        r"""
+        Returns the device on which the pipeline's models will be executed. After calling
+        `pipeline.enable_sequential_cpu_offload()` the execution device can only be inferred from Accelerate's module
+        hooks.
+        """
+        for model in self.components.values():
+            if not isinstance(model, torch.nn.Module):
+                continue
+
+            if not hasattr(model, "_hf_hook"):
+                return self.device
+            for module in model.modules():
+                if (
+                    hasattr(module, "_hf_hook")
+                    and hasattr(module._hf_hook, "execution_device")
+                    and module._hf_hook.execution_device is not None
+                ):
+                    return torch.device(module._hf_hook.execution_device)
+        return self.device
+
+    def enable_sequential_cpu_offload(self, gpu_id: int = 0, device: Union[torch.device, str] = "cuda"):
+        r"""
+        Offloads all models to CPU using accelerate, significantly reducing memory usage. When called, unet,
+        text_encoder, vae and safety checker have their state dicts saved to CPU and then are moved to a
+        `torch.device('meta') and loaded to GPU only when their specific submodule has its `forward` method called.
+        Note that offloading happens on a submodule basis. Memory savings are higher than with
+        `enable_model_cpu_offload`, but performance is lower.
+        """
+        if is_accelerate_available() and is_accelerate_version(">=", "0.14.0"):
+            from accelerate import cpu_offload
+        else:
+            raise ImportError("`enable_sequential_cpu_offload` requires `accelerate v0.14.0` or higher")
+
+        if device == "cuda":
+            device = torch.device(f"{device}:{gpu_id}")
+
+        if self.device.type != "cpu":
+            self.to("cpu", silence_dtype_warnings=True)
+            torch.cuda.empty_cache()  # otherwise we don't see the memory savings (but they probably exist)
+
+        for model in self.components.values():
+            if not isinstance(model, torch.nn.Module):
+                continue
+
+            # make sure to offload buffers if not all high level weights
+            # are of type nn.Module
+            offload_buffers = len(model._parameters) > 0 
+            cpu_offload(cpu_offloaded_model, device, offload_buffers=offload_buffers)
+
     @classmethod
     def download(cls, pretrained_model_name, **kwargs) -> Union[str, os.PathLike]:
         r"""
