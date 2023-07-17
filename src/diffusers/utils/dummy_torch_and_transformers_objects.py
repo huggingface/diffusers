9974663256dcdb9e67977ade1bdc3e7bfb52b4b5
isamu-isozaki <isamu.website@gmail.com>
2023-07-16 22:38:45 -0400

    Fixing

--- src/diffusers/utils/dummy_torch_and_transformers_objects.py
+++ src/diffusers/utils/dummy_torch_and_transformers_objects.py
@@ -615,3 +615,17 @@ class VQDiffusionPipeline(metaclass=DummyObject):
     @classmethod
     def from_pretrained(cls, *args, **kwargs):
         requires_backends(cls, ["torch", "transformers"])
+
+class RDMPipeline(metaclass=DummyObject):
+    _backends = ["torch", "transformers"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch", "transformers"])
+
+    @classmethod
+    def from_config(cls, *args, **kwargs):
+        requires_backends(cls, ["torch", "transformers"])
+
+    @classmethod
+    def from_pretrained(cls, *args, **kwargs):
+        requires_backends(cls, ["torch", "transformers"])
