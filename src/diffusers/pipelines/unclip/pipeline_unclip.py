ee8f16b327ed5027610a628f180566df800f39ce
Patrick von Platen <patrick.v.platen@gmail.com>
2023-07-18 15:49:49 +0000

    Just to show

--- src/diffusers/pipelines/unclip/pipeline_unclip.py
+++ src/diffusers/pipelines/unclip/pipeline_unclip.py
@@ -79,28 +79,28 @@ class UnCLIPPipeline(DiffusionPipeline):
 
     def __init__(
         self,
-        prior: PriorTransformer,
+        # prior: PriorTransformer,
         decoder: UNet2DConditionModel,
         text_encoder: CLIPTextModelWithProjection,
         tokenizer: CLIPTokenizer,
         text_proj: UnCLIPTextProjModel,
         super_res_first: UNet2DModel,
         super_res_last: UNet2DModel,
-        prior_scheduler: UnCLIPScheduler,
+        # prior_scheduler: UnCLIPScheduler,
         decoder_scheduler: UnCLIPScheduler,
         super_res_scheduler: UnCLIPScheduler,
     ):
         super().__init__()
 
         self.register_modules(
-            prior=prior,
+            # prior=prior,
             decoder=decoder,
             text_encoder=text_encoder,
             tokenizer=tokenizer,
             text_proj=text_proj,
             super_res_first=super_res_first,
             super_res_last=super_res_last,
-            prior_scheduler=prior_scheduler,
+            # prior_scheduler=prior_scheduler,
             decoder_scheduler=decoder_scheduler,
             super_res_scheduler=super_res_scheduler,
         )
