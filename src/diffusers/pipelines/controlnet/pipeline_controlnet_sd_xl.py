fdd3488f6265f58b35b5e1e8f504999209f7e326
Hu Ye <xiaohuzc@gmail.com>
2023-07-21 19:33:07 +0800

    Update pipeline_controlnet_sd_xl.py

--- src/diffusers/pipelines/controlnet/pipeline_controlnet_sd_xl.py
+++ src/diffusers/pipelines/controlnet/pipeline_controlnet_sd_xl.py
@@ -362,7 +362,7 @@ class StableDiffusionXLControlNetPipeline(DiffusionPipeline, TextualInversionLoa
             negative_prompt_embeds = negative_prompt_embeds.to(dtype=self.text_encoder.dtype, device=device)
             negative_prompt_embeds = negative_prompt_embeds.repeat(1, num_images_per_prompt, 1)
             negative_prompt_embeds = negative_prompt_embeds.view(batch_size * num_images_per_prompt, seq_len, -1)
-        bs_embed = pooled_prompt_embeds.shape[0]
+
         pooled_prompt_embeds = pooled_prompt_embeds.repeat(1, num_images_per_prompt).view(
             bs_embed * num_images_per_prompt, -1
         )
