5282e3cbfcb9d63e5e9099701621cfafd309534a
Neil Wang <39628660+isNeil@users.noreply.github.com>
2023-07-25 19:49:30 +0100

    type conversion
    Default value of `control_guidance_start` and `control_guidance_end` in `StableDiffusionControlNetPipeline.check_inputs` causes `TypeError: object of type 'float' has no len()`
    
    Proposed fix: 
    Convert `control_guidance_start` and `control_guidance_end` to list if float

--- src/diffusers/pipelines/controlnet/pipeline_controlnet.py
+++ src/diffusers/pipelines/controlnet/pipeline_controlnet.py
@@ -554,6 +554,12 @@ class StableDiffusionControlNetPipeline(
         else:
             assert False
 
+        if isinstance(control_guidance_start, float):
+            control_guidance_start = [control_guidance_start]
+            
+        if isinstance(control_guidance_end, float):
+            control_guidance_end = [control_guidance_start]
+            
         if len(control_guidance_start) != len(control_guidance_end):
             raise ValueError(
                 f"`control_guidance_start` has {len(control_guidance_start)} elements, but `control_guidance_end` has {len(control_guidance_end)} elements. Make sure to provide the same number of elements to each list."
