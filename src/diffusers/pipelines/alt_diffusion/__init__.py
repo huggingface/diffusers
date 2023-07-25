b37dc3b3cdda401ee370a5e2a8d2e27a016078ac
Patrick von Platen <patrick.v.platen@gmail.com>
2023-07-26 01:46:05 +0200

    Fix all missing optional import statements from pipeline folders (#4272)
    * fix circular import
    
    * fix imports when watermark not specified
    
    * fix all pipelines

--- src/diffusers/pipelines/alt_diffusion/__init__.py
+++ src/diffusers/pipelines/alt_diffusion/__init__.py
@@ -5,7 +5,7 @@ import numpy as np
 import PIL
 from PIL import Image
 
-from ...utils import BaseOutput, is_torch_available, is_transformers_available
+from ...utils import BaseOutput, OptionalDependencyNotAvailable, is_torch_available, is_transformers_available
 
 
 @dataclass
@@ -27,7 +27,12 @@ class AltDiffusionPipelineOutput(BaseOutput):
     nsfw_content_detected: Optional[List[bool]]
 
 
-if is_transformers_available() and is_torch_available():
+try:
+    if not (is_transformers_available() and is_torch_available()):
+        raise OptionalDependencyNotAvailable()
+except OptionalDependencyNotAvailable:
+    from ...utils.dummy_torch_and_transformers_objects import ShapEPipeline
+else:
     from .modeling_roberta_series import RobertaSeriesModelWithTransformation
     from .pipeline_alt_diffusion import AltDiffusionPipeline
     from .pipeline_alt_diffusion_img2img import AltDiffusionImg2ImgPipeline
--- src/diffusers/pipelines/semantic_stable_diffusion/__init__.py
+++ src/diffusers/pipelines/semantic_stable_diffusion/__init__.py
@@ -6,7 +6,7 @@ import numpy as np
 import PIL
 from PIL import Image
 
-from ...utils import BaseOutput, is_torch_available, is_transformers_available
+from ...utils import BaseOutput, OptionalDependencyNotAvailable, is_torch_available, is_transformers_available
 
 
 @dataclass
@@ -27,5 +27,10 @@ class SemanticStableDiffusionPipelineOutput(BaseOutput):
     nsfw_content_detected: Optional[List[bool]]
 
 
-if is_transformers_available() and is_torch_available():
+try:
+    if not (is_transformers_available() and is_torch_available()):
+        raise OptionalDependencyNotAvailable()
+except OptionalDependencyNotAvailable:
+    from ...utils.dummy_torch_and_transformers_objects import *  # noqa F403
+else:
     from .pipeline_semantic_stable_diffusion import SemanticStableDiffusionPipeline
