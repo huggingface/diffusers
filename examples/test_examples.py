fed12376c5c6dda416f7408aec69e242f227c810
Sayak Paul <spsayakpaul@gmail.com>
2023-07-25 05:31:48 +0530

    [ControlNet SDXL training] fixes in the training script (#4223)
    * fix: #4206
    
    * add: sdxl controlnet training smoketest.
    
    * remove unnecessary token inits.
    
    * add: licensing to model card.
    
    * include SDXL licensing in the model card and make public visibility default
    
    * debugging
    
    * debugging
    
    * disable local file download.
    
    * fix: training test.
    
    * fix: ckpt prefix.

--- examples/test_examples.py
+++ examples/test_examples.py
@@ -1296,6 +1296,25 @@ class ExamplesTestsAccelerate(unittest.TestCase):
                 {"checkpoint-8", "checkpoint-10", "checkpoint-12"},
             )
 
+    def test_controlnet_sdxl(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            test_args = f"""
+            examples/controlnet/train_controlnet_sdxl.py
+            --pretrained_model_name_or_path=hf-internal-testing/tiny-stable-diffusion-xl-pipe
+            --dataset_name=hf-internal-testing/fill10
+            --output_dir={tmpdir}
+            --resolution=64
+            --train_batch_size=1
+            --gradient_accumulation_steps=1
+            --controlnet_model_name_or_path=hf-internal-testing/tiny-controlnet-sdxl
+            --max_train_steps=9
+            --checkpointing_steps=2
+            """.split()
+
+            run_command(self._launch_args + test_args)
+
+            self.assertTrue(os.path.isfile(os.path.join(tmpdir, "diffusion_pytorch_model.bin")))
+
     def test_custom_diffusion_checkpointing_checkpoints_total_limit(self):
         with tempfile.TemporaryDirectory() as tmpdir:
             test_args = f"""
