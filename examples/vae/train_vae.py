b5abbc9121ae3b92860c6b0e90e06f330994e362
Pie31415 <andy999coc@gmail.com>
2023-07-26 19:04:43 -0400

    added normalization

--- examples/vae/train_vae.py
+++ examples/vae/train_vae.py
@@ -380,6 +380,7 @@ def main():
             ),
             transforms.CenterCrop(args.resolution),
             transforms.ToTensor(),
+            transforms.Normalize([0.5], [0.5]),
         ]
     )
 
