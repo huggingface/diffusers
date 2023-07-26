0ce05446ef64e2fa4c177d3b6b2034a1c29cbf7f
Patrick von Platen <patrick.v.platen@gmail.com>
2023-07-26 18:34:24 +0000

    save intermediate

--- src/diffusers/pipelines/stable_diffusion_xl/pipeline_stable_diffusion_xl.py
+++ src/diffusers/pipelines/stable_diffusion_xl/pipeline_stable_diffusion_xl.py
@@ -853,7 +853,9 @@ class StableDiffusionXLPipeline(DiffusionPipeline, FromSingleFileMixin, LoraLoad
 
     # Overrride to properly handle the loading and unloading of the additional text encoder.
     def load_lora_weights(self, pretrained_model_name_or_path_or_dict: Union[str, Dict[str, torch.Tensor]], **kwargs):
-        state_dict, network_alphas = self.lora_state_dict(pretrained_model_name_or_path_or_dict, **kwargs)
+        unet_state_keys = self.unet.state_dict().keys()
+
+        state_dict, network_alphas = self.lora_state_dict(pretrained_model_name_or_path_or_dict, unet_config=self.unet.config, unet_state_keys=unet_state_keys, **kwargs)
         self.load_lora_into_unet(state_dict, network_alphas=network_alphas, unet=self.unet)
 
         text_encoder_state_dict = {k: v for k, v in state_dict.items() if "text_encoder." in k}
