2e6fbaab8976a2af0d7189e990679c8c0b105f4b
Sayak Paul <spsayakpaul@gmail.com>
2023-07-27 23:40:42 +0530

    Update tests/others/test_dependencies.py
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- tests/others/test_dependencies.py
+++ tests/others/test_dependencies.py
@@ -44,6 +44,6 @@ class DependencyTester(unittest.TestCase):
 
         all_classes = inspect.getmembers(diffusers, inspect.isclass)
         for cls_name, cls_module in all_classes:
-            if "Pipeline" in cls_name:
+            if hasattr(diffusers.pipelines, cls_name):
                 pipeline_folder_module = ".".join(str(cls_module.__module__).split(".")[:3])
                 _ = import_module(pipeline_folder_module, str(cls_name))
