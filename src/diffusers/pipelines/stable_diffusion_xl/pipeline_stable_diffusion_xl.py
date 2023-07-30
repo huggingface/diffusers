ebcb4c99397c0a92bbeaab2393c46af43c9b1d81
Sayak Paul <spsayakpaul@gmail.com>
2023-07-30 11:24:29 +0530

    temporarily disable text encoder loras.

--- src/diffusers/pipelines/stable_diffusion_xl/pipeline_stable_diffusion_xl.py
+++ src/diffusers/pipelines/stable_diffusion_xl/pipeline_stable_diffusion_xl.py
@@ -876,25 +876,25 @@ class StableDiffusionXLPipeline(DiffusionPipeline, FromSingleFileMixin, LoraLoad
         )
         self.load_lora_into_unet(state_dict, network_alphas=network_alphas, unet=self.unet)
 
-        text_encoder_state_dict = {k: v for k, v in state_dict.items() if "text_encoder." in k}
-        if len(text_encoder_state_dict) > 0:
-            self.load_lora_into_text_encoder(
-                text_encoder_state_dict,
-                network_alphas=network_alphas,
-                text_encoder=self.text_encoder,
-                prefix="text_encoder",
-                lora_scale=self.lora_scale,
-            )
+        # text_encoder_state_dict = {k: v for k, v in state_dict.items() if "text_encoder." in k}
+        # if len(text_encoder_state_dict) > 0:
+        #     self.load_lora_into_text_encoder(
+        #         text_encoder_state_dict,
+        #         network_alphas=network_alphas,
+        #         text_encoder=self.text_encoder,
+        #         prefix="text_encoder",
+        #         lora_scale=self.lora_scale,
+        #     )
 
-        text_encoder_2_state_dict = {k: v for k, v in state_dict.items() if "text_encoder_2." in k}
-        if len(text_encoder_2_state_dict) > 0:
-            self.load_lora_into_text_encoder(
-                text_encoder_2_state_dict,
-                network_alphas=network_alphas,
-                text_encoder=self.text_encoder_2,
-                prefix="text_encoder_2",
-                lora_scale=self.lora_scale,
-            )
+        # text_encoder_2_state_dict = {k: v for k, v in state_dict.items() if "text_encoder_2." in k}
+        # if len(text_encoder_2_state_dict) > 0:
+        #     self.load_lora_into_text_encoder(
+        #         text_encoder_2_state_dict,
+        #         network_alphas=network_alphas,
+        #         text_encoder=self.text_encoder_2,
+        #         prefix="text_encoder_2",
+        #         lora_scale=self.lora_scale,
+        #     )
 
     @classmethod
     def save_lora_weights(
