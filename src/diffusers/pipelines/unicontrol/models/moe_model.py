a470941ea11dbdf16ba852f29c933642c763f9aa
Abhinay1997 <devarintinagasaiabhinay@gmail.com>
2023-07-31 08:08:25 +0530

    Base commit for UniControl

---
+++ src/diffusers/pipelines/unicontrol/models/moe_model.py
@@ -0,0 +1,6 @@
+# File where the Mixture-Of-Experts-Adapter class will reside. 
+from ....models import ModelMixin
+
+class MOEAdapterModel(ModelMixin):
+    def __init__(self) -> None:
+        pass
---
+++ src/diffusers/pipelines/unicontrol/models/ta_hypernet_model.py
@@ -0,0 +1,7 @@
+# File where the Task Aware Hypernet Model class resides. 
+
+from ....models import ModelMixin
+
+class TaskAwareHyperNetModel(ModelMixin):
+    def __init__(self) -> None:
+        pass
