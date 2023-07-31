63fa2a8a833d53e8b0b3d1e5804b16f4640fa3e5
Sayak Paul <spsayakpaul@gmail.com>
2023-07-31 18:11:49 +0530

    pack into dict.

--- examples/instruct_pix2pix/dataloader.py
+++ examples/instruct_pix2pix/dataloader.py
@@ -1,4 +1,5 @@
 import math
+from argparse import Namespace
 
 import numpy as np
 import PIL
@@ -22,47 +23,37 @@ def convert_to_np(image, resolution):
     return np.array(image).transpose(2, 0, 1)
 
 
-def get_dataloader(
-    dataset_path,
-    num_train_examples,
-    per_gpu_batch_size,
-    global_batch_size,
-    num_workers,
-    center_crop,
-    random_flip,
-    resolution=256,
-):
+def get_dataloader(args):
     # num_train_examples: 313,010
-    num_batches = math.ceil(num_train_examples / global_batch_size)
-    num_worker_batches = math.ceil(num_train_examples / (global_batch_size * num_workers))  # per dataloader worker
-    num_batches = num_worker_batches * num_workers
-    num_samples = num_batches * global_batch_size
+    num_batches = math.ceil(args.num_train_examples / args.global_batch_size)
+    num_worker_batches = math.ceil(
+        args.num_train_examples / (args.global_batch_size * args.num_workers)
+    )  # per dataloader worker
+    num_batches = num_worker_batches * args.num_workers
+    num_samples = num_batches * args.global_batch_size
 
     # Preprocessing the datasets.
     train_transforms = transforms.Compose(
         [
-            transforms.CenterCrop(resolution) if center_crop else transforms.RandomCrop(resolution),
-            transforms.RandomHorizontalFlip() if random_flip else transforms.Lambda(lambda x: x),
+            transforms.CenterCrop(args.resolution) if args.center_crop else transforms.RandomCrop(args.resolution),
+            transforms.RandomHorizontalFlip() if args.random_flip else transforms.Lambda(lambda x: x),
         ]
     )
 
     def preprocess_images(sample):
         # We need to ensure that the original and the edited images undergo the same
         # augmentation transforms.
-        images = np.concatenate([sample["original_image"], sample["edited_image"]])
-        images = torch.tensor(images)
-        images = 2 * (images / 255) - 1
+        images = torch.stack(
+            [transforms.ToTensor()(sample["original_image"]), transforms.ToTensor()(sample["edited_image"])]
+        )
         transformed_images = train_transforms(images)
 
-        # Separate the original and edited images.
+        # Separate the original and edited images and the edit prompt.
         original_image, edited_image = transformed_images.chunk(2)
-        original_image = original_image.reshape(3, resolution, resolution)
-        edited_image = edited_image.reshape(-1, 3, resolution, resolution)
-
-        return original_image, edited_image, sample["edit_prompt"]
+        return {"original_image": original_image, "edited_image": edited_image, "edit_prompt": sample["edit_prompt"]}
 
     dataset = (
-        wds.WebDataset(dataset_path, resampled=True, handler=wds.warn_and_continue)
+        wds.WebDataset(args.dataset_path, resampled=True, handler=wds.warn_and_continue)
         .shuffle(690, handler=wds.warn_and_continue)
         .decode("pil", handler=wds.warn_and_continue)
         .rename(
@@ -70,11 +61,14 @@ def get_dataloader(
             original_image="original_image.jpg",
             edit_prompt="edit_prompt.txt",
             edited_image="edited_image.jpg",
-            handler=wds.warn_and_continue
-        ) \
-        .map(filter_keys({"original_image", "edit_prompt", "edited_image"}), handler=wds.warn_and_continue)
+            handler=wds.warn_and_continue,
+        )
+        .map(
+            filter_keys({args.original_image_column, args.edit_prompt_column, args.edited_image_column}),
+            handler=wds.warn_and_continue,
+        )
         .map(preprocess_images, handler=wds.warn_and_continue)
-        .batched(per_gpu_batch_size, partial=False, collation_fn=default_collate)
+        .batched(args.per_gpu_batch_size, partial=False, collation_fn=default_collate)
         .with_epoch(num_worker_batches)
     )
 
@@ -82,7 +76,7 @@ def get_dataloader(
         dataset,
         batch_size=None,
         shuffle=False,
-        num_workers=num_workers,
+        num_workers=args.num_workers,
         pin_memory=True,
         persistent_workers=True,
     )
@@ -95,15 +89,20 @@ def get_dataloader(
 
 
 if __name__ == "__main__":
-    dataloader = get_dataloader(
-        "pipe:aws s3 cp s3://muse-datasets/instructpix2pix-clip-filtered-wds/{000000..000062}.tar",
+    args = Namespace(
+        dataset_path="pipe:aws s3 cp s3://muse-datasets/instructpix2pix-clip-filtered-wds/{000000..000062}.tar",
         num_train_examples=313010,
         per_gpu_batch_size=8,
         global_batch_size=64,
         num_workers=4,
         center_crop=False,
         random_flip=True,
+        resolution=256,
+        original_image_column="original_image",
+        edit_prompt_column="edit_prompt",
+        edited_image_column="edited_image",
     )
+    dataloader = get_dataloader(args)
     for sample in dataloader:
         print(sample.keys())
         print(sample["original_image"].shape)
