e05b7303426f6e8b2411f14b279e516e077b033a
Sayak Paul <spsayakpaul@gmail.com>
2023-08-02 19:36:39 +0530

    fix 2

--- tests/models/test_models_vae.py
+++ tests/models/test_models_vae.py
@@ -270,6 +270,9 @@ class AutoencoderTinyIntegrationTests(unittest.TestCase):
         gc.collect()
         torch.cuda.empty_cache()
 
+    def get_file_format(self, seed, shape):
+        return f"gaussian_noise_s={seed}_shape={'_'.join([str(s) for s in shape])}.npy"
+
     def get_sd_image(self, seed=0, shape=(4, 3, 512, 512), fp16=False):
         dtype = torch.float16 if fp16 else torch.float32
         image = torch.from_numpy(load_hf_numpy(self.get_file_format(seed, shape))).to(torch_device).to(dtype)
