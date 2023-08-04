6aa4114a9b25ee755eddb5646c954e707a86c1ae
yiyixuxu <yixu310@gmail,com>
2023-08-04 21:25:36 +0000

    apply feedback

--- src/diffusers/image_processor.py
+++ src/diffusers/image_processor.py
@@ -155,13 +155,13 @@ class VaeImageProcessor(ConfigMixin):
 
         Args:
             image(`PIL.Image.Image`, `np.ndarray` or `torch.Tensor`):
-                the image input, can be a PIL image, numpy array or pytorch tensor. if it is a numpy array, should have
-                shape [batch, height, width] or [batch, height, width, channel] if it is a pytorch tensor, should have
-                shape [batch, channel, height, width]
+                The image input, can be a PIL image, numpy array or pytorch tensor. if it is a numpy array, should have
+                shape `[batch, height, width]` or `[batch, height, width, channel]` if it is a pytorch tensor, should
+                have shape `[batch, channel, height, width]`.
             height (`int`, *optional*, defaults to `None`):
-                The height in preprocessed image. If `None`, will use the height of `image` input
+                The height in preprocessed image. If `None`, will use the height of `image` input.
             width (`int`, *optional*`, defaults to `None`):
-                The width in preprocessed. If `None`, will use the width of the `image` input
+                The width in preprocessed. If `None`, will use the width of the `image` input.
         """
 
         if height is None:
