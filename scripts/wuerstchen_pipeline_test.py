fix attention mask

scripts/wuerstchen_pipeline_test.py
@@ -61,28 +61,45 @@ batch_size = 4
 # for i, image in enumerate(images):
 #     image.save(os.path.join("samples", caption.replace(" ", "_").replace("|", "") + f"_{i}.png"))
 
+torch.manual_seed(42)
 
-prior_pipeline = WuerstchenPriorPipeline.from_pretrained("C:\\Users\\d6582\\Documents\\ml\\diffusers\\scripts\\warp-diffusion\\WuerstchenPriorPipeline", torch_dtype=dtype)
-generator_pipeline = WuerstchenGeneratorPipeline.from_pretrained("C:\\Users\\d6582\\Documents\\ml\\diffusers\\scripts\\warp-diffusion\\WuerstchenGeneratorPipeline", torch_dtype=dtype)
+prior_pipeline = WuerstchenPriorPipeline.from_pretrained("warp-diffusion/WuerstchenPriorPipeline", torch_dtype=dtype)
+
+# from diffusers import DDPMScheduler
+# noise_scheduler = DDPMScheduler.from_config("runwayml/stable-diffusion-v1-5", subfolder="scheduler")
+# prior_pipeline.scheduler = noise_scheduler
+
+generator_pipeline = WuerstchenGeneratorPipeline.from_pretrained("warp-diffusion/WuerstchenGeneratorPipeline", torch_dtype=dtype)
+
 prior_pipeline = prior_pipeline.to("cuda")
 generator_pipeline = generator_pipeline.to("cuda")
+
+def embed_clip(clip_model, clip_tokenizer, caption, negative_caption="", batch_size=4, device="cuda"):
+    clip_tokens = clip_tokenizer([caption] * batch_size, truncation=True, padding="max_length", max_length=clip_tokenizer.model_max_length, return_tensors="pt").to(device)
+    clip_text_embeddings = clip_model(**clip_tokens).last_hidden_state
+
+    clip_tokens_uncond = clip_tokenizer([negative_caption] * batch_size, truncation=True, padding="max_length", max_length=clip_tokenizer.model_max_length, return_tensors="pt").to(device)
+    clip_text_embeddings_uncond = clip_model(**clip_tokens_uncond).last_hidden_state
+    return clip_text_embeddings, clip_text_embeddings_uncond
+
 # generator_pipeline.vqgan.to(torch.float16)
-text_encoder = CLIPTextModel.from_pretrained("laion/CLIP-ViT-H-14-laion2B-s32B-b79K").to("cpu")
-tokenizer = AutoTokenizer.from_pretrained("laion/CLIP-ViT-H-14-laion2B-s32B-b79K")
+# clip_model_G = CLIPTextModel.from_pretrained("laion/CLIP-ViT-bigG-14-laion2B-39B-b160k").to("cpu")
+# clip_tokenizer_G = AutoTokenizer.from_pretrained("laion/CLIP-ViT-bigG-14-laion2B-39B-b160k")
+clip_model_H = CLIPTextModel.from_pretrained("laion/CLIP-ViT-H-14-laion2B-s32B-b79K").to("cpu")
+clip_tokenizer_H = AutoTokenizer.from_pretrained("laion/CLIP-ViT-H-14-laion2B-s32B-b79K")
 
 # negative_prompt = "low resolution, low detail, bad quality, blurry"
 negative_prompt = "bad anatomy, blurry, fuzzy, extra arms, extra fingers, poorly drawn hands, disfigured, tiling, deformed, mutated, drawing, helmet"
 # negative_prompt = ""
 # caption = "Bee flying out of a glass jar in a green and red leafy basket, glass and lens flare, diffuse lighting elegant"
 # caption = "princess | centered| key visual| intricate| highly detailed| breathtaking beauty| precise lineart| vibrant| comprehensive cinematic| Carne Griffiths| Conrad Roset"
-caption = "An armchair in the shape of an avocado"
-clip_tokens = tokenizer([caption] * batch_size, truncation=True, padding="max_length", max_length=tokenizer.model_max_length, return_tensors="pt")
-clip_text_embeddings = text_encoder(**clip_tokens).last_hidden_state.to(dtype).to(device)
-clip_tokens_uncond = tokenizer([negative_prompt] * batch_size, truncation=True, padding="max_length", max_length=tokenizer.model_max_length, return_tensors="pt")
-clip_text_embeddings_uncond = text_encoder(**clip_tokens_uncond).last_hidden_state.to(dtype).to(device)
+caption = "An astronaut riding a horse"
+# clip_text_embeddings, clip_text_embeddings_uncond = embed_clip(clip_model_G, clip_tokenizer_G, caption, negative_prompt, batch_size, "cpu")
+# embeds = torch.cat([clip_text_embeddings, clip_text_embeddings_uncond]).to(device).to(dtype)
 
 prior_output = prior_pipeline(caption, guidance_scale=8.0, num_images_per_prompt=batch_size, negative_prompt=negative_prompt)
-generator_output = generator_pipeline(prior_output.image_embeds, clip_text_embeddings, guidance_scale=0.0, output_type="np").images
+clip_text_embeddings, clip_text_embeddings_uncond = embed_clip(clip_model_H, clip_tokenizer_H, caption, negative_prompt, batch_size, "cpu")
+generator_output = generator_pipeline(prior_output.image_embeds, clip_text_embeddings.to(device).to(dtype), guidance_scale=0.0, output_type="np").images
 images = numpy_to_pil(generator_output)
 os.makedirs("samples", exist_ok=True)
 for i, image in enumerate(images):

src/diffusers/pipelines/wuerstchen/pipeline_wuerstchen_prior.py
@@ -129,20 +129,20 @@ class WuerstchenPriorPipeline(DiffusionPipeline):
             max_length=self.tokenizer.model_max_length,
             truncation=True,
             return_tensors="pt",
-        )
-        text_input_ids = text_inputs.input_ids
+        ).to(device)
+        # text_input_ids = text_inputs.input_ids
 
-        untruncated_ids = self.tokenizer(prompt, padding="longest", return_tensors="pt").input_ids
+        # untruncated_ids = self.tokenizer(prompt, padding="longest", return_tensors="pt").input_ids
 
-        if untruncated_ids.shape[-1] >= text_input_ids.shape[-1] and not torch.equal(text_input_ids, untruncated_ids):
-            removed_text = self.tokenizer.batch_decode(untruncated_ids[:, self.tokenizer.model_max_length - 1 : -1])
-            logger.warning(
-                "The following part of your input was truncated because CLIP can only handle sequences up to"
-                f" {self.tokenizer.model_max_length} tokens: {removed_text}"
-            )
-            text_input_ids = text_input_ids[:, : self.tokenizer.model_max_length]
+        # if untruncated_ids.shape[-1] >= text_input_ids.shape[-1] and not torch.equal(text_input_ids, untruncated_ids):
+        #     removed_text = self.tokenizer.batch_decode(untruncated_ids[:, self.tokenizer.model_max_length - 1 : -1])
+        #     logger.warning(
+        #         "The following part of your input was truncated because CLIP can only handle sequences up to"
+        #         f" {self.tokenizer.model_max_length} tokens: {removed_text}"
+        #     )
+        #     text_input_ids = text_input_ids[:, : self.tokenizer.model_max_length]
 
-        text_encoder_output = self.text_encoder(text_input_ids.to(device))
+        text_encoder_output = self.text_encoder(**text_inputs)
 
         text_encoder_hidden_states = text_encoder_output.last_hidden_state
 
@@ -174,8 +174,8 @@ class WuerstchenPriorPipeline(DiffusionPipeline):
                 max_length=self.tokenizer.model_max_length,
                 truncation=True,
                 return_tensors="pt",
-            )
-            negative_prompt_embeds_text_encoder_output = self.text_encoder(uncond_input.input_ids.to(device))
+            ).to(device)
+            negative_prompt_embeds_text_encoder_output = self.text_encoder(**uncond_input)
 
             uncond_text_encoder_hidden_states = negative_prompt_embeds_text_encoder_output.last_hidden_state
 
@@ -195,46 +195,6 @@ class WuerstchenPriorPipeline(DiffusionPipeline):
 
         return text_encoder_hidden_states
 
-    # @torch.no_grad()
-    # def inference_loop(
-    #     self, latents, steps, text_encoder_hidden_states, do_classifier_free_guidance, guidance_scale, generator
-    # ):
-    #     print(steps)
-    #     print(steps[:-1])
-    #     for i, t in enumerate(self.progress_bar(steps[:-1])):
-    #         # print(torch.cat([latents] * 2).shape, latents.dtype)
-    #         # print(t.expand(latents.size(0) * 2).shape, t.dtype)
-    #         # print(text_encoder_hidden_states.shape, text_encoder_hidden_states.dtype)
-    #         predicted_image_embedding = self.prior(
-    #             torch.cat([latents] * 2) if do_classifier_free_guidance else latents,
-    #             r=t.expand(latents.size(0) * 2) if do_classifier_free_guidance else t,
-    #             c=text_encoder_hidden_states,
-    #         )
-
-    #         # print(t.expand(latents.size(0) * 2))
-    #         # print(i, predicted_image_embedding[0, 0, :4, :4])
-    #         # print(text_encoder_hidden_states[0, 4, :4])
-
-    #         if do_classifier_free_guidance:
-    #             predicted_image_embedding_text, predicted_image_embedding_uncond = predicted_image_embedding.chunk(2)
-    #             predicted_image_embedding = predicted_image_embedding_uncond + guidance_scale * (
-    #                 predicted_image_embedding_text - predicted_image_embedding_uncond
-    #             )
-    #         # print(t)
-
-    #         # latents = self.diffuzz.undiffuse(latents, t[None], steps[i + 1][None], predicted_image_embedding).to(
-    #         #     dtype=t.dtype
-    #         # )
-    #         timestep = (t * 999).cpu().int()
-    #         # print(timestep)
-    #         latents = self.scheduler.step(
-    #             predicted_image_embedding,
-    #             timestep=timestep - 1,
-    #             sample=latents,
-    #             generator=generator,
-    #         ).prev_sample
-
-    #     return latents
 
     @torch.no_grad()
     def __call__(
@@ -274,6 +234,7 @@ class WuerstchenPriorPipeline(DiffusionPipeline):
         text_encoder_hidden_states = self._encode_prompt(
             prompt, device, num_images_per_prompt, do_classifier_free_guidance, negative_prompt
         )
+        
         dtype = text_encoder_hidden_states.dtype
         latent_height = 128 * (height // 128) // (1024 // 24)
         latent_width = 128 * (width // 128) // (1024 // 24)