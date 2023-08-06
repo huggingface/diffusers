71c3710f64776b134f938e7484c66f2aedef9833
yiyixuxu <yixu310@gmail,com>
2023-08-06 02:43:51 +0000

    make copy

--- src/diffusers/schedulers/scheduling_dpmsolver_sde.py
+++ src/diffusers/schedulers/scheduling_dpmsolver_sde.py
@@ -374,7 +374,25 @@ class DPMSolverSDEScheduler(SchedulerMixin, ConfigMixin):
     @property
     def state_in_first_order(self):
         return self.sample is None
+    
+    # Copied from diffusers.schedulers.scheduling_heun_discrete.HeunDiscreteScheduler._init_step_index
+    def _init_step_index(self, timestep):
+        if isinstance(timestep, torch.Tensor):
+            timestep = timestep.to(self.timesteps.device)
 
+        index_candidates = (self.timesteps == timestep).nonzero()
+
+        # The sigma index that is taken for the **very** first `step`
+        # is always the second index (or the last index if there is only 1)
+        # This way we can ensure we don't accidentally skip a sigma in
+        # case we start in the middle of the denoising schedule (e.g. for image-to-image)
+        if len(index_candidates) > 1:
+            step_index = index_candidates[1]
+        else:
+            step_index = index_candidates[0]
+
+        return step_index.item()
+    
     def step(
         self,
         model_output: Union[torch.FloatTensor, np.ndarray],
@@ -488,15 +506,9 @@ class DPMSolverSDEScheduler(SchedulerMixin, ConfigMixin):
     ) -> torch.FloatTensor:
         # Make sure sigmas and timesteps have the same device and dtype as original_samples
         sigmas = self.sigmas.to(device=original_samples.device, dtype=original_samples.dtype)
-        if original_samples.device.type == "mps" and torch.is_floating_point(timesteps):
-            # mps does not support float64
-            schedule_timesteps = self.timesteps.to(original_samples.device, dtype=torch.float32)
-            timesteps = timesteps.to(original_samples.device, dtype=torch.float32)
-        else:
-            schedule_timesteps = self.timesteps.to(original_samples.device)
-            timesteps = timesteps.to(original_samples.device)
 
-        step_indices = [self.index_for_timestep(t, schedule_timesteps) for t in timesteps]
+        self.timesteps.to(original_samples.device)
+        step_indices = [self._init_step_index(t) for t in timesteps]
 
         sigma = sigmas[step_indices].flatten()
         while len(sigma.shape) < len(original_samples.shape):
