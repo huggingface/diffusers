fix resolution calculation for prior

scripts/wuerstchen_pipeline_test.py
@@ -42,7 +42,7 @@ def numpy_to_pil(images: np.ndarray) -> list[Image.Image]:
 # )
 device = "cuda"
 dtype = torch.float16
-batch_size = 2
+batch_size = 1
 
 # generator_pipeline = WuerstchenGeneratorPipeline.from_pretrained("C:\\Users\\d6582\\Documents\\ml\\diffusers\\scripts\\warp-diffusion\\WuerstchenGeneratorPipeline", torch_dtype=dtype)
 # generator_pipeline = generator_pipeline.to("cuda")
@@ -76,64 +76,55 @@ generator_pipeline = WuerstchenGeneratorPipeline.from_pretrained(
 )
 prior_pipeline = prior_pipeline.to("cuda")
 generator_pipeline = generator_pipeline.to("cuda")
-# generator_pipeline.vqgan.to(torch.float16)
-# text_encoder = CLIPTextModel.from_pretrained("laion/CLIP-ViT-H-14-laion2B-s32B-b79K").to("cpu")
-# tokenizer = AutoTokenizer.from_pretrained("laion/CLIP-ViT-H-14-laion2B-s32B-b79K")
-
-# negative_prompt = "low resolution, low detail, bad quality, blurry"
-negative_prompt = "bad anatomy, blurry, fuzzy, extra arms, extra fingers, poorly drawn hands, disfigured, tiling, deformed, mutated, drawing, helmet"
-# negative_prompt = ""
+negative_prompt = "bad anatomy, blurry, fuzzy, extra arms, extra fingers, poorly drawn hands, disfigured, tiling, deformed, mutated"
 caption = (
-    "Bee flying out of a glass jar in a green and red leafy basket, glass and lens flare, diffuse lighting elegant"
+    "A captivating artwork of a mysterious stone golem"
 )
 # caption = "princess | centered| key visual| intricate| highly detailed| breathtaking beauty| precise lineart| vibrant| comprehensive cinematic| Carne Griffiths| Conrad Roset"
-caption = "An armchair in the shape of an avocado"
-# clip_tokens = tokenizer(
-#     [caption] * batch_size,
-#     truncation=True,
-#     padding="max_length",
-#     max_length=tokenizer.model_max_length,
-#     return_tensors="pt",
+
+# prior_output = prior_pipeline(
+#     caption,
+#     height=1024,
+#     width=1024,
+#     guidance_scale=8.0,
+#     num_images_per_prompt=batch_size,
+#     negative_prompt=negative_prompt,
 # )
-# clip_text_embeddings = text_encoder(**clip_tokens).last_hidden_state.to(dtype).to(device)
-# clip_tokens_uncond = tokenizer(
-#     [negative_prompt] * batch_size,
-#     truncation=True,
-#     padding="max_length",
-#     max_length=tokenizer.model_max_length,
-#     return_tensors="pt",
-# )
-# clip_text_embeddings_uncond = text_encoder(**clip_tokens_uncond).last_hidden_state.to(dtype).to(device)
-
-prior_output = prior_pipeline(
-    caption,
-    height=1024,
-    width=1536,
-    guidance_scale=8.0,
-    num_images_per_prompt=batch_size,
-    negative_prompt=negative_prompt,
-)
-generator_output = generator_pipeline(
-    predicted_image_embeddings=prior_output.image_embeds,
-    prompt=caption,
-    negative_prompt=negative_prompt,
-    guidance_scale=0.0,
-    output_type="np",
-).images
-images = numpy_to_pil(generator_output)
-os.makedirs("samples", exist_ok=True)
-for i, image in enumerate(images):
-    image.save(os.path.join("samples", caption.replace(" ", "_").replace("|", "") + f"_{i}.png"))
+# generator_output = generator_pipeline(
+#     predicted_image_embeddings=prior_output.image_embeds,
+#     prompt=caption,
+#     negative_prompt=negative_prompt,
+#     guidance_scale=0.0,
+#     output_type="np",
+# ).images
+# images = numpy_to_pil(generator_output)
+# os.makedirs("samples", exist_ok=True)
+# for i, image in enumerate(images):
+#     image.save(os.path.join("samples", caption.replace(" ", "_").replace("|", "") + f"_{i}.png"))
 
 
-# caption = input("Prompt please: ")
-# while caption != "q":
-#     prior_output = prior_pipeline(caption, num_images_per_prompt=4, negative_prompt=negative_prompt)
-#     generator_output = generator_pipeline(prior_output.image_embeds, prior_output.text_embeds, output_type="np").images
-#     images = numpy_to_pil(generator_output)
+caption = input("Prompt please: ")
+while caption != "q":
+    prior_output = prior_pipeline(
+        caption,
+        height=1024,
+        width=4096,
+        guidance_scale=8.0,
+        num_images_per_prompt=batch_size,
+        negative_prompt=negative_prompt,
+    )
+    generator_output = generator_pipeline(
+        predicted_image_embeddings=prior_output.image_embeds,
+        prompt=caption,
+        negative_prompt=negative_prompt,
+        guidance_scale=0.0,
+        output_type="np",
+    ).images
+    print("Done Sampling")
+    images = numpy_to_pil(generator_output)
 
-#     os.makedirs("samples", exist_ok=True)
-#     for i, image in enumerate(images):
-#         image.save(os.path.join("samples", caption.replace(" ", "_").replace("|", "") + f"_{i}.png"))
+    os.makedirs("samples", exist_ok=True)
+    for i, image in enumerate(images):
+        image.save(os.path.join("samples", caption.replace(" ", "_").replace("|", "") + f"_{i}.png"))
 
-#     caption = input("Prompt please: ")
+    caption = input("Prompt please: ")