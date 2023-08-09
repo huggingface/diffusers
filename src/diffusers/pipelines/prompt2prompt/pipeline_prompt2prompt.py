0bf2cce03e1302bbc4ad342b124432254f93e9ee
UmerHA <40663591+UmerHA@users.noreply.github.com>
2023-08-09 21:38:32 +0200

    Removed unused function

--- src/diffusers/pipelines/prompt2prompt/pipeline_prompt2prompt.py
+++ src/diffusers/pipelines/prompt2prompt/pipeline_prompt2prompt.py
@@ -247,20 +247,6 @@ class Prompt2PromptPipeline(StableDiffusionPipeline):
         controller.num_att_layers = cross_att_count
 
 
-    def aggregate_attention(self, prompts, attention_store: AttentionStore, res: int, from_where: List[str], is_cross: bool, select: int):
-        out = []
-        attention_maps = attention_store.get_average_attention()
-        num_pixels = res ** 2
-        for location in from_where:
-            for item in attention_maps[f"{location}_{'cross' if is_cross else 'self'}"]:
-                if item.shape[1] == num_pixels:
-                    cross_maps = item.reshape(len(prompts), -1, res, res, item.shape[-1])[select]
-                    out.append(cross_maps)
-        out = torch.cat(out, dim=0)
-        out = out.sum(0) / out.shape[0]
-        return out.cpu()
-
-
 class P2PCrossAttnProcessor:
 
     def __init__(self, controller, place_in_unet):
