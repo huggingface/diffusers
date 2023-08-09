ba01e99b917140fa022d028c8967151beb428a63
VV-_-VV <wangyanbops@163.com>
2023-08-09 15:03:45 +0100

    fix some typo error

--- scripts/convert_original_controlnet_to_diffusers.py
+++ scripts/convert_original_controlnet_to_diffusers.py
@@ -2,7 +2,7 @@
 # Copyright 2023 The HuggingFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
+# You may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" Conversion script for stable diffusion checkpoints which _only_ contain a contrlnet. """
+""" Conversion script for stable diffusion checkpoints which _only_ contain a controlnet. """
 
 import argparse
 
