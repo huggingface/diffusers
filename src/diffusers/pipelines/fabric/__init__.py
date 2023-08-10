adebe8f25c931c05c162b45ff185c9cded4f3a86
shauray8 <shauray9@gmail.com>
2023-08-11 01:52:06 +0530

    almost done testing

--- src/diffusers/pipelines/fabric/__init__.py
+++ src/diffusers/pipelines/fabric/__init__.py
@@ -1,7 +1,29 @@
+from dataclasses import dataclass
 from ...utils import (
+    BaseOutput,
     OptionalDependencyNotAvailable,
     is_torch_available,
 )
+from typing import Union, Optional, List
+import numpy as np
+import PIL
+
+@dataclass
+class FabricPipelineOutput(BaseOutput):
+    """
+    Output class for Stable Diffusion pipelines.
+
+    Args:
+        images (`List[PIL.Image.Image]` or `np.ndarray`)
+            List of denoised PIL images of length `batch_size` or numpy array of shape `(batch_size, height, width,
+            num_channels)`. PIL images or numpy array present the denoised images of the diffusion pipeline.
+        nsfw_content_detected (`Optional[List[bool]]`)
+            List of flags denoting whether the corresponding generated image likely represents "not-safe-for-work"
+            (nsfw) content, or `None` if safety checking could not be performed.
+    """
+
+    images: Union[List[PIL.Image.Image], np.ndarray]
+    nsfw_content_detected: Optional[List[bool]]
 
 try:
     if not is_torch_available():
