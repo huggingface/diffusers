76dbce570fdc04fd6fe97f90e7957d85bc546b78
yiyixuxu <yixu310@gmail,com>
2023-08-10 02:25:09 +0000

    style

--- tests/schedulers/test_scheduler_ipndm.py
+++ tests/schedulers/test_scheduler_ipndm.py
@@ -103,7 +103,7 @@ class IPNDMSchedulerTest(SchedulerCommonTest):
         for i, t in enumerate(scheduler.timesteps):
             residual = model(sample, t)
             sample = scheduler.step(residual, t, sample).prev_sample
-        
+
         scheduler._step_index = None
 
         for i, t in enumerate(scheduler.timesteps):
