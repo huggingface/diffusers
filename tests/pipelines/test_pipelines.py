3056493964b8ca9623d849d7f50ae8e318bd07df
Jacqui Wei <jwei@canva.com>
2023-08-17 09:26:20 +1000

    split test cases

--- tests/pipelines/test_pipelines.py
+++ tests/pipelines/test_pipelines.py
@@ -76,6 +76,7 @@ from diffusers.utils.testing_utils import (
     load_numpy,
     require_compel,
     require_flax,
+    require_onnxruntime,
     require_torch_gpu,
     run_test_in_subprocess,
 )
@@ -339,6 +340,8 @@ class DownloadTests(unittest.TestCase):
             assert all((f.endswith(".json") or f.endswith(".bin") or f.endswith(".txt")) for f in files)
             assert not any((f.endswith(".onnx") or f.endswith(".pb")) for f in files)
 
+    @require_onnxruntime
+    def test_download_onnx_by_default_for_onnx_pipelines(self):
         with tempfile.TemporaryDirectory() as tmpdirname:
             tmpdirname = DiffusionPipeline.download(
                 "hf-internal-testing/tiny-random-OnnxStableDiffusionPipeline",
