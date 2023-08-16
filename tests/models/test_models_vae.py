716d453b3f54dcd5d89a59994596db661c4d9700
Isotr0py <2037008807@qq.com>
2023-08-16 15:58:53 +0800

    add parameterized test

--- tests/models/test_models_vae.py
+++ tests/models/test_models_vae.py
@@ -285,17 +285,22 @@ class AutoencoderTinyIntegrationTests(unittest.TestCase):
         model.to(torch_device).eval()
         return model
 
-    def test_tae_tiling(self):
+    @parameterized.expand(
+        [
+            [(1, 4, 73, 97), (1, 3, 584, 776)],
+            [(1, 4, 97, 73), (1, 3, 776, 584)],
+            [(1, 4, 49, 65), (1, 3, 520, 392)],
+            [(1, 4, 65, 49), (1, 3, 392, 520)],
+            [(1, 4, 49, 49), (1, 3, 392, 392)],
+        ]
+    )
+    def test_tae_tiling(self, in_shape, out_shape):
         model = self.get_sd_vae_model()
         model.enable_tiling()
-        in_shapes = [(1, 4, 73, 97), (1, 4, 97, 73), (1, 4, 49, 65), (1, 4, 65, 49), (1, 4, 49, 49)]
-        out_shapes = [torch.Size([1, 3, 584, 776]), torch.Size([1, 3, 776, 584]), torch.Size([1, 3, 392, 520]), 
-                      torch.Size([1, 3, 520, 392]), torch.Size([1, 3, 392, 392])]
         with torch.no_grad():
-            for i, shape in enumerate(in_shapes):
-                zeros = torch.zeros(shape).to(torch_device)
-                dec = model.decode(zeros).sample
-                assert dec.shape == out_shapes[i]
+            zeros = torch.zeros(in_shape).to(torch_device)
+            dec = model.decode(zeros).sample
+            assert dec.shape == out_shape
 
     def test_stable_diffusion(self):
         model = self.get_sd_vae_model()
