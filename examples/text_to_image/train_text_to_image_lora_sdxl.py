f4ed5e84382b27744f0979a5fa9b0bf55c018ecf
Sayak Paul <spsayakpaul@gmail.com>
2023-08-16 15:28:26 +0530

    fix: casting issues.

--- examples/text_to_image/train_text_to_image_lora_sdxl.py
+++ examples/text_to_image/train_text_to_image_lora_sdxl.py
@@ -1002,9 +1002,12 @@ def main(args):
                 continue
 
             with accelerator.accumulate(unet):
-                pixel_values = batch["pixel_values"].to(dtype=weight_dtype)
-
                 # Convert images to latent space
+                if args.pretrained_vae_model_name_or_path is not None:
+                    pixel_values = batch["pixel_values"].to(dtype=weight_dtype)
+                else:
+                    pixel_values = batch["pixel_values"]
+
                 model_input = vae.encode(pixel_values).latent_dist.sample()
                 model_input = model_input * vae.config.scaling_factor
                 if args.pretrained_vae_model_name_or_path is None:
