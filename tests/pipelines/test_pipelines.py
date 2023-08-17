4f4774e2a11dc9407c25381498e7ca37b46bbe62
Jacqui Wei <jwei@canva.com>
2023-08-17 16:19:32 +1000

    Fix `use_onnx` parameter usage in `from_pretrained` func and update `test_download_no_onnx_by_default` test (#4508)
    * add missing use_onnx in from_pretrained func
    
    * fix test_download_no_onnx_by_default test func
    
    * address comments
    
    * split test cases

--- tests/pipelines/test_pipelines.py
+++ tests/pipelines/test_pipelines.py
@@ -76,6 +76,7 @@ from diffusers.utils.testing_utils import (
     load_numpy,
     require_compel,
     require_flax,
+    require_onnxruntime,
     require_torch_gpu,
     run_test_in_subprocess,
 )
@@ -327,28 +328,30 @@ class DownloadTests(unittest.TestCase):
     def test_download_no_onnx_by_default(self):
         with tempfile.TemporaryDirectory() as tmpdirname:
             tmpdirname = DiffusionPipeline.download(
-                "hf-internal-testing/tiny-random-OnnxStableDiffusionPipeline",
+                "hf-internal-testing/tiny-stable-diffusion-xl-pipe",
                 cache_dir=tmpdirname,
+                use_safetensors=False,
             )
 
             all_root_files = [t[-1] for t in os.walk(os.path.join(tmpdirname))]
             files = [item for sublist in all_root_files for item in sublist]
 
-            # make sure that by default no onnx weights are downloaded
+            # make sure that by default no onnx weights are downloaded for non-ONNX pipelines
             assert all((f.endswith(".json") or f.endswith(".bin") or f.endswith(".txt")) for f in files)
             assert not any((f.endswith(".onnx") or f.endswith(".pb")) for f in files)
 
+    @require_onnxruntime
+    def test_download_onnx_by_default_for_onnx_pipelines(self):
         with tempfile.TemporaryDirectory() as tmpdirname:
             tmpdirname = DiffusionPipeline.download(
                 "hf-internal-testing/tiny-random-OnnxStableDiffusionPipeline",
                 cache_dir=tmpdirname,
-                use_onnx=True,
             )
 
             all_root_files = [t[-1] for t in os.walk(os.path.join(tmpdirname))]
             files = [item for sublist in all_root_files for item in sublist]
 
-            # if `use_onnx` is specified make sure weights are downloaded
+            # make sure that by default onnx weights are downloaded for ONNX pipelines
             assert any((f.endswith(".json") or f.endswith(".bin") or f.endswith(".txt")) for f in files)
             assert any((f.endswith(".onnx")) for f in files)
             assert any((f.endswith(".pb")) for f in files)
