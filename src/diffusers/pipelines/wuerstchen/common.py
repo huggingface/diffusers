17d28e3809e6f4898decccbd772d5a6dd9c199ce
Kashif Rasul <kashif.rasul@gmail.com>
2023-08-17 22:16:39 +0200

    concat with normed input

--- src/diffusers/pipelines/wuerstchen/common.py
+++ src/diffusers/pipelines/wuerstchen/common.py
@@ -83,11 +83,11 @@ class AttnBlock(nn.Module):
         self.attention = Attention(query_dim=c, heads=nhead, dim_head=c // nhead, dropout=dropout, bias=True)
         self.kv_mapper = nn.Sequential(nn.SiLU(), nn.Linear(c_cond, c))
 
-    def forward(self, x, kv=None):
-        if kv is not None:
-            kv = self.kv_mapper(kv)
-        if self.self_attn and kv is not None:
-            batch_size, channel, height, width = x.shape
-            kv = torch.cat([x.view(batch_size, channel, height * width).transpose(1, 2), kv], dim=1)
-        x = x + self.attention(self.norm(x), encoder_hidden_states=kv)
+    def forward(self, x, kv):
+        kv = self.kv_mapper(kv)
+        norm_x = self.norm(x)
+        if self.self_attn:
+            batch_size, channel, _, _ = x.shape
+            kv = torch.cat([norm_x.view(batch_size, channel, -1).transpose(1, 2), kv], dim=1)
+        x = x + self.attention(norm_x, encoder_hidden_states=kv)
         return x
