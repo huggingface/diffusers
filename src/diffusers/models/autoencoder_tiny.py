a1bc87335a69f228d3176d460f8df83292f7fae3
Ollin Boer Bohan <madebyollin@gmail.com>
2023-08-20 21:37:39 -0700

    Add comments to AutoencoderTiny.forward

--- src/diffusers/models/autoencoder_tiny.py
+++ src/diffusers/models/autoencoder_tiny.py
@@ -330,8 +330,15 @@ class AutoencoderTiny(ModelMixin, ConfigMixin):
                 Whether or not to return a [`DecoderOutput`] instead of a plain tuple.
         """
         enc = self.encode(sample).latents
+
+        # scale latents to be in [0, 1], then quantize latents to a byte tensor,
+        # as if we were storing the latents in an RGBA uint8 image.
         scaled_enc = self.scale_latents(enc).mul_(255).round_().byte()
+
+        # unquantize latents back into [0, 1], then unscale latents back to their original range,
+        # as if we were loading the latents from an RGBA uint8 image.
         unscaled_enc = self.unscale_latents(scaled_enc / 255.0)
+
         dec = self.decode(unscaled_enc)
 
         if not return_dict:
