54e683f77397a8115a5d20bc885c8353ee07588e
sayakpaul <spsayakpaul@gmail.com>
2023-08-23 16:30:51 +0530

    debugging

--- src/diffusers/models/adapter.py
+++ src/diffusers/models/adapter.py
@@ -164,7 +164,7 @@ class FullAdapter(nn.Module):
         for block in self.body:
             x = block(x)
             features.append(x)
-
+        print(f"Number of features: {len(features)}")
         return features
 
 
@@ -293,7 +293,7 @@ class LightAdapter(nn.Module):
         for block in self.body:
             x = block(x)
             features.append(x)
-
+        print(f"Number of features: {len(features)}")
         return features
 
 
