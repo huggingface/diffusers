8c3516bb04ec4abf578e8e8c6937739caa194f31
shauray8 <shauray9@gmail.com>
2023-08-25 11:30:09 +0530

    new images

--- tests/pipelines/stable_diffusion/test_fabric.py
+++ tests/pipelines/stable_diffusion/test_fabric.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import gc
 import unittest
 
 import numpy as np
@@ -29,8 +28,6 @@ from diffusers import (
 )
 from diffusers.utils.testing_utils import (
     enable_full_determinism,
-    nightly,
-    require_torch_gpu,
 )
 
 from ..pipeline_params import TEXT_TO_IMAGE_BATCH_PARAMS, TEXT_TO_IMAGE_IMAGE_PARAMS, TEXT_TO_IMAGE_PARAMS
@@ -191,4 +188,3 @@ class FabricPipelineFastTests(PipelineTesterMixin, unittest.TestCase):
         ).flatten()
 
         self.assertTrue(np.allclose(image_slice.flatten(), expected_slice, atol=1e-2))
-
