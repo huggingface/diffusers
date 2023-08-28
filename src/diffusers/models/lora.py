caa79ed18126144c944ef3ee0d9c4e39e3657a07
Sayak Paul <spsayakpaul@gmail.com>
2023-08-28 11:59:14 +0530

    debugging

--- src/diffusers/models/lora.py
+++ src/diffusers/models/lora.py
@@ -161,7 +161,6 @@ class LoRACompatibleLinear(nn.Linear):
     def __init__(self, *args, lora_layer: Optional[LoRALinearLayer] = None, **kwargs):
         super().__init__(*args, **kwargs)
         self.lora_layer = lora_layer
-        self.hello = torch.tensor([0.20]).cpu()
 
     def set_lora_layer(self, lora_layer: Optional[LoRAConv2dLayer]):
         self.lora_layer = lora_layer
@@ -189,8 +188,7 @@ class LoRACompatibleLinear(nn.Linear):
         # offload the up and down matrices to CPU to not blow the memory
         self.w_up = w_up.cpu()
         self.w_down = w_down.cpu()
-        self.hello = self.hello.cpu()
-        print(f"From {self.__class__}: {self.w_up.shape}, {self.w_down.shape}")
+        self.hello = torch.tensor([20]).cpu()
 
     def _unfuse_lora(self):
         if not (hasattr(self, "w_up") and hasattr(self, "w_down")):
