1794494dc96c19680cab700ba5e995d10695f308
vahram.tadevosyan <vahram.tadevosyan@picsart.com>
2023-08-30 10:34:37 -0500

    added device compatibility for fast tests

--- tests/pipelines/text_to_video/test_text_to_video_zero_sdxl.py
+++ tests/pipelines/text_to_video/test_text_to_video_zero_sdxl.py
@@ -19,49 +19,66 @@ import numpy as np
 import torch
 
 from diffusers import DDIMScheduler, TextToVideoZeroSDXLPipeline
-from diffusers.utils import require_torch_gpu, slow
+from diffusers.utils import require_torch_gpu, slow, torch_device
 
 
-@require_torch_gpu
 class TextToVideoZeroSDXLPipelineFastTests(unittest.TestCase):
     def test_forward_loop(self):
         model_id = "stabilityai/stable-diffusion-xl-base-1.0"
         pipe = TextToVideoZeroSDXLPipeline.from_pretrained(
             model_id, torch_dtype=torch.float16, variant="fp16", use_safetensors=True
-        )
+        ).to(torch_device)
         pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config)
-        generator = torch.Generator().manual_seed(0)
-        latents = torch.randn((1, 4, 128, 128), dtype=torch.float32, generator=generator)
+        generator = torch.Generator(device=torch_device).manual_seed(0)
+        latents = torch.randn((1, 4, 128, 128), dtype=torch.float32, device=torch_device, generator=generator)
 
-        output = pipe.forward_loop(latents, t0=10, t1=30, generator=generator).numpy()
+        output = pipe.forward_loop(latents, t0=10, t1=30, generator=generator).cpu().numpy()
 
         slice1 = output[0, -1, -3:, -3:]
         slice2 = output[-1, 0, -3:, -3:]
 
-        expected_slice1 = np.array([0.70, -1.63, 0.43, 0.20, -1.46, -0.08, -0.11, 0.40, -1.53])
-        expected_slice2 = np.array([1.26, 0.53, 1.43, -0.36, -0.62, -0.26, 0.07, -1.22, -0.96])
+        if torch_device == "cuda":
+            expected_slice1 = np.array([0.25, 2.51, -0.95, 0.93, -3.50, -1.03, 0.63, -1.14, -0.81])
+            expected_slice2 = np.array([-0.45, -0.63, 0.93, 0.22, 0.86, -0.04, -0.08, -0.43, -3.12])
+        else:
+            expected_slice1 = np.array([0.70, -1.63, 0.43, 0.20, -1.46, -0.08, -0.11, 0.40, -1.53])
+            expected_slice2 = np.array([1.26, 0.53, 1.43, -0.36, -0.62, -0.26, 0.07, -1.22, -0.96])
 
         assert np.abs(slice1.flatten() - expected_slice1).max() < 1e-2
         assert np.abs(slice2.flatten() - expected_slice2).max() < 1e-2
 
     def test_full_model(self):
         model_id = "stabilityai/stable-diffusion-xl-base-1.0"
+
         pipe = TextToVideoZeroSDXLPipeline.from_pretrained(
-            model_id, torch_dtype=torch.float16, variant="fp16", use_safetensors=True
-        ).to("cuda")
+            model_id, torch_dtype=torch.float32, use_safetensors=True
+        ).to(torch_device)
+
         pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config)
-        generator = torch.Generator(device="cuda").manual_seed(0)
+        generator = torch.Generator(device=torch_device).manual_seed(0)
 
         prompt = "A panda dancing in Antarctica"
         result = pipe(
-            prompt=prompt, generator=generator, video_length=3, num_inference_steps=5, height=64, width=64, t0=1, t1=3
+            prompt=prompt,
+            generator=generator,
+            video_length=3,
+            num_inference_steps=5,
+            height=64,
+            width=64,
+            t0=1,
+            t1=3,
+            output_type="np",
         ).images
 
         first_frame_slice = result[0, -3:, -3:, -1]
         last_frame_slice = result[-1, -3:, -3:, 0]
 
-        expected_slice1 = np.array([0.04, 0.06, 0.06, 0.12, 0.13, 0.13, 0.20, 0.21, 0.20])
-        expected_slice2 = np.array([0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.07, 0.07, 0.06])
+        if torch_device == "cuda":
+            expected_slice1 = np.array([0.044, 0.061, 0.066, 0.124, 0.134, 0.135, 0.211, 0.220, 0.200])
+            expected_slice2 = np.array([0.000, 0.000, 0.000, 0.000, 0.000, 0.000, 0.076, 0.075, 0.064])
+        else:
+            expected_slice1 = np.array([0.005, 0.005, 0.017, 0.004, 0.011, 0.013, 0.020, 0.034, 0.022])
+            expected_slice2 = np.array([0.009, 0.000, 0.000, 0.008, 0.000, 0.000, 0.000, 0.000, 0.000])
 
         assert np.abs(first_frame_slice.flatten() - expected_slice1).max() < 1e-2
         assert np.abs(last_frame_slice.flatten() - expected_slice2).max() < 1e-2
