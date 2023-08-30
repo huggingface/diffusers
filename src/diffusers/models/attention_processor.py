b275947053459b264980c732c534b2e9ebc7a70a
sayakpaul <spsayakpaul@gmail.com>
2023-08-31 00:56:11 +0530

    debugging

--- src/diffusers/models/attention_processor.py
+++ src/diffusers/models/attention_processor.py
@@ -416,7 +416,6 @@ class Attention(nn.Module):
         # The `Attention` class can call different attention processors / attention functions
         # here we simply pass along all tensors to the selected processor class
         # For standard processors that are defined here, `**cross_attention_kwargs` is empty
-        print(f"{self.__class__.__name__} {cross_attention_kwargs}")
         return self.processor(
             self,
             hidden_states,
