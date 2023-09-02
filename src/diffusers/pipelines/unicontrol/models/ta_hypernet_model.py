74ae5ff32c97e4e569417373c05e0b96e2cc5e7f
Abhinay1997 <devarintinagasaiabhinay@gmail.com>
2023-09-02 08:44:21 +0530

    Files refactored + added task input checks

--- src/diffusers/pipelines/unicontrol/models/ta_hypernet_model.py
+++
@@ -1,7 +0,0 @@
-# File where the Task Aware Hypernet Model class resides. 
-
-from ....models import ModelMixin
-
-class TaskAwareHyperNetModel(ModelMixin):
-    def __init__(self) -> None:
-        pass
