c2758505928627f32ebbb80c35992ec288f80062
isamu-isozaki <isamu.website@gmail.com>
2023-09-03 22:21:00 -0400

    Remove diff

--- utils/check_dummies.py
+++ utils/check_dummies.py
@@ -149,7 +149,6 @@ def check_dummies(overwrite=False):
 
     for backend in dummy_files.keys():
         if dummy_files[backend] != actual_dummies[backend]:
-            print(dummy_files[backend])
             if overwrite:
                 print(
                     f"Updating diffusers.utils.dummy_{short_names.get(backend, backend)}_objects.py as the main "
