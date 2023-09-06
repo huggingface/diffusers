2b821643b7ec3b5d87b254e95a90a575395bc0b3
Dhruv Nair <dhruv.nair@gmail.com>
2023-09-06 10:04:59 +0000

    remove torch import from xformers check

--- src/diffusers/utils/import_utils.py
+++ src/diffusers/utils/import_utils.py
@@ -221,10 +221,10 @@ _xformers_available = importlib.util.find_spec("xformers") is not None
 try:
     _xformers_version = importlib_metadata.version("xformers")
     if _torch_available:
-        import torch
-
-        if version.Version(torch.__version__) < version.Version("1.12"):
+        _torch_version = importlib_metadata.version("torch")
+        if version.Version(_torch_version) < version.Version("1.12"):
             raise ValueError("PyTorch should be >= 1.12")
+
     logger.debug(f"Successfully imported xformers version {_xformers_version}")
 except importlib_metadata.PackageNotFoundError:
     _xformers_available = False
