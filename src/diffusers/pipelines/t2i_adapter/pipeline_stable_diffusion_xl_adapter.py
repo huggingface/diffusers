5fa46f4b015d5be3064c48a0f584b414ed2a652b
patil-suraj <surajp815@gmail.com>
2023-09-07 19:27:40 +0200

    style

--- src/diffusers/pipelines/t2i_adapter/pipeline_stable_diffusion_xl_adapter.py
+++ src/diffusers/pipelines/t2i_adapter/pipeline_stable_diffusion_xl_adapter.py
@@ -971,7 +971,7 @@ class StableDiffusionXLAdapterPipeline(
             return (image,)
 
         return StableDiffusionXLPipelineOutput(images=image)
-    
+
     # Overrride to properly handle the loading and unloading of the additional text encoder.
     def load_lora_weights(self, pretrained_model_name_or_path_or_dict: Union[str, Dict[str, torch.Tensor]], **kwargs):
         # We could have accessed the unet config from `lora_state_dict()` too. We pass
