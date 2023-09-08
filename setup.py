9cc6a01b7d152d9b739eb8abf436456722f8a522
Wauplin <lucainp@gmail.com>
2023-09-08 16:26:01 +0200

    [don't merge] Test hfh v0.17.0.rc0

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ _deps = [
     "filelock",
     "flax>=0.4.1",
     "hf-doc-builder>=0.3.0",
-    "huggingface-hub>=0.13.2",
+    "huggingface-hub==v0.17.0.rc0",
     "requests-mock==1.10.0",
     "importlib_metadata",
     "invisible-watermark>=0.2.0",
