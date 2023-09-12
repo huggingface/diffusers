05e750f321082759362ef1d0c81cdae0739af09c
Daniel Gu <dgu8957@gmail.com>
2023-09-11 23:39:49 -0700

    Update EDM pipeline import to follow the new lazy import structure.

--- src/diffusers/pipelines/edm/__init__.py
+++ src/diffusers/pipelines/edm/__init__.py
@@ -1,1 +1,17 @@
-from .pipeline_edm import KarrasEDMPipeline
+from ...utils import (
+    _LazyModule,
+)
+
+
+_import_structure = {}
+_import_structure["pipeline_edm"] = ["KarrasEDMPipeline"]
+
+import sys
+
+
+sys.modules[__name__] = _LazyModule(
+    __name__,
+    globals()["__file__"],
+    _import_structure,
+    module_spec=__spec__,
+)
