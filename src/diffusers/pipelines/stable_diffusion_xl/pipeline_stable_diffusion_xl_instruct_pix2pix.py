0ea51627f190db401d195399051bc5015b6bc35a
Sayak Paul <spsayakpaul@gmail.com>
2023-09-13 10:50:24 +0100

    [Core] Fix dtype in InstructPix2Pix SDXL while computing `image_latents` (#5013)
    * check out dtypes.
    
    * check out dtypes.
    
    * check out dtypes.
    
    * check out dtypes.
    
    * check out dtypes.
    
    * check out dtypes.
    
    * check out dtypes.
    
    * potential fix
    
    * check out dtypes.
    
    * check out dtypes.
    
    * working?

--- src/diffusers/pipelines/stable_diffusion_xl/pipeline_stable_diffusion_xl_instruct_pix2pix.py
+++ src/diffusers/pipelines/stable_diffusion_xl/pipeline_stable_diffusion_xl_instruct_pix2pix.py
@@ -495,7 +495,8 @@ class StableDiffusionXLInstructPix2PixPipeline(
             image_latents = image
         else:
             # make sure the VAE is in float32 mode, as it overflows in float16
-            if self.vae.dtype == torch.float16 and self.vae.config.force_upcast:
+            needs_upcasting = self.vae.dtype == torch.float16 and self.vae.config.force_upcast
+            if needs_upcasting:
                 self.upcast_vae()
                 image = image.to(next(iter(self.vae.post_quant_conv.parameters())).dtype)
 
@@ -510,6 +511,10 @@ class StableDiffusionXLInstructPix2PixPipeline(
                 image_latents = torch.cat(image_latents, dim=0)
             else:
                 image_latents = self.vae.encode(image).latent_dist.mode()
+
+            # cast back to fp16 if needed
+            if needs_upcasting:
+                self.vae.to(dtype=torch.float16)
 
         if batch_size > image_latents.shape[0] and batch_size % image_latents.shape[0] == 0:
             # expand image_latents for batch_size
@@ -532,6 +537,9 @@ class StableDiffusionXLInstructPix2PixPipeline(
         if do_classifier_free_guidance:
             uncond_image_latents = torch.zeros_like(image_latents)
             image_latents = torch.cat([image_latents, image_latents, uncond_image_latents], dim=0)
+
+        if image_latents.dtype != self.vae.dtype:
+            image_latents = image_latents.to(dtype=self.vae.dtype)
 
         return image_latents
 
