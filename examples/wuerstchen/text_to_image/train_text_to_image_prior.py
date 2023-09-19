8f9a6837bdcb9ae0a1c53cd7ee80ef4a6773a3f3
Kashif Rasul <kashif.rasul@gmail.com>
2023-09-19 15:16:11 +0200

    accelerator check

--- examples/wuerstchen/text_to_image/train_text_to_image_prior.py
+++ examples/wuerstchen/text_to_image/train_text_to_image_prior.py
@@ -16,6 +16,7 @@ import logging
 import math
 import os
 import random
+import shutil
 from pathlib import Path
 
 import accelerate
@@ -670,6 +671,47 @@ def main():
             optimizer.step()
             lr_scheduler.step()
             optimizer.zero_grad()
+       
+        # Checks if the accelerator has performed an optimization step behind the scenes
+        if accelerator.sync_gradients:
+            if args.use_ema:
+                ema_prior.step(prior.parameters())
+            progress_bar.update(1)
+            global_step += 1
+            accelerator.log({"train_loss": train_loss}, step=global_step)
+            train_loss = 0.0
+
+            if global_step % args.checkpointing_steps == 0:
+                if accelerator.is_main_process:
+                    # _before_ saving state, check if this save would set us over the `checkpoints_total_limit`
+                    if args.checkpoints_total_limit is not None:
+                        checkpoints = os.listdir(args.output_dir)
+                        checkpoints = [d for d in checkpoints if d.startswith("checkpoint")]
+                        checkpoints = sorted(checkpoints, key=lambda x: int(x.split("-")[1]))
+
+                        # before we save the new checkpoint, we need to have at _most_ `checkpoints_total_limit - 1` checkpoints
+                        if len(checkpoints) >= args.checkpoints_total_limit:
+                            num_to_remove = len(checkpoints) - args.checkpoints_total_limit + 1
+                            removing_checkpoints = checkpoints[0:num_to_remove]
+
+                            logger.info(
+                                f"{len(checkpoints)} checkpoints already exist, removing {len(removing_checkpoints)} checkpoints"
+                            )
+                            logger.info(f"removing checkpoints: {', '.join(removing_checkpoints)}")
+
+                            for removing_checkpoint in removing_checkpoints:
+                                removing_checkpoint = os.path.join(args.output_dir, removing_checkpoint)
+                                shutil.rmtree(removing_checkpoint)
+
+                    save_path = os.path.join(args.output_dir, f"checkpoint-{global_step}")
+                    accelerator.save_state(save_path)
+                    logger.info(f"Saved state to {save_path}")
+
+        logs = {"step_loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0]}
+        progress_bar.set_postfix(**logs)
+
+        if global_step >= args.max_train_steps:
+            break
 
 
 
