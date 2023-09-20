2c2ef0c80c4fd042c50151112a3c69b8d061b8ff
Younes Belkada <49240599+younesbelkada@users.noreply.github.com>
2023-09-20 17:29:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -269,7 +269,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
