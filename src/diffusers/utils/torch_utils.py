6d77fff824c192b8dc02ed012a6fad59e934199f
Sayak Paul <spsayakpaul@gmail.com>
2023-09-27 14:24:57 +0530

    move unet to its original form and add fourier_filter to torch_utils.

--- src/diffusers/utils/torch_utils.py
+++ src/diffusers/utils/torch_utils.py
@@ -22,6 +22,7 @@ from .import_utils import is_torch_available, is_torch_version
 
 if is_torch_available():
     import torch
+    from torch.fft import fftn, fftshift, ifftn, ifftshift
 
 logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
 
@@ -86,3 +87,34 @@ def is_compiled_module(module):
     if is_torch_version("<", "2.0.0") or not hasattr(torch, "_dynamo"):
         return False
     return isinstance(module, torch._dynamo.eval_frame.OptimizedModule)
+
+
+def fourier_filter(x_in, threshold, scale):
+    """Fourier filter as introduced in FreeU (https://arxiv.org/abs/2309.11497).
+
+    This version of the method comes from here:
+    https://github.com/huggingface/diffusers/pull/5164#issuecomment-1732638706
+    """
+    x = x_in
+    B, C, H, W = x.shape
+
+    # Non-power of 2 images must be float32
+    if (W & (W - 1)) != 0 or (H & (H - 1)) != 0:
+        x = x.to(dtype=torch.float32)
+
+    # FFT
+    x_freq = fftn(x, dim=(-2, -1))
+    x_freq = fftshift(x_freq, dim=(-2, -1))
+
+    B, C, H, W = x_freq.shape
+    mask = torch.ones((B, C, H, W), device=x.device)
+
+    crow, ccol = H // 2, W // 2
+    mask[..., crow - threshold : crow + threshold, ccol - threshold : ccol + threshold] = scale
+    x_freq = x_freq * mask
+
+    # IFFT
+    x_freq = ifftshift(x_freq, dim=(-2, -1))
+    x_filtered = ifftn(x_freq, dim=(-2, -1)).real
+
+    return x_filtered.to(dtype=x_in.dtype)
