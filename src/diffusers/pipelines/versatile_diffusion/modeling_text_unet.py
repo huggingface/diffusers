785c0a077c5b2523e07f4f10f9f75c99a44a568a
Sayak Paul <spsayakpaul@gmail.com>
2023-09-27 21:30:00 +0530

    fix copies

--- src/diffusers/pipelines/versatile_diffusion/modeling_text_unet.py
+++ src/diffusers/pipelines/versatile_diffusion/modeling_text_unet.py
@@ -941,19 +941,19 @@ class UNetFlatConditionModel(ModelMixin, ConfigMixin):
         if hasattr(module, "gradient_checkpointing"):
             module.gradient_checkpointing = value
 
-    def enable_freeu(self, **kwargs):
+    def enable_freeu(self, s1, s2, b1, b2):
         for i, upsample_block in enumerate(self.up_blocks):
-            setattr(upsample_block, "b1", kwargs["b1"])
-            setattr(upsample_block, "b2", kwargs["b2"])
-            setattr(upsample_block, "s1", kwargs["s1"])
-            setattr(upsample_block, "s2", kwargs["s2"])
+            setattr(upsample_block, "s1", s1)
+            setattr(upsample_block, "s2", s2)
+            setattr(upsample_block, "b1", b1)
+            setattr(upsample_block, "b2", b2)
 
     def disable_freeu(self):
         freeu_keys = {"s1", "s2", "b1", "b2"}
         for i, upsample_block in enumerate(self.up_blocks):
             for k in freeu_keys:
                 if hasattr(upsample_block, k) or getattr(upsample_block, k) is not None:
-                    setattr(self.config, k, None)
+                    setattr(upsample_block, k, None)
 
     def forward(
         self,
