535eb590274c58f37e1161307fa486bcce837e95
Sayak Paul <spsayakpaul@gmail.com>
2023-09-28 11:50:26 +0530

    fix copies

--- src/diffusers/pipelines/versatile_diffusion/modeling_text_unet.py
+++ src/diffusers/pipelines/versatile_diffusion/modeling_text_unet.py
@@ -1751,7 +1751,7 @@ class CrossAttnUpBlockFlat(nn.Module):
         out_channels: int,
         prev_output_channel: int,
         temb_channels: int,
-        resolution_idx: int,
+        resolution_idx: int = None,
         dropout: float = 0.0,
         num_layers: int = 1,
         transformer_layers_per_block: int = 1,
