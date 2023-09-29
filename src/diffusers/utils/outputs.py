85a8f6f33ea83023dfe1616aee4feeceffba60fc
cbensimon <charles@huggingface.co>
2023-09-29 11:01:08 +0000

    Simpler and safer

--- src/diffusers/utils/outputs.py
+++ src/diffusers/utils/outputs.py
@@ -104,9 +104,9 @@ class BaseOutput(OrderedDict):
     def __reduce__(self):
         if not is_dataclass(self):
             return super().__reduce__()
-        callable, _args, state, istate, dstate = super().__reduce__()
+        callable, _args, *remaining = super().__reduce__()
         args = tuple(getattr(self, field.name) for field in fields(self))
-        return callable, args, state, istate, dstate
+        return callable, args, *remaining
 
     def to_tuple(self) -> Tuple[Any]:
         """
