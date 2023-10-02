4e02e814e30544411ff4621b26ecd42ae35b5ada
Patrick von Platen <patrick.v.platen@gmail.com>
2023-10-02 19:52:53 +0200

    [Schedulers] Fix callback steps (#5261)
    * fix all
    
    * make fix copies
    
    * make fix copies

--- examples/community/masked_stable_diffusion_img2img.py
+++ examples/community/masked_stable_diffusion_img2img.py
@@ -202,7 +202,8 @@ class MaskedStableDiffusionImg2ImgPipeline(StableDiffusionImg2ImgPipeline):
                 if i == len(timesteps) - 1 or ((i + 1) > num_warmup_steps and (i + 1) % self.scheduler.order == 0):
                     progress_bar.update()
                     if callback is not None and i % callback_steps == 0:
-                        callback(i, t, latents)
+                        step_idx = i // getattr(self.scheduler, "order", 1)
+                        callback(step_idx, t, latents)
 
         if not output_type == "latent":
             scaled = latents / self.vae.config.scaling_factor
