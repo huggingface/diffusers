cad5a4b0ef8bcfc4abdcb76cdc5175812a247146
younesbelkada <younesbelkada@gmail.com>
2023-10-09 16:36:07 +0000

    add `safe_merge` tests

--- tests/lora/test_lora_layers_peft.py
+++ tests/lora/test_lora_layers_peft.py
@@ -775,6 +775,37 @@ class PeftLoraLoaderMixinTests:
             "output with no lora and output with lora disabled should give same results",
         )
 
+    def test_lora_fuse_nan(self):
+        components, _, text_lora_config, unet_lora_config = self.get_dummy_components()
+        pipe = self.pipeline_class(**components)
+        pipe = pipe.to(self.torch_device)
+        pipe.set_progress_bar_config(disable=None)
+        _, _, inputs = self.get_dummy_inputs(with_generator=False)
+
+        pipe.text_encoder.add_adapter(text_lora_config, "adapter-1")
+
+        pipe.unet.add_adapter(unet_lora_config, "adapter-1")
+
+        self.assertTrue(self.check_if_lora_correctly_set(pipe.text_encoder), "Lora not correctly set in text encoder")
+        self.assertTrue(self.check_if_lora_correctly_set(pipe.unet), "Lora not correctly set in Unet")
+
+        # corrupt one LoRA weight with `inf` values
+        with torch.no_grad():
+            pipe.unet.mid_block.attentions[0].transformer_blocks[0].attn1.to_q.lora_A["adapter-1"].weight += float(
+                "inf"
+            )
+
+        # with `safe_fusing=True` we should see an Error
+        with self.assertRaises(ValueError):
+            pipe.fuse_lora(safe_fusing=True)
+
+        # without we should not see an error, but every image will be black
+        pipe.fuse_lora(safe_fusing=False)
+
+        out = pipe("test", num_inference_steps=2, output_type="np").images
+
+        self.assertTrue(np.isnan(out).all())
+
     @unittest.skip("This is failing for now - need to investigate")
     def test_simple_inference_with_text_unet_lora_unfused_torch_compile(self):
         """
