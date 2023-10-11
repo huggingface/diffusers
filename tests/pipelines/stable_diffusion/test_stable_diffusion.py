670f0b7a5ffa76a6fbe287d081eb4b6821112bda
Arkajyoti Mitra <axm3158@mavs.uta.edu>
2023-10-11 00:54:48 -0500

    made change to UNET2dConditionModel, VAE, and textencoder
    Please enter the commit message for your changes. Lines starting

--- tests/pipelines/stable_diffusion/test_stable_diffusion.py
+++ tests/pipelines/stable_diffusion/test_stable_diffusion.py
@@ -103,14 +103,14 @@ class StableDiffusionPipelineFastTests(
     def get_dummy_components(self):
         torch.manual_seed(0)
         unet = UNet2DConditionModel(
-            block_out_channels=(32, 64),
+            block_out_channels=(32,),
             layers_per_block=2,
             sample_size=32,
             in_channels=4,
             out_channels=4,
-            down_block_types=("DownBlock2D", "CrossAttnDownBlock2D"),
-            up_block_types=("CrossAttnUpBlock2D", "UpBlock2D"),
-            cross_attention_dim=32,
+            down_block_types=("CrossAttnDownBlock2D",),
+            up_block_types=("CrossAttnUpBlock2D",),
+            cross_attention_dim=16,
         )
         scheduler = DDIMScheduler(
             beta_start=0.00085,
@@ -121,22 +121,22 @@ class StableDiffusionPipelineFastTests(
         )
         torch.manual_seed(0)
         vae = AutoencoderKL(
-            block_out_channels=[32, 64],
+            block_out_channels=[32,],
             in_channels=3,
             out_channels=3,
-            down_block_types=["DownEncoderBlock2D", "DownEncoderBlock2D"],
-            up_block_types=["UpDecoderBlock2D", "UpDecoderBlock2D"],
-            latent_channels=4,
+            down_block_types=["DownEncoderBlock2D",],
+            up_block_types=["UpDecoderBlock2D",],
+            latent_channels=2,
         )
         torch.manual_seed(0)
         text_encoder_config = CLIPTextConfig(
             bos_token_id=0,
             eos_token_id=2,
-            hidden_size=32,
-            intermediate_size=37,
+            hidden_size=16,
+            intermediate_size=21,
             layer_norm_eps=1e-05,
-            num_attention_heads=4,
-            num_hidden_layers=5,
+            num_attention_heads=2,
+            num_hidden_layers=3,
             pad_token_id=1,
             vocab_size=1000,
         )
