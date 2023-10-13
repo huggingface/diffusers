bc65f829b73ed0546b2ff67cd6bb6428f8b5bdb6
Sayak Paul <spsayakpaul@gmail.com>
2023-10-13 14:41:43 +0530

    debug

--- src/diffusers/models/attention.py
+++ src/diffusers/models/attention.py
@@ -216,6 +216,7 @@ class BasicTransformerBlock(nn.Module):
         cross_attention_kwargs = cross_attention_kwargs.copy() if cross_attention_kwargs is not None else {}
         gligen_kwargs = cross_attention_kwargs.pop("gligen", None)
 
+        print(f"encoder_hidden_states: {encoder_hidden_states.dtype}")
         attn_output = self.attn1(
             norm_hidden_states,
             encoder_hidden_states=encoder_hidden_states if self.only_cross_attention else None,
