5146dcb98140afbbd54450e6a34258d421b63d88
sayakpaul <spsayakpaul@gmail.com>
2023-10-15 14:51:46 +0530

    audioldm prepare_latents is not an exact copy.

--- src/diffusers/pipelines/audioldm/pipeline_audioldm.py
+++ src/diffusers/pipelines/audioldm/pipeline_audioldm.py
@@ -340,7 +340,6 @@ class AudioLDMPipeline(DiffusionPipeline):
                     f" {negative_prompt_embeds.shape}."
                 )
 
-    # Copied from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion.StableDiffusionPipeline.prepare_latents with width->self.vocoder.config.model_in_dim
     def prepare_latents(self, batch_size, num_channels_latents, height, dtype, device, generator, latents=None):
         shape = (
             batch_size,
