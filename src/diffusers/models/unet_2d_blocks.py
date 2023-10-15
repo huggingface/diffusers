545a0b0fbf86f7310d07929df9713672c03d6f95
Vishnu V Jaddipal <95531133+Gothos@users.noreply.github.com>
2023-10-15 18:04:17 +0530

    Update unet_2d_blocks.py

--- src/diffusers/models/unet_2d_blocks.py
+++ src/diffusers/models/unet_2d_blocks.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from typing import Any, Dict, Optional, Tuple
+from typing import Any, Dict, Optional, Tuple, Union
 
 import numpy as np
 import torch
