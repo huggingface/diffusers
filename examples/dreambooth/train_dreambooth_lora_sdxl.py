0ea78f9707f3ccb0fe3eaa74f247e4d3d8f47b6b
Heinz-Alexander Fuetterer <35225576+afuetterer@users.noreply.github.com>
2023-10-16 15:23:37 +0200

    chore: fix typos (#5386)
    * chore: fix typos
    
    * Update src/diffusers/pipelines/shap_e/renderer.py
    
    Co-authored-by: psychedelicious <4822129+psychedelicious@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: psychedelicious <4822129+psychedelicious@users.noreply.github.com>

--- examples/dreambooth/train_dreambooth_lora_sdxl.py
+++ examples/dreambooth/train_dreambooth_lora_sdxl.py
@@ -707,7 +707,7 @@ def main(args):
     text_encoder_two.requires_grad_(False)
     unet.requires_grad_(False)
 
-    # For mixed precision training we cast all non-trainable weigths (vae, non-lora text_encoder and non-lora unet) to half-precision
+    # For mixed precision training we cast all non-trainable weights (vae, non-lora text_encoder and non-lora unet) to half-precision
     # as these weights are only used for inference, keeping weights in full precision is not required.
     weight_dtype = torch.float32
     if accelerator.mixed_precision == "fp16":
