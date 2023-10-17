7a5142ea085e95302038b3fc8fc4b53a7d2672ad
tuanh123789 <nguyencongtuanh.99@gmail.com>
2023-10-18 03:05:23 +0700

    add TTS to AudioLDM2Pipeline

--- src/diffusers/pipelines/audioldm2/modeling_audioldm2.py
+++ src/diffusers/pipelines/audioldm2/modeling_audioldm2.py
@@ -95,7 +95,7 @@ class AudioLDM2ProjectionModel(ModelMixin, ConfigMixin):
     """
 
     @register_to_config
-    def __init__(self, text_encoder_dim, text_encoder_1_dim, langauge_model_dim):
+    def __init__(self, text_encoder_dim, text_encoder_1_dim, langauge_model_dim, max_seq_length=None):
         super().__init__()
         # additional projection layers for each text encoder
         self.projection = nn.Linear(text_encoder_dim, langauge_model_dim)
@@ -108,6 +108,12 @@ class AudioLDM2ProjectionModel(ModelMixin, ConfigMixin):
         self.sos_embed_1 = nn.Parameter(torch.ones(langauge_model_dim))
         self.eos_embed_1 = nn.Parameter(torch.ones(langauge_model_dim))
 
+        # learable positional embedding for vits encoder
+        if text_encoder_1_dim == 192:
+            self.learnable_positional_embedding = torch.nn.Parameter(
+                torch.zeros((1, text_encoder_1_dim, max_seq_length))
+            )
+
     def forward(
         self,
         hidden_states: Optional[torch.FloatTensor] = None,
@@ -119,6 +125,10 @@ class AudioLDM2ProjectionModel(ModelMixin, ConfigMixin):
         hidden_states, attention_mask = add_special_tokens(
             hidden_states, attention_mask, sos_token=self.sos_embed, eos_token=self.eos_embed
         )
+
+        # Add positional embedding for Vits hidden state
+        if hidden_states_1.shape[2] == 192:
+            hidden_states_1 = (hidden_states_1.permute(0, 2, 1) + self.learnable_positional_embedding).permute(0, 2, 1)
 
         hidden_states_1 = self.projection_1(hidden_states_1)
         hidden_states_1, attention_mask_1 = add_special_tokens(
