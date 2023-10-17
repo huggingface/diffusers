e16ecdf577f19556028f26f9416eed3e9b0993bd
Dhruv Nair <dhruv.nair@gmail.com>
2023-10-17 08:40:44 +0000

    fix pipe fetcher

--- utils/fetch_torch_cuda_pipeline_test_matrix.py
+++ utils/fetch_torch_cuda_pipeline_test_matrix.py
@@ -70,7 +70,12 @@ def fetch_pipeline_modules_to_test():
 
     test_modules = []
     for pipeline_name in pipeline_objects:
-        module = getattr(diffusers, pipeline_name)
+        try:
+            module = getattr(diffusers, pipeline_name)
+        except Exception:
+            logger.warning(f"Unable to fetch {pipeline_name} from diffusers")
+            continue
+
         test_module = module.__module__.split(".")[-2].strip()
         test_modules.append(test_module)
 
