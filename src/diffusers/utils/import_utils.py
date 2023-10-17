4b6eea09216f90d9daeb5d34c06970d016a289f6
François Ponchon <francois.ponchon@michelin.com>
2023-10-17 22:52:01 +0200

    bitsandbytes in imports

--- src/diffusers/utils/import_utils.py
+++ src/diffusers/utils/import_utils.py
@@ -276,6 +276,13 @@ try:
 except importlib_metadata.PackageNotFoundError:
     _peft_available = False
 
+_bitsandbytes_available = importlib.util.find_spec("bitsandbytes") is not None
+try:
+    _bitsandbytes_version = importlib_metadata.version("bitsandbytes")
+    logger.debug(f"Successfully imported bitsandbytes version {_bitsandbytes_version}")
+except importlib_metadata.PackageNotFoundError:
+    _bitsandbytes_available = False
+
 
 def is_torch_available():
     return _torch_available
@@ -364,6 +371,8 @@ def is_invisible_watermark_available():
 def is_peft_available():
     return _peft_available
 
+def is_bitsandbytes_available():
+    return _bitsandbytes_available
 
 # docstyle-ignore
 FLAX_IMPORT_ERROR = """
