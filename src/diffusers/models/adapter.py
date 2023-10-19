2a99be92ad7f8d935f360ef33a77fc78d6087b26
Ryan Dick <ryanjdick3@gmail.com>
2023-10-19 08:46:31 -0400

    Fix static checks.

--- src/diffusers/models/adapter.py
+++ src/diffusers/models/adapter.py
@@ -279,9 +279,8 @@ class T2IAdapter(ModelMixin, ConfigMixin):
 
     @property
     def downscale_factor(self):
-        """The downscale factor applied in the T2I-Adapter's initial pixel
-        unshuffle operation. If an input image's dimensions are not evenly
-        divisible by the downscale_factor then an exception will be raised.
+        """The downscale factor applied in the T2I-Adapter's initial pixel unshuffle operation. If an input image's dimensions are
+        not evenly divisible by the downscale_factor then an exception will be raised.
         """
         return self.adapter.unshuffle.downscale_factor
 
