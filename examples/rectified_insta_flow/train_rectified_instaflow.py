484865bddeba8ed45c7d8e59da2a74bcee14b3d1
isamu-isozaki <isamu.website@gmail.com>
2023-10-21 17:39:36 -0400

    Updated project name

--- examples/rectified_insta_flow/train_rectified_instaflow.py
+++ examples/rectified_insta_flow/train_rectified_instaflow.py
@@ -520,7 +520,7 @@ def parse_args():
     parser.add_argument(
         "--tracker_project_name",
         type=str,
-        default="text2image-fine-tune",
+        default="rectified-flow",
         help=(
             "The `project_name` argument passed to Accelerator.init_trackers for"
             " more information see https://huggingface.co/docs/accelerate/v0.17.0/en/package_reference/accelerator#accelerate.Accelerator"
