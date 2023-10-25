c0c49d9fdbba9a51554e3e56bd8dfe03e0cd12b7
Patrick von Platen <patrick.v.platen@gmail.com>
2023-10-25 18:43:42 +0200

    fix more

--- src/diffusers/pipelines/pipeline_utils.py
+++ src/diffusers/pipelines/pipeline_utils.py
@@ -307,14 +307,15 @@ def get_class_obj_and_candidates(
     library_name, class_name, importable_classes, pipelines, is_pipeline_module, component_name=None, cache_dir=None
 ):
     """Simple helper method to retrieve class object of module as well as potential parent class objects"""
+    component_folder = os.path.join(cache_dir, component_name)
+
     if is_pipeline_module:
         pipeline_module = getattr(pipelines, library_name)
 
         class_obj = getattr(pipeline_module, class_name)
         class_candidates = {c: class_obj for c in importable_classes.keys()}
-    elif library_name not in LOADABLE_CLASSES.keys():
+    elif os.path.isfile(os.path.join(component_folder, library_name + ".py")):
         # load custom component
-        component_folder = os.path.join(cache_dir, component_name)
         class_obj = get_class_from_dynamic_module(
             component_folder, module_file=library_name + ".py", class_name=class_name
         )
