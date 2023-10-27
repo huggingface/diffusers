1aa95017bf4b4ac13a312d7239af1e14e054d392
sayakpaul <spsayakpaul@gmail.com>
2023-10-27 09:24:54 +0530

    disable freeu debug

--- src/diffusers/models/unet_2d_condition.py
+++ src/diffusers/models/unet_2d_condition.py
@@ -790,6 +790,7 @@ class UNet2DConditionModel(ModelMixin, ConfigMixin, UNet2DConditionLoadersMixin)
         """Disables the FreeU mechanism."""
         freeu_keys = {"s1", "s2", "b1", "b2"}
         for i, upsample_block in enumerate(self.up_blocks):
+            print(f"from disable_freeu: {hasattr(upsample_block, k)}")
             for k in freeu_keys:
                 if hasattr(upsample_block, k) or getattr(upsample_block, k) is not None:
                     setattr(upsample_block, k, None)
