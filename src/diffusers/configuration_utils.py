462d443c148a0ef9c79eac51ffd794113c40dae3
sayakpaul <spsayakpaul@gmail.com>
2023-10-27 11:50:28 +0530

    fix: serialization

--- src/diffusers/configuration_utils.py
+++ src/diffusers/configuration_utils.py
@@ -590,7 +590,11 @@ class ConfigMixin:
                 String containing all the attributes that make up the configuration instance in JSON format.
         """
         config_dict = self._internal_dict if hasattr(self, "_internal_dict") else {}
-        config_dict["_class_name"] = self.__class__.__name__
+        cls_name = self.__class__.__name__
+        if cls_name not in _all_available_pipeline_component_modules:
+            config_dict["_class_name"] = [str(self.__class__.__module__), cls_name]
+        else:
+            config_dict["_class_name"] = cls_name
         config_dict["_diffusers_version"] = __version__
 
         def to_json_saveable(value):
