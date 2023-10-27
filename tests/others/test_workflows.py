d06cc7eb6f7db6e06db034a6ebe95ebf482c6f15
sayakpaul <spsayakpaul@gmail.com>
2023-10-27 15:30:54 +0530

    fix: test

--- tests/others/test_workflows.py
+++ tests/others/test_workflows.py
@@ -177,10 +177,10 @@ class WorkflowPushToHubTester(unittest.TestCase):
             bos_token_id=0,
             eos_token_id=2,
             hidden_size=32,
-            intermediate_size=64,
+            intermediate_size=37,
             layer_norm_eps=1e-05,
-            num_attention_heads=8,
-            num_hidden_layers=3,
+            num_attention_heads=4,
+            num_hidden_layers=5,
             pad_token_id=1,
             vocab_size=1000,
         )
