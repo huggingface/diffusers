f4364201066b0f92eeeb6251bc34f84cb9dd2b67
Patrick von Platen <patrick.v.platen@gmail.com>
2023-10-30 14:01:48 +0100

    finish

--- src/diffusers/pipelines/blip_diffusion/modeling_ctx_clip.py
+++ src/diffusers/pipelines/blip_diffusion/modeling_ctx_clip.py
@@ -21,10 +21,20 @@ from transformers.modeling_outputs import BaseModelOutputWithPooling
 from transformers.models.clip.configuration_clip import CLIPTextConfig
 from transformers.models.clip.modeling_clip import CLIPEncoder
 
-try:
-    from transformers.models.clip.modeling_clip import _expand_mask
-except ImportError:
-    from transformers.modeling_attn_mask_utils import _prepare_4d_attention_mask as _expand_mask
+
+def _expand_mask(mask: torch.Tensor, dtype: torch.dtype, tgt_len: Optional[int] = None):
+    """
+    Expands attention_mask from `[bsz, seq_len]` to `[bsz, 1, tgt_seq_len, src_seq_len]`.
+    """
+    bsz, src_len = mask.size()
+    tgt_len = tgt_len if tgt_len is not None else src_len
+
+    expanded_mask = mask[:, None, None, :].expand(bsz, 1, tgt_len, src_len).to(dtype)
+
+    inverted_mask = 1.0 - expanded_mask
+
+    return inverted_mask.masked_fill(inverted_mask.to(torch.bool), torch.finfo(dtype).min)
+
 
 # This is a modified version of the CLIPTextModel from transformers.models.clip.modeling_clip
 # Which allows for an extra input of "context embeddings", which are the query embeddings used in Qformer
