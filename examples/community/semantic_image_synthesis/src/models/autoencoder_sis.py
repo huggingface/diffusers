c3613178ef2e75e5bb9010aaeaa8e8daf549eef5
François Ponchon <francois.ponchon@michelin.com>
2023-10-30 11:50:25 +0100

    Add Spatial compression Rate

--- examples/community/semantic_image_synthesis/src/models/autoencoder_sis.py
+++ examples/community/semantic_image_synthesis/src/models/autoencoder_sis.py
@@ -1,5 +1,6 @@
 from diffusers import AutoencoderKL
 import torch
+import math
 
 class AutoencoderSIS(AutoencoderKL):
     
@@ -13,34 +14,26 @@ class AutoencoderSIS(AutoencoderKL):
 
     def get_config(
             sample_size:int,
-            latent_size:int,
+            compression:4,
             in_channels:int,
             out_channels:int,
             latent_channels:int=4):
         # Inspired from here : 
         # https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/blob/main/vae/config.json
+        n_blocks_minus_1=math.log(compression)/math.log(2)
+        assert n_blocks_minus_1 == int(n_blocks_minus_1),f"compression should be 2**n {compression} given"
+        n_blocks = int(n_blocks_minus_1)+1
+        down_block_types = ["DownEncoderBlock2D"]*n_blocks
+        up_block_types = ["UpDecoderBlock2D"]*n_blocks
+        block_out_channels = [min(128*(2**i),512) for i in range(n_blocks)]
         SIS_CONFIG_1024 = {
             "sample_size": 1024,
-            "scaling_factor": latent_size/1024,
+            "scaling_factor": 0.18215,
             "in_channels":in_channels,
             "out_channels":out_channels,
-            "down_block_types": [
-                "DownEncoderBlock2D",
-                "DownEncoderBlock2D",
-                "DownEncoderBlock2D",
-                "DownEncoderBlock2D"],
-            "up_block_types": [
-                "UpDecoderBlock2D",
-                "UpDecoderBlock2D",
-                "UpDecoderBlock2D",
-                "UpDecoderBlock2D"
-            ],
-            "block_out_channels": [
-                128,
-                256,
-                512,
-                512
-            ],
+            "down_block_types": down_block_types,
+            "up_block_types": up_block_types,
+            "block_out_channels": block_out_channels,
             "latent_channels":latent_channels,          
             "layers_per_block": 2,
         }
@@ -48,26 +41,12 @@ class AutoencoderSIS(AutoencoderKL):
         # https://huggingface.co/stabilityai/stable-diffusion-2-1/blob/main/vae/config.json
         SIS_CONFIG_768 = {
             "sample_size": 768,
-            "scaling_factor": latent_size/768,
+            "scaling_factor": 0.18215,
             "in_channels":in_channels,
             "out_channels":out_channels,
-            "down_block_types": [
-                "DownEncoderBlock2D",
-                "DownEncoderBlock2D",
-                "DownEncoderBlock2D",
-                "DownEncoderBlock2D"],
-            "up_block_types": [
-                "UpDecoderBlock2D",
-                "UpDecoderBlock2D",
-                "UpDecoderBlock2D",
-                "UpDecoderBlock2D"
-            ],
-            "block_out_channels": [
-                128,
-                256,
-                512,
-                512
-            ],
+            "down_block_types": down_block_types,
+            "up_block_types": up_block_types,
+            "block_out_channels": block_out_channels,
             "latent_channels":latent_channels,          
             "layers_per_block": 2,
         }
@@ -75,73 +54,34 @@ class AutoencoderSIS(AutoencoderKL):
         # https://huggingface.co/CompVis/stable-diffusion-v1-4/blob/main/vae/config.json
         SIS_CONFIG_512 = {
             "sample_size": 512,
-            "scaling_factor": latent_size/512,
+            "scaling_factor": 0.18215,
             "in_channels":in_channels,
             "out_channels":out_channels,
-            "down_block_types": [
-                "DownEncoderBlock2D",
-                "DownEncoderBlock2D",
-                "DownEncoderBlock2D",
-                "DownEncoderBlock2D"],
-            "up_block_types": [
-                "UpDecoderBlock2D",
-                "UpDecoderBlock2D",
-                "UpDecoderBlock2D",
-                "UpDecoderBlock2D"
-            ],
-            "block_out_channels": [
-                128,
-                256,
-                512,
-                512
-            ],
+            "down_block_types": down_block_types,
+            "up_block_types": up_block_types,
+            "block_out_channels": block_out_channels,
             "latent_channels":latent_channels,          
             "layers_per_block": 2,
         }
         SIS_CONFIG_256 = {
             "sample_size": 256,
-            "scaling_factor": latent_size/256,
+            "scaling_factor": 0.18215,
             "in_channels":in_channels,
             "out_channels":out_channels,
-            "down_block_types": [
-                "DownEncoderBlock2D",
-                "DownEncoderBlock2D",
-                "DownEncoderBlock2D",
-                "DownEncoderBlock2D"],
-            "up_block_types": [
-                "UpDecoderBlock2D",
-                "UpDecoderBlock2D",
-                "UpDecoderBlock2D",
-                "UpDecoderBlock2D"
-            ],
-            "block_out_channels": [
-                128,
-                256,
-                512,
-                512
-            ],
+            "down_block_types": down_block_types,
+            "up_block_types": up_block_types,
+            "block_out_channels": block_out_channels,
             "latent_channels":latent_channels,          
             "layers_per_block": 2,
         }
         SIS_CONFIG_128 = {
             "sample_size": 128,
-            "scaling_factor": latent_size/128,
+            "scaling_factor": 0.18215,
             "in_channels":in_channels,
             "out_channels":out_channels,
-            "down_block_types": [
-                "DownEncoderBlock2D",
-                "DownEncoderBlock2D",
-                "DownEncoderBlock2D"],
-            "up_block_types": [
-                "UpDecoderBlock2D",
-                "UpDecoderBlock2D",
-                "UpDecoderBlock2D"
-            ],
-            "block_out_channels": [
-                128,
-                256,
-                512
-            ],
+            "down_block_types": down_block_types,
+            "up_block_types": up_block_types,
+            "block_out_channels": block_out_channels,
             "latent_channels":latent_channels,          
             "layers_per_block": 1,
         }
