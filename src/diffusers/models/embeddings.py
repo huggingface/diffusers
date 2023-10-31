a9e179b1458b05249c5786a794faee0f6c8d4f2d
sayakpaul <spsayakpaul@gmail.com>
2023-10-31 15:33:30 +0530

    debugging'

--- src/diffusers/models/embeddings.py
+++ src/diffusers/models/embeddings.py
@@ -717,11 +717,10 @@ class SizeEmbedder(nn.Module):
             assert size.shape[0] == batch_size
         current_batch_size, dims = size.shape[0], size.shape[1]
         size = size.reshape(-1)
-        print(f"size: {size}")
         size_freq = get_timestep_embedding(size, self.frequency_embedding_size, downscale_freq_shift=0, flip_sin_to_cos=True)
-        print(f"size_freq: {size_freq[0, :5]}")
         
         size_emb = self.mlp(size_freq)
+        print(f"size_emb: {size_emb[0: 3]}")
         size_emb = size_emb.reshape(current_batch_size, dims * self.outdim)
         return size_emb
 
