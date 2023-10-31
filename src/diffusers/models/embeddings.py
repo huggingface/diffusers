163167a6f30b0a8b762e05daa2365eac63928502
sayakpaul <spsayakpaul@gmail.com>
2023-10-31 14:46:41 +0530

    debugging'

--- src/diffusers/models/embeddings.py
+++ src/diffusers/models/embeddings.py
@@ -748,6 +748,7 @@ class CombinedTimestepSizeEmbeddings(nn.Module):
 
         resolution = self.resolution_embedder(resolution, batch_size=batch_size)
         aspect_ratio = self.aspect_ratio_embedder(aspect_ratio, batch_size=batch_size)
+        print(f"Aspect, resolution: {aspect_ratio[0, :3]} {resolution[0, :3]}")
         conditioning = timesteps_emb + torch.cat([resolution, aspect_ratio], dim=1)
         print(f"Final time embedding: {conditioning[0, :3]} {conditioning.dtype}")
 
