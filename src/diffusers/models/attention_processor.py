0278462676dfc0e0b6e25f2c37b6748120009f77
sayakpaul <spsayakpaul@gmail.com>
2023-11-01 12:27:28 +0530

    debugging

--- src/diffusers/models/attention_processor.py
+++ src/diffusers/models/attention_processor.py
@@ -1233,7 +1233,6 @@ class AttnProcessor2_0:
 
         # the output of sdp = (batch, num_heads, seq_len, head_dim)
         # TODO: add support for attn.scale when we move to Torch 2.1
-        print(query.shape, key.shape, value.shape)
         hidden_states = F.scaled_dot_product_attention(
             query, key, value, attn_mask=attention_mask, dropout_p=0.0, is_causal=False
         )
