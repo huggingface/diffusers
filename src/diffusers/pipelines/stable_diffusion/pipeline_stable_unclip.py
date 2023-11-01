151998e1c27d0e4432b3d2c488e1cfce4acfc8f3
clarencechen <clarencechenct@gmail.com>
2023-11-01 13:22:56 -0700

    Update final CPU offloading code for more diffusion pipelines (#5589)
    * Update final model offload for more pipelines
    
    Add test to ensure all pipeline components are returned to CPU after
    execution with model offloading
    
    * Add comment to explain early UNet offload in Text-to-Video pipeline
    
    * Style

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_unclip.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_unclip.py
@@ -942,9 +942,8 @@ class StableUnCLIPPipeline(DiffusionPipeline, TextualInversionLoaderMixin, LoraL
 
         image = self.image_processor.postprocess(image, output_type=output_type)
 
-        # Offload last model to CPU
-        if hasattr(self, "final_offload_hook") and self.final_offload_hook is not None:
-            self.final_offload_hook.offload()
+        # Offload all models
+        self.maybe_free_model_hooks()
 
         if not return_dict:
             return (image,)
