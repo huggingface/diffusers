b848369fdb92db8c5022f321139e6868039d82e3
sayakpaul <spsayakpaul@gmail.com>
2023-11-03 21:29:06 +0530

    boom

--- src/diffusers/models/attention_processor.py
+++ src/diffusers/models/attention_processor.py
@@ -1181,7 +1181,6 @@ class AttnProcessor2_0:
         if attn.spatial_norm is not None:
             hidden_states = attn.spatial_norm(hidden_states, temb)
 
-        print(f"Within self attention: {hidden_states.shape}")
         input_ndim = hidden_states.ndim
 
         if input_ndim == 4:
@@ -1226,15 +1225,12 @@ class AttnProcessor2_0:
 
         # the output of sdp = (batch, num_heads, seq_len, head_dim)
         # TODO: add support for attn.scale when we move to Torch 2.1
-        print(f"query: {query.shape}, key: {key.shape}, value: {value.shape}")
         hidden_states = F.scaled_dot_product_attention(
             query, key, value, attn_mask=attention_mask, dropout_p=0.0, is_causal=False
         )
-        print(f"Within self attention: {hidden_states.shape}")
 
         hidden_states = hidden_states.transpose(1, 2).reshape(batch_size, -1, attn.heads * head_dim)
         hidden_states = hidden_states.to(query.dtype)
-        print(f"Within self attention: {hidden_states.shape}")
 
         # linear proj
         hidden_states = (
@@ -1242,7 +1238,6 @@ class AttnProcessor2_0:
         )
         # dropout
         hidden_states = attn.to_out[1](hidden_states)
-        print(f"Within self attention: {hidden_states.shape}")
 
         if input_ndim == 4:
             hidden_states = hidden_states.transpose(-1, -2).reshape(batch_size, channel, height, width)
@@ -1251,7 +1246,6 @@ class AttnProcessor2_0:
             hidden_states = hidden_states + residual
 
         hidden_states = hidden_states / attn.rescale_output_factor
-        print(f"Within self attention: {hidden_states.shape}")
 
         return hidden_states
 
