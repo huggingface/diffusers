aa73b983d6702420f9bec2d0415825cf92e21dbb
lauraibnz <lauraim1995@gmail.com>
2023-11-08 01:30:43 +0100

    Modify test

--- tests/pipelines/controlnet/test_controlnet_audioldm.py
+++ tests/pipelines/controlnet/test_controlnet_audioldm.py
@@ -154,7 +154,7 @@ class AudioLDMControlNetPipelineFastTests(PipelineTesterMixin, unittest.TestCase
             generator = torch.Generator(device=device).manual_seed(seed)
 
         midi = randn_tensor(
-            (1, 1, 20, 32),
+            (1, 1, 20, 8),
             generator=generator,
             device=torch.device(device),
         )
