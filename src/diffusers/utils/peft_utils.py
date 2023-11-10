163e8e32ce00324f88bd5f4d907de00128fe2c2a
Sourab Mangrulkar <13534540+pacman100@users.noreply.github.com>
2023-11-10 12:08:45 +0530

    Update peft_utils.py

--- src/diffusers/utils/peft_utils.py
+++ src/diffusers/utils/peft_utils.py
@@ -24,54 +24,18 @@ from .import_utils import is_peft_available, is_torch_available
 
 
 def recurse_remove_peft_layers(model):
-    if is_torch_available():
-        import torch
-
     r"""
     Recursively replace all instances of `LoraLayer` with corresponding new layers in `model`.
     """
-    from peft.tuners.lora import LoraLayer
+    from peft.utils import _get_submodules
 
-    for name, module in model.named_children():
-        if len(list(module.children())) > 0:
-            ## compound module, go inside it
-            recurse_remove_peft_layers(module)
-
-        module_replaced = False
-
-        if isinstance(module, LoraLayer) and isinstance(module, torch.nn.Linear):
-            new_module = torch.nn.Linear(module.in_features, module.out_features, bias=module.bias is not None).to(
-                module.weight.device
-            )
-            new_module.weight = module.weight
-            if module.bias is not None:
-                new_module.bias = module.bias
-
-            module_replaced = True
-        elif isinstance(module, LoraLayer) and isinstance(module, torch.nn.Conv2d):
-            new_module = torch.nn.Conv2d(
-                module.in_channels,
-                module.out_channels,
-                module.kernel_size,
-                module.stride,
-                module.padding,
-                module.dilation,
-                module.groups,
-            ).to(module.weight.device)
-
-            new_module.weight = module.weight
-            if module.bias is not None:
-                new_module.bias = module.bias
-
-            module_replaced = True
-
-        if module_replaced:
-            setattr(model, name, new_module)
-            del module
-
-            if torch.cuda.is_available():
-                torch.cuda.empty_cache()
-
+    key_list = [key for key, _ in model.named_modules() if "lora" not in key]
+    for key in key_list:
+        try:
+            parent, target, target_name = _get_submodules(model, key)
+        except AttributeError:
+            continue
+        setattr(parent, target_name, target.get_base_layer())
     return model
 
 
