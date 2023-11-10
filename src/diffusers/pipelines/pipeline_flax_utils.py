286cb1a95418282c1447b248dce40e7b2772edb7
Patrick von Platen <patrick.v.platen@gmail.com>
2023-11-10 18:19:50 +0100

    add tests

--- src/diffusers/pipelines/pipeline_flax_utils.py
+++ src/diffusers/pipelines/pipeline_flax_utils.py
@@ -544,12 +544,6 @@ class FlaxDiffusionPipeline(ConfigMixin, PushToHubMixin):
         optional_parameters = set({k for k, v in parameters.items() if v.default != inspect._empty})
         expected_modules = set(required_parameters.keys()) - {"self"}
 
-        optional_names = list(optional_parameters)
-        for name in optional_names:
-            if name in cls._optional_components:
-                expected_modules.add(name)
-                optional_parameters.remove(name)
-
         return expected_modules, optional_parameters
 
     @property
