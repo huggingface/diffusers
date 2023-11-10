dc7681681ba1e5626033368601494e4edcf49211
sayakpaul <spsayakpaul@gmail.com>
2023-11-10 18:27:06 +0530

    debug

--- src/diffusers/pipelines/pipeline_utils.py
+++ src/diffusers/pipelines/pipeline_utils.py
@@ -683,8 +683,7 @@ class DiffusionPipeline(ConfigMixin, PushToHubMixin):
                 continue
 
             save_method = getattr(sub_model, save_method_name)
-            print(f"save_method: {save_method}")
-
+            
             # Call the save method with the argument safe_serialization only if it's supported
             save_method_signature = inspect.signature(save_method)
             save_method_accept_safe = "safe_serialization" in save_method_signature.parameters
