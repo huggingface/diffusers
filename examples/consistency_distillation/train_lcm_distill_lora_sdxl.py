c7f28284f9a0efaed9a1504c1f5383cf992ea482
sayakpaul <spsayakpaul@gmail.com>
2023-11-13 10:38:28 +0530

    fix microconditions.

--- examples/consistency_distillation/train_lcm_distill_lora_sdxl.py
+++ examples/consistency_distillation/train_lcm_distill_lora_sdxl.py
@@ -1006,27 +1006,23 @@ def main(args):
     )
 
     # 14. Embeddings for the UNet.
+    # Adapted from pipeline.StableDiffusionXLPipeline._get_add_time_ids
     def compute_embeddings(
         prompt_batch, original_sizes, crop_coords, proportion_empty_prompts, text_encoders, tokenizers, is_train=True
     ):
-        target_size = (args.resolution, args.resolution)
-        original_sizes = list(map(list, zip(*original_sizes)))
-        crops_coords_top_left = list(map(list, zip(*crop_coords)))
-
-        original_sizes = torch.tensor(original_sizes, dtype=torch.long)
-        crops_coords_top_left = torch.tensor(crops_coords_top_left, dtype=torch.long)
+        def compute_time_ids(original_size, crops_coords_top_left):
+            target_size = (args.resolution, args.resolution)
+            add_time_ids = list(original_size + crops_coords_top_left + target_size)
+            add_time_ids = torch.tensor([add_time_ids])
+            add_time_ids = add_time_ids.to(accelerator.device, dtype=weight_dtype)
+            return add_time_ids
 
         prompt_embeds, pooled_prompt_embeds = encode_prompt(
             prompt_batch, text_encoders, tokenizers, proportion_empty_prompts, is_train
         )
         add_text_embeds = pooled_prompt_embeds
 
-        # Adapted from pipeline.StableDiffusionXLPipeline._get_add_time_ids
-        add_time_ids = list(target_size)
-        add_time_ids = torch.tensor([add_time_ids])
-        add_time_ids = add_time_ids.repeat(len(prompt_batch), 1)
-        add_time_ids = torch.cat([original_sizes, crops_coords_top_left, add_time_ids], dim=-1)
-        add_time_ids = add_time_ids.to(accelerator.device, dtype=prompt_embeds.dtype)
+        add_time_ids = torch.cat([compute_time_ids(s, c) for s, c in zip(original_sizes, crop_coords)])
 
         prompt_embeds = prompt_embeds.to(accelerator.device)
         add_text_embeds = add_text_embeds.to(accelerator.device)
