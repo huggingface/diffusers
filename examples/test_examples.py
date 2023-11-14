842df25c101e5e8c237cc1b1996bc94ca3bfcb2f
sayakpaul <spsayakpaul@gmail.com>
2023-11-14 13:23:18 +0530

    add test

--- examples/test_examples.py
+++ examples/test_examples.py
@@ -1680,3 +1680,29 @@ class ExamplesTestsAccelerate(unittest.TestCase):
                 k.startswith("unet") or k.startswith("text_encoder") or k.startswith("text_encoder_2") for k in keys
             )
             self.assertTrue(starts_with_unet)
+
+    def test_text_to_image_lcm__lora_sdxl(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            test_args = f"""
+                examples/consistency_distillation/train_lcm_distill_lora_sdxl.py
+                --pretrained_teacher_model hf-internal-testing/tiny-stable-diffusion-xl-pipe
+                --dataset_name hf-internal-testing/dummy_image_text_data
+                --resolution 64
+                --train_batch_size 1
+                --gradient_accumulation_steps 1
+                --max_train_steps 2
+                --learning_rate 5.0e-04
+                --scale_lr
+                --lr_scheduler constant
+                --lr_warmup_steps 0
+                --output_dir {tmpdir}
+                """.split()
+
+            run_command(self._launch_args + test_args)
+            # save_pretrained smoke test
+            self.assertTrue(os.path.isfile(os.path.join(tmpdir, "pytorch_lora_weights.safetensors")))
+
+            # make sure the state_dict has the correct naming in the parameters.
+            lora_state_dict = safetensors.torch.load_file(os.path.join(tmpdir, "pytorch_lora_weights.safetensors"))
+            is_lora = all("lora" in k for k in lora_state_dict.keys())
+            self.assertTrue(is_lora)
