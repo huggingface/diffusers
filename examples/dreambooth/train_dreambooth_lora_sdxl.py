b9f9da610a862b519952b309fa495548cb86bbdb
Linoy Tsaban <linoy.tsaban@gmail.com>
2023-11-19 11:22:24 +0200

    changed dataset loading logic according to the following usecases (to avoid unnecessary dependency on datasets)- 1. user provides --dataset_name 2. user provides local dir --instance_data_dir that contains a metadata .jsonl file 3. user provides local dir --instance_data_dir that contains only images in cases [1,2] we import datasets and use load_dataset method, in case [3] we process the data same as in the original script setting

--- examples/dreambooth/train_dreambooth_lora_sdxl.py
+++ examples/dreambooth/train_dreambooth_lora_sdxl.py
@@ -32,7 +32,6 @@ import transformers
 from accelerate import Accelerator
 from accelerate.logging import get_logger
 from accelerate.utils import DistributedDataParallelKwargs, ProjectConfiguration, set_seed
-from datasets import load_dataset
 from huggingface_hub import create_repo, upload_folder
 from packaging import version
 from PIL import Image
@@ -163,10 +162,18 @@ def parse_args(input_args=None):
         type=str,
         default=None,
         help=(
-            "A folder containing the training data. Folder contents must follow the structure described in"
-            " https://huggingface.co/docs/datasets/image_dataset#imagefolder. In particular, a `metadata.jsonl` file"
-            " must exist to provide the captions for the images. Ignored if `dataset_name` is specified."
+            "A folder containing the training data. "
         ),
+    )
+
+    parser.add_argument(
+        "--instance_data_metadata_file_name",
+        type=str,
+        default="metadata.jsonl",
+        help="file name(relative path) of a jsonl metadata file located in --instance_data_dir. In particular, "
+             "if you wish to use custom captions, a `metadata.jsonl` file must exist to provide captions for the "
+             "images. Ignored if `dataset_name` is specified. see "
+             "https://huggingface.co/docs/datasets/image_dataset#imagefolder for more information"
     )
 
     parser.add_argument(
@@ -550,15 +557,15 @@ class DreamBoothDataset(Dataset):
     """
 
     def __init__(
-        self,
-        instance_data_root,
-        instance_prompt,
-        class_prompt,
-        class_data_root=None,
-        class_num=None,
-        size=1024,
-        repeats=1,
-        center_crop=False,
+            self,
+            instance_data_root,
+            instance_prompt,
+            class_prompt,
+            class_data_root=None,
+            class_num=None,
+            size=1024,
+            repeats=1,
+            center_crop=False,
     ):
         self.size = size
         self.center_crop = center_crop
@@ -569,7 +576,12 @@ class DreamBoothDataset(Dataset):
         self.custom_instance_prompts = None
         self.class_prompt = class_prompt
 
+        load_as_dataset = False
+
+        # if --dataset_name is provided or a metadata jsonl file is provided in the local --instance_data directory,
+        # we load the training data using load_dataset
         if args.dataset_name is not None:
+            from datasets import load_dataset
             # Downloading and loading a dataset from the hub.
             # See more about loading custom images at
             # https://huggingface.co/docs/datasets/v2.0.0/en/dataset_script
@@ -578,45 +590,54 @@ class DreamBoothDataset(Dataset):
                 args.dataset_config_name,
                 cache_dir=args.cache_dir,
             )
+            load_as_dataset = True
         else:
             if not self.instance_data_root.exists():
                 raise ValueError("Instance images root doesn't exists.")
 
-            dataset = load_dataset(
-                instance_data_root,
-                cache_dir=args.cache_dir,
-            )
-
-        # Preprocessing the datasets.
-        column_names = dataset["train"].column_names
-
-        # 6. Get the column names for input/target.
-        if args.image_column is None:
-            image_column = column_names[0]
-            logger.info(f"image column defaulting to {image_column}")
-        else:
-            image_column = args.image_column
-            if image_column not in column_names:
-                raise ValueError(
-                    f"`--image_column` value '{args.image_column}' not found in dataset columns. Dataset columns are: {', '.join(column_names)}"
+            if args.instance_data_metadata_file_name in os.listdir(instance_data_root):
+                from datasets import load_dataset
+                dataset = load_dataset(
+                    instance_data_root,
+                    cache_dir=args.cache_dir,
                 )
-        self.instance_images_path = dataset["train"][image_column]
+                self.load_as_dataset = True
 
-        if args.caption_column is None:
-            logger.info(
-                "No caption column provided, defaulting to instance_prompt for all images. If your dataset "
-                "contains captions/prompts for the images, make sure to specify the "
-                "column as --caption_column"
-            )
+        if load_as_dataset:
+            # Preprocessing the datasets.
+            column_names = dataset["train"].column_names
+
+            # 6. Get the column names for input/target.
+            if args.image_column is None:
+                image_column = column_names[0]
+                logger.info(f"image column defaulting to {image_column}")
+            else:
+                image_column = args.image_column
+                if image_column not in column_names:
+                    raise ValueError(
+                        f"`--image_column` value '{args.image_column}' not found in dataset columns. Dataset columns are: {', '.join(column_names)}"
+                    )
+            self.instance_images = dataset["train"][image_column]
+
+            if args.caption_column is None:
+                logger.info(
+                    "No caption column provided, defaulting to instance_prompt for all images. If your dataset "
+                    "contains captions/prompts for the images, make sure to specify the "
+                    "column as --caption_column"
+                )
+                self.custom_instance_prompts = None
+            else:
+                if args.caption_column not in column_names:
+                    raise ValueError(
+                        f"`--caption_column` value '{args.caption_column}' not found in dataset columns. Dataset columns are: {', '.join(column_names)}"
+                    )
+                self.custom_instance_prompts = dataset["train"][args.caption_column]
+
+        else:
+            self.instance_images = [Image.open(path) for path in list(Path(instance_data_root).iterdir())]
             self.custom_instance_prompts = None
-        else:
-            if args.caption_column not in column_names:
-                raise ValueError(
-                    f"`--caption_column` value '{args.caption_column}' not found in dataset columns. Dataset columns are: {', '.join(column_names)}"
-                )
-            self.custom_instance_prompts = dataset["train"][args.caption_column]
 
-        self.num_instance_images = len(self.instance_images_path)
+        self.num_instance_images = len(self.instance_images)
         self._length = self.num_instance_images * repeats
 
         if class_data_root is not None:
@@ -645,7 +666,7 @@ class DreamBoothDataset(Dataset):
 
     def __getitem__(self, index):
         example = {}
-        instance_image = self.instance_images_path[index % self.num_instance_images]
+        instance_image = self.instance_images[index % self.num_instance_images]
         instance_image = exif_transpose(instance_image)
 
         if not instance_image.mode == "RGB":
