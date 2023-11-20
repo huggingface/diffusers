2695ba8e9d4fa9a51e8041b7ec5681d8955eb9ae
Roy Hvaara <hvaara@google.com>
2023-11-20 03:32:50 -0800

    [JAX] Replace uses of jax.devices("cpu") with jax.local_devices(backend="cpu") (#5864)
    An upcoming change to JAX will include non-local (addressable) CPU devices in jax.devices() when JAX is used multicontroller-style, where there are multiple Python processes.
    
    This change preserves the current behavior by replacing uses of jax.devices("cpu"), which previously only returned local devices, with jax.local_devices("cpu"), which will return local devices both now and in the future.
    
    This change is always safe (i.e., it should always preserve the previous behavior), but it may sometimes be unnecessary if code is never used in a multicontroller setting.
    
    Co-authored-by: Peter Hawkins <phawkins@google.com>

--- src/diffusers/models/modeling_flax_utils.py
+++ src/diffusers/models/modeling_flax_utils.py
@@ -437,7 +437,7 @@ class FlaxModelMixin(PushToHubMixin):
             # make sure all arrays are stored as jnp.ndarray
             # NOTE: This is to prevent a bug this will be fixed in Flax >= v0.3.4:
             # https://github.com/google/flax/issues/1261
-        state = jax.tree_util.tree_map(lambda x: jax.device_put(x, jax.devices("cpu")[0]), state)
+        state = jax.tree_util.tree_map(lambda x: jax.device_put(x, jax.local_devices(backend="cpu")[0]), state)
 
         # flatten dicts
         state = flatten_dict(state)
