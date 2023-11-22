55079df16a8f4a09c9b007657be96e2d06e6828d
estelleafl <estelle.aflalo@intel.com>
2023-11-22 08:59:05 +0200

    Update src/diffusers/image_processor.py
    Co-authored-by: Patrick von Platen <patrick.v.platen@gmail.com>

--- src/diffusers/image_processor.py
+++ src/diffusers/image_processor.py
@@ -451,7 +451,6 @@ class VaeImageProcessorLDM3D(VaeImageProcessor):
         return pil_images
 
     @staticmethod
-
     def depth_pil_to_numpy(images: Union[List[PIL.Image.Image], PIL.Image.Image]) -> np.ndarray:
         """
         Convert a PIL image or a list of PIL images to NumPy arrays.
