d38ae6584aef3ecd0adcaf4c26f321bfe263e1c2
Linoy Tsaban <linoy.tsaban@gmail.com>
2023-11-23 19:39:39 +0200

    readme improvement and metadata fix

--- examples/dreambooth/train_dreambooth_lora_sdxl.py
+++ examples/dreambooth/train_dreambooth_lora_sdxl.py
@@ -127,7 +127,6 @@ tags:
 - template:sd-lora
 widget:
 {img_str}
----
 base_model: {base_model}
 instance_prompt: {instance_prompt}
 license: openrail++
@@ -142,8 +141,11 @@ license: openrail++
 ## Model description
 
 These are {repo_id} LoRA adaption weights for {base_model}.
+
 The weights were trained  using [DreamBooth](https://dreambooth.github.io/).
+
 LoRA for the text encoder was enabled: {train_text_encoder}.
+
 Special VAE used for training: {vae_path}.
 
 ## Trigger words
