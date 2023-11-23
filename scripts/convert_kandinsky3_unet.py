5781f7304a7ad4621d419e44ecda0c8b6c13c5af
Patrick von Platen <patrick.v.platen@gmail.com>
2023-11-23 21:04:31 +0100

    fix more

--- scripts/convert_kandinsky3_unet.py
+++ scripts/convert_kandinsky3_unet.py
@@ -2,14 +2,14 @@
 import torch
 import argparse
 from your_unet_module import UNet  # Replace with your actual import
-from your_diffusers_module import UNetKandi3, YourConfigClass  # Replace with your actual import
+from your_diffusers_module import Kandinsky3UNet, YourConfigClass  # Replace with your actual import
 
 def convert_state_dict(unet_model, unet_kandi3_model):
     """
-    Convert the state dict of a U-Net model to match the key format expected by UNetKandi3 model.
+    Convert the state dict of a U-Net model to match the key format expected by Kandinsky3UNet model.
     Args:
         unet_model (torch.nn.Module): The original U-Net model.
-        unet_kandi3_model (torch.nn.Module): The UNetKandi3 model to match keys with.
+        unet_kandi3_model (torch.nn.Module): The Kandinsky3UNet model to match keys with.
 
     Returns:
         OrderedDict: The converted state dictionary.
@@ -32,9 +32,9 @@ def main(model_path, output_path):
     unet_model = UNet()  # Initialize with appropriate arguments
     unet_model.load_state_dict(torch.load(model_path))
 
-    # Initialize your UNetKandi3 model
+    # Initialize your Kandinsky3UNet model
     config = YourConfigClass()  # Initialize this with the appropriate configuration
-    unet_kandi3_model = UNetKandi3(config)
+    unet_kandi3_model = Kandinsky3UNet(config)
 
     # Convert the state dict
     converted_state_dict = convert_state_dict(unet_model, unet_kandi3_model)
@@ -44,10 +44,9 @@ def main(model_path, output_path):
     print(f"Converted model saved to {output_path}")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Convert U-Net PyTorch model to UNetKandi3 format")
+    parser = argparse.ArgumentParser(description="Convert U-Net PyTorch model to Kandinsky3UNet format")
     parser.add_argument("--model_path", type=str, required=True, help="Path to the original U-Net PyTorch model")
     parser.add_argument("--output_path", type=str, required=True, help="Path to save the converted model")
 
     args = parser.parse_args()
     main(args.model_path, args.output_path)
-
