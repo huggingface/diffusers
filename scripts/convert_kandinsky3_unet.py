22dfa36ebadd043bc1b306e1bca4c50e8675a589
Patrick von Platen <patrick.v.platen@gmail.com>
2023-11-23 21:04:11 +0100

    fix more

--- scripts/convert_kandinsky3_unet.py
+++ scripts/convert_kandinsky3_unet.py
@@ -1,1 +1,53 @@
 #!/usr/bin/env python3
+import torch
+import argparse
+from your_unet_module import UNet  # Replace with your actual import
+from your_diffusers_module import UNetKandi3, YourConfigClass  # Replace with your actual import
+
+def convert_state_dict(unet_model, unet_kandi3_model):
+    """
+    Convert the state dict of a U-Net model to match the key format expected by UNetKandi3 model.
+    Args:
+        unet_model (torch.nn.Module): The original U-Net model.
+        unet_kandi3_model (torch.nn.Module): The UNetKandi3 model to match keys with.
+
+    Returns:
+        OrderedDict: The converted state dictionary.
+    """
+    unet_state_dict = unet_model.state_dict()
+    unet_kandi3_state_dict = unet_kandi3_model.state_dict()
+
+    # Example of renaming logic (this will vary based on your model's architecture)
+    converted_state_dict = {}
+    for key in unet_state_dict:
+        new_key = key  # Logic to rename the key
+        # Example: If original key is 'encoder.conv1.weight', and you want to rename it to 'unet_kandi3.encoder.conv1.weight'
+        # new_key = key.replace('encoder', 'unet_kandi3.encoder')
+        converted_state_dict[new_key] = unet_state_dict[key]
+
+    return converted_state_dict
+
+def main(model_path, output_path):
+    # Load your original U-Net model
+    unet_model = UNet()  # Initialize with appropriate arguments
+    unet_model.load_state_dict(torch.load(model_path))
+
+    # Initialize your UNetKandi3 model
+    config = YourConfigClass()  # Initialize this with the appropriate configuration
+    unet_kandi3_model = UNetKandi3(config)
+
+    # Convert the state dict
+    converted_state_dict = convert_state_dict(unet_model, unet_kandi3_model)
+
+    # Save the converted state dict
+    torch.save(converted_state_dict, output_path)
+    print(f"Converted model saved to {output_path}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Convert U-Net PyTorch model to UNetKandi3 format")
+    parser.add_argument("--model_path", type=str, required=True, help="Path to the original U-Net PyTorch model")
+    parser.add_argument("--output_path", type=str, required=True, help="Path to save the converted model")
+
+    args = parser.parse_args()
+    main(args.model_path, args.output_path)
+
