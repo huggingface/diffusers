21148de8533e450cc52d35076e8916ad8e395a6a
patil-suraj <surajp815@gmail.com>
2023-11-26 12:45:01 +0100

    fix typo

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_video.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_video.py
@@ -492,7 +492,7 @@ class StableDiffusionVideoPipeline(DiffusionPipeline):
                 # perform guidance
                 if do_classifier_free_guidance:
                     noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
-                    noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
+                    noise_pred = noise_pred_uncond + guidance_scales * (noise_pred_text - noise_pred_uncond)
 
                 # compute the previous noisy sample x_t -> x_t-1
                 latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs).prev_sample
