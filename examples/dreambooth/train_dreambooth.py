7bd9881367aa6ed13f5a3de5d4f266ea2b932596
sayakpaul <spsayakpaul@gmail.com>
2023-11-27 10:06:00 +0530

    fix: variant in model_info

--- examples/dreambooth/train_dreambooth.py
+++ examples/dreambooth/train_dreambooth.py
@@ -762,9 +762,7 @@ def model_has_vae(args):
         config_file_name = os.path.join(args.pretrained_model_name_or_path, config_file_name)
         return os.path.isfile(config_file_name)
     else:
-        files_in_repo = model_info(
-            args.pretrained_model_name_or_path, revision=args.revision, variant=args.variant
-        ).siblings
+        files_in_repo = model_info(args.pretrained_model_name_or_path, revision=args.revision).siblings
         return any(file.rfilename == config_file_name for file in files_in_repo)
 
 
