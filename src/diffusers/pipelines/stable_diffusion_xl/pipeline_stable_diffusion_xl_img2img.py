e550163b9f503bfd3c941cbd0d9f31dcdc313429
Patrick von Platen <patrick.v.platen@gmail.com>
2023-11-27 14:17:47 +0100

    [Vae] Make sure all vae's work with latent diffusion models (#5880)
    * add comments to explain the code better
    
    * add comments to explain the code better
    
    * add comments to explain the code better
    
    * add comments to explain the code better
    
    * add comments to explain the code better
    
    * fix more
    
    * fix more
    
    * fix more
    
    * fix more
    
    * fix more
    
    * fix more

--- src/diffusers/pipelines/stable_diffusion_xl/pipeline_stable_diffusion_xl_img2img.py
+++ src/diffusers/pipelines/stable_diffusion_xl/pipeline_stable_diffusion_xl_img2img.py
@@ -105,9 +105,13 @@ def rescale_noise_cfg(noise_cfg, noise_pred_text, guidance_rescale=0.0):
 
 
 # Copied from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion_img2img.retrieve_latents
-def retrieve_latents(encoder_output, generator):
-    if hasattr(encoder_output, "latent_dist"):
+def retrieve_latents(
+    encoder_output: torch.Tensor, generator: Optional[torch.Generator] = None, sample_mode: str = "sample"
+):
+    if hasattr(encoder_output, "latent_dist") and sample_mode == "sample":
         return encoder_output.latent_dist.sample(generator)
+    elif hasattr(encoder_output, "latent_dist") and sample_mode == "argmax":
+        return encoder_output.latent_dist.mode()
     elif hasattr(encoder_output, "latents"):
         return encoder_output.latents
     else:
