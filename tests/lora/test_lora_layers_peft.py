fd7620c9c991a6fe66be5ed1c11e0f6bbb271f69
sayakpaul <spsayakpaul@gmail.com>
2023-11-28 17:35:27 +0530

    change unet init dict.

--- tests/lora/test_lora_layers_peft.py
+++ tests/lora/test_lora_layers_peft.py
@@ -1396,19 +1396,14 @@ class UNet2DConditionModelLoRATests(unittest.TestCase):
     def get_dummy_components(self):
         unet_kwargs = {
             "block_out_channels": (32, 64),
+            "down_block_types": ("CrossAttnDownBlock2D", "DownBlock2D"),
+            "up_block_types": ("UpBlock2D", "CrossAttnUpBlock2D"),
+            "cross_attention_dim": 32,
+            "attention_head_dim": 8,
+            "out_channels": 4,
+            "in_channels": 4,
             "layers_per_block": 2,
             "sample_size": 32,
-            "in_channels": 4,
-            "out_channels": 4,
-            "down_block_types": ("DownBlock2D", "CrossAttnDownBlock2D"),
-            "up_block_types": ("CrossAttnUpBlock2D", "UpBlock2D"),
-            "attention_head_dim": (2, 4),
-            "use_linear_projection": True,
-            "addition_embed_type": "text_time",
-            "addition_time_embed_dim": 8,
-            "transformer_layers_per_block": (1, 2),
-            "projection_class_embeddings_input_dim": 80,  # 6 * 8 + 32
-            "cross_attention_dim": 64,
         }
         torch.manual_seed(0)
         unet = UNet2DConditionModel(**unet_kwargs)
