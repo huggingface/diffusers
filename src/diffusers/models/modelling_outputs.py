381ea5616df056ef965624f8ca124c47f3b348a8
Dhruv Nair <dhruv.nair@gmail.com>
2023-11-28 15:39:09 +0000

    add modelling outputs

---
+++ src/diffusers/models/modelling_outputs.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+
+from ..utils import BaseOutput
+from .vae import DiagonalGaussianDistribution
+
+
+@dataclass
+class AutoencoderKLOutput(BaseOutput):
+    """
+    Output of AutoencoderKL encoding method.
+
+    Args:
+        latent_dist (`DiagonalGaussianDistribution`):
+            Encoded outputs of `Encoder` represented as the mean and logvar of `DiagonalGaussianDistribution`.
+            `DiagonalGaussianDistribution` allows for sampling latents from the distribution.
+    """
+
+    latent_dist: "DiagonalGaussianDistribution"
