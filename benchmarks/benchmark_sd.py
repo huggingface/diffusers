3c05e4179b91a3c88e6b6a3458862c46e8abde31
sayakpaul <spsayakpaul@gmail.com>
2023-11-28 09:28:16 +0530

    output csv files.

--- benchmarks/benchmark_sd.py
+++ benchmarks/benchmark_sd.py
@@ -1,23 +1,29 @@
 import argparse
 import os
+import sys
+
 import torch
+
 from diffusers import DiffusionPipeline
 
-import sys 
 
 sys.path.append(".")
+from benchmark_utils import (  # noqa: E402
+    BASE_PATH,
+    PROMPT,
+    BenchmarkInfo,
+    benchmark_fn,
+    bytes_to_giga_bytes,
+    generate_csv_dict,
+    write_to_csv,
+)
 
-from benchmark_utils import benchmark_fn, bytes_to_giga_bytes, BenchmarkInfo, generate_markdown_table
 
 CKPT = "CompVis/stable-diffusion-v1-4"
-PROMPT = "ghibli style, a fantasy landscape with castles"
-BASE_PATH = "benchmark_outputs"
 
 
 def load_pipeline(run_compile=False):
-    pipe = DiffusionPipeline.from_pretrained(
-        CKPT, torch_dtype=torch.float16, use_safetensors=True
-    )
+    pipe = DiffusionPipeline.from_pretrained(CKPT, torch_dtype=torch.float16, use_safetensors=True)
     pipe = pipe.to("cuda")
 
     if run_compile:
@@ -36,27 +42,30 @@ def run_inference(pipe, args):
         num_images_per_prompt=args.batch_size,
     )
 
-def main(args):
+
+def main(args) -> dict:
     pipeline = load_pipeline(run_compile=args.run_compile)
-    
-    time = benchmark_fn(run_inference, pipeline, args) # in seconds.
-    memory = bytes_to_giga_bytes(torch.cuda.max_memory_allocated()) # in GBs.
+
+    time = benchmark_fn(run_inference, pipeline, args)  # in seconds.
+    memory = bytes_to_giga_bytes(torch.cuda.max_memory_allocated())  # in GBs.
     benchmark_info = BenchmarkInfo(time=time, memory=memory)
-    
-    markdown_report = generate_markdown_table(pipeline_name=CKPT, args=args, benchmark_info=benchmark_info)  
-    return markdown_report
+
+    csv_dict = generate_csv_dict(pipeline=CKPT, args=args, benchmark_info=benchmark_info)
+    return csv_dict
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--batch_size", type=int, default=1)
     parser.add_argument("--num_inference_steps", type=int, default=50)
+    parser.add_argument("--model_cpu_offload", action="store_true")
     parser.add_argument("--run_compile", action="store_true")
     args = parser.parse_args()
-    markdown_report = main(args)
+    csv_dict = main(args)
 
-    name = CKPT.replace("/", "_") + f"-batch_sze@{args.batch_size}-num_inference_steps@{args.num_inference_steps}--run_compile@{args.run_compile}.md"
+    name = (
+        CKPT.replace("/", "_")
+        + f"-bs@{args.batch_size}-steps@{args.num_inference_steps}-mco@{args.model_cpu_offload}-compile@{args.run_compile}.csv"
+    )
     filepath = os.path.join(BASE_PATH, name)
-    with open(filepath, "w") as f:
-        f.write(markdown_report)
-    
-    
+    write_to_csv(filepath, csv_dict)
--- benchmarks/benchmark_utils.py
+++ benchmarks/benchmark_utils.py
@@ -1,46 +1,88 @@
-import gc  
-import torch 
-import torch.utils.benchmark as benchmark
-from dataclasses import dataclass
 import argparse
+import csv
+import gc
+import os
+from dataclasses import dataclass
+from typing import Any, Dict, List
+
+import torch
+import torch.utils.benchmark as benchmark
+
+
+GITHUB_SHA = os.getenv("GITHUB_SHA", None)
+BENCHMARK_FIELDS = [
+    "pipeline",
+    "batch_size",
+    "num_inference_steps",
+    "model_cpu_offload",
+    "run_compile",
+    "time (secs)",
+    "memory (gbs)",
+    "github_sha",
+]
+
+PROMPT = "ghibli style, a fantasy landscape with castles"
+BASE_PATH = "benchmark_outputs"
+
 
 @dataclass
 class BenchmarkInfo:
-    time: float 
+    time: float
     memory: float
 
 
 def flush():
+    """Wipes off memory."""
     gc.collect()
     torch.cuda.empty_cache()
+    torch.cuda.reset_max_memory_allocated()
+    torch.cuda.reset_peak_memory_stats()
+
 
 def bytes_to_giga_bytes(bytes):
     return bytes / 1024 / 1024 / 1024
 
 
-# Adapted from
-# https://pytorch.org/tutorials/intermediate/scaled_dot_product_attention_tutorial.html
 def benchmark_fn(f, *args, **kwargs):
     t0 = benchmark.Timer(
-        stmt="f(*args, **kwargs)", globals={"args": args, "kwargs": kwargs, "f": f}
+        stmt="f(*args, **kwargs)",
+        globals={"args": args, "kwargs": kwargs, "f": f},
+        num_threads=torch.get_num_threads(),
     )
-    return f"{(t0.blocked_autorange().mean):.3f}" 
+    return f"{(t0.blocked_autorange().mean):.3f}"
 
-def generate_markdown_table(pipeline_name: str, args: argparse.Namespace, benchmark_info: BenchmarkInfo) -> str:
-    headers = ["**Parameter**", "**Value**"]
-    data = [
-        ["Batch Size", args.batch_size],
-        ["Number of Inference Steps", args.num_inference_steps],
-        ["Run Compile", args.run_compile],
-        ["Time (seconds)", benchmark_info.time],
-        ["Memory (GBs)", benchmark_info.memory]
-    ]
 
-    # Formatting the table.
-    markdown_table = f"## {pipeline_name}\n\n"
-    markdown_table += "| " + " | ".join(headers) + " |\n"
-    markdown_table += "|-" + "-|-".join(['' for _ in headers]) + "-|\n"
-    for row in data:
-        markdown_table += "| " + " | ".join(str(item) for item in row) + " |\n"
+def generate_csv_dict(pipeline: str, args: argparse.Namespace, benchmark_info: BenchmarkInfo) -> Dict[str, Any]:
+    """Packs benchmarking data into a dictionary for latter serialization."""
+    data_dict = {
+        "pipeline": pipeline,
+        "batch_size": args.batch_size,
+        "num_inference_steps": args.num_inference_steps,
+        "model_cpu_offload": args.model_cpu_offload,
+        "run_compile": args.run_compile,
+        "time (secs)": benchmark_info.time,
+        "memory (gbs)": benchmark_info.memory,
+        "github_sha": GITHUB_SHA,
+    }
+    return data_dict
 
-    return markdown_table
+
+def write_to_csv(file_name: str, data_dict: Dict[str, Any]):
+    """Serializes a dictionary into a CSV file."""
+    with open(file_name, mode="w", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=BENCHMARK_FIELDS)
+        writer.writeheader()
+        writer.writerow(data_dict)
+
+
+def collate_csv(input_files: List[str], output_file: str):
+    """Collates multiple identically structured CSVs into a single CSV file."""
+    with open(output_file, mode="w", newline="") as outfile:
+        writer = csv.DictWriter(outfile, fieldnames=BENCHMARK_FIELDS)
+        writer.writeheader()
+
+        for file in input_files:
+            with open(file, mode="r") as infile:
+                reader = csv.DictReader(infile)
+                for row in reader:
+                    writer.writerow(row)
