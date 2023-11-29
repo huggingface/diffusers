abaa053db1c01b324fa2a023ac49eaa5393261d5
Johnson yue <Johnson-yue@users.noreply.github.com>
2023-11-29 11:37:05 +0800

    reduce a lot of GPU memory for final inference
    delete unuse parameters and models before final inference

--- examples/dreambooth/train_dreambooth_lora_sdxl.py
+++ examples/dreambooth/train_dreambooth_lora_sdxl.py
@@ -1677,6 +1677,14 @@ def main(args):
             text_encoder_2_lora_layers=text_encoder_2_lora_layers,
         )
 
+        # Remove unuse parameters and models to save a lot of GPU memory for Final inference
+        unet = unet.cpu()
+        text_encoder_one = text_encoder_one.cpu()
+        text_encoder_two = text_encoder_two.cpu()
+        del unet,text_encoder_one, text_encoder_two
+        del optimizer
+        torch.cuda.empty_cache()
+        
         # Final inference
         # Load previous pipeline
         vae = AutoencoderKL.from_pretrained(
