bab91cbbb6544b4ad8f5d9974de5ac8f8102bfad
UmerHA <40663591+UmerHA@users.noreply.github.com>
2023-12-04 17:57:05 +0100

    Update src/diffusers/models/controlnetxs.py
    Co-authored-by: Steven Liu <59462357+stevhliu@users.noreply.github.com>

--- src/diffusers/models/controlnetxs.py
+++ src/diffusers/models/controlnetxs.py
@@ -143,7 +143,7 @@ class ControlNetXSModel(ModelMixin, ConfigMixin):
 
         Parameters:
             base_model (`UNet2DConditionModel`):
-                Base unet model. Needs to be either StableDiffusion or StableDiffusion-XL.
+                Base UNet model. Needs to be either StableDiffusion or StableDiffusion-XL.
             is_sdxl (`bool`, defaults to `True`):
                 Whether passed `base_model` is a StableDiffusion-XL model.
         """
