3dc2362b5a89380f66ac006b1a787411fa1a9574
Beinsezii <39478211+Beinsezii@users.noreply.github.com>
2023-12-06 23:51:04 -0800

    EulerDiscreteScheduler add `rescale_betas_zero_snr` (#6024)
    * EulerDiscreteScheduler add `rescale_betas_zero_snr`

--- tests/schedulers/test_scheduler_euler.py
+++ tests/schedulers/test_scheduler_euler.py
@@ -45,6 +45,10 @@ class EulerDiscreteSchedulerTest(SchedulerCommonTest):
     def test_karras_sigmas(self):
         self.check_over_configs(use_karras_sigmas=True, sigma_min=0.02, sigma_max=700.0)
 
+    def test_rescale_betas_zero_snr(self):
+        for rescale_betas_zero_snr in [True, False]:
+            self.check_over_configs(rescale_betas_zero_snr=rescale_betas_zero_snr)
+
     def test_full_loop_no_noise(self):
         scheduler_class = self.scheduler_classes[0]
         scheduler_config = self.get_scheduler_config()
