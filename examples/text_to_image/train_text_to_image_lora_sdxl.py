9d1f39fa39118de01b07f4f777af2ef80338285d
Samir <118690585+sr5434@users.noreply.github.com>
2023-12-10 17:04:22 -0500

    Fix Issue in SDXL LoRA Script
    Address the same [issue](https://discuss.huggingface.co/t/sdxl-finetuning-script-not-working/65267) found in the full fine-tune script, but in the LoRA script.

--- examples/text_to_image/train_text_to_image_lora_sdxl.py
+++ examples/text_to_image/train_text_to_image_lora_sdxl.py
@@ -526,7 +526,9 @@ def encode_prompt(text_encoders, tokenizers, prompt, text_input_ids_list=None):
     return prompt_embeds, pooled_prompt_embeds
 
 
-def main(args):
+def main():
+    args = parse_args()
+    
     logging_dir = Path(args.output_dir, args.logging_dir)
 
     accelerator_project_config = ProjectConfiguration(project_dir=args.output_dir, logging_dir=logging_dir)
@@ -1277,5 +1279,4 @@ def main(args):
 
 
 if __name__ == "__main__":
-    args = parse_args()
-    main(args)
+    main()
