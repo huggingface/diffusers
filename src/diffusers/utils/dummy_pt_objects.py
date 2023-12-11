c4b16b88894640ff2fc8127b60fde70362591e6d
Daniel Gu <dgu8957@gmail.com>
2023-12-11 15:11:38 -0800

    make fix-copies

--- src/diffusers/utils/dummy_pt_objects.py
+++ src/diffusers/utils/dummy_pt_objects.py
@@ -1005,6 +1005,21 @@ class ScoreSdeVeScheduler(metaclass=DummyObject):
         requires_backends(cls, ["torch"])
 
 
+class UFOGenScheduler(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+    @classmethod
+    def from_config(cls, *args, **kwargs):
+        requires_backends(cls, ["torch"])
+
+    @classmethod
+    def from_pretrained(cls, *args, **kwargs):
+        requires_backends(cls, ["torch"])
+
+
 class UnCLIPScheduler(metaclass=DummyObject):
     _backends = ["torch"]
 
