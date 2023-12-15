38ada6a526229540cc623a3916485d7f00d9ee29
sayakpaul <spsayakpaul@gmail.com>
2023-12-15 11:42:13 +0530

    remove prints.

--- src/diffusers/models/attention.py
+++ src/diffusers/models/attention.py
@@ -287,9 +287,6 @@ class BasicTransformerBlock(nn.Module):
         cross_attention_kwargs = cross_attention_kwargs.copy() if cross_attention_kwargs is not None else {}
         gligen_kwargs = cross_attention_kwargs.pop("gligen", None)
 
-        print(
-            f"Encode hidden states from transformer block (self-attention): {encoder_hidden_states if self.only_cross_attention else None}"
-        )
         attn_output = self.attn1(
             norm_hidden_states,
             encoder_hidden_states=encoder_hidden_states if self.only_cross_attention else None,
@@ -325,7 +322,6 @@ class BasicTransformerBlock(nn.Module):
             if self.pos_embed is not None and self.use_ada_layer_norm_single is False:
                 norm_hidden_states = self.pos_embed(norm_hidden_states)
 
-            print(f"Encode hidden states from transformer block (cross-attention): {encoder_hidden_states is None}")
             attn_output = self.attn2(
                 norm_hidden_states,
                 encoder_hidden_states=encoder_hidden_states,
