3297f4b27718f022b3aa3d8af2a7feefc4abd437
Abhinay1997 <devarintinagasaiabhinay@gmail.com>
2023-12-17 20:20:57 +0530

    fix make-copies

--- src/diffusers/pipelines/controlnet/pipeline_unicontrol.py
+++ src/diffusers/pipelines/controlnet/pipeline_unicontrol.py
@@ -456,7 +456,7 @@ class StableDiffusionUniControlPipeline(
 
         if isinstance(self, LoraLoaderMixin) and USE_PEFT_BACKEND:
             # Retrieve the original scale by scaling back the LoRA layers
-            unscale_lora_layers(self.text_encoder)
+            unscale_lora_layers(self.text_encoder, lora_scale)
 
         return prompt_embeds, negative_prompt_embeds
 
