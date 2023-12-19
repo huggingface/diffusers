c3d71a8eddf8754d40c95cb26c58a9dd44951c70
sayakpaul <spsayakpaul@gmail.com>
2023-12-19 07:24:59 +0530

    correct assertion value for test_inference

--- tests/pipelines/pixart/test_pixart.py
+++ tests/pipelines/pixart/test_pixart.py
@@ -186,11 +186,9 @@ class PixArtAlphaPipelineFastTests(PipelineTesterMixin, unittest.TestCase):
         inputs = self.get_dummy_inputs(device)
         image = pipe(**inputs).images
         image_slice = image[0, -3:, -3:, -1]
-        image_slice_str = ", ".join([str(round(x ,4)) for x in image_slice.flatten().tolist()])
-        print(image_slice_str)
         
         self.assertEqual(image.shape, (1, 8, 8, 3))
-        expected_slice = np.array([0.5303, 0.2658, 0.7979, 0.1182, 0.3304, 0.4608, 0.5195, 0.4261, 0.4675])
+        expected_slice = np.array([0.6319, 0.3526, 0.3806, 0.6327, 0.4639, 0.483, 0.2583, 0.5331, 0.4852])
         max_diff = np.abs(image_slice.flatten() - expected_slice).max()
         self.assertLessEqual(max_diff, 1e-3)
 
