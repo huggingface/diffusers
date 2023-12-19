0def66d8b3ae36fc6fb6205f783aa247528bcad4
sayakpaul <spsayakpaul@gmail.com>
2023-12-19 07:26:59 +0530

    run styling

--- tests/pipelines/pixart/test_pixart.py
+++ tests/pipelines/pixart/test_pixart.py
@@ -186,7 +186,7 @@ class PixArtAlphaPipelineFastTests(PipelineTesterMixin, unittest.TestCase):
         inputs = self.get_dummy_inputs(device)
         image = pipe(**inputs).images
         image_slice = image[0, -3:, -3:, -1]
-        
+
         self.assertEqual(image.shape, (1, 8, 8, 3))
         expected_slice = np.array([0.6319, 0.3526, 0.3806, 0.6327, 0.4639, 0.483, 0.2583, 0.5331, 0.4852])
         max_diff = np.abs(image_slice.flatten() - expected_slice).max()
