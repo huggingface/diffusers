cc494fbac06eaaf050645f90f2d4381a27fb511d
sayakpaul <spsayakpaul@gmail.com>
2023-12-19 17:20:14 +0530

    move downsample_2d to downsamplers.py

--- src/diffusers/models/samplers/__init__.py
+++ src/diffusers/models/samplers/__init__.py
@@ -1,3 +1,3 @@
-from .downsamplers import Downsample1D, Downsample2D, FirDownsample2D, KDownsample2D
+from .downsamplers import Downsample1D, Downsample2D, FirDownsample2D, KDownsample2D, downsample_2d
 from .upsamplers import FirUpsample2D, KUpsample2D, Upsample1D, Upsample2D
-from .utils import downsample_2d, upfirdn2d_native, upsample_2d
+from .utils import upfirdn2d_native, upsample_2d
