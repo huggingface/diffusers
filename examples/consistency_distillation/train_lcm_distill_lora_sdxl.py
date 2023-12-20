b7c0f95ff7b86258368b4a380fb83294da15e5b8
sayakpaul <spsayakpaul@gmail.com>
2023-12-20 10:13:12 +0530

    make vae casting conditional/

--- examples/consistency_distillation/train_lcm_distill_lora_sdxl.py
+++ examples/consistency_distillation/train_lcm_distill_lora_sdxl.py
@@ -755,7 +755,10 @@ def main(args):
     # Move unet, vae and text_encoder to device and cast to weight_dtype
     # The VAE is in float32 to avoid NaN losses.
     unet.to(accelerator.device, dtype=weight_dtype)
-    vae.to(accelerator.device, dtype=torch.float32)
+    if args.pretrained_vae_model_name_or_path is None:
+        vae.to(accelerator.device, dtype=torch.float32)
+    else:
+        vae.to(accelerator.device, dtype=weight_dtype)
     text_encoder_one.to(accelerator.device, dtype=weight_dtype)
     text_encoder_two.to(accelerator.device, dtype=weight_dtype)
 
