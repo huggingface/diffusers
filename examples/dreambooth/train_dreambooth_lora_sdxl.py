6338ad5b0bb0cdcee49ece5af7f8f5fde1710627
apolinário <joaopaulo.passos@gmail.com>
2023-12-22 05:55:38 -0600

    Fix ProdigyOPT in SDXL Dreambooth script

--- examples/dreambooth/train_dreambooth_lora_sdxl.py
+++ examples/dreambooth/train_dreambooth_lora_sdxl.py
@@ -1141,13 +1141,29 @@ def main(args):
             import prodigyopt
         except ImportError:
             raise ImportError("To use Prodigy, please install the prodigyopt library: `pip install prodigyopt`")
-
+        
         optimizer_class = prodigyopt.Prodigy
 
+        if args.learning_rate <= 0.1:
+            logger.warn(
+                "Learning rate is too low. When using prodigy, it's generally better to set learning rate around 1.0"
+            )
+        if args.train_text_encoder and args.text_encoder_lr:
+            logger.warn(
+                f"Learning rates were provided both for the unet and the text encoder- e.g. text_encoder_lr:"
+                f" {args.text_encoder_lr} and learning_rate: {args.learning_rate}. "
+                f"When using prodigy only learning_rate is used as the initial learning rate."
+            )
+            # changes the learning rate of text_encoder_parameters_one and text_encoder_parameters_two to be
+            # --learning_rate
+            params_to_optimize[1]["lr"] = args.learning_rate
+            params_to_optimize[2]["lr"] = args.learning_rate
+        
         optimizer = optimizer_class(
             params_to_optimize,
             lr=args.learning_rate,
             betas=(args.adam_beta1, args.adam_beta2),
+            beta3=args.prodigy_beta3,
             weight_decay=args.adam_weight_decay,
             eps=args.adam_epsilon,
             decouple=args.prodigy_decouple,
