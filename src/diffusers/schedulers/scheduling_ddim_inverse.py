08f39953b1bcc7461009e0c4c6c2b5ccfd7d50b9
Justin Ruan <justin900429@gmail.com>
2023-12-25 14:38:48 +0800

    Fixed wrong config instance

--- src/diffusers/schedulers/scheduling_ddim_inverse.py
+++ src/diffusers/schedulers/scheduling_ddim_inverse.py
@@ -329,7 +329,7 @@ class DDIMInverseScheduler(SchedulerMixin, ConfigMixin):
         # 1. get previous step value (=t+1)
         prev_timestep = timestep
         timestep = min(
-            timestep - self.config.num_train_timesteps // self.config.num_inference_steps, self.num_train_timesteps - 1
+            timestep - self.config.num_train_timesteps // self.num_inference_steps, self.config.num_train_timesteps - 1
         )
 
         # 2. compute alphas, betas
