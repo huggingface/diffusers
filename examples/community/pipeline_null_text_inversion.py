3529275ecb92ca13e0a39d112f75b5125a0328b8
junsheng121 <l.junsheng121@gmail.com>
2023-12-26 20:27:32 +0800

    edited

--- examples/community/pipeline_null_text_inversion.py
+++ examples/community/pipeline_null_text_inversion.py
@@ -1,5 +1,5 @@
+from diffusers import StableDiffusionPipeline
 from diffusers.pipelines.stable_diffusion import StableDiffusionPipelineOutput
-from diffusers import StableDiffusionPipeline, DDIMScheduler
 from tqdm import tqdm
 from PIL import Image
 import inspect
@@ -8,7 +8,6 @@ import os
 import torch.nn.functional as nnf
 import torch
 from torch.optim.adam import Adam
-
 def retrieve_timesteps(
     scheduler,
     num_inference_steps = None,
@@ -19,7 +18,6 @@ def retrieve_timesteps(
     """
     Calls the scheduler's `set_timesteps` method and retrieves timesteps from the scheduler after the call. Handles
     custom timesteps. Any kwargs will be supplied to `scheduler.set_timesteps`.
-
     Args:
         scheduler (`SchedulerMixin`):
             The scheduler to get timesteps from.
@@ -51,10 +49,7 @@ def retrieve_timesteps(
         scheduler.set_timesteps(num_inference_steps, device=device, **kwargs)
         timesteps = scheduler.timesteps
     return timesteps, num_inference_steps
-
-
 class NullTextPipeline(StableDiffusionPipeline):
-
     def get_noise_pred(self, latents, t, context):
         latents_input = torch.cat([latents] * 2)
         guidance_scale = 7.5
@@ -63,11 +58,9 @@ class NullTextPipeline(StableDiffusionPipeline):
         noise_pred = noise_pred_uncond + guidance_scale * (noise_prediction_text - noise_pred_uncond)
         latents = self.prev_step(noise_pred, t, latents)
         return latents
-    
     def get_noise_pred_single(self, latents, t, context):
         noise_pred = self.unet(latents, t, encoder_hidden_states=context)["sample"]
         return noise_pred
-    
     @torch.no_grad()
     def image2latent(self, image_path):
         image = Image.open(image_path).convert("RGB")
@@ -77,14 +70,12 @@ class NullTextPipeline(StableDiffusionPipeline):
         latents = self.vae.encode(image)['latent_dist'].mean
         latents = latents * 0.18215
         return latents
-    
     @torch.no_grad()
     def latent2image(self, latents):
         latents = 1 / 0.18215 * latents.detach()
         image = self.vae.decode(latents)['sample'].detach()
         image = self.processor.postprocess(image,output_type='pil')[0]
         return image
-    
     def prev_step(self, model_output, timestep, sample):
         prev_timestep = timestep - self.scheduler.config.num_train_timesteps // self.scheduler.num_inference_steps
         alpha_prod_t = self.scheduler.alphas_cumprod[timestep]
@@ -94,7 +85,6 @@ class NullTextPipeline(StableDiffusionPipeline):
         pred_sample_direction = (1 - alpha_prod_t_prev) ** 0.5 * model_output
         prev_sample = alpha_prod_t_prev ** 0.5 * pred_original_sample + pred_sample_direction
         return prev_sample
-
     def next_step(self, model_output, timestep, sample):
         timestep, next_timestep = min(timestep - self.scheduler.config.num_train_timesteps // self.num_inference_steps, 999), timestep
         alpha_prod_t = self.scheduler.alphas_cumprod[timestep] if timestep >= 0 else self.scheduler.final_alpha_cumprod
@@ -104,7 +94,6 @@ class NullTextPipeline(StableDiffusionPipeline):
         next_sample_direction = (1 - alpha_prod_t_next) ** 0.5 * model_output  
         next_sample =  alpha_prod_t_next ** 0.5 * next_original_sample + next_sample_direction
         return next_sample
-
     def null_optimization(self, latents, context, num_inner_steps, epsilon):
         uncond_embeddings, cond_embeddings = context.chunk(2)
         uncond_embeddings_list = []
@@ -138,7 +127,6 @@ class NullTextPipeline(StableDiffusionPipeline):
                 latent_cur = self.get_noise_pred(latent_cur, t, context)
         bar.close()
         return uncond_embeddings_list
-    
     @torch.no_grad()
     def ddim_inversion_loop(self, latent, context):
         self.scheduler.set_timesteps(self.num_inference_steps)
@@ -153,7 +141,6 @@ class NullTextPipeline(StableDiffusionPipeline):
                 latent = self.next_step(noise_pred, t, latent)
                 all_latent.append(latent)
         return all_latent
-
     def get_context(self, prompt):
         uncond_input = self.tokenizer([""], padding="max_length", max_length=self.tokenizer.model_max_length,return_tensors="pt")
         uncond_embeddings = self.text_encoder(uncond_input.input_ids.to(self.device))[0]
@@ -167,7 +154,6 @@ class NullTextPipeline(StableDiffusionPipeline):
         text_embeddings = self.text_encoder(text_input.input_ids.to(self.device))[0]
         context = torch.cat([uncond_embeddings, text_embeddings])
         return context
-
     def invert(self, image_path: str, prompt: str, num_inner_steps=10, early_stop_epsilon=1e-6, num_inference_steps = 50):
         self.num_inference_steps = num_inference_steps
         context = self.get_context(prompt)
@@ -179,8 +165,7 @@ class NullTextPipeline(StableDiffusionPipeline):
             uncond_embeddings = self.null_optimization(ddim_latents, context, num_inner_steps, early_stop_epsilon)
             uncond_embeddings = torch.stack(uncond_embeddings, 0)
             torch.save(uncond_embeddings, image_path+".pt")
-        return ddim_latents[-1], uncond_embeddings
-            
+        return ddim_latents[-1], uncond_embeddings         
     @torch.no_grad()
     def __call__(
         self,
@@ -198,13 +183,11 @@ class NullTextPipeline(StableDiffusionPipeline):
         negative_prompt_embeds = None,
         output_type = "pil",
     ):
-
         self._guidance_scale = guidance_scale
         # 0. Default height and width to unet
         height = self.unet.config.sample_size * self.vae_scale_factor
         width = self.unet.config.sample_size * self.vae_scale_factor
-        # to deal with lora scaling and other possible forward hooks
-
+        # to deal with lora scaling and other possible forward hook
         callback_steps = None
         # 1. Check inputs. Raise error if not correct
         self.check_inputs(
@@ -216,11 +199,8 @@ class NullTextPipeline(StableDiffusionPipeline):
             prompt_embeds,
             negative_prompt_embeds,
         )
-
-        # 2. Define call parameters
-
+        # 2. Define call parameter
         device = self._execution_device
-
         # 3. Encode input prompt
         prompt_embeds, _ = self.encode_prompt(
             prompt,
@@ -234,18 +214,15 @@ class NullTextPipeline(StableDiffusionPipeline):
         )
         # 4. Prepare timesteps
         timesteps, num_inference_steps = retrieve_timesteps(self.scheduler, num_inference_steps, device, timesteps)
-
         latents = inverted_latent
         with self.progress_bar(total=num_inference_steps) as progress_bar:
             for i, t in enumerate(timesteps):
-
                 noise_pred_uncond = self.unet(latents, t, encoder_hidden_states=uncond_embeddings[i])["sample"]
                 noise_pred = self.unet(latents, t, encoder_hidden_states=prompt_embeds)["sample"]
                 noise_pred = noise_pred_uncond + guidance_scale * (noise_pred - noise_pred_uncond)
                 # compute the previous noisy sample x_t -> x_t-1
                 latents = self.scheduler.step(noise_pred, t, latents, return_dict=False)[0]
                 progress_bar.update()
-
         if not output_type == "latent":
             image = self.vae.decode(latents / self.vae.config.scaling_factor, return_dict=False, generator=generator)[
                 0
@@ -255,7 +232,6 @@ class NullTextPipeline(StableDiffusionPipeline):
         image = self.image_processor.postprocess(image, output_type=output_type, do_denormalize=[True] * image.shape[0])
         # Offload all models
         self.maybe_free_model_hooks()
-
         return StableDiffusionPipelineOutput(images=image, nsfw_content_detected=False)
 
 
