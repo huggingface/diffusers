8f7e72f186b12e1d2814cedcb3d5c75d04c6ae36
junsheng121 <l.junsheng121@gmail.com>
2023-12-26 20:50:25 +0800

    edited

--- examples/community/pipeline_null_text_inversion.py
+++ examples/community/pipeline_null_text_inversion.py
@@ -1,13 +1,17 @@
 import inspect
 import os
+
 import numpy as np
 import torch
 import torch.nn.functional as nnf
 from PIL import Image
 from torch.optim.adam import Adam
 from tqdm import tqdm
+
 from diffusers import StableDiffusionPipeline
 from diffusers.pipelines.stable_diffusion import StableDiffusionPipelineOutput
+
+
 def retrieve_timesteps(
     scheduler,
     num_inference_steps = None,
