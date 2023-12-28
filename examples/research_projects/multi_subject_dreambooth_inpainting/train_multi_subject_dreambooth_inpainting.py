cc9de1cd12cb182b10c6e2ac6b8c580dc4c989f3
gzguevara <erich.ganz@hotmail.com>
2023-12-28 22:12:15 +0000

    ruff on script

--- examples/research_projects/multi_subject_dreambooth_inpainting/train_multi_subject_dreambooth_inpainting.py
+++ examples/research_projects/multi_subject_dreambooth_inpainting/train_multi_subject_dreambooth_inpainting.py
@@ -39,6 +39,7 @@ check_min_version("0.13.0.dev0")
 
 logger = get_logger(__name__)
 
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Simple example of a training script.")
     parser.add_argument(
@@ -48,23 +49,14 @@ def parse_args():
         required=True,
         help="Path to pretrained model or model identifier from huggingface.co/models.",
     )
-    parser.add_argument(
-        '--instance_data_dir',
-        nargs='+',
-        help='Instance data directories'
-    )
+    parser.add_argument("--instance_data_dir", nargs="+", help="Instance data directories")
     parser.add_argument(
         "--output_dir",
         type=str,
         default="text-inversion-model",
         help="The output directory where the model predictions and checkpoints will be written.",
     )
-    parser.add_argument(
-        "--seed",
-        type=int,
-        default=None,
-        help="A seed for reproducible training."
-    )
+    parser.add_argument("--seed", type=int, default=None, help="A seed for reproducible training.")
     parser.add_argument(
         "--resolution",
         type=int,
@@ -75,22 +67,13 @@ def parse_args():
         ),
     )
     parser.add_argument(
-        "--train_text_encoder",
-        default=False,
-        action="store_true",
-        help="Whether to train the text encoder"
+        "--train_text_encoder", default=False, action="store_true", help="Whether to train the text encoder"
     )
     parser.add_argument(
-        "--train_batch_size",
-        type=int,
-        default=4,
-        help="Batch size (per device) for the training dataloader."
+        "--train_batch_size", type=int, default=4, help="Batch size (per device) for the training dataloader."
     )
     parser.add_argument(
-        "--sample_batch_size",
-        type=int,
-        default=4,
-        help="Batch size (per device) for sampling images."
+        "--sample_batch_size", type=int, default=4, help="Batch size (per device) for sampling images."
     )
     parser.add_argument(
         "--max_train_steps",
@@ -126,41 +109,13 @@ def parse_args():
         ),
     )
     parser.add_argument(
-        "--lr_warmup_steps",
-        type=int,
-        default=500,
-        help="Number of steps for the warmup in the lr scheduler."
+        "--lr_warmup_steps", type=int, default=500, help="Number of steps for the warmup in the lr scheduler."
     )
-    parser.add_argument(
-        "--adam_beta1",
-        type=float,
-        default=0.9,
-        help="The beta1 parameter for the Adam optimizer."
-    )
-    parser.add_argument(
-        "--adam_beta2",
-        type=float,
-        default=0.999,
-        help="The beta2 parameter for the Adam optimizer."
-    )
-    parser.add_argument(
-        "--adam_weight_decay",
-        type=float,
-        default=1e-2,
-        help="Weight decay to use."
-    )
-    parser.add_argument(
-        "--adam_epsilon",
-        type=float,
-        default=1e-08,
-        help="Epsilon value for the Adam optimizer"
-    )
-    parser.add_argument(
-        "--max_grad_norm",
-        default=1.0,
-        type=float,
-        help="Max gradient norm."
-    )
+    parser.add_argument("--adam_beta1", type=float, default=0.9, help="The beta1 parameter for the Adam optimizer.")
+    parser.add_argument("--adam_beta2", type=float, default=0.999, help="The beta2 parameter for the Adam optimizer.")
+    parser.add_argument("--adam_weight_decay", type=float, default=1e-2, help="Weight decay to use.")
+    parser.add_argument("--adam_epsilon", type=float, default=1e-08, help="Epsilon value for the Adam optimizer")
+    parser.add_argument("--max_grad_norm", default=1.0, type=float, help="Max gradient norm.")
     parser.add_argument(
         "--logging_dir",
         type=str,
@@ -195,9 +150,7 @@ def parse_args():
         "--checkpointing_from",
         type=int,
         default=1000,
-        help=(
-            "Start to checkpoint from step"
-        ),
+        help=("Start to checkpoint from step"),
     )
     parser.add_argument(
         "--validation_steps",
@@ -213,9 +166,7 @@ def parse_args():
         "--validation_from",
         type=int,
         default=0,
-        help=(
-            "Start to validate from step"
-        ),
+        help=("Start to validate from step"),
     )
     parser.add_argument(
         "--checkpoints_total_limit",
@@ -243,15 +194,13 @@ def parse_args():
         help="The w&b name.",
     )
     parser.add_argument(
-        "--report_to_wandb",
-        default=False,
-        action="store_true",
-        help="Whether to report to weights and biases"
+        "--report_to_wandb", default=False, action="store_true", help="Whether to report to weights and biases"
     )
 
     args = parser.parse_args()
 
     return args
+
 
 def prepare_mask_and_masked_image(image, mask):
     image = np.array(image.convert("RGB"))
@@ -269,19 +218,18 @@ def prepare_mask_and_masked_image(image, mask):
 
     return mask, masked_image
 
-class DreamBoothDataset(Dataset):
 
+class DreamBoothDataset(Dataset):
     def __init__(
         self,
         tokenizer,
         datasets_paths,
     ):
-
-        self.tokenizer      = tokenizer
-        self.datasets_paths = datasets_paths,
-        self.datasets       = [load_dataset(dataset_path) for dataset_path in self.datasets_paths[0]]
-        self.train_data     = concatenate_datasets([dataset['train'] for dataset in self.datasets])
-        self.test_data      = concatenate_datasets([dataset['test'] for dataset in self.datasets])
+        self.tokenizer = tokenizer
+        self.datasets_paths = (datasets_paths,)
+        self.datasets = [load_dataset(dataset_path) for dataset_path in self.datasets_paths[0]]
+        self.train_data = concatenate_datasets([dataset["train"] for dataset in self.datasets])
+        self.test_data = concatenate_datasets([dataset["test"] for dataset in self.datasets])
 
         self.image_normalize = transforms.Compose(
             [
@@ -291,7 +239,6 @@ class DreamBoothDataset(Dataset):
         )
 
     def set_image(self, img, switch):
-
         if img.mode not in ["RGB", "L"]:
             img = img.convert("RGB")
 
@@ -306,45 +253,46 @@ class DreamBoothDataset(Dataset):
         return len(self.train_data)
 
     def __getitem__(self, index):
-
         # Lettings
         example = {}
         img_idx = index % len(self.train_data)
-        switch  = random.choice([True, False])
+        switch = random.choice([True, False])
 
         # Load image
-        image = self.set_image(self.train_data[img_idx]['image'], switch)
+        image = self.set_image(self.train_data[img_idx]["image"], switch)
 
         # Normalize image
         image_norm = self.image_normalize(image)
 
         # Tokenise prompt
         tokenized_prompt = self.tokenizer(
-            self.train_data[img_idx]['prompt'],
+            self.train_data[img_idx]["prompt"],
             padding="do_not_pad",
             truncation=True,
             max_length=self.tokenizer.model_max_length,
-            ).input_ids
+        ).input_ids
 
         # Load masks for image
-        masks = [self.set_image(self.train_data[img_idx][key], switch) for key in self.train_data[img_idx] if 'mask' in key]
+        masks = [
+            self.set_image(self.train_data[img_idx][key], switch) for key in self.train_data[img_idx] if "mask" in key
+        ]
 
         # Build example
-        example["PIL_image"]          = image
-        example["instance_image"]     = image_norm
+        example["PIL_image"] = image
+        example["instance_image"] = image_norm
         example["instance_prompt_id"] = tokenized_prompt
-        example["instance_masks"]     = masks
+        example["instance_masks"] = masks
 
         return example
 
-def weighted_mask(masks):
 
+def weighted_mask(masks):
     # Convert each mask to a NumPy array and ensure it's binary
     mask_arrays = [np.array(mask) / 255 for mask in masks]  # Normalizing to 0-1 range
 
     # Generate random weights and apply them to each mask
-    weights        = [random.random() for _ in masks]
-    weights        = [weight/sum(weights) for weight in weights]
+    weights = [random.random() for _ in masks]
+    weights = [weight / sum(weights) for weight in weights]
     weighted_masks = [mask * weight for mask, weight in zip(mask_arrays, weights)]
 
     # Sum the weighted masks
@@ -357,15 +305,14 @@ def weighted_mask(masks):
     # Convert the result back to a PIL image
     return Image.fromarray(result_mask.astype(np.uint8) * 255)
 
-def collate_fn(examples, tokenizer):
 
-    input_ids    = [example["instance_prompt_id"] for example in examples]
+def collate_fn(examples, tokenizer):
+    input_ids = [example["instance_prompt_id"] for example in examples]
     pixel_values = [example["instance_image"] for example in examples]
 
     masks, masked_images = [], []
 
     for example in examples:
-
         # generate a random mask
         mask = weighted_mask(example["instance_masks"])
 
@@ -375,51 +322,50 @@ def collate_fn(examples, tokenizer):
         masks.append(mask)
         masked_images.append(masked_image)
 
-    pixel_values  = torch.stack(pixel_values).to(memory_format=torch.contiguous_format).float()
-    masks         = torch.stack(masks)
+    pixel_values = torch.stack(pixel_values).to(memory_format=torch.contiguous_format).float()
+    masks = torch.stack(masks)
     masked_images = torch.stack(masked_images)
-    input_ids     = tokenizer.pad({"input_ids": input_ids}, padding=True, return_tensors="pt").input_ids
+    input_ids = tokenizer.pad({"input_ids": input_ids}, padding=True, return_tensors="pt").input_ids
 
     batch = {"input_ids": input_ids, "pixel_values": pixel_values, "masks": masks, "masked_images": masked_images}
 
     return batch
 
-def log_validation(pipeline, text_encoder, unet, val_pairs, accelerator):
 
+def log_validation(pipeline, text_encoder, unet, val_pairs, accelerator):
     # update pipeline (note: unet and vae are loaded again in float32)
     pipeline.text_encoder = accelerator.unwrap_model(text_encoder)
-    pipeline.unet         = accelerator.unwrap_model(unet)
+    pipeline.unet = accelerator.unwrap_model(unet)
 
     with torch.autocast("cuda"):
-
-        val_results = [{'data_or_path':pipeline(**pair).images[0], 'caption':pair['prompt']} for pair in val_pairs]
+        val_results = [{"data_or_path": pipeline(**pair).images[0], "caption": pair["prompt"]} for pair in val_pairs]
 
     torch.cuda.empty_cache()
 
     wandb.log({"validation": [wandb.Image(**val_result) for val_result in val_results]})
 
-def checkpoint(args, global_step, accelerator):
 
+def checkpoint(args, global_step, accelerator):
     save_path = os.path.join(args.output_dir, f"checkpoint-{global_step}")
     accelerator.save_state(save_path)
     logger.info(f"Saved state to {save_path}")
 
-def main():
 
+def main():
     args = parse_args()
 
     project_config = ProjectConfiguration(
         total_limit=args.checkpoints_total_limit,
         project_dir=args.output_dir,
-        logging_dir=Path(args.output_dir, args.logging_dir)
-        )
+        logging_dir=Path(args.output_dir, args.logging_dir),
+    )
 
     accelerator = Accelerator(
         gradient_accumulation_steps=args.gradient_accumulation_steps,
         mixed_precision=args.mixed_precision,
         project_config=project_config,
-        log_with= 'wandb' if args.report_to_wandb else None,
-        )
+        log_with="wandb" if args.report_to_wandb else None,
+    )
 
     if args.report_to_wandb and not is_wandb_available():
         raise ImportError("Make sure to install wandb if you want to use it for logging during training.")
@@ -436,40 +382,41 @@ def main():
     logger.info(accelerator.state, main_process_only=False)
 
     # Load the tokenizer & models and create wrapper for stable diffusion
-    tokenizer    = CLIPTokenizer.from_pretrained(args.pretrained_model_name_or_path, subfolder="tokenizer")
-    text_encoder = CLIPTextModel.from_pretrained(args.pretrained_model_name_or_path, subfolder="text_encoder").requires_grad_(args.train_text_encoder)
-    vae          = AutoencoderKL.from_pretrained(args.pretrained_model_name_or_path, subfolder="vae").requires_grad_(False)
-    unet         = UNet2DConditionModel.from_pretrained(args.pretrained_model_name_or_path, subfolder="unet")
+    tokenizer = CLIPTokenizer.from_pretrained(args.pretrained_model_name_or_path, subfolder="tokenizer")
+    text_encoder = CLIPTextModel.from_pretrained(
+        args.pretrained_model_name_or_path, subfolder="text_encoder"
+    ).requires_grad_(args.train_text_encoder)
+    vae = AutoencoderKL.from_pretrained(args.pretrained_model_name_or_path, subfolder="vae").requires_grad_(False)
+    unet = UNet2DConditionModel.from_pretrained(args.pretrained_model_name_or_path, subfolder="unet")
 
     if args.scale_lr:
         args.learning_rate = (
             args.learning_rate * args.gradient_accumulation_steps * args.train_batch_size * accelerator.num_processes
-            )
+        )
 
     optimizer = torch.optim.AdamW(
-        params       = itertools.chain(unet.parameters(), text_encoder.parameters()) if args.train_text_encoder else unet.parameters(),
-        lr           = args.learning_rate,
-        betas        = (args.adam_beta1, args.adam_beta2),
-        weight_decay = args.adam_weight_decay,
-        eps          = args.adam_epsilon,
-        )
+        params=itertools.chain(unet.parameters(), text_encoder.parameters())
+        if args.train_text_encoder
+        else unet.parameters(),
+        lr=args.learning_rate,
+        betas=(args.adam_beta1, args.adam_beta2),
+        weight_decay=args.adam_weight_decay,
+        eps=args.adam_epsilon,
+    )
 
-    noise_scheduler = DDPMScheduler.from_pretrained(
-        args.pretrained_model_name_or_path,
-        subfolder="scheduler"
-        )
+    noise_scheduler = DDPMScheduler.from_pretrained(args.pretrained_model_name_or_path, subfolder="scheduler")
 
     train_dataset = DreamBoothDataset(
-        tokenizer = tokenizer,
-        datasets_paths = args.instance_data_dir,
-        )
+        tokenizer=tokenizer,
+        datasets_paths=args.instance_data_dir,
+    )
 
     train_dataloader = torch.utils.data.DataLoader(
         train_dataset,
         batch_size=args.train_batch_size,
         shuffle=True,
-        collate_fn=lambda examples: collate_fn(examples, tokenizer)
-        )
+        collate_fn=lambda examples: collate_fn(examples, tokenizer),
+    )
 
     # Scheduler and math around the number of training steps.
     num_update_steps_per_epoch = math.ceil(len(train_dataloader) / args.gradient_accumulation_steps)
@@ -515,32 +462,31 @@ def main():
     # We need to initialize the trackers we use, and also store our configuration.
     # The trackers initializes automatically on the main process.
     if accelerator.is_main_process:
-
         tracker_config = vars(copy.deepcopy(args))
         accelerator.init_trackers(args.validation_project_name, config=tracker_config)
 
     # create validation pipeline (note: unet and vae are loaded again in float32)
     val_pipeline = StableDiffusionInpaintPipeline.from_pretrained(
         args.pretrained_model_name_or_path,
-        tokenizer      = tokenizer,
-        text_encoder   = text_encoder,
-        unet           = unet,
-        vae            = vae,
-        torch_dtype    = weight_dtype,
-        safety_checker = None,
-        )
+        tokenizer=tokenizer,
+        text_encoder=text_encoder,
+        unet=unet,
+        vae=vae,
+        torch_dtype=weight_dtype,
+        safety_checker=None,
+    )
     val_pipeline.set_progress_bar_config(disable=True)
 
     # prepare validation dataset
     val_pairs = [
         {
-            'image':       example['image'],
-            'mask_image':  mask,
-            'prompt':      example['prompt'],
+            "image": example["image"],
+            "mask_image": mask,
+            "prompt": example["prompt"],
         }
         for example in train_dataset.test_data
-        for mask in [example[key] for key in example if 'mask' in key]
-        ]
+        for mask in [example[key] for key in example if "mask" in key]
+    ]
 
     # create custom saving & loading hooks so that `accelerator.save_state(...)` serializes in a nice format
     def save_model_hook(models, weights, output_dir):
@@ -608,13 +554,14 @@ def main():
                 continue
 
             with accelerator.accumulate(unet):
-
                 # Convert images to latent space
                 latents = vae.encode(batch["pixel_values"].to(dtype=weight_dtype)).latent_dist.sample()
                 latents = latents * vae.config.scaling_factor
 
                 # Convert masked images to latent space
-                masked_latents = vae.encode(batch["masked_images"].reshape(batch["pixel_values"].shape).to(dtype=weight_dtype)).latent_dist.sample()
+                masked_latents = vae.encode(
+                    batch["masked_images"].reshape(batch["pixel_values"].shape).to(dtype=weight_dtype)
+                ).latent_dist.sample()
                 masked_latents = masked_latents * vae.config.scaling_factor
 
                 masks = batch["masks"]
@@ -663,7 +610,7 @@ def main():
                         itertools.chain(unet.parameters(), text_encoder.parameters())
                         if args.train_text_encoder
                         else unet.parameters()
-                        )
+                    )
                     accelerator.clip_grad_norm_(params_to_clip, args.max_grad_norm)
 
                 optimizer.step()
@@ -676,22 +623,25 @@ def main():
                 global_step += 1
 
                 if accelerator.is_main_process:
-
-                    if global_step % args.validation_steps == 0 and global_step >= args.validation_from and args.report_to_wandb:
+                    if (
+                        global_step % args.validation_steps == 0
+                        and global_step >= args.validation_from
+                        and args.report_to_wandb
+                    ):
                         log_validation(
                             val_pipeline,
                             text_encoder,
                             unet,
                             val_pairs,
                             accelerator,
-                            )
+                        )
 
                     if global_step % args.checkpointing_steps == 0 and global_step >= args.checkpointing_from:
                         checkpoint(
                             args,
                             global_step,
                             accelerator,
-                            )
+                        )
 
             # Step logging
             logs = {"loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0]}
@@ -703,8 +653,9 @@ def main():
 
         accelerator.wait_for_everyone()
 
-    #Terminate training
+    # Terminate training
     accelerator.end_training()
+
 
 if __name__ == "__main__":
     main()
