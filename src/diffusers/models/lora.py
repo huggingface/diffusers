d15af62ad05a73f06e237202d82aae989e4be70f
sayakpaul <spsayakpaul@gmail.com>
2023-12-28 11:40:09 +0530

    debug nan

--- src/diffusers/models/lora.py
+++ src/diffusers/models/lora.py
@@ -308,6 +308,7 @@ class LoRACompatibleConv(nn.Conv2d):
         w_orig = self.weight.data.float()
         w_up = self.lora_layer.up.weight.data.float()
         w_down = self.lora_layer.down.weight.data.float()
+        print(w_down[0, :3], torch.isnan(w_down).any().item())
 
         if self.lora_layer.network_alpha is not None:
             w_up = w_up * self.lora_layer.network_alpha / self.lora_layer.rank
