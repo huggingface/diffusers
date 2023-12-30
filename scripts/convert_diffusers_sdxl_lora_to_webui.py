7b958cbf00661a2aa6429d1379c8a4bde5d4ab28
multimodalart <joaopaulo.passos+multimodal@gmail.com>
2023-12-30 08:52:12 -0300

    Move some conversion logic to utils

--- scripts/convert_diffusers_sdxl_lora_to_webui.py
+++ scripts/convert_diffusers_sdxl_lora_to_webui.py
@@ -1,52 +1,22 @@
-# Script for converting a HF Diffusers trained SDXL LoRAs (be it in the old, new or PEFT format)
-# To the Kohya format used by some WebUIs such as AUTOMATIC1111, ComfyUI, SD.Next and others.
+# Script for converting a Hugging Face Diffusers trained SDXL LoRAs to Kohya format
+# This means that you can input your diffusers-trained LoRAs and
+# Get the output to work with WebUIs such as AUTOMATIC1111, ComfyUI, SD.Next and others.
+
+# Here are some diffusers training scripts and utils that you can use and then convert:
+# LoRA Ease - no code SDXL Dreambooth LoRA trainer: https://huggingface.co/spaces/multimodalart/lora-ease
+# Dreambooth Advanced Training Script - state of the art techniques such as pivotal tuning and prodigy optimizer:
+# - Script: https://github.com/huggingface/diffusers/blob/main/examples/advanced_diffusion_training/train_dreambooth_lora_sdxl_advanced.py
+# - Colab (only on Pro): https://colab.research.google.com/github/huggingface/notebooks/blob/main/diffusers/SDXL_Dreambooth_LoRA_advanced_example.ipynb
+# Canonical diffusers training scripts:
+# - Script: https://github.com/huggingface/diffusers/blob/main/examples/dreambooth/train_dreambooth_lora_sdxl.py
+# - Colab (runs on free tier): https://colab.research.google.com/github/huggingface/notebooks/blob/main/diffusers/SDXL_DreamBooth_LoRA_.ipynb
 
 import argparse
 import os
 
-import torch
 from safetensors.torch import load_file, save_file
 
-from diffusers.utils import convert_state_dict_to_peft, convert_unet_state_dict_to_peft
-
-
-def convert_all_diffusers_to_peft(diffusers_dict):
-    try:
-        peft_dict = convert_state_dict_to_peft(diffusers_dict)
-    except Exception as e:
-        if str(e) == "Could not automatically infer state dict type":
-            peft_dict = convert_unet_state_dict_to_peft(diffusers_dict)
-        else:
-            raise
-
-    if not any("lora_A" in key or "lora_B" in key for key in peft_dict.keys()):
-        raise ValueError(
-            "Your LoRA could not be converted to PEFT. Hence, it could not be converted to Kohya/AUTOMATIC1111 format"
-        )
-
-    return peft_dict
-
-
-def convert_peft_to_kohya(state_dict):
-    kohya_ss_state_dict = {}
-    for peft_key, weight in state_dict.items():
-        if "text_encoder_2." in peft_key:
-            kohya_key = peft_key.replace("text_encoder_2.", "lora_te2.")
-        elif "text_encoder." in peft_key:
-            kohya_key = peft_key.replace("text_encoder.", "lora_te1.")
-        elif "unet" in peft_key:
-            kohya_key = peft_key.replace("unet", "lora_unet")
-        kohya_key = kohya_key.replace("lora_A", "lora_down")
-        kohya_key = kohya_key.replace("lora_B", "lora_up")
-        kohya_key = kohya_key.replace(".", "_", kohya_key.count(".") - 2)
-        kohya_ss_state_dict[kohya_key] = weight
-
-        # Set alpha parameter
-        if "lora_down" in kohya_key:
-            alpha_key = f'{kohya_key.split(".")[0]}.alpha'
-            kohya_ss_state_dict[alpha_key] = torch.tensor(len(weight))
-
-    return kohya_ss_state_dict
+from diffusers.utils import convert_all_state_dict_to_peft, convert_state_dict_to_kohya
 
 
 def convert_and_save(input_lora, output_lora=None):
@@ -55,8 +25,8 @@ def convert_and_save(input_lora, output_lora=None):
         output_lora = f"{base_name}_webui.safetensors"
 
     diffusers_state_dict = load_file(input_lora)
-    peft_state_dict = convert_all_diffusers_to_peft(diffusers_state_dict)
-    kohya_state_dict = convert_peft_to_kohya(peft_state_dict)
+    peft_state_dict = convert_all_state_dict_to_peft(diffusers_state_dict)
+    kohya_state_dict = convert_state_dict_to_kohya(peft_state_dict)
     save_file(kohya_state_dict, output_lora)
 
 
