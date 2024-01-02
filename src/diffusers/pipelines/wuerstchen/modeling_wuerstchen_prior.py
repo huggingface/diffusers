0f6a9a2435cc0f38a5e3a12c75602dd57788979c
sayakpaul <spsayakpaul@gmail.com>
2024-01-02 18:53:35 +0530

    handle rest of the stuff related to deprecated lora stuff.

--- src/diffusers/pipelines/wuerstchen/modeling_wuerstchen_prior.py
+++ src/diffusers/pipelines/wuerstchen/modeling_wuerstchen_prior.py
@@ -117,9 +117,9 @@ class WuerstchenPrior(ModelMixin, ConfigMixin, UNet2DConditionLoadersMixin):
         def fn_recursive_attn_processor(name: str, module: torch.nn.Module, processor):
             if hasattr(module, "set_processor"):
                 if not isinstance(processor, dict):
-                    module.set_processor(processor, _remove_lora=_remove_lora)
+                    module.set_processor(processor)
                 else:
-                    module.set_processor(processor.pop(f"{name}.processor"), _remove_lora=_remove_lora)
+                    module.set_processor(processor.pop(f"{name}.processor"))
 
             for sub_name, child in module.named_children():
                 fn_recursive_attn_processor(f"{name}.{sub_name}", child, processor)
