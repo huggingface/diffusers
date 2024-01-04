e2a90d27cc2f2faabdcb0621cb45ea5a8babbcfa
sayakpaul <spsayakpaul@gmail.com>
2024-01-04 18:12:07 +0530

    fix model_max_length

--- examples/research_projects/diffusion_dpo/train_diffusion_dpo_sdxl.py
+++ examples/research_projects/diffusion_dpo/train_diffusion_dpo_sdxl.py
@@ -446,10 +446,10 @@ def tokenize_captions(tokenizers, examples):
         captions.append(caption)
 
     tokens_one = tokenizers[0](
-        captions, truncation=True, padding="max_length", max_length=tokenizers[0].max_length, return_tensors="pt"
+        captions, truncation=True, padding="max_length", max_length=tokenizers[0].model_max_length, return_tensors="pt"
     ).input_ids
     tokens_two = tokenizers[1](
-        captions, truncation=True, padding="max_length", max_length=tokenizers[1].max_length, return_tensors="pt"
+        captions, truncation=True, padding="max_length", max_length=tokenizers[1].model_max_length, return_tensors="pt"
     ).input_ids
 
     return tokens_one, tokens_two
