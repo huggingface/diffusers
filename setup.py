dfc18f71de6941dc1b990b4d8eb70b209bda7d7d
Lucain <lucainp@gmail.com>
2024-01-05 15:34:40 +0100

    Fix offline mode import (#6467)

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ _deps = [
     "filelock",
     "flax>=0.4.1",
     "hf-doc-builder>=0.3.0",
-    "huggingface-hub>=0.19.4",
+    "huggingface-hub>=0.20.2",
     "requests-mock==1.10.0",
     "importlib_metadata",
     "invisible-watermark>=0.2.0",
