ae060fc4f1ba8b9b9a7de35888138415808bfcd6
Sayak Paul <spsayakpaul@gmail.com>
2024-01-05 16:22:11 +0530

    [feat] introduce `unload_lora()`. (#6451)
    * introduce unload_lora.
    
    * fix-copies

--- src/diffusers/pipelines/deprecated/versatile_diffusion/modeling_text_unet.py
+++ src/diffusers/pipelines/deprecated/versatile_diffusion/modeling_text_unet.py
@@ -1034,6 +1034,17 @@ class UNetFlatConditionModel(ModelMixin, ConfigMixin):
         if self.original_attn_processors is not None:
             self.set_attn_processor(self.original_attn_processors)
 
+    def unload_lora(self):
+        """Unloads LoRA weights."""
+        deprecate(
+            "unload_lora",
+            "0.28.0",
+            "Calling `unload_lora()` is deprecated and will be removed in a future version. Please install `peft` and then call `disable_adapters().",
+        )
+        for module in self.modules():
+            if hasattr(module, "set_lora_layer"):
+                module.set_lora_layer(None)
+
     def forward(
         self,
         sample: torch.FloatTensor,
