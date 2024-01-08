c7996f4d8dbbf1d07d42061bd4d043cf77de4795
sayakpaul <spsayakpaul@gmail.com>
2024-01-08 11:37:12 +0530

    fix?

--- src/diffusers/pipelines/stable_video_diffusion/pipeline_stable_video_diffusion.py
+++ src/diffusers/pipelines/stable_video_diffusion/pipeline_stable_video_diffusion.py
@@ -431,6 +431,7 @@ class StableVideoDiffusionPipeline(DiffusionPipeline):
         # 4. Encode input image using VAE
         image = self.image_processor.preprocess(image, height=height, width=width)
         noise = randn_tensor(image.shape, generator=generator, device=self.vae.device, dtype=image.dtype)
+        print(noise.device, image.device)
         image = image + noise_aug_strength * noise
 
         needs_upcasting = self.vae.dtype == torch.float16 and self.vae.config.force_upcast
