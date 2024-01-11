2692721496e65847fddc5b3af0dd0a96ce584a0e
Daniel Gu <dgu8957@gmail.com>
2024-01-11 10:40:36 -0800

    Add update_ema function back to SD-XL LCM-LoRA WDS script.

--- examples/consistency_distillation/train_lcm_distill_lora_sdxl_wds.py
+++ examples/consistency_distillation/train_lcm_distill_lora_sdxl_wds.py
@@ -422,6 +422,20 @@ class DDIMSolver:
         return x_prev
 
 
+@torch.no_grad()
+def update_ema(target_params, source_params, rate=0.99):
+    """
+    Update target parameters to be closer to those of source parameters using
+    an exponential moving average.
+
+    :param target_params: the target parameter sequence.
+    :param source_params: the source parameter sequence.
+    :param rate: the EMA rate (closer to 1 means slower).
+    """
+    for targ, src in zip(target_params, source_params):
+        targ.detach().mul_(rate).add_(src, alpha=1 - rate)
+
+
 def import_model_class_from_model_name_or_path(
     pretrained_model_name_or_path: str, revision: str, subfolder: str = "text_encoder"
 ):
