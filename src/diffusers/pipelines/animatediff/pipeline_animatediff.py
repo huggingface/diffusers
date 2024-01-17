ed37cae9d83443ae1676559ae049a975a6d7f03a
a-r-r-o-w <contact.aryanvs@gmail.com>
2024-01-17 19:05:28 +0530

    fix merge

--- src/diffusers/pipelines/animatediff/pipeline_animatediff.py
+++ src/diffusers/pipelines/animatediff/pipeline_animatediff.py
@@ -14,7 +14,6 @@
 
 import inspect
 import math
-from dataclasses import dataclass
 from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 
 import torch
@@ -36,7 +35,6 @@ from ...schedulers import (
 )
 from ...utils import (
     USE_PEFT_BACKEND,
-    BaseOutput,
     deprecate,
     logging,
     replace_example_docstring,
@@ -145,11 +143,6 @@ def _freq_mix_3d(x: torch.Tensor, noise: torch.Tensor, LPF: torch.Tensor) -> tor
     x_mixed = fft.ifftn(x_freq_mixed, dim=(-3, -2, -1)).real
 
     return x_mixed
-
-
-@dataclass
-class AnimateDiffPipelineOutput(BaseOutput):
-    frames: Union[torch.Tensor, np.ndarray]
 
 
 class AnimateDiffPipeline(DiffusionPipeline, TextualInversionLoaderMixin, IPAdapterMixin, LoraLoaderMixin):
