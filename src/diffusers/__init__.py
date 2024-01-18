9bec7753b99e92b9f5fc74dfe73dbad2429711f0
sayakpaul <spsayakpaul@gmail.com>
2024-01-18 20:04:19 +0530

    fix flax unet2dcondition model import

--- src/diffusers/__init__.py
+++ src/diffusers/__init__.py
@@ -381,7 +381,7 @@ except OptionalDependencyNotAvailable:
 else:
     _import_structure["models.controlnet_flax"] = ["FlaxControlNetModel"]
     _import_structure["models.modeling_flax_utils"] = ["FlaxModelMixin"]
-    _import_structure["models.unet_2d_condition_flax"] = ["FlaxUNet2DConditionModel"]
+    _import_structure["models.unets.unet_2d_condition_flax"] = ["FlaxUNet2DConditionModel"]
     _import_structure["models.vae_flax"] = ["FlaxAutoencoderKL"]
     _import_structure["pipelines"].extend(["FlaxDiffusionPipeline"])
     _import_structure["schedulers"].extend(
