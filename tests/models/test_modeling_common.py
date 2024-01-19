e9290c9ceb6db55b1d40cd0d18069d4e86785055
sayakpaul <spsayakpaul@gmail.com>
2024-01-19 10:49:28 +0530

    add disk offload test without safetensors.

--- tests/models/test_modeling_common.py
+++ tests/models/test_modeling_common.py
@@ -708,7 +708,38 @@ class ModelTesterMixin:
                 self.assertTrue(torch.allclose(base_output[0], new_output[0], atol=1e-5))
 
     @require_torch_gpu
-    def test_disk_offload(self):
+    def test_disk_offload_without_safetensors(self):
+        config, inputs_dict = self.prepare_init_args_and_inputs_for_common()
+        model = self.model_class(**config).eval()
+        model = model.to(torch_device)
+
+        torch.manual_seed(0)
+        base_output = model(**inputs_dict)
+
+        model_size = compute_module_sizes(model)[""]
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            model.cpu().save_pretrained(tmp_dir, safe_serialization=False)
+
+            with self.assertRaises(ValueError):
+                max_size = int(self.model_split_percents[1] * model_size)
+                max_memory = {0: max_size, "cpu": max_size}
+                # This errors out because it's missing an offload folder
+                new_model = self.model_class.from_pretrained(tmp_dir, device_map="auto", max_memory=max_memory)
+
+            max_size = int(self.model_split_percents[1] * model_size)
+            max_memory = {0: max_size, "cpu": max_size}
+            new_model = self.model_class.from_pretrained(
+                tmp_dir, device_map="auto", max_memory=max_memory, offload_folder=tmp_dir
+            )
+
+            self.check_device_map_is_respected(new_model, new_model.hf_device_map)
+            torch.manual_seed(0)
+            new_output = new_model(**inputs_dict)
+
+            self.assertTrue(torch.allclose(base_output[0], new_output[0], atol=1e-5))
+
+    @require_torch_gpu
+    def test_disk_offload_with_safetensors(self):
         config, inputs_dict = self.prepare_init_args_and_inputs_for_common()
         model = self.model_class(**config).eval()
         model = model.to(torch_device)
@@ -722,7 +753,7 @@ class ModelTesterMixin:
 
             max_size = int(self.model_split_percents[1] * model_size)
             max_memory = {0: max_size, "cpu": max_size}
-
+            # This doesn't error out as it's in safetensors and doesn't need an offload folder
             new_model = self.model_class.from_pretrained(tmp_dir, device_map="auto", max_memory=max_memory)
 
             self.check_device_map_is_respected(new_model, new_model.hf_device_map)
