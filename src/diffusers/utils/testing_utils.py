26fd4af2e8d2522e79d7376a84a31bfdd00489c8
sayakpaul <spsayakpaul@gmail.com>
2024-01-19 12:07:18 +0530

    add: utility for checking multi-gpu requirement.

--- src/diffusers/utils/testing_utils.py
+++ src/diffusers/utils/testing_utils.py
@@ -333,6 +333,21 @@ def require_python39_or_higher(test_case):
     return unittest.skipUnless(python39_available(), "test requires Python 3.9 or higher")(test_case)
 
 
+def require_torch_multi_gpu(test_case):
+    """
+    Decorator marking a test that requires a multi-GPU setup (in PyTorch). These tests are skipped on a machine without
+    multiple GPUs.
+
+    To run *only* the multi_gpu tests, assuming all test names contain multi_gpu: $ pytest -sv ./tests -k "multi_gpu"
+    """
+    if not is_torch_available():
+        return unittest.skip("test requires PyTorch")(test_case)
+
+    import torch
+
+    return unittest.skipUnless(torch.cuda.device_count() > 1, "test requires multiple GPUs")(test_case)
+
+
 def load_numpy(arry: Union[str, np.ndarray], local_path: Optional[str] = None) -> np.ndarray:
     if isinstance(arry, str):
         if local_path is not None:
