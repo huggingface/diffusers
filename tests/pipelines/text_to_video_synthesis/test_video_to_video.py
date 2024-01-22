341f48da0d43ec6d75d1f29658e3f278c38dbcfd
Dhruv Nair <dhruv.nair@gmail.com>
2024-01-22 11:46:36 +0000

    clean up

--- tests/pipelines/text_to_video_synthesis/test_video_to_video.py
+++ tests/pipelines/text_to_video_synthesis/test_video_to_video.py
@@ -33,7 +33,6 @@ from diffusers.utils.testing_utils import (
     is_flaky,
     nightly,
     numpy_cosine_similarity_distance,
-    print_tensor_test,
     skip_mps,
     torch_device,
 )
@@ -218,6 +217,8 @@ class VideoToVideoSDPipelineSlowTests(unittest.TestCase):
         generator = torch.Generator(device="cpu").manual_seed(0)
         video_frames = pipe(prompt, video=video, generator=generator, num_inference_steps=3, output_type="np").frames
 
-        expected_array = np.array([0.17114258, 0.13720703, 0.08886719, 0.14819336, 0.1730957, 0.24584961, 0.22021484, 0.35180664, 0.2607422])
+        expected_array = np.array(
+            [0.17114258, 0.13720703, 0.08886719, 0.14819336, 0.1730957, 0.24584961, 0.22021484, 0.35180664, 0.2607422]
+        )
         output_array = video_frames[0, 0, :3, :3, 0].flatten()
         assert numpy_cosine_similarity_distance(expected_array, output_array) < 1e-3
