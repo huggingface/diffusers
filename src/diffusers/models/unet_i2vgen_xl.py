fix: masking

src/diffusers/models/unet_i2vgen_xl.py
@@ -683,8 +683,8 @@ class I2VGenXLUNet(ModelMixin, ConfigMixin, UNet2DConditionLoadersMixin):
             # This one needs to be turned into something humans can understand.
             mask_pos = torch.cat(
                 [
-                    (torch.ones_like(image_latents[:, :, :1]) * ((tpos + 1) / (fps - 1))).to(image_latents.device)
-                    for tpos in range(fps - 1)
+                    (torch.ones_like(image_latents[:, :, :1]) * ((tpos + 1) / (fps[0] - 1))).to(image_latents.device)
+                    for tpos in range(fps[0] - 1)
                 ],
                 dim=2,
             ).to(dtype=image_latents.dtype)