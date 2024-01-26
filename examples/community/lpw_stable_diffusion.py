b7f45f4e3d1f385bb26eec19f3126a72c8f655c9
calmiAI <xinyu_std@163.com>
2024-01-27 00:37:54 +0800

    Fix indentation

--- examples/community/lpw_stable_diffusion.py
+++ examples/community/lpw_stable_diffusion.py
@@ -793,7 +793,7 @@ class StableDiffusionLongPromptWeightingPipeline(
             init_timestep = min(int(num_inference_steps * strength), num_inference_steps)
 
             t_start = max(num_inference_steps - init_timestep, 0)
-            timesteps = self.scheduler.timesteps[t_start * self.scheduler.order:]
+            timesteps = self.scheduler.timesteps[t_start * self.scheduler.order :]
 
             return timesteps, num_inference_steps - t_start
 
