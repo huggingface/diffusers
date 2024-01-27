389adaa292ebbd336a40172c16fff52510bccbfc
a-r-r-o-w <contact.aryanvs@gmail.com>
2024-01-27 07:08:35 +0530

    update example

--- src/diffusers/pipelines/animatediff/pipeline_animatediff_sdxl.py
+++ src/diffusers/pipelines/animatediff/pipeline_animatediff_sdxl.py
@@ -66,47 +66,46 @@ logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
 EXAMPLE_DOC_STRING = """
     Examples:
         ```py
-        import torch
-        from diffusers.models import MotionAdapter
-        from diffusers import AnimateDiffSDXLPipeline, DDIMScheduler
-        from diffusers.utils import export_to_gif
+        >>> import torch
+        >>> from diffusers.models import MotionAdapter
+        >>> from diffusers import AnimateDiffSDXLPipeline, DDIMScheduler
+        >>> from diffusers.utils import export_to_gif
 
-        # TODO(aryan): ask team to move checkpoint to authors account
-        adapter = MotionAdapter.from_pretrained("a-r-r-o-w/animatediff-motion-adapter-sdxl-beta", torch_dtype=torch.float16)
+        >>> adapter = MotionAdapter.from_pretrained("a-r-r-o-w/animatediff-motion-adapter-sdxl-beta", torch_dtype=torch.float16)
 
-        model_id = "stabilityai/stable-diffusion-xl-base-1.0"
-        scheduler = DDIMScheduler.from_pretrained(
-            model_id,
-            subfolder="scheduler",
-            clip_sample=False,
-            timestep_spacing="linspace",
-            beta_schedule="linear",
-            steps_offset=1,
-        )
-        pipe = AnimateDiffSDXLPipeline.from_pretrained(
-            model_id,
-            motion_adapter=adapter,
-            scheduler=scheduler,
-            torch_dtype=torch.float16,
-            variant="fp16",
-        ).to("cuda")
+        >>> model_id = "stabilityai/stable-diffusion-xl-base-1.0"
+        >>> scheduler = DDIMScheduler.from_pretrained(
+        ...     model_id,
+        ...     subfolder="scheduler",
+        ...     clip_sample=False,
+        ...     timestep_spacing="linspace",
+        ...     beta_schedule="linear",
+        ...     steps_offset=1,
+        >>> )
+        >>> pipe = AnimateDiffSDXLPipeline.from_pretrained(
+        ...     model_id,
+        ...     motion_adapter=adapter,
+        ...     scheduler=scheduler,
+        ...     torch_dtype=torch.float16,
+        ...     variant="fp16",
+        >>> ).to("cuda")
 
-        # enable memory savings
-        pipe.enable_vae_slicing()
-        pipe.enable_vae_tiling()
+        >>> # enable memory savings
+        >>> pipe.enable_vae_slicing()
+        >>> pipe.enable_vae_tiling()
 
-        output = pipe(
-            prompt="a panda surfing in the ocean, realistic, high quality",
-            negative_prompt="low quality, worst quality",
-            num_inference_steps=20,
-            guidance_scale=8,
-            width=1024,
-            height=1024,
-            num_frames=16,
-        )
+        >>> output = pipe(
+        ...     prompt="a panda surfing in the ocean, realistic, high quality",
+        ...     negative_prompt="low quality, worst quality",
+        ...     num_inference_steps=20,
+        ...     guidance_scale=8,
+        ...     width=1024,
+        ...     height=1024,
+        ...     num_frames=16,
+        >>> )
 
-        frames = output.frames[0]
-        export_to_gif(frames, "animation.gif")
+        >>> frames = output.frames[0]
+        >>> export_to_gif(frames, "animation.gif")
         ```
 """
 
