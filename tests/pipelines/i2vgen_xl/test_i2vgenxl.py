7fad5851396436597016f5a3384f0ae167f71d73
sayakpaul <spsayakpaul@gmail.com>
2024-01-30 11:35:18 +0530

    fix a bit more

--- tests/pipelines/i2vgen_xl/test_i2vgenxl.py
+++ tests/pipelines/i2vgen_xl/test_i2vgenxl.py
@@ -207,11 +207,9 @@ class I2VGenPipelineFastTests(PipelineTesterMixin, unittest.TestCase):
         frames = pipe(**inputs, num_videos_per_prompt=2).frames
 
         assert frames.shape == (2, 4, 32, 32, 3)
+        assert frames[0][0].shape == (32, 32, 3)
 
         image_slice = frames[0][0][-3:, -3:, -1]
-        print_tensor_test(image_slice)
-
-        assert frames[0][0].shape == (32, 32, 3)
         expected_slice = np.array([0.5146, 0.6525, 0.6032, 0.5204, 0.5675, 0.4125, 0.3016, 0.5172, 0.4095])
 
         assert np.abs(image_slice.flatten() - expected_slice).max() < 1e-2
