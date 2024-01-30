8c20445d536af9bc79073db24d8c907269ac78f1
sayakpaul <spsayakpaul@gmail.com>
2024-01-30 11:32:05 +0530

    fix: test_num_videos_per_prompt

--- tests/pipelines/i2vgen_xl/test_i2vgenxl.py
+++ tests/pipelines/i2vgen_xl/test_i2vgenxl.py
@@ -207,9 +207,9 @@ class I2VGenPipelineFastTests(PipelineTesterMixin, unittest.TestCase):
 
         print(frames.shape)
 
-        # image_slice = frames[0][0][-3:, -3:, -1]
+        image_slice = frames[0][0][-3:, -3:, -1]
 
-        # assert frames[0][0].shape == (32, 32, 3)
+        assert frames[0][0].shape == (32, 32, 3)
         # expected_slice = np.array([0.5146, 0.6525, 0.6032, 0.5204, 0.5675, 0.4125, 0.3016, 0.5172, 0.4095])
 
         # assert np.abs(image_slice.flatten() - expected_slice).max() < 1e-2
