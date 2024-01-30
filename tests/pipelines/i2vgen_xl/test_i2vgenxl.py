76a42b38a40c2279e3facb79f0585e028963e01c
sayakpaul <spsayakpaul@gmail.com>
2024-01-30 12:17:42 +0530

    finish slow test fixes

--- tests/pipelines/i2vgen_xl/test_i2vgenxl.py
+++ tests/pipelines/i2vgen_xl/test_i2vgenxl.py
@@ -179,11 +179,14 @@ class I2VGenXLPipelineFastTests(PipelineTesterMixin, unittest.TestCase):
     def test_save_load_local(self):
         super().test_save_load_local(expected_max_difference=0.006)
 
+    def test_sequential_cpu_offload_forward_pass(self):
+        super().test_sequential_cpu_offload_forward_pass(expected_max_diff=0.008)
+
     def test_dict_tuple_outputs_equivalent(self):
         super().test_dict_tuple_outputs_equivalent(expected_max_difference=0.008)
 
     def test_save_load_optional_components(self):
-        super().test_save_load_optional_components(expected_max_difference=0.004)
+        super().test_save_load_optional_components(expected_max_difference=0.008)
 
     @unittest.skip("Deprecated functionality")
     def test_attention_slicing_forward_pass(self):
@@ -198,6 +201,9 @@ class I2VGenXLPipelineFastTests(PipelineTesterMixin, unittest.TestCase):
 
     def test_inference_batch_single_identical(self):
         super().test_inference_batch_single_identical(batch_size=2, expected_max_diff=0.008)
+
+    def test_model_cpu_offload_forward_pass(self):
+        super().test_model_cpu_offload_forward_pass(expected_max_diff=0.008)
 
     def test_num_videos_per_prompt(self):
         device = "cpu"  # ensure determinism for the device-dependent torch.Generator
