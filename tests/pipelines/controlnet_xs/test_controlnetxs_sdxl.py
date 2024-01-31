0be0d6fa128be84f6835f801e7f8a9bcb9a41b1f
UmerHA <40663591+UmerHA@users.noreply.github.com>
2024-01-31 14:42:49 +0100

    Fixed test_save_load_float16

--- tests/pipelines/controlnet_xs/test_controlnetxs_sdxl.py
+++ tests/pipelines/controlnet_xs/test_controlnetxs_sdxl.py
@@ -425,6 +425,54 @@ class StableDiffusionXLControlNetXSPipelineFastTests(
         max_diff = np.abs(to_np(output) - to_np(output_loaded)).max()
         self.assertLess(max_diff, expected_max_difference)
 
+    # copied from test_controlnetxs.py
+    @unittest.skipIf(torch_device != "cuda", reason="float16 requires CUDA")
+    def test_save_load_float16(self, expected_max_diff=1e-2):
+        components = self.get_dummy_components()
+        for name, module in components.items():
+            if hasattr(module, "half"):
+                components[name] = module.to(torch_device).half()
+
+        pipe = self.pipeline_class(**components)
+        for component in pipe.components.values():
+            if hasattr(component, "set_default_attn_processor"):
+                component.set_default_attn_processor()
+        pipe.to(torch_device)
+        pipe.set_progress_bar_config(disable=None)
+
+        inputs = self.get_dummy_inputs(torch_device)
+        output = pipe(**inputs)[0]
+
+        with tempfile.TemporaryDirectory() as tmpdir_components:
+            with tempfile.TemporaryDirectory() as tmpdir_addon:
+                pipe.save_pretrained(
+                    base_path=tmpdir_components,
+                    addon_path=tmpdir_addon,
+                    base_kwargs={"safe_serialization": False},
+                    addon_kwargs={"safe_serialization": False},
+                )
+
+                pipe_loaded = self.pipeline_class.from_pretrained(base_path=tmpdir_components, addon_path=tmpdir_addon)
+                for component in pipe_loaded.components.values():
+                    if hasattr(component, "set_default_attn_processor"):
+                        component.set_default_attn_processor()
+                pipe_loaded.to(torch_device)
+                pipe_loaded.set_progress_bar_config(disable=None)
+
+        for name, component in pipe_loaded.components.items():
+            if hasattr(component, "dtype"):
+                self.assertTrue(
+                    component.dtype == torch.float16,
+                    f"`{name}.dtype` switched from `float16` to {component.dtype} after loading.",
+                )
+
+        inputs = self.get_dummy_inputs(torch_device)
+        output_loaded = pipe_loaded(**inputs)[0]
+        max_diff = np.abs(to_np(output) - to_np(output_loaded)).max()
+        self.assertLess(
+            max_diff, expected_max_diff, "The output of the fp16 pipeline changed after saving and loading."
+        )
+
 
 @slow
 @require_torch_gpu
