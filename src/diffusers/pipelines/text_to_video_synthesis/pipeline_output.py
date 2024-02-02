9237ea5787f493890359ca99c4bf4a62552527ec
Dhruv Nair <dhruv.nair@gmail.com>
2024-02-02 05:07:12 +0000

    update

--- src/diffusers/pipelines/text_to_video_synthesis/pipeline_output.py
+++ src/diffusers/pipelines/text_to_video_synthesis/pipeline_output.py
@@ -17,9 +17,9 @@ class TextToVideoSDPipelineOutput(BaseOutput):
 
     Args:
         frames (`torch.Tensor`, `np.ndarray`, or List[List[PIL.Image.Image]]):
-        Nested list of length `batch_size` with denoised PIL image sequences of length `num_frames`,
-        NumPy array of shape `(batch_size, num_frames, channels, height, width,
-        Torch tensor of shape `(batch_size, num_frames, channels, height, width)`.
+            List of video outputs - It can be a nested list of length `batch_size,` with each sub-list containing denoised
+    PIL image sequences of length `num_frames.` It can also be a NumPy array or Torch tensor of shape
+   `(batch_size, num_frames, channels, height, width)`
     """
 
     frames: Union[torch.Tensor, np.ndarray, List[List[PIL.Image.Image]]]
