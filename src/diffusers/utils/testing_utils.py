1630bac667282facebc3e58232cb16974493f7d6
UmerHA <40663591+UmerHA@users.noreply.github.com>
2024-02-02 09:10:13 +0100

    Update testing_utils.py

--- src/diffusers/utils/testing_utils.py
+++ src/diffusers/utils/testing_utils.py
@@ -840,7 +840,7 @@ def _is_torch_fp16_available(device):
         return True
 
     except Exception as e:
-        if device == "cuda":
+        if device.type == "cuda":
             raise ValueError(
                 f"You have passed a device of type 'cuda' which should work with 'fp16', but 'cuda' does not seem to be correctly installed on your machine: {e}"
             )
@@ -854,13 +854,15 @@ def _is_torch_fp64_available(device):
 
     import torch
 
+    device = torch.device(device)
+
     try:
         x = torch.zeros((2, 2), dtype=torch.float64).to(device)
         _ = torch.mul(x, x)
         return True
 
     except Exception as e:
-        if device == "cuda":
+        if device.type == "cuda":
             raise ValueError(
                 f"You have passed a device of type 'cuda' which should work with 'fp64', but 'cuda' does not seem to be correctly installed on your machine: {e}"
             )
