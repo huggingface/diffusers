af02b68789031d12af00f238ca6b82ed18d3c609
Kashif Rasul <kashif.rasul@gmail.com>
2024-02-04 15:00:25 +0100

    cleanup

--- src/diffusers/pipelines/wuerstchen3/pipeline_wuerstchen3.py
+++ src/diffusers/pipelines/wuerstchen3/pipeline_wuerstchen3.py
@@ -1,4 +1,4 @@
-# Copyright 2023 The HuggingFace Team. All rights reserved.
+# Copyright 2024 The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -82,7 +82,6 @@ class WuerstchenV3DecoderPipeline(DiffusionPipeline):
 
     def __init__(
         self,
-        # encoder: None,
         decoder: WuerstchenV3Unet,
         tokenizer: CLIPTokenizer,
         text_encoder: CLIPTextModel,
@@ -92,7 +91,6 @@ class WuerstchenV3DecoderPipeline(DiffusionPipeline):
     ) -> None:
         super().__init__()
         self.register_modules(
-            # encoder=encoder,
             decoder=decoder,
             tokenizer=tokenizer,
             text_encoder=text_encoder,
