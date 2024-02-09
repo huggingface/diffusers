4763c82c700f8dafc164a10fe3bb43e842498ade
Fabio Rigano <fabio2rigano@gmail.com>
2024-02-09 20:29:49 +0100

    Fix init + docstring

--- src/diffusers/image_processor.py
+++ src/diffusers/image_processor.py
@@ -890,16 +890,42 @@ class IPAdapterMaskProcessor(VaeImageProcessor):
     """
     Image processor for IP Adapter image masks.
 
+    Args:
+        do_resize (`bool`, *optional*, defaults to `True`):
+            Whether to downscale the image's (height, width) dimensions to multiples of `vae_scale_factor`.
+        vae_scale_factor (`int`, *optional*, defaults to `8`):
+            VAE scale factor. If `do_resize` is `True`, the image is automatically resized to multiples of this factor.
+        resample (`str`, *optional*, defaults to `lanczos`):
+            Resampling filter to use when resizing the image.
+        do_normalize (`bool`, *optional*, defaults to `False`):
+            Whether to normalize the image to [-1,1].
+        do_binarize (`bool`, *optional*, defaults to `True`):
+            Whether to binarize the image to 0/1.
+        do_convert_grayscale (`bool`, *optional*, defaults to be `True`):
+            Whether to convert the images to grayscale format.
+
     """
 
-    def __init__(self):
+    config_name = CONFIG_NAME
+
+    @register_to_config
+    def __init__(
+        self,
         do_resize: bool = True,
         vae_scale_factor: int = 8,
         resample: str = "lanczos",
         do_normalize: bool = False,
         do_binarize: bool = True,
-        do_convert_grayscale: bool = True)
-        super().__init__(do_normalize=False, do_binarize=True, do_convert_grayscale=True)
+        do_convert_grayscale: bool = True,
+    ):
+        super().__init__(
+            do_resize=do_resize,
+            vae_scale_factor=vae_scale_factor,
+            resample=resample,
+            do_normalize=do_normalize,
+            do_binarize=do_binarize,
+            do_convert_grayscale=do_convert_grayscale,
+        )
 
     @staticmethod
     def downsample(mask: torch.FloatTensor, batch_size: int, num_queries: int, value_embed_dim: int):
