3ac557fd0250829ce4442527794c8251bed7eda1
a-r-r-o-w <contact.aryanvs@gmail.com>
2024-02-09 22:00:54 +0530

    add check for ip_adapter_image in pipeline call as well

--- tests/pipelines/test_pipelines_common.py
+++ tests/pipelines/test_pipelines_common.py
@@ -72,6 +72,11 @@ class IPAdapterTesterMixin:
 
         assert issubclass(self.pipeline_class, IPAdapterMixin)
         self.assertIn(
+            "ip_adapter_image",
+            parameters,
+            "`ip_adapter_image` argument must be supported by the `__call__` method",
+        )
+        self.assertIn(
             "ip_adapter_image_embeds",
             parameters,
             "`ip_adapter_image_embeds` argument must be supported by the `__call__` method",
