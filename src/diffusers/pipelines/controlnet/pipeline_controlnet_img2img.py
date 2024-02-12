ab426f415b98369fb7da307864deef2d6b14585f
tlpss <thomas17.lips@gmail.com>
2024-02-12 17:34:02 +0100

    copy docstring for `strength` from  stablediffusion img2img pipeline to controlnet img2img pipelines

--- src/diffusers/pipelines/controlnet/pipeline_controlnet_img2img.py
+++ src/diffusers/pipelines/controlnet/pipeline_controlnet_img2img.py
@@ -972,6 +972,12 @@ class StableDiffusionControlNetImg2ImgPipeline(
                 The height in pixels of the generated image.
             width (`int`, *optional*, defaults to `self.unet.config.sample_size * self.vae_scale_factor`):
                 The width in pixels of the generated image.
+            strength (`float`, *optional*, defaults to 0.8):
+                Indicates extent to transform the reference `image`. Must be between 0 and 1. `image` is used as a
+                starting point and more noise is added the higher the `strength`. The number of denoising steps depends
+                on the amount of noise initially added. When `strength` is 1, added noise is maximum and the denoising
+                process runs for the full number of iterations specified in `num_inference_steps`. A value of 1
+                essentially ignores `image`.
             num_inference_steps (`int`, *optional*, defaults to 50):
                 The number of denoising steps. More denoising steps usually lead to a higher quality image at the
                 expense of slower inference.
