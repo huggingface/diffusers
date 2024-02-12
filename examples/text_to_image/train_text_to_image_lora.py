a857655af5beb4273cf1ab2724077f88307f429e
cosmo3769 <piyushthakur3769@gmail.com>
2024-02-12 16:22:35 +0530

    fixed type annotations

--- examples/text_to_image/train_text_to_image_lora.py
+++ examples/text_to_image/train_text_to_image_lora.py
@@ -56,7 +56,9 @@ check_min_version("0.27.0.dev0")
 logger = get_logger(__name__, log_level="INFO")
 
 
-def save_model_card(repo_id: str, images=None, base_model=str, dataset_name=str, repo_folder=None):
+def save_model_card(
+    repo_id: str, images: list = None, base_model: str = None, dataset_name: str = None, repo_folder: str = None
+):
     img_str = ""
     for i, image in enumerate(images):
         image.save(os.path.join(repo_folder, f"image_{i}.png"))
