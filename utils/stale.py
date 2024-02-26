6d82a30aadd8d1855a5f8585e0259d20ae3997ec
yiyixuxu <yixu310@gmail,com>
2024-02-26 20:28:28 +0000

    fix

--- utils/stale.py
+++ utils/stale.py
@@ -39,25 +39,22 @@ def main():
     open_issues = repo.get_issues(state="open")
 
     for issue in open_issues:
-        comments = sorted(issue.get_comments(), key=lambda i: i.created_at, reverse=True)
-        last_comment = comments[0] if len(comments) > 0 else None
-        if (
-            last_comment is not None
-            and last_comment.user.login == "github-actions[bot]"
-            and (dt.now(timezone.utc) - issue.updated_at).days > 7
-            and (dt.now(timezone.utc) - issue.created_at).days >= 30
-            and not any(label.name.lower() in LABELS_TO_EXEMPT for label in issue.get_labels())
-        ):
-            # Closes the issue after 7 days of inactivity since the Stalebot notification.
-            issue.edit(state="closed")
-        elif (
-            "stale" in issue.get_labels()
-            and last_comment is not None
-            and last_comment.user.login != "github-actions[bot]"
-        ):
-            # Opens the issue if someone other than Stalebot commented.
-            issue.edit(state="open")
-            issue.remove_from_labels("stale")
+        if "stale" in issue.get_labels():
+            comments = sorted(issue.get_comments(), key=lambda i: i.created_at, reverse=True)
+            last_comment = comments[0] if len(comments) > 0 else None
+            if (
+                last_comment is not None
+                and last_comment.user.login == "github-actions[bot]"
+                and (dt.now(timezone.utc) - issue.updated_at).days > 7
+                and (dt.now(timezone.utc) - issue.created_at).days >= 30
+                and not any(label.name.lower() in LABELS_TO_EXEMPT for label in issue.get_labels())
+            ):
+                # Closes the issue after 7 days of inactivity since the Stalebot notification.
+                issue.edit(state="closed")
+            elif last_comment is not None and last_comment.user.login != "github-actions[bot]":
+                # Opens the issue if someone other than Stalebot commented.
+                issue.edit(state="open")
+                issue.remove_from_labels("stale")
         elif (
             (dt.now(timezone.utc) - issue.updated_at).days > 23
             and (dt.now(timezone.utc) - issue.created_at).days >= 30
