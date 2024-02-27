1ddc565d653229350e2a88722a1601ec8e5219d3
a-r-r-o-w <contact.aryanvs@gmail.com>
2024-02-27 07:40:57 +0530

    make fix-copies

--- src/diffusers/pipelines/stable_video_diffusion/pipeline_stable_video_diffusion.py
+++ src/diffusers/pipelines/stable_video_diffusion/pipeline_stable_video_diffusion.py
@@ -47,14 +47,17 @@ def tensor2vid(video: torch.Tensor, processor: "VaeImageProcessor", output_type:
     for batch_idx in range(batch_size):
         batch_vid = video[batch_idx].permute(1, 0, 2, 3)
         batch_output = processor.postprocess(batch_vid, output_type)
+
         outputs.append(batch_output)
 
     if output_type == "np":
         outputs = np.stack(outputs)
+
     elif output_type == "pt":
         outputs = torch.stack(outputs)
+
     elif not output_type == "pil":
-        raise ValueError(f"{output_type} does not exist. Please choose one of ['np', 'pt', 'pil', 'latent']")
+        raise ValueError(f"{output_type} does not exist. Please choose one of ['np', 'pt', 'pil]")
 
     return outputs
 
