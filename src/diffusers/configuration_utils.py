979b766f463553b85a3724509e768c0496ebe7a4
yiyixuxu <yixu310@gmail,com>
2024-03-03 05:48:43 +0000

    fix

--- src/diffusers/configuration_utils.py
+++ src/diffusers/configuration_utils.py
@@ -259,6 +259,11 @@ class ConfigMixin:
         model = cls(**init_dict)
 
         # make sure to also save config parameters that might be used for compatible classes
+        # update _class_name and _diffusers_versionl
+        if "_class_name" in hidden_dict:
+            hidden_dict["_class_name"] = cls.__name__
+        if "_diffusers_version" in hidden_dict:
+            hidden_dict["_diffusers_version"] = __version__
         model.register_to_config(**hidden_dict)
 
         # add hidden kwargs of compatible classes to unused_kwargs
