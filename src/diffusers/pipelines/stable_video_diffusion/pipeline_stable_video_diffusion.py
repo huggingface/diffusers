f12ffac5320df4829dfd3c6f1498dccf919a8579
Aryan <contact.aryanvs@gmail.com>
2024-03-05 00:50:15 +0530

    Update src/diffusers/pipelines/stable_video_diffusion/pipeline_stable_video_diffusion.py
    Co-authored-by: Steven Liu <59462357+stevhliu@users.noreply.github.com>

--- src/diffusers/pipelines/stable_video_diffusion/pipeline_stable_video_diffusion.py
+++ src/diffusers/pipelines/stable_video_diffusion/pipeline_stable_video_diffusion.py
@@ -40,7 +40,7 @@ EXAMPLE_DOC_STRING = """
         >>> pipe = StableVideoDiffusionPipeline.from_pretrained("stabilityai/stable-video-diffusion-img2vid-xt", torch_dtype=torch.float16, variant="fp16")
         >>> pipe.to("cuda")
 
-        >>> image = load_image("https://lh3.googleusercontent.com/y-iFOHfLTwkuQSUegpwDdgKmOjRSTvPxat63dQLB25xkTs4lhIbRUFeNBWZzYf370g=s1200")
+        >>> image = load_image("https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/diffusers/svd-docstring-example.jpeg")
         >>> image = image.resize((1024, 576))
 
         >>> frames = pipe(image, num_frames=25, decode_chunk_size=8).frames[0]
