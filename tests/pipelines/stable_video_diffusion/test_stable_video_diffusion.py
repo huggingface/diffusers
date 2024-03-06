8b0d08bb4cbe0c4b696f8337e5e07d2c2217028b
Jinay Jain <30903578+JinayJain@users.noreply.github.com>
2024-03-06 03:35:07 -0500

    [bug] Fix float/int guidance scale not working in `StableVideoDiffusionPipeline` (#7143)
    * [bug] Fix float/int guidance scale not working in `StableVideoDiffusionPipeline`
    
    * Add test to disable CFG on SVD
    
    ---------
    
    Co-authored-by: Sayak Paul <spsayakpaul@gmail.com>

--- tests/pipelines/stable_video_diffusion/test_stable_video_diffusion.py
+++ tests/pipelines/stable_video_diffusion/test_stable_video_diffusion.py
@@ -496,6 +496,22 @@ class StableVideoDiffusionPipelineFastTests(PipelineTesterMixin, unittest.TestCa
         max_diff = np.abs(to_np(output_with_offload) - to_np(output_without_offload)).max()
         self.assertLess(max_diff, expected_max_diff, "XFormers attention should not affect the inference results")
 
+    def test_disable_cfg(self):
+        components = self.get_dummy_components()
+        pipe = self.pipeline_class(**components)
+        for component in pipe.components.values():
+            if hasattr(component, "set_default_attn_processor"):
+                component.set_default_attn_processor()
+
+        pipe.to(torch_device)
+        pipe.set_progress_bar_config(disable=None)
+
+        generator_device = "cpu"
+        inputs = self.get_dummy_inputs(generator_device)
+        inputs["max_guidance_scale"] = 1.0
+        output = pipe(**inputs).frames
+        self.assertEqual(len(output.shape), 5)
+
 
 @slow
 @require_torch_gpu
