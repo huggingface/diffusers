99cdecc979d172264a407a3f38b5fb697b8524c4
yiyixuxu <yixu310@gmail,com>
2024-03-06 18:33:39 +0000

    add from_pipe

--- src/diffusers/pipelines/pipeline_utils.py
+++ src/diffusers/pipelines/pipeline_utils.py
@@ -22,7 +22,7 @@ import sys
 import warnings
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Any, Callable, Dict, List, Optional, Union
+from typing import Any, Callable, Dict, List, Optional, Union, get_origin, get_args
 
 import numpy as np
 import PIL.Image
@@ -2095,6 +2095,119 @@ class DiffusionPipeline(ConfigMixin, PushToHubMixin):
 
         for module in modules:
             module.set_attention_slice(slice_size)
+    
+    @classmethod
+    def from_pipe(cls, pipeline: "DiffusionPipeline", **kwargs) -> "DiffusionPipeline":
+        r"""
+        Create a new pipeline from a given pipeline. This method is useful to create a new pipeline with the same
+        weights and configurations without reallocating additional memory.
+
+        Arguments:
+            pipeline (`DiffusionPipeline`):
+                The pipeline from which to create a new pipeline.
+
+        Returns:
+            `DiffusionPipeline`:
+                A new pipeline with the same weights and configurations as `pipeline`.
+
+        Examples:
+
+        ```py
+        >>> from diffusers import StableDiffusionPipeline
+
+        >>> pipe = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+        >>> new_pipe = StableDiffusionPipeline.from_pipe(pipe)
+        ```
+        """
+        original_config = dict(pipeline.config)
+        torch_dtype = kwargs.pop("torch_dtype", None)
+        
+        # derive the pipeline class to instantiate
+        from .pipeline_utils import get_class_from_dynamic_module
+        custom_pipeline = kwargs.pop("custom_pipeline", None)
+
+        if custom_pipeline is not None:
+            pipeline_class = get_class_from_dynamic_module(custom_pipeline, module_file=f"{custom_pipeline}.py")
+        else:
+            pipeline_class = cls
+
+        expected_modules, optional_kwargs = cls._get_signature_keys(pipeline_class)
+        
+        def get_signature_types(cls):
+            signature_types = {}
+            for k, v in inspect.signature(cls.__init__).parameters.items():
+                if inspect.isclass(v.annotation):
+                    signature_types[k] = (v.annotation,)
+                elif get_origin(v.annotation) == Union:
+                    signature_types[k] = get_args(v.annotation)
+                else:
+                    logger.warn(f"cannot get type annotation for Parameter {k} of {cls}.")
+            return signature_types
+        
+        signature_types = get_signature_types(pipeline_class)
+
+        pretrained_model_name_or_path = original_config.pop("_name_or_path", None)
+        # allow users pass modules in `kwargs` to override the original pipeline's components
+        passed_class_obj = {k: kwargs.pop(k) for k in expected_modules if k in kwargs}
+
+        original_class_obj = {}
+        for k, v in pipeline.components.items():
+            if k in expected_modules and k not in passed_class_obj:
+                if k == "scheduler" or type(v) in signature_types[k]:
+                    original_class_obj[k] = v
+                else:
+                    logger.warn(
+                        f"component {k} is not switched over to new pipeline because type does not match the expected."
+                        f" k is {type(v)} while the new pipeline expect {signature_types[k]}."
+                        f" please pass the correct type of component to the new pipeline. `from_pipe(..., {k}={k})`")
+        
+        # allow users pass optional kwargs to override the original pipelines config attribute
+        passed_pipe_kwargs = {k: kwargs.pop(k) for k in optional_kwargs if k in kwargs}
+        original_pipe_kwargs = {
+            k: original_config[k]
+            for k, v in original_config.items()
+            if k in optional_kwargs and k not in passed_pipe_kwargs
+        }
+
+        # config attribute that were not expected by original pipeline is stored as its private attribute
+        # (i.e. when the original pipeline was also instantiated with `from_pipe` from another pipeline that has this config)
+        # in this case, we will pass them as optional arguments if they can be accepted by the new pipeline
+        additional_pipe_kwargs = [
+            k[1:]
+            for k in original_config.keys()
+            if k.startswith("_") and k[1:] in optional_kwargs and k[1:] not in passed_pipe_kwargs
+        ]
+        for k in additional_pipe_kwargs:
+            original_pipe_kwargs[k] = original_config.pop(f"_{k}")
+
+        pipeline_kwargs = {**passed_class_obj, **original_class_obj, **passed_pipe_kwargs, **original_pipe_kwargs, **kwargs}
+
+        # store unused config as private attribute in the new pipeline
+        unused_original_config = {
+            f"{'' if k.startswith('_') else '_'}{k}": original_config[k]
+            for k, v in original_config.items()
+            if k not in pipeline_kwargs
+        }
+        
+        missing_modules = set(expected_modules) - set(pipeline._optional_components) - set(pipeline_kwargs.keys())
+
+        if len(missing_modules) > 0:
+            raise ValueError(
+                f"Pipeline {pipeline_class} expected {expected_modules}, but only {set(list(passed_class_obj.keys()) + list(original_class_obj.keys()))} were passed"
+            )
+
+        new_pipeline = pipeline_class(**pipeline_kwargs)
+        new_pipeline.register_to_config(_name_or_path=pretrained_model_name_or_path, **unused_original_config)
+
+        if torch_dtype is not None:
+            new_pipeline.to(dtype=torch_dtype)
+
+        if hasattr(pipeline, "_offload_device"):
+            new_pipeline.enable_model_cpu_offload(device=pipeline._offload_device)
+
+        return new_pipeline
+
+
 
 
 class StableDiffusionMixin:
