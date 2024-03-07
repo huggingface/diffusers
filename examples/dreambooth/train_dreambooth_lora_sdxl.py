57a3be2e2886210f0a8f8b62711add862d61e42e
Sayak Paul <spsayakpaul@gmail.com>
2024-03-07 15:19:58 +0530

    [Examples] fix: prior preservation setting in DreamBooth LoRA SDXL script. (#7242)
    fix: prior preservation setting in DreamBooth LoRA SDXL script.
    
    Co-authored-by: Linoy Tsaban <57615435+linoytsaban@users.noreply.github.com>

--- examples/dreambooth/train_dreambooth_lora_sdxl.py
+++ examples/dreambooth/train_dreambooth_lora_sdxl.py
@@ -877,6 +877,8 @@ def collate_fn(examples, with_prior_preservation=False):
     if with_prior_preservation:
         pixel_values += [example["class_images"] for example in examples]
         prompts += [example["class_prompt"] for example in examples]
+        original_sizes += [example["original_size"] for example in examples]
+        crop_top_lefts += [example["crop_top_left"] for example in examples]
 
     pixel_values = torch.stack(pixel_values)
     pixel_values = pixel_values.to(memory_format=torch.contiguous_format).float()
