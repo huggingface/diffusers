da58475f40fc844ad20df9fbcdeda9e5c6772c8e
Sayak Paul <spsayakpaul@gmail.com>
2024-03-08 14:42:26 +0530

    [Utils] Improve " # Copied from  ..." statements in the pipelines  (#6917)
    * copied from for t2i pipelines without ip adapter support.
    
    * two more pipelines with proper copied from comments.
    
    * revert to the original implementation

--- src/diffusers/pipelines/stable_diffusion_sag/pipeline_stable_diffusion_sag.py
+++ src/diffusers/pipelines/stable_diffusion_sag/pipeline_stable_diffusion_sag.py
@@ -446,6 +446,7 @@ class StableDiffusionSAGPipeline(DiffusionPipeline, StableDiffusionMixin, Textua
             extra_step_kwargs["generator"] = generator
         return extra_step_kwargs
 
+    # Copied from diffusers.pipelines.stable_diffusion_k_diffusion.pipeline_stable_diffusion_k_diffusion.StableDiffusionKDiffusionPipeline.check_inputs
     def check_inputs(
         self,
         prompt,
--- src/diffusers/pipelines/text_to_video_synthesis/pipeline_text_to_video_synth.py
+++ src/diffusers/pipelines/text_to_video_synthesis/pipeline_text_to_video_synth.py
@@ -374,6 +374,7 @@ class TextToVideoSDPipeline(DiffusionPipeline, StableDiffusionMixin, TextualInve
             extra_step_kwargs["generator"] = generator
         return extra_step_kwargs
 
+    # Copied from diffusers.pipelines.stable_diffusion_k_diffusion.pipeline_stable_diffusion_k_diffusion.StableDiffusionKDiffusionPipeline.check_inputs
     def check_inputs(
         self,
         prompt,
