b8eed8db14148e8eb8384ac9db79981c3d793016
Fabio Rigano <fabio2rigano@gmail.com>
2024-03-12 20:30:08 +0100

    Fix copies

--- src/diffusers/pipelines/controlnet/pipeline_controlnet_inpaint_sd_xl.py
+++ src/diffusers/pipelines/controlnet/pipeline_controlnet_inpaint_sd_xl.py
@@ -482,27 +482,40 @@ class StableDiffusionXLControlNetInpaintPipeline(
 
     # Copied from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion.StableDiffusionPipeline.encode_image
     def encode_image(self, image, device, num_images_per_prompt, output_hidden_states=None):
-        dtype = next(self.image_encoder.parameters()).dtype
+        if self.image_encoder is not None:
+            dtype = next(self.image_encoder.parameters()).dtype
 
-        if not isinstance(image, torch.Tensor):
-            image = self.feature_extractor(image, return_tensors="pt").pixel_values
+            if not isinstance(image, torch.Tensor):
+                image = self.feature_extractor(image, return_tensors="pt").pixel_values
 
-        image = image.to(device=device, dtype=dtype)
-        if output_hidden_states:
-            image_enc_hidden_states = self.image_encoder(image, output_hidden_states=True).hidden_states[-2]
-            image_enc_hidden_states = image_enc_hidden_states.repeat_interleave(num_images_per_prompt, dim=0)
-            uncond_image_enc_hidden_states = self.image_encoder(
-                torch.zeros_like(image), output_hidden_states=True
-            ).hidden_states[-2]
-            uncond_image_enc_hidden_states = uncond_image_enc_hidden_states.repeat_interleave(
-                num_images_per_prompt, dim=0
-            )
-            return image_enc_hidden_states, uncond_image_enc_hidden_states
+            image = image.to(device=device, dtype=dtype)
+            if output_hidden_states:
+                image_enc_hidden_states = self.image_encoder(image, output_hidden_states=True).hidden_states[-2]
+                image_enc_hidden_states = image_enc_hidden_states.repeat_interleave(num_images_per_prompt, dim=0)
+                uncond_image_enc_hidden_states = self.image_encoder(
+                    torch.zeros_like(image), output_hidden_states=True
+                ).hidden_states[-2]
+                uncond_image_enc_hidden_states = uncond_image_enc_hidden_states.repeat_interleave(
+                    num_images_per_prompt, dim=0
+                )
+                return image_enc_hidden_states, uncond_image_enc_hidden_states
+            else:
+                image_embeds = self.image_encoder(image).image_embeds
+                image_embeds = image_embeds.repeat_interleave(num_images_per_prompt, dim=0)
+                uncond_image_embeds = torch.zeros_like(image_embeds)
+
+                return image_embeds, uncond_image_embeds
         else:
-            image_embeds = self.image_encoder(image).image_embeds
-            image_embeds = image_embeds.repeat_interleave(num_images_per_prompt, dim=0)
-            uncond_image_embeds = torch.zeros_like(image_embeds)
+            dtype = next(self.unet.parameters()).dtype
 
+            if not isinstance(image, torch.Tensor):
+                raise ValueError("When no image encoder is loaded, `image` must be a torch.Tensor")
+
+            if image.ndim < 2:
+                image = image.unsqueeze(0)
+
+            image_embeds = image.to(device=device, dtype=dtype)
+            uncond_image_embeds = torch.zeros_like(image_embeds)
             return image_embeds, uncond_image_embeds
 
     # Copied from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion.StableDiffusionPipeline.prepare_ip_adapter_image_embeds
