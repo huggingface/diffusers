2e1178bb4413dc48f67afc845edea9a6480c5f56
standardAI <mtcangoz@gmail.com>
2024-03-13 20:53:08 +0300

    Add type hints and fix var name

--- src/diffusers/pipelines/latent_consistency_models/pipeline_latent_consistency_img2img.py
+++ src/diffusers/pipelines/latent_consistency_models/pipeline_latent_consistency_img2img.py
@@ -548,32 +548,34 @@ class LatentConsistencyModelImg2ImgPipeline(
         return latents
 
     # Copied from diffusers.pipelines.latent_consistency_models.pipeline_latent_consistency_text2img.LatentConsistencyModelPipeline.get_guidance_scale_embedding
-    def get_guidance_scale_embedding(self, timesteps, embedding_dim=512, dtype=torch.float32):
+    def get_guidance_scale_embedding(
+        self, w: torch.Tensor, embedding_dim: int = 512, dtype: torch.dtype = torch.float32
+    ) -> torch.FloatTensor:
         """
         See https://github.com/google-research/vdm/blob/dc27b98a554f65cdc654b800da5aa1846545d41b/model_vdm.py#L298
 
         Args:
-            timesteps (`torch.Tensor`):
-                generate embedding vectors at these timesteps
+            w (`torch.Tensor`):
+                Generate embedding vectors with a specified guidance scale to subsequently enrich timestep embeddings.
             embedding_dim (`int`, *optional*, defaults to 512):
-                dimension of the embeddings to generate
-            dtype:
-                data type of the generated embeddings
+            Dimension of the embeddings to generate.
+            dtype (`torch.dtype`, *optional*, defaults to `torch.float32`):
+                Data type of the generated embeddings.
 
         Returns:
-            `torch.FloatTensor`: Embedding vectors with shape `(len(timesteps), embedding_dim)`
+            `torch.FloatTensor`: Embedding vectors with shape `(len(w), embedding_dim)`.
         """
-        assert len(timesteps.shape) == 1
-        timesteps = timesteps * 1000.0
+        assert len(w.shape) == 1
+        w = w * 1000.0
 
         half_dim = embedding_dim // 2
         emb = torch.log(torch.tensor(10000.0)) / (half_dim - 1)
         emb = torch.exp(torch.arange(half_dim, dtype=dtype) * -emb)
-        emb = timesteps.to(dtype)[:, None] * emb[None, :]
+        emb = w.to(dtype)[:, None] * emb[None, :]
         emb = torch.cat([torch.sin(emb), torch.cos(emb)], dim=1)
         if embedding_dim % 2 == 1:  # zero pad
             emb = torch.nn.functional.pad(emb, (0, 1))
-        assert emb.shape == (timesteps.shape[0], embedding_dim)
+        assert emb.shape == (w.shape[0], embedding_dim)
         return emb
 
     # Copied from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion.StableDiffusionPipeline.prepare_extra_step_kwargs
