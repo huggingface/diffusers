26d9fec810f2eacdfed7fb3db3b3b3d881a32dd3
sayakpaul <spsayakpaul@gmail.com>
2024-03-14 21:36:29 +0530

    post-release

--- examples/community/marigold_depth_estimation.py
+++ examples/community/marigold_depth_estimation.py
@@ -40,8 +40,7 @@ from diffusers.utils import BaseOutput, check_min_version
 
 
 # Will error if the minimal version of diffusers is not installed. Remove at your own risks.
-check_min_version("0.27.0.dev0")
-
+check_min_version("0.28.0.dev0")
 
 class MarigoldDepthOutput(BaseOutput):
     """
