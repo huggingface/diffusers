95235a0e121247eb2c6dd89c13256be9e1fffd5b
standardAI <mtcangoz@gmail.com>
2024-03-14 09:34:09 +0300

    Revert "Fix PyTorch's convention for inplace functions"
    This reverts commit 74350cf65b2c9aa77f08bec7937d7a8b13edb509.

--- tests/pipelines/controlnet/test_controlnet_img2img.py
+++ tests/pipelines/controlnet/test_controlnet_img2img.py
@@ -210,7 +210,7 @@ class StableDiffusionMultiControlNetPipelineFastTests(
 
         def init_weights(m):
             if isinstance(m, torch.nn.Conv2d):
-                torch.nn.init.normal_(m.weight)
+                torch.nn.init.normal(m.weight)
                 m.bias.data.fill_(1.0)
 
         controlnet1 = ControlNetModel(
