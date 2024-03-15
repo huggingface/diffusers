52d004b19a344f9d2f6ea1c9e0cceaeb7adb54cb
e-yi <e_yi@outlook.com>
2024-03-15 22:04:09 +0800

    remove redundant computations #7342

--- examples/controlnet/train_controlnet_flax.py
+++ examples/controlnet/train_controlnet_flax.py
@@ -838,13 +838,11 @@ def main():
         Computes SNR as per https://github.com/TiankaiHang/Min-SNR-Diffusion-Training/blob/521b624bd70c67cee4bdf49225915f5945a872e3/guided_diffusion/gaussian_diffusion.py#L847-L849
         """
         alphas_cumprod = noise_scheduler_state.common.alphas_cumprod
-        sqrt_alphas_cumprod = alphas_cumprod**0.5
-        sqrt_one_minus_alphas_cumprod = (1.0 - alphas_cumprod) ** 0.5
 
-        alpha = sqrt_alphas_cumprod[timesteps]
-        sigma = sqrt_one_minus_alphas_cumprod[timesteps]
+        alpha_squared = alphas_cumprod[timesteps]
+        sigma_squared = 1 - alpha_squared
         # Compute SNR.
-        snr = (alpha / sigma) ** 2
+        snr = alpha_squared / sigma_squared
         return snr
 
     def train_step(state, unet_params, text_encoder_params, vae_params, batch, train_rng):
--- src/diffusers/training_utils.py
+++ src/diffusers/training_utils.py
@@ -53,23 +53,18 @@ def compute_snr(noise_scheduler, timesteps):
     https://github.com/TiankaiHang/Min-SNR-Diffusion-Training/blob/521b624bd70c67cee4bdf49225915f5945a872e3/guided_diffusion/gaussian_diffusion.py#L847-L849
     """
     alphas_cumprod = noise_scheduler.alphas_cumprod
-    sqrt_alphas_cumprod = alphas_cumprod**0.5
-    sqrt_one_minus_alphas_cumprod = (1.0 - alphas_cumprod) ** 0.5
 
     # Expand the tensors.
     # Adapted from https://github.com/TiankaiHang/Min-SNR-Diffusion-Training/blob/521b624bd70c67cee4bdf49225915f5945a872e3/guided_diffusion/gaussian_diffusion.py#L1026
-    sqrt_alphas_cumprod = sqrt_alphas_cumprod.to(device=timesteps.device)[timesteps].float()
-    while len(sqrt_alphas_cumprod.shape) < len(timesteps.shape):
-        sqrt_alphas_cumprod = sqrt_alphas_cumprod[..., None]
-    alpha = sqrt_alphas_cumprod.expand(timesteps.shape)
+    alphas_cumprod = alphas_cumprod.to(device=timesteps.device)[timesteps].float()
+    while len(alphas_cumprod.shape) < len(timesteps.shape):
+        alphas_cumprod = alphas_cumprod[..., None]
+    alpha_squared = alphas_cumprod.expand(timesteps.shape)
 
-    sqrt_one_minus_alphas_cumprod = sqrt_one_minus_alphas_cumprod.to(device=timesteps.device)[timesteps].float()
-    while len(sqrt_one_minus_alphas_cumprod.shape) < len(timesteps.shape):
-        sqrt_one_minus_alphas_cumprod = sqrt_one_minus_alphas_cumprod[..., None]
-    sigma = sqrt_one_minus_alphas_cumprod.expand(timesteps.shape)
+    sigma_squared = 1 - alpha_squared
 
     # Compute SNR.
-    snr = (alpha / sigma) ** 2
+    snr = alpha_squared / sigma_squared
     return snr
 
 
