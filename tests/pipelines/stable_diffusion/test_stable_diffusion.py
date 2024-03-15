2ea57ddd5a42b52fddeb3508b64317a1c746e1da
sayakpaul <spsayakpaul@gmail.com>
2024-03-15 11:32:50 +0530

    fix test

--- tests/pipelines/stable_diffusion/test_stable_diffusion.py
+++ tests/pipelines/stable_diffusion/test_stable_diffusion.py
@@ -1473,4 +1473,4 @@ class StableDiffusionPipelineDeviceMapTests(unittest.TestCase):
             "runwayml/stable-diffusion-v1-5", device_map="balanced", torch_dtype=torch.float16
         )
 
-        assert len(sd_pipe_with_device_map.hf_device_map) >= 2
+        assert len(set(sd_pipe_with_device_map.hf_device_map.values())) >= 2
