209efa01acaeb6c6c970e65824a677e40915e037
Dhruv Nair <dhruv.nair@gmail.com>
2024-03-18 11:28:59 +0530

    [Tests] Fix ControlNet Single File tests  (#7315)
    * update
    
    * update
    
    ---------
    
    Co-authored-by: Sayak Paul <spsayakpaul@gmail.com>

--- tests/pipelines/controlnet/test_controlnet_sdxl.py
+++ tests/pipelines/controlnet/test_controlnet_sdxl.py
@@ -1002,6 +1002,11 @@ class ControlNetSDXLPipelineSlowTests(unittest.TestCase):
         for param_name, param_value in single_file_pipe.unet.config.items():
             if param_name in PARAMS_TO_IGNORE:
                 continue
+
+            # Upcast attention might be set to None in a config file, which is incorrect. It should default to False in the model
+            if param_name == "upcast_attention" and pipe.unet.config[param_name] is None:
+                pipe.unet.config[param_name] = False
+
             assert (
                 pipe.unet.config[param_name] == param_value
             ), f"{param_name} differs between single file loading and pretrained loading"
