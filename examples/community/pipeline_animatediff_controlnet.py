cdc19f01592a94d51400dd5c26f8d8998b7b607e
Abhinav Gopal <abhinavg@berkeley.edu>
2024-03-20 22:45:18 -0700

    fixed encode_image function signature in controlnet animatediff

--- examples/community/pipeline_animatediff_controlnet.py
+++ examples/community/pipeline_animatediff_controlnet.py
@@ -373,7 +373,7 @@ class AnimateDiffControlNetPipeline(
         return prompt_embeds, negative_prompt_embeds
 
     # Copied from diffusers.pipelines.stable_diffusion.pipeline_stable_diffusion.StableDiffusionPipeline.encode_image
-    def encode_image(self, image, device, num_images_per_prompt):
+    def encode_image(self, image, device, num_images_per_prompt, output_hidden_states=None):
         dtype = next(self.image_encoder.parameters()).dtype
 
         if not isinstance(image, torch.Tensor):
