48a75f246b833dbd9e67d2faa737e25a7dede895
sayakpaul <spsayakpaul@gmail.com>
2024-03-22 10:20:22 +0530

    let's see.

--- examples/research_projects/diffusion_orpo/train_diffusion_orpo_sdxl_lora_wds.py
+++ examples/research_projects/diffusion_orpo/train_diffusion_orpo_sdxl_lora_wds.py
@@ -950,7 +950,7 @@ def main(args):
                 def compute_time_ids(original_size, crops_coords_top_left):
                     # Adapted from pipeline.StableDiffusionXLPipeline._get_add_time_ids
                     target_size = (args.resolution, args.resolution)
-                    add_time_ids = list(original_size + crops_coords_top_left + target_size)
+                    add_time_ids = list(tuple(original_size) + tuple(crops_coords_top_left) + target_size)
                     add_time_ids = torch.tensor([add_time_ids])
                     add_time_ids = add_time_ids.to(accelerator.device, dtype=weight_dtype)
                     return add_time_ids
