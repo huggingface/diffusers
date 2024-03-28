9eaa190368b1512ce50169de98bc18019f75e083
linoytsaban <linoy@huggingface.co>
2024-03-28 18:36:12 +0200

    check insightface import

--- examples/community/pipeline_stable_diffusion_xl_instandid_img2img.py
+++ examples/community/pipeline_stable_diffusion_xl_instandid_img2img.py
@@ -46,6 +46,15 @@ except Exception:
 
 logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
 
+try:
+    import insightface
+except ImportError:
+    logger.warning(
+        "To use instant id pipelines, please install the insightface library: `pip install insightface`."
+        "Please refer to: https://huggingface.co/InstantX/InstantID for further instructions"
+    )
+
+optimizer_class = bnb.optim.AdamW8bit
 
 def FeedForward(dim, mult=4):
     inner_dim = int(dim * mult)
