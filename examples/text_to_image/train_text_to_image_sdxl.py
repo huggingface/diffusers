77697d44f5fdf029655d637d5c5ada4c17230d70
aihao <aihao@xiaohongshu.com>
2024-03-29 16:18:29 +0800

    add data_dir parameter to load_dataset

--- examples/text_to_image/train_text_to_image_sdxl.py
+++ examples/text_to_image/train_text_to_image_sdxl.py
@@ -472,8 +472,6 @@ def parse_args(input_args=None):
         args.local_rank = env_local_rank
 
     # Sanity checks
-    if args.dataset_name is None and args.train_data_dir is None:
-        raise ValueError("Need either a dataset name or a training folder.")
 
     if args.proportion_empty_prompts < 0 or args.proportion_empty_prompts > 1:
         raise ValueError("`--proportion_empty_prompts` must be in the range [0, 1].")
@@ -799,6 +797,7 @@ def main(args):
             args.dataset_name,
             args.dataset_config_name,
             cache_dir=args.cache_dir,
+            data_dir=args.train_data_dir
         )
     else:
         data_files = {}
