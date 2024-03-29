ab13de38749983fd1655f9c368f8961be3d1c1a6
sayakpaul <spsayakpaul@gmail.com>
2024-03-29 21:06:30 +0530

    make style

--- src/diffusers/pipelines/pia/pipeline_pia.py
+++ src/diffusers/pipelines/pia/pipeline_pia.py
@@ -136,9 +136,9 @@ class PIAPipelineOutput(BaseOutput):
 
     Args:
         frames (`torch.Tensor`, `np.ndarray`, or List[List[PIL.Image.Image]]):
-            Nested list of length `batch_size` with denoised PIL image sequences of length `num_frames`,
-            NumPy array of shape `(batch_size, num_frames, channels, height, width,
-            Torch tensor of shape `(batch_size, num_frames, channels, height, width)`.
+            Nested list of length `batch_size` with denoised PIL image sequences of length `num_frames`, NumPy array of
+            shape `(batch_size, num_frames, channels, height, width, Torch tensor of shape `(batch_size, num_frames,
+            channels, height, width)`.
     """
 
     frames: Union[torch.Tensor, np.ndarray, List[List[PIL.Image.Image]]]
