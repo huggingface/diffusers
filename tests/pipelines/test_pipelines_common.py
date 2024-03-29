34c90dbb31f4956e72086ac43ca7d8154c2aadae
YiYi Xu <yixu310@gmail.com>
2024-03-28 16:52:39 -1000

    fix OOM for test_vae_tiling (#7510)
    use float16 and add torch.no_grad()

--- tests/pipelines/test_pipelines_common.py
+++ tests/pipelines/test_pipelines_common.py
@@ -124,9 +124,10 @@ class SDFunctionTesterMixin:
 
         # test that tiled decode works with various shapes
         shapes = [(1, 4, 73, 97), (1, 4, 97, 73), (1, 4, 49, 65), (1, 4, 65, 49)]
-        for shape in shapes:
-            zeros = torch.zeros(shape).to(torch_device)
-            pipe.vae.decode(zeros)
+        with torch.no_grad():
+            for shape in shapes:
+                zeros = torch.zeros(shape).to(torch_device)
+                pipe.vae.decode(zeros)
 
     def test_freeu_enabled(self):
         components = self.get_dummy_components()
