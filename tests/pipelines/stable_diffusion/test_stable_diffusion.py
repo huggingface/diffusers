9e628cf19a9b03b6228430dab01d0bdd119e44bb
sayakpaul <spsayakpaul@gmail.com>
2024-03-29 09:30:11 +0530

    add: test for reset_hf_device_map

--- tests/pipelines/stable_diffusion/test_stable_diffusion.py
+++ tests/pipelines/stable_diffusion/test_stable_diffusion.py
@@ -1495,3 +1495,15 @@ class StableDiffusionPipelineDeviceMapTests(unittest.TestCase):
 
         max_diff = np.abs(device_map_image - no_device_map_image).max()
         assert max_diff < 1e-3
+
+    def test_reset_device_map(self):
+        sd_pipe_with_device_map = StableDiffusionPipeline.from_pretrained(
+            "runwayml/stable-diffusion-v1-5", device_map="balanced", torch_dtype=torch.float16
+        )
+        sd_pipe_with_device_map.reset_device_map()
+
+        assert sd_pipe_with_device_map.hf_device_map is None
+
+        for name, component in sd_pipe_with_device_map.components.items():
+            if isinstance(component, torch.nn.Module):
+                assert component.device.type == "cpu"
