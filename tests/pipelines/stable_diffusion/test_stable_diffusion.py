3bccdd05aa09a25c041e298e0390963e22e6ec31
sayakpaul <spsayakpaul@gmail.com>
2024-04-01 07:19:32 +0530

    add tests for checking reset_device_map doesn't have unintended consequences.

--- tests/pipelines/stable_diffusion/test_stable_diffusion.py
+++ tests/pipelines/stable_diffusion/test_stable_diffusion.py
@@ -1523,3 +1523,39 @@ class StableDiffusionPipelineDeviceMapTests(unittest.TestCase):
         for name, component in sd_pipe_with_device_map.components.items():
             if isinstance(component, torch.nn.Module):
                 assert component.device.type == "cpu"
+
+    def test_reset_device_map_to(self):
+        sd_pipe_with_device_map = StableDiffusionPipeline.from_pretrained(
+            "runwayml/stable-diffusion-v1-5", device_map="balanced", torch_dtype=torch.float16
+        )
+        sd_pipe_with_device_map.reset_device_map()
+
+        assert sd_pipe_with_device_map.hf_device_map is None
+
+        # Make sure `to()` can be used and the pipeline can be called.
+        pipe = sd_pipe_with_device_map.to("cuda")
+        _ = pipe("hello", num_inference_steps=2)
+
+    def test_reset_device_map_enable_model_cpu_offload(self):
+        sd_pipe_with_device_map = StableDiffusionPipeline.from_pretrained(
+            "runwayml/stable-diffusion-v1-5", device_map="balanced", torch_dtype=torch.float16
+        )
+        sd_pipe_with_device_map.reset_device_map()
+
+        assert sd_pipe_with_device_map.hf_device_map is None
+
+        # Make sure `enable_model_cpu_offload()` can be used and the pipeline can be called.
+        pipe = sd_pipe_with_device_map.enable_model_cpu_offload()
+        _ = pipe("hello", num_inference_steps=2)
+
+    def test_reset_device_map_enable_sequential_cpu_offload(self):
+        sd_pipe_with_device_map = StableDiffusionPipeline.from_pretrained(
+            "runwayml/stable-diffusion-v1-5", device_map="balanced", torch_dtype=torch.float16
+        )
+        sd_pipe_with_device_map.reset_device_map()
+
+        assert sd_pipe_with_device_map.hf_device_map is None
+
+        # Make sure `enable_sequential_cpu_offload()` can be used and the pipeline can be called.
+        pipe = sd_pipe_with_device_map.enable_sequential_cpu_offload()
+        _ = pipe("hello", num_inference_steps=2)
