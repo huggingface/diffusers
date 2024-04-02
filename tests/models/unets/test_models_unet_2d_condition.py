2148b42173a9a40235af0aba9ab549beb71522bb
Sayak Paul <spsayakpaul@gmail.com>
2024-04-02 15:18:18 +0530

    reduce block sizes for unet2dconditionmodel tests

--- tests/models/unets/test_models_unet_2d_condition.py
+++ tests/models/unets/test_models_unet_2d_condition.py
@@ -265,11 +265,12 @@ class UNet2DConditionModelTests(ModelTesterMixin, UNetTesterMixin, unittest.Test
 
     def prepare_init_args_and_inputs_for_common(self):
         init_dict = {
-            "block_out_channels": (32, 64),
+            "block_out_channels": (16, 32),
+            "norm_num_groups": 16,
             "down_block_types": ("CrossAttnDownBlock2D", "DownBlock2D"),
             "up_block_types": ("UpBlock2D", "CrossAttnUpBlock2D"),
             "cross_attention_dim": 32,
-            "attention_head_dim": 8,
+            "attention_head_dim": 4,
             "out_channels": 4,
             "in_channels": 4,
             "layers_per_block": 1,
