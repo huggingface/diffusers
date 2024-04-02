694fda198235944f000ee403cff3f5b58615b451
Sayak Paul <spsayakpaul@gmail.com>
2024-04-02 15:44:41 +0530

    make style.

--- tests/models/unets/test_models_unet_spatiotemporal.py
+++ tests/models/unets/test_models_unet_spatiotemporal.py
@@ -24,9 +24,9 @@ from diffusers.utils.import_utils import is_xformers_available
 from diffusers.utils.testing_utils import (
     enable_full_determinism,
     floats_tensor,
+    skip_mps,
     torch_all_close,
     torch_device,
-    skip_mps
 )
 
 from ..test_modeling_common import ModelTesterMixin, UNetTesterMixin
@@ -35,6 +35,7 @@ from ..test_modeling_common import ModelTesterMixin, UNetTesterMixin
 logger = logging.get_logger(__name__)
 
 enable_full_determinism()
+
 
 @skip_mps
 class UNetSpatioTemporalConditionModelTests(ModelTesterMixin, UNetTesterMixin, unittest.TestCase):
