247bc88e5e53469f7f211692ddd25720361d2b60
sayakpaul <spsayakpaul@gmail.com>
2024-04-02 14:41:16 +0530

    reduce block size for unet_motion

--- tests/models/unets/test_models_unet_motion.py
+++ tests/models/unets/test_models_unet_motion.py
@@ -65,11 +65,12 @@ class UNetMotionModelTests(ModelTesterMixin, UNetTesterMixin, unittest.TestCase)
 
     def prepare_init_args_and_inputs_for_common(self):
         init_dict = {
-            "block_out_channels": (32, 64),
+            "block_out_channels": (16, 32),
+            "norm_num_groups": 16,
             "down_block_types": ("CrossAttnDownBlockMotion", "DownBlockMotion"),
             "up_block_types": ("UpBlockMotion", "CrossAttnUpBlockMotion"),
             "cross_attention_dim": 32,
-            "num_attention_heads": 4,
+            "num_attention_heads": 2,
             "out_channels": 4,
             "in_channels": 4,
             "layers_per_block": 1,
