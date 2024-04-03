9390e24d3b60c5252a6a25924cf425feb634dc24
sayakpaul <spsayakpaul@gmail.com>
2024-04-03 07:37:47 +0530

    styling.

--- src/diffusers/utils/testing_utils.py
+++ src/diffusers/utils/testing_utils.py
@@ -258,8 +258,8 @@ def require_torch_accelerator(test_case):
 def require_torch_multi_gpu(test_case):
     """
     Decorator marking a test that requires a multi-GPU setup (in PyTorch). These tests are skipped on a machine without
-    multiple GPUs.
-    To run *only* the multi_gpu tests, assuming all test names contain multi_gpu: $ pytest -sv ./tests -k "multi_gpu"
+    multiple GPUs. To run *only* the multi_gpu tests, assuming all test names contain multi_gpu: $ pytest -sv ./tests
+    -k "multi_gpu"
     """
     if not is_torch_available():
         return unittest.skip("test requires PyTorch")(test_case)
