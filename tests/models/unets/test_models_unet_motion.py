7d00d06444a721815d744e04e081fadb37b82f3d
sayakpaul <spsayakpaul@gmail.com>
2024-04-04 09:34:19 +0530

    up a bit.

--- tests/models/unets/test_models_unet_motion.py
+++ tests/models/unets/test_models_unet_motion.py
@@ -65,11 +65,11 @@ class UNetMotionModelTests(ModelTesterMixin, UNetTesterMixin, unittest.TestCase)
 
     def prepare_init_args_and_inputs_for_common(self):
         init_dict = {
-            "block_out_channels": (4, 8),
+            "block_out_channels": (8, 16),
             "norm_num_groups": 4,
             "down_block_types": ("CrossAttnDownBlockMotion", "DownBlockMotion"),
             "up_block_types": ("UpBlockMotion", "CrossAttnUpBlockMotion"),
-            "cross_attention_dim": 8,
+            "cross_attention_dim": 16,
             "num_attention_heads": 2,
             "out_channels": 4,
             "in_channels": 4,
