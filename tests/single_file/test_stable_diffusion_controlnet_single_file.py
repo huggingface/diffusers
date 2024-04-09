e1c760746d3cefc2409ce49edb66fde47f03bf0d
Dhruv Nair <dhruv.nair@gmail.com>
2024-04-09 13:54:23 +0000

    update

--- tests/single_file/test_stable_diffusion_controlnet_single_file.py
+++ tests/single_file/test_stable_diffusion_controlnet_single_file.py
@@ -143,7 +143,7 @@ class StableDiffusionControlNetPipelineSingleFileSlowTests(unittest.TestCase, SD
             self.ckpt_path, controlnet=controlnet, config=self.repo_id
         )
 
-        super()._compare_component_configs(pipe, pipe_single_file)
+        super()._compare_component_configs(pipe, pipe_single_file, safety_checker=False)
 
     def test_single_file_components_with_diffusers_config_local_files_only(self):
         controlnet = ControlNetModel.from_pretrained(
@@ -162,4 +162,4 @@ class StableDiffusionControlNetPipelineSingleFileSlowTests(unittest.TestCase, SD
             pipe_single_file = self.pipeline_class.from_single_file(
                 local_ckpt_path, config=local_diffusers_config, controlnet=controlnet, local_files_only=True
             )
-        super()._compare_component_configs(pipe, pipe_single_file)
+        super()._compare_component_configs(pipe, pipe_single_file, safety_checker=False)
