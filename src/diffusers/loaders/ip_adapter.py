07a18fd0e785ea2d7bd888a5e7439ca95ab6ba61
DannHuang <jenyuan@stu.pku.edu.cn>
2024-04-18 19:41:23 +0800

    fixed arg name

--- src/diffusers/loaders/ip_adapter.py
+++ src/diffusers/loaders/ip_adapter.py
@@ -229,40 +229,40 @@ class IPAdapterMixin:
         unet = getattr(self, self.unet_name) if not hasattr(self, "unet") else self.unet
         unet._load_ip_adapter_weights(state_dicts, low_cpu_mem_usage=low_cpu_mem_usage)
 
-    def set_ip_adapter_scale(self, scale_configs: Union[float, Dict, List[Union[float, Dict]]], default_scale=0.0):
+    def set_ip_adapter_scale(self, scale: Union[float, Dict, List[Union[float, Dict]]], default_scale=0.0):
         """
-        Set IP-Adapter scales per-transformer block. Input `scale_configs` could be a single config or a list of
-        configs for granular control over each IP-Adapter behavior. A config can be a float or a dictionary.
+        Set IP-Adapter scales per-transformer block. Input `scale` could be a single config or a list of configs for
+        granular control over each IP-Adapter behavior. A config can be a float or a dictionary.
 
         Example:
 
         ```py
         # To use original IP-Adapter
-        scale_configs = 1.0
-        pipeline.set_ip_adapter_scale(scale_configs)
+        scale = 1.0
+        pipeline.set_ip_adapter_scale(scale)
 
         # To use style block only
-        scale_configs = {
+        scale = {
             "up": {"block_0": [0.0, 1.0, 0.0]},
         }
-        pipeline.set_ip_adapter_scale(scale_configs)
+        pipeline.set_ip_adapter_scale(scale)
 
         # To use style+layout blocks
-        scale_configs = {
+        scale = {
             "down": {"block_2": [0.0, 1.0]},
             "up": {"block_0": [0.0, 1.0, 0.0]},
         }
-        pipeline.set_ip_adapter_scale(scale_configs)
+        pipeline.set_ip_adapter_scale(scale)
 
         # To use style and layout from 2 reference images
-        scale_configs = [{"down": {"block_2": [0.0, 1.0]}}, {"up": {"block_0": [0.0, 1.0, 0.0]}}]
-        pipeline.set_ip_adapter_scale(scale_configs)
+        scales = [{"down": {"block_2": [0.0, 1.0]}}, {"up": {"block_0": [0.0, 1.0, 0.0]}}]
+        pipeline.set_ip_adapter_scale(scales)
         ```
         """
         unet = getattr(self, self.unet_name) if not hasattr(self, "unet") else self.unet
-        if not isinstance(scale_configs, list):
-            scale_configs = [scale_configs]
-        scale_configs = _maybe_expand_lora_scales(unet, scale_configs, default_scale=default_scale)
+        if not isinstance(scale, list):
+            scale = [scale]
+        scale_configs = _maybe_expand_lora_scales(unet, scale, default_scale=default_scale)
 
         for attn_name, attn_processor in unet.attn_processors.items():
             if isinstance(attn_processor, (IPAdapterAttnProcessor, IPAdapterAttnProcessor2_0)):
@@ -275,9 +275,9 @@ class IPAdapterMixin:
                     scale_configs = scale_configs * len(attn_processor.scale)
                 for i, scale_config in enumerate(scale_configs):
                     if isinstance(scale_config, dict):
-                        for key, scale in scale_config.items():
-                            if attn_name.startswith(key):
-                                attn_processor.scale[i] = scale
+                        for k, s in scale_config.items():
+                            if attn_name.startswith(k):
+                                attn_processor.scale[i] = s
                     else:
                         attn_processor.scale[i] = scale_config
 
