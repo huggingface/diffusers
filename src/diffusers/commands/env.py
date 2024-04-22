baa822eb192463f2f66a3211f5a59f99dc6cc7a9
Tolga Cangöz <mtcangoz@gmail.com>
2024-04-22 14:49:18 +0300

    Fix import order in env.py

--- src/diffusers/commands/env.py
+++ src/diffusers/commands/env.py
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import getpass
 import os
 import platform
 import subprocess
-import getpass
 from argparse import ArgumentParser
 
 import huggingface_hub
