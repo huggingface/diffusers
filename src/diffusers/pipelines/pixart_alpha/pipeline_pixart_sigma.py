86f1a6ecd1fee856eb5364fafbc0737ddfb52673
Junsong Chen <cjs1020440147@icloud.com>
2024-04-23 09:20:02 +0800

    Update src/diffusers/pipelines/pixart_alpha/pipeline_pixart_sigma.py
    Co-authored-by: YiYi Xu <yixu310@gmail.com>

--- src/diffusers/pipelines/pixart_alpha/pipeline_pixart_sigma.py
+++ src/diffusers/pipelines/pixart_alpha/pipeline_pixart_sigma.py
@@ -732,9 +732,6 @@ class PixArtSigmaPipeline(DiffusionPipeline):
                 If `return_dict` is `True`, [`~pipelines.ImagePipelineOutput`] is returned, otherwise a `tuple` is
                 returned where the first element is a list with the generated images
         """
-        if "mask_feature" in kwargs:
-            deprecation_message = "The use of `mask_feature` is deprecated. It is no longer used in any computation and that doesn't affect the end results. It will be removed in a future version."
-            deprecate("mask_feature", "1.0.0", deprecation_message, standard_warn=False)
         # 1. Check inputs. Raise error if not correct
         height = height or self.transformer.config.sample_size * self.vae_scale_factor
         width = width or self.transformer.config.sample_size * self.vae_scale_factor
