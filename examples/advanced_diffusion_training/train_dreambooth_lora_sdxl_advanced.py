c5823838ebaf1eb2d8ace74144b8d805c87417f2
linoytsaban <linoy@huggingface.co>
2024-04-25 18:20:04 +0300

    minor changes

--- examples/advanced_diffusion_training/train_dreambooth_lora_sdxl_advanced.py
+++ examples/advanced_diffusion_training/train_dreambooth_lora_sdxl_advanced.py
@@ -1546,7 +1546,7 @@ def main(args):
             if args.train_text_encoder:
                 models.extend([text_encoder_one_, text_encoder_two_])
                 # only upcast trainable parameters (LoRA) into fp32
-                cast_training_params(models)
+            cast_training_params(models)
 
     accelerator.register_save_state_pre_hook(save_model_hook)
     accelerator.register_load_state_pre_hook(load_model_hook)
