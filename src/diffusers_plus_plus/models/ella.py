b171b124df46d5d4f06237d7344e03b43d7a9796
shauray8 <shauray9@gmail.com>
2024-04-25 10:22:06 +0530

    make unet config

--- src/diffusers_plus_plus/models/ella.py
+++ src/diffusers_plus_plus/models/ella.py
@@ -149,3 +149,63 @@ class PerceiverResampler(nn.Module):
 
         return latents
 
+class ELLAProxyUNet(torch.nn.Module):
+    def __init__(self, unet):
+        super().__init__()
+        # In order to still use the diffusers pipeline, including various workaround
+
+        self.ella = ELLA()
+        self.unet = unet
+        self.config = unet.config
+        self.dtype = unet.dtype
+        self.device = unet.device
+
+        self.flexible_max_length_workaround = None
+
+    def forward(
+        self,
+        sample: torch.FloatTensor,
+        timestep: Union[torch.Tensor, float, int],
+        encoder_hidden_states: torch.Tensor,
+        class_labels: Optional[torch.Tensor] = None,
+        timestep_cond: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        cross_attention_kwargs: Optional[dict[str, Any]] = None,
+        added_cond_kwargs: Optional[dict[str, torch.Tensor]] = None,
+        down_block_additional_residuals: Optional[tuple[torch.Tensor]] = None,
+        mid_block_additional_residual: Optional[torch.Tensor] = None,
+        down_intrablock_additional_residuals: Optional[tuple[torch.Tensor]] = None,
+        encoder_attention_mask: Optional[torch.Tensor] = None,
+        return_dict: bool = True,
+    ):
+        if self.flexible_max_length_workaround is not None:
+            time_aware_encoder_hidden_state_list = []
+            for i, max_length in enumerate(self.flexible_max_length_workaround):
+                time_aware_encoder_hidden_state_list.append(
+                    self.ella(encoder_hidden_states[i : i + 1, :max_length], timestep)
+                )
+            # No matter how many tokens are text features, the ella output must be 64 tokens.
+            time_aware_encoder_hidden_states = torch.cat(
+                time_aware_encoder_hidden_state_list, dim=0
+            )
+        else:
+            time_aware_encoder_hidden_states = self.ella(
+                encoder_hidden_states, timestep
+            )
+
+        return self.unet(
+            sample=sample,
+            timestep=timestep,
+            encoder_hidden_states=time_aware_encoder_hidden_states,
+            class_labels=class_labels,
+            timestep_cond=timestep_cond,
+            attention_mask=attention_mask,
+            cross_attention_kwargs=cross_attention_kwargs,
+            added_cond_kwargs=added_cond_kwargs,
+            down_block_additional_residuals=down_block_additional_residuals,
+            mid_block_additional_residual=mid_block_additional_residual,
+            down_intrablock_additional_residuals=down_intrablock_additional_residuals,
+            encoder_attention_mask=encoder_attention_mask,
+            return_dict=return_dict,
+        )
+
