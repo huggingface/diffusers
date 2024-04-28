e2e3274bec2e0b806274e5ec8261b26fae2d2ad1
bssrdf <bssrdf@gmail.com>
2024-04-27 22:04:50 -0400

    bring back import model_info

--- examples/dreambooth/train_dreambooth.py
+++ examples/dreambooth/train_dreambooth.py
@@ -34,7 +34,7 @@ import transformers
 from accelerate import Accelerator
 from accelerate.logging import get_logger
 from accelerate.utils import ProjectConfiguration, set_seed
-from huggingface_hub import create_repo, upload_folder
+from huggingface_hub import create_repo, model_info, upload_folder
 from huggingface_hub.utils import insecure_hashlib
 from packaging import version
 from PIL import Image
