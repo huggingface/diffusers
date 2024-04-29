1a9b15e82aa0753fc44cd5d52b969d03c2d9a171
bssrdf <bssrdf@gmail.com>
2024-04-29 08:56:41 -0400

    switch to using Path.as_posix() to resolve OS dependence

--- examples/dreambooth/train_dreambooth.py
+++ examples/dreambooth/train_dreambooth.py
@@ -758,7 +758,7 @@ class PromptDataset(Dataset):
         return example
 
 def model_has_vae(args):
-    config_file_name = f"vae/{AutoencoderKL.config_name}"
+    config_file_name = Path("vae", AutoencoderKL.config_name).as_posix()
     if os.path.isdir(args.pretrained_model_name_or_path):
         config_file_name = os.path.join(args.pretrained_model_name_or_path, config_file_name)
         return os.path.isfile(config_file_name)
