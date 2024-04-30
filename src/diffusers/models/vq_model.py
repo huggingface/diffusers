adbed4591a8fd0c01473b1df339014ea6ac98f71
isamu-isozaki <isamu.website@gmail.com>
2024-04-29 23:54:28 -0400

    Remove diffs

--- src/diffusers/models/vq_model.py
+++ src/diffusers/models/vq_model.py
@@ -167,6 +167,7 @@ class VQModel(ModelMixin, ConfigMixin):
             sample (`torch.FloatTensor`): Input sample.
             return_dict (`bool`, *optional*, defaults to `True`):
                 Whether or not to return a [`models.vq_model.VQEncoderOutput`] instead of a plain tuple.
+
         Returns:
             [`~models.vq_model.VQEncoderOutput`] or `tuple`:
                 If return_dict is True, a [`~models.vq_model.VQEncoderOutput`] is returned, otherwise a plain `tuple`
