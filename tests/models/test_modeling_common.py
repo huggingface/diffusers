642ee39af5c773970805adca071a1b76bc62d9fe
sayakpaul <spsayakpaul@gmail.com>
2024-05-01 17:39:40 +0530

    debug

--- tests/models/test_modeling_common.py
+++ tests/models/test_modeling_common.py
@@ -125,6 +125,7 @@ class ModelUtilsTest(unittest.TestCase):
                 )
 
             download_requests = [r.method for r in m.request_history]
+            print(f"{download_requests=}")
             assert download_requests.count("HEAD") == 2, "2 HEAD requests one for config, one for model"
             assert download_requests.count("GET") == 2, "2 GET requests one for config, one for model"
 
