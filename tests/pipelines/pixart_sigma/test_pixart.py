ffed0b2c10242f989e951d8ec40eaaf66297ab44
sayakpaul <spsayakpaul@gmail.com>
2024-05-02 09:41:22 +0530

    debug

--- tests/pipelines/pixart_sigma/test_pixart.py
+++ tests/pipelines/pixart_sigma/test_pixart.py
@@ -336,6 +336,7 @@ class PixArtSigmaPipelineIntegrationTests(unittest.TestCase):
         image = pipe(prompt, generator=generator, num_inference_steps=2, output_type="np").images
 
         image_slice = image[0, -3:, -3:, -1]
+        print(", ".join([str(round(x, 4)) for x in image_slice.tolist()]))
         expected_slice = np.array([0.0742, 0.0835, 0.2114, 0.0295, 0.0784, 0.2361, 0.1738, 0.2251, 0.3589])
 
         max_diff = numpy_cosine_similarity_distance(image_slice.flatten(), expected_slice)
