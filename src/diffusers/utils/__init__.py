21334ad4f2be38d6734753f57d7e1daf4de930dd
Shauray Singh <39147312+shauray8@users.noreply.github.com>
2024-05-03 17:42:27 +0530

    parent import

--- src/diffusers/utils/__init__.py
+++ src/diffusers/utils/__init__.py
@@ -108,7 +108,7 @@ from .state_dict_utils import (
     convert_state_dict_to_peft,
     convert_unet_state_dict_to_peft,
 )
-from torch_utils import (
+from .torch_utils import (
     randn_tensor,
 )
 
