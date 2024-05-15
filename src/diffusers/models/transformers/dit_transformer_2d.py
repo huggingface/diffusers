57f367c689e507143cac50a6e9b8c4f3cad7e0fd
sayakpaul <spsayakpaul@gmail.com>
2024-05-15 15:52:06 +0530

    fix tests

--- src/diffusers/models/transformers/dit_transformer_2d.py
+++ src/diffusers/models/transformers/dit_transformer_2d.py
@@ -73,7 +73,6 @@ class DiTTransformer2DModel(ModelMixin, ConfigMixin):
     """
 
     _supports_gradient_checkpointing = True
-    _no_split_modules = ["BasicTransformerBlock"]
 
     @register_to_config
     def __init__(
--- tests/models/transformers/test_models_pixart_transformer2d.py
+++ tests/models/transformers/test_models_pixart_transformer2d.py
@@ -34,8 +34,8 @@ enable_full_determinism()
 class PixArtTransformer2DModelTests(ModelTesterMixin, unittest.TestCase):
     model_class = PixArtTransformer2DModel
     main_input_name = "hidden_states"
-    # We override the items here because the unet under consideration is small.
-    model_split_percents = [0.5, 0.2, 0.4]
+    # We override the items here because the transformer under consideration is small.
+    model_split_percents = [0.7, 0.6, 0.6]
 
     @property
     def dummy_input(self):
