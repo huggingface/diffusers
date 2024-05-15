9d016fcd1dc4b9f947e3bab4dcbd0f36fb0ee3b5
sayakpaul <spsayakpaul@gmail.com>
2024-05-15 13:43:22 +0530

    pixart tests

--- tests/models/transformers/test_models_pixart_transformer2d.py
+++ tests/models/transformers/test_models_pixart_transformer2d.py
@@ -17,7 +17,7 @@ import unittest
 
 import torch
 
-from diffusers import DiTTransformer2DModel, Transformer2DModel
+from diffusers import PixArtTransformer2DModel, Transformer2DModel
 from diffusers.utils.testing_utils import (
     enable_full_determinism,
     floats_tensor,
@@ -31,8 +31,8 @@ from ..test_modeling_common import ModelTesterMixin
 enable_full_determinism()
 
 
-class DiTTransformer2DModelTests(ModelTesterMixin, unittest.TestCase):
-    model_class = DiTTransformer2DModel
+class PixArtTransformer2DModelTests(ModelTesterMixin, unittest.TestCase):
+    model_class = PixArtTransformer2DModel
     main_input_name = "hidden_states"
 
     @property
@@ -41,13 +41,19 @@ class DiTTransformer2DModelTests(ModelTesterMixin, unittest.TestCase):
         in_channels = 4
         sample_size = 8
         scheduler_num_train_steps = 1000
-        num_class_labels = 4
+        cross_attention_dim = 8
+        seq_len = 8
 
         hidden_states = floats_tensor((batch_size, in_channels, sample_size, sample_size)).to(torch_device)
         timesteps = torch.randint(0, scheduler_num_train_steps, size=(batch_size,)).to(torch_device)
-        class_label_ids = torch.randint(0, num_class_labels, size=(batch_size,)).to(torch_device)
+        encoder_hidden_states = floats_tensor((batch_size, seq_len, cross_attention_dim)).to(torch_device)
 
-        return {"hidden_states": hidden_states, "timestep": timesteps, "class_labels": class_label_ids}
+        return {
+            "hidden_states": hidden_states,
+            "timestep": timesteps,
+            "encoder_hidden_states": encoder_hidden_states,
+            "added_cond_kwargs": {"aspect_ratio": None, "resolution": None},
+        }
 
     @property
     def input_shape(self):
@@ -59,17 +65,22 @@ class DiTTransformer2DModelTests(ModelTesterMixin, unittest.TestCase):
 
     def prepare_init_args_and_inputs_for_common(self):
         init_dict = {
-            "in_channels": 4,
-            "out_channels": 8,
-            "activation_fn": "gelu-approximate",
-            "num_attention_heads": 2,
-            "attention_head_dim": 4,
-            "attention_bias": True,
-            "num_layers": 1,
-            "norm_type": "ada_norm_zero",
-            "num_embeds_ada_norm": 8,
-            "patch_size": 2,
             "sample_size": 8,
+            "num_layers": 1,
+            "patch_size": 2,
+            "attention_head_dim": 2,
+            "num_attention_heads": 2,
+            "in_channels": 4,
+            "cross_attention_dim": 8,
+            "out_channels": 8,
+            "attention_bias": True,
+            "activation_fn": "gelu-approximate",
+            "num_embeds_ada_norm": 8,
+            "norm_type": "ada_norm_single",
+            "norm_elementwise_affine": False,
+            "norm_eps": 1e-6,
+            "use_additional_conditions": False,
+            "caption_channels": None,
         }
         inputs_dict = self.dummy_input
         return init_dict, inputs_dict
@@ -79,5 +90,5 @@ class DiTTransformer2DModelTests(ModelTesterMixin, unittest.TestCase):
 
     @slow
     def test_correct_class_remapping(self):
-        model = Transformer2DModel.from_pretrained("facebook/DiT-XL-2-256", subfolder="transformer")
-        assert isinstance(model, DiTTransformer2DModel)
+        model = Transformer2DModel.from_pretrained("PixArt-alpha/PixArt-XL-2-1024-MS", subfolder="transformer")
+        assert isinstance(model, PixArtTransformer2DModel)
