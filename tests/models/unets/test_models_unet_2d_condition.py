d144526f02688fbe82e874a2257a75906fe78db0
sayakpaul <spsayakpaul@gmail.com>
2024-05-15 11:03:56 +0530

    add a test to check if loading of sharded checkpoints from the Hub is okay

--- tests/models/unets/test_models_unet_2d_condition.py
+++ tests/models/unets/test_models_unet_2d_condition.py
@@ -983,6 +983,15 @@ class UNet2DConditionModelTests(ModelTesterMixin, UNetTesterMixin, unittest.Test
         assert sample2.allclose(sample5, atol=1e-4, rtol=1e-4)
         assert sample2.allclose(sample6, atol=1e-4, rtol=1e-4)
 
+    @require_torch_gpu
+    def test_load_sharded_checkpoint_from_hub(self):
+        _, inputs_dict = self.prepare_init_args_and_inputs_for_common()
+        loaded_model = self.model_class.from_pretrained("hf-internal-testing/unet2d-sharded-dummy", device_map="auto")
+        new_output = loaded_model(**inputs_dict)
+
+        assert loaded_model
+        assert new_output.sample.shape == (4, 4, 16, 16)
+
 
 @slow
 class UNet2DConditionModelIntegrationTests(unittest.TestCase):
