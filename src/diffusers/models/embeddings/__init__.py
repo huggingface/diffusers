fa21b54b917d350a9b21add2b5ca344886ef6323
sayakpaul <spsayakpaul@gmail.com>
2024-05-21 15:39:43 +0530

    register the util blocks.

--- src/diffusers/models/embeddings/__init__.py
+++ src/diffusers/models/embeddings/__init__.py
@@ -1,3 +1,10 @@
+from .embeddings_utils import (
+    get_1d_sincos_pos_embed_from_grid,
+    get_2d_sincos_pos_embed,
+    get_2d_sincos_pos_embed_from_grid,
+    get_fourier_embeds_from_boundingbox,
+    get_timestep_embedding,
+)
 from .ip_adapter import (
     IPAdapterFaceIDImageProjection,
     IPAdapterFaceIDPlusImageProjection,
