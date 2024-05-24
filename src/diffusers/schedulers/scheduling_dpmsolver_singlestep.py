a21e518741ac3e343f9be3cacb842ace67d729e5
Tolga Cangöz <mtcangoz@gmail.com>
2024-05-24 17:15:01 +0300

    refactor: Move randn_tensor import to torch_utils module

--- src/diffusers/schedulers/scheduling_dpmsolver_singlestep.py
+++ src/diffusers/schedulers/scheduling_dpmsolver_singlestep.py
@@ -21,7 +21,8 @@ import numpy as np
 import torch
 
 from ..configuration_utils import ConfigMixin, register_to_config
-from ..utils import deprecate, logging, randn_tensor
+from ..utils import deprecate, logging
+from ..utils.torch_utils import randn_tensor
 from .scheduling_utils import KarrasDiffusionSchedulers, SchedulerMixin, SchedulerOutput
 
 
