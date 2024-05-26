a0f7e183630bcb3eb5a96d0f6e2fd6f77562fddf
PommesPeter <me@pommespeter.space>
2024-05-26 22:36:49 +0800

    fixed typo and added test code

--- src/diffusers/utils/dummy_torch_and_transformers_objects.py
+++ src/diffusers/utils/dummy_torch_and_transformers_objects.py
@@ -692,6 +692,21 @@ class LEditsPPPipelineStableDiffusionXL(metaclass=DummyObject):
         requires_backends(cls, ["torch", "transformers"])
 
 
+class LuminaText2ImgPipeline(metaclass=DummyObject):
+    _backends = ["torch", "transformers"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch", "transformers"])
+
+    @classmethod
+    def from_config(cls, *args, **kwargs):
+        requires_backends(cls, ["torch", "transformers"])
+
+    @classmethod
+    def from_pretrained(cls, *args, **kwargs):
+        requires_backends(cls, ["torch", "transformers"])
+
+
 class MusicLDMPipeline(metaclass=DummyObject):
     _backends = ["torch", "transformers"]
 
