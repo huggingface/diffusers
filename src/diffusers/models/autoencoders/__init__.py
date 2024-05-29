3a932ec0bd4a632d4f425d0039e30c23e183a3ff
Sayak Paul <spsayakpaul@gmail.com>
2024-05-29 06:30:35 +0530

    move `vqmodel` to `models.autoencoders`. (#8292)
    move vqmodel to models.autoencoders.

--- src/diffusers/models/autoencoders/__init__.py
+++ src/diffusers/models/autoencoders/__init__.py
@@ -3,3 +3,4 @@ from .autoencoder_kl import AutoencoderKL
 from .autoencoder_kl_temporal_decoder import AutoencoderKLTemporalDecoder
 from .autoencoder_tiny import AutoencoderTiny
 from .consistency_decoder_vae import ConsistencyDecoderVAE
+from .vq_model import VQModel
