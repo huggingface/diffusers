211062b5879a2daa21fb8d3b67e596ec81cace6d
sayakpaul <spsayakpaul@gmail.com>
2024-06-03 14:30:10 +0400

    reuse self.input_shape in unet2dcondition tests

--- tests/models/unets/test_models_unet_2d_condition.py
+++ tests/models/unets/test_models_unet_2d_condition.py
@@ -328,10 +328,8 @@ class UNet2DConditionModelTests(ModelTesterMixin, UNetTesterMixin, unittest.Test
     @property
     def dummy_input(self):
         batch_size = 4
-        num_channels = 4
-        sizes = (16, 16)
 
-        noise = floats_tensor((batch_size, num_channels) + sizes).to(torch_device)
+        noise = floats_tensor((batch_size,) + self.input_shape).to(torch_device)
         time_step = torch.tensor([10]).to(torch_device)
         encoder_hidden_states = floats_tensor((batch_size, 4, 8)).to(torch_device)
 
