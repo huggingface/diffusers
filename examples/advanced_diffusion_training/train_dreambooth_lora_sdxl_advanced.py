1a4ac4dd9bf1628eec3069ece740707b518de4f9
linoytsaban <linoy@huggingface.co>
2024-06-05 10:38:16 +0300

    aligning with blora script

--- examples/advanced_diffusion_training/train_dreambooth_lora_sdxl_advanced.py
+++ examples/advanced_diffusion_training/train_dreambooth_lora_sdxl_advanced.py
@@ -1659,7 +1659,7 @@ def main(args):
             if args.train_text_encoder:
                 models.extend([text_encoder_one_, text_encoder_two_])
                 # only upcast trainable parameters (LoRA) into fp32
-                cast_training_params(models)
+            cast_training_params(models)
 
     accelerator.register_save_state_pre_hook(save_model_hook)
     accelerator.register_load_state_pre_hook(load_model_hook)
@@ -2048,6 +2048,7 @@ def main(args):
                         tokens_two = tokenize_prompt(tokenizer_two, prompts, add_special_tokens)
 
                 if args.cache_latents:
+                    print("NOOOOOOOOO 1")
                     model_input = latents_cache[step].sample()
                 else:
                     pixel_values = batch["pixel_values"].to(dtype=vae.dtype)
@@ -2255,6 +2256,7 @@ def main(args):
 
                 # every step, we reset the embeddings to the original embeddings.
                 if args.train_text_encoder_ti:
+                    print("NOOOOOOOOO 2")
                     embedding_handler.retract_embeddings()
 
             # Checks if the accelerator has performed an optimization step behind the scenes
@@ -2341,6 +2343,7 @@ def main(args):
         unet_lora_layers = convert_state_dict_to_diffusers(get_peft_model_state_dict(unet))
 
         if args.train_text_encoder:
+            print("NOOOOOOOOO 3")
             text_encoder_one = unwrap_model(text_encoder_one)
             text_encoder_lora_layers = convert_state_dict_to_diffusers(
                 get_peft_model_state_dict(text_encoder_one.to(torch.float32))
