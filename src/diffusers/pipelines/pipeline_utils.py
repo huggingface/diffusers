b55372263958a26629f333fb00dcafb87906b3e7
sayakpaul <spsayakpaul@gmail.com>
2024-06-07 15:14:18 +0530

    add suport for using sharded models in the pipeline context

--- src/diffusers/pipelines/pipeline_utils.py
+++ src/diffusers/pipelines/pipeline_utils.py
@@ -376,11 +376,12 @@ class DiffusionPipeline(ConfigMixin, PushToHubMixin):
             if not is_accelerate_available() or is_accelerate_version("<", "0.14.0"):
                 return False
 
-            return hasattr(module, "_hf_hook") and (
-                isinstance(module._hf_hook, accelerate.hooks.AlignDevicesHook)
-                or hasattr(module._hf_hook, "hooks")
-                and isinstance(module._hf_hook.hooks[0], accelerate.hooks.AlignDevicesHook)
-            )
+            if hasattr(module, "_no_split_modules") and module._no_split_modules is None:
+                return hasattr(module, "_hf_hook") and (
+                    isinstance(module._hf_hook, accelerate.hooks.AlignDevicesHook)
+                    or hasattr(module._hf_hook, "hooks")
+                    and isinstance(module._hf_hook.hooks[0], accelerate.hooks.AlignDevicesHook)
+                )
 
         def module_is_offloaded(module):
             if not is_accelerate_available() or is_accelerate_version("<", "0.17.0.dev0"):
