ca0887fe16108c07d438f8b1906908dfb7fba3e2
Jianqi Pan <jannchie@gmail.com>
2024-06-08 22:00:40 +0900

    :wrench: chore: use modeling_outputs.Transformer2DModelOutput

--- src/diffusers/models/transformers/dual_transformer_2d.py
+++ src/diffusers/models/transformers/dual_transformer_2d.py
@@ -15,7 +15,8 @@ from typing import Optional
 
 from torch import nn
 
-from .transformer_2d import Transformer2DModel, Transformer2DModelOutput
+from .transformer_2d import Transformer2DModel
+from ..modeling_outputs import Transformer2DModelOutput
 
 
 class DualTransformer2DModel(nn.Module):
