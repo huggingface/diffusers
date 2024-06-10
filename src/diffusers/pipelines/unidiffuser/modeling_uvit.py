cbcd5c2c7d92b72af9da2b3931b80fc67c7f0053
Jianqi Pan <jannchie@gmail.com>
2024-06-10 19:07:54 +0900

    :wrench: chore: isort

--- src/diffusers/pipelines/unidiffuser/modeling_uvit.py
+++ src/diffusers/pipelines/unidiffuser/modeling_uvit.py
@@ -8,8 +8,7 @@ from ...configuration_utils import ConfigMixin, register_to_config
 from ...models import ModelMixin
 from ...models.attention import FeedForward
 from ...models.attention_processor import Attention
-from ...models.embeddings import (TimestepEmbedding, Timesteps,
-                                  get_2d_sincos_pos_embed)
+from ...models.embeddings import TimestepEmbedding, Timesteps, get_2d_sincos_pos_embed
 from ...models.modeling_outputs import Transformer2DModelOutput
 from ...models.normalization import AdaLayerNorm
 from ...utils import logging
