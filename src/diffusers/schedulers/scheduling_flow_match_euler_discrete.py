f22c199fd56696fa37ac816acdc4a6546d4a8d47
Dhruv Nair <dhruv.nair@gmail.com>
2024-06-12 23:49:44 +0530

    Update src/diffusers/schedulers/scheduling_flow_match_euler_discrete.py
    Co-authored-by: YiYi Xu <yixu310@gmail.com>

--- src/diffusers/schedulers/scheduling_flow_match_euler_discrete.py
+++ src/diffusers/schedulers/scheduling_flow_match_euler_discrete.py
@@ -36,9 +36,6 @@ class FlowMatchEulerDiscreteSchedulerOutput(BaseOutput):
         prev_sample (`torch.FloatTensor` of shape `(batch_size, num_channels, height, width)` for images):
             Computed sample `(x_{t-1})` of previous timestep. `prev_sample` should be used as next model input in the
             denoising loop.
-        pred_original_sample (`torch.FloatTensor` of shape `(batch_size, num_channels, height, width)` for images):
-            The predicted denoised sample `(x_{0})` based on the model output from the current timestep.
-            `pred_original_sample` can be used to preview progress or for guidance.
     """
 
     prev_sample: torch.FloatTensor
