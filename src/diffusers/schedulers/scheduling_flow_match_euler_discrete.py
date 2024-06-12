cbd29d27e6e169d38e8580f6954ee4ed66558798
Dhruv Nair <dhruv.nair@gmail.com>
2024-06-12 23:48:17 +0530

    Update src/diffusers/schedulers/scheduling_flow_match_euler_discrete.py
    Co-authored-by: YiYi Xu <yixu310@gmail.com>

--- src/diffusers/schedulers/scheduling_flow_match_euler_discrete.py
+++ src/diffusers/schedulers/scheduling_flow_match_euler_discrete.py
@@ -117,8 +117,7 @@ class FlowMatchEulerDiscreteScheduler(SchedulerMixin, ConfigMixin):
         noise: Optional[torch.FloatTensor] = None,
     ) -> torch.FloatTensor:
         """
-        Ensures interchangeability with schedulers that need to scale the denoising model input depending on the
-        current timestep. Scales the denoising model input by `(sigma**2 + 1) ** 0.5` to match the Euler algorithm.
+        Foward process in flow-matching
 
         Args:
             sample (`torch.FloatTensor`):
