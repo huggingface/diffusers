556aff4706524021d6f55b982488fc91478e2568
Shauray Singh <39147312+shauray8@users.noreply.github.com>
2024-06-13 13:27:45 +0530

    Update lpw_stable_diffusion_xl.py

--- examples/community/lpw_stable_diffusion_xl.py
+++ examples/community/lpw_stable_diffusion_xl.py
@@ -1178,13 +1178,9 @@ class SDXLLongPromptWeightingPipeline(
             shape = (
                 batch_size,
                 num_channels_latents,
-<<<<<<< HEAD
-                height // self.vae_scale_factor,
-                width // self.vae_scale_factor,
-=======
                 int(height) // self.vae_scale_factor,
                 int(width) // self.vae_scale_factor,
->>>>>>> 7f51f286a5397cb3e5c5a25693681aa4955e6241
+
             )
             if isinstance(generator, list) and len(generator) != batch_size:
                 raise ValueError(
