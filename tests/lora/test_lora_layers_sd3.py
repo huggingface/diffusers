701f60234ff78df11675974e2491cb63d04f5ade
Álvaro Somoza <somoza.alvaro@gmail.com>
2024-06-21 10:26:37 -0400

    changed testing repo, back to old class

--- tests/lora/test_lora_layers_sd3.py
+++ tests/lora/test_lora_layers_sd3.py
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import gc
 import os
 import sys
 import tempfile
@@ -28,13 +27,7 @@ from diffusers import (
     SD3Transformer2DModel,
     StableDiffusion3Pipeline,
 )
-from diffusers.utils.testing_utils import (
-    is_peft_available,
-    require_peft_backend,
-    require_torch_gpu,
-    slow,
-    torch_device,
-)
+from diffusers.utils.testing_utils import is_peft_available, require_peft_backend, require_torch_gpu, torch_device
 
 
 if is_peft_available():
@@ -295,120 +288,19 @@ class SD3LoRATests(unittest.TestCase):
             np.allclose(ouput_fused, output_unfused_lora, atol=1e-3, rtol=1e-3), "Fused lora should change the output"
         )
 
-
-@require_peft_backend
-@slow
-@require_torch_gpu
-class SD3LoRASlowTests(unittest.TestCase):
-    pipeline_class = StableDiffusion3Pipeline
-    params = frozenset(
-        [
-            "prompt",
-            "height",
-            "width",
-            "guidance_scale",
-            "negative_prompt",
-            "prompt_embeds",
-            "negative_prompt_embeds",
-        ]
-    )
-    batch_params = frozenset(["prompt", "negative_prompt"])
-
-    def setUp(self):
-        super().setUp()
-        gc.collect()
-        torch.cuda.empty_cache()
-
-    def tearDown(self):
-        super().tearDown()
-        gc.collect()
-        torch.cuda.empty_cache()
-
-    def get_dummy_components(self):
-        torch.manual_seed(0)
-        transformer = SD3Transformer2DModel(
-            sample_size=32,
-            patch_size=1,
-            in_channels=4,
-            num_layers=1,
-            attention_head_dim=8,
-            num_attention_heads=4,
-            caption_projection_dim=32,
-            joint_attention_dim=32,
-            pooled_projection_dim=64,
-            out_channels=4,
-        )
-        clip_text_encoder_config = CLIPTextConfig(
-            bos_token_id=0,
-            eos_token_id=2,
-            hidden_size=32,
-            intermediate_size=37,
-            layer_norm_eps=1e-05,
-            num_attention_heads=4,
-            num_hidden_layers=5,
-            pad_token_id=1,
-            vocab_size=1000,
-            hidden_act="gelu",
-            projection_dim=32,
-        )
-
-        torch.manual_seed(0)
-        text_encoder = CLIPTextModelWithProjection(clip_text_encoder_config)
-
-        torch.manual_seed(0)
-        text_encoder_2 = CLIPTextModelWithProjection(clip_text_encoder_config)
-
-        text_encoder_3 = T5EncoderModel.from_pretrained("hf-internal-testing/tiny-random-t5")
-
-        tokenizer = CLIPTokenizer.from_pretrained("hf-internal-testing/tiny-random-clip")
-        tokenizer_2 = CLIPTokenizer.from_pretrained("hf-internal-testing/tiny-random-clip")
-        tokenizer_3 = AutoTokenizer.from_pretrained("hf-internal-testing/tiny-random-t5")
-
-        torch.manual_seed(0)
-        vae = AutoencoderKL(
-            sample_size=32,
-            in_channels=3,
-            out_channels=3,
-            block_out_channels=(4,),
-            layers_per_block=1,
-            latent_channels=4,
-            norm_num_groups=1,
-            use_quant_conv=False,
-            use_post_quant_conv=False,
-            shift_factor=0.0609,
-            scaling_factor=1.5035,
-        )
-
-        scheduler = FlowMatchEulerDiscreteScheduler()
-
-        return {
-            "scheduler": scheduler,
-            "text_encoder": text_encoder,
-            "text_encoder_2": text_encoder_2,
-            "text_encoder_3": text_encoder_3,
-            "tokenizer": tokenizer,
-            "tokenizer_2": tokenizer_2,
-            "tokenizer_3": tokenizer_3,
-            "transformer": transformer,
-            "vae": vae,
-        }
-
+    @require_torch_gpu
     def test_sd3_lora(self):
-        """
-        Test loading the loras that are saved with the diffusers and peft formats.
-        Related PR: https://github.com/huggingface/diffusers/pull/8584
-        """
         components = self.get_dummy_components()
 
         pipe = self.pipeline_class(**components)
         pipe = pipe.to(torch_device)
         pipe.set_progress_bar_config(disable=None)
 
-        lora_model_id = "OzzyGT/lora_test"
+        lora_model_id = "hf-internal-testing/tiny-sd3-loras"
+
         lora_filename = "lora_diffusers_format.safetensors"
         pipe.load_lora_weights(lora_model_id, weight_name=lora_filename)
         pipe.unload_lora_weights()
 
-        lora_model_id = "OzzyGT/lora_test"
         lora_filename = "lora_peft_format.safetensors"
         pipe.load_lora_weights(lora_model_id, weight_name=lora_filename)
