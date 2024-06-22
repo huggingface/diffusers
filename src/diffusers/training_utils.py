8bdca089ad2c66645a7f2bb40855b5c8a12c4ab7
drhead <1313496+drhead@users.noreply.github.com>
2024-06-21 23:29:33 -0400

    add foreach to from_pretrained

--- src/diffusers/training_utils.py
+++ src/diffusers/training_utils.py
@@ -350,11 +350,11 @@ class EMAModel:
         self.model_config = model_config
 
     @classmethod
-    def from_pretrained(cls, path, model_cls) -> "EMAModel":
+    def from_pretrained(cls, path, model_cls, foreach) -> "EMAModel":
         _, ema_kwargs = model_cls.load_config(path, return_unused_kwargs=True)
         model = model_cls.from_pretrained(path)
 
-        ema_model = cls(model.parameters(), model_cls=model_cls, model_config=model.config)
+        ema_model = cls(model.parameters(), model_cls=model_cls, model_config=model.config, foreach=foreach)
 
         ema_model.load_state_dict(ema_kwargs)
         return ema_model
