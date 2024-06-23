47b327e705f922093895bafe2fc85082ff663860
RandomGitUser321 <ruinerx@gmail.com>
2024-06-23 12:29:31 -0400

    more minor fixes

--- src/diffusers/pipelines/pixart_alpha/pipeline_pixart_sigma.py
+++ src/diffusers/pipelines/pixart_alpha/pipeline_pixart_sigma.py
@@ -618,10 +618,7 @@ class PixArtSigmaPipeline(DiffusionPipeline):
                 f" size of {batch_size}. Make sure the batch size matches the length of the generators."
             )
 
-        if latents is None:
-            latents = randn_tensor(shape, generator=generator, device=device, dtype=dtype)
-        else:
-            latents = latents.to(device)
+        latents = randn_tensor(shape, generator=generator, device=device, dtype=dtype)
 
         # scale the initial noise by the standard deviation required by the scheduler
         latents = latents * self.scheduler.init_noise_sigma
