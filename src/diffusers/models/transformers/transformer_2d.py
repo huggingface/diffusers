56ae0d8b863e29d87d661e4f2ef51c05625502b3
sayakpaul <spsayakpaul@gmail.com>
2024-06-25 20:25:01 +0530

    remove deprecation from transformer2d regarding the output class.

--- src/diffusers/models/transformers/transformer_2d.py
+++ src/diffusers/models/transformers/transformer_2d.py
@@ -29,11 +29,6 @@ from ..normalization import AdaLayerNormSingle
 logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
 
 
-class Transformer2DModelOutput(Transformer2DModelOutput):
-    deprecation_message = "Importing `Transformer2DModelOutput` from `diffusers.models.transformer_2d` is deprecated and this will be removed in a future version. Please use `from diffusers.models.modeling_outputs import Transformer2DModelOutput`, instead."
-    deprecate("Transformer2DModelOutput", "1.0.0", deprecation_message)
-
-
 class Transformer2DModel(LegacyModelMixin, LegacyConfigMixin):
     """
     A 2D Transformer model for image-like data.
