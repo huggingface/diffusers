1479729dde391f5c9a477a8b900f8ba969b4007c
Sayak Paul <spsayakpaul@gmail.com>
2024-06-26 23:08:57 +0530

    [LoRA] fix vanilla fine-tuned lora loading. (#8691)
    fix vanilla fine-tuned lora loading.

--- src/diffusers/loaders/lora.py
+++ src/diffusers/loaders/lora.py
@@ -395,8 +395,7 @@ class LoraLoaderMixin:
         # their prefixes.
         keys = list(state_dict.keys())
         only_text_encoder = all(key.startswith(cls.text_encoder_name) for key in keys)
-
-        if any(key.startswith(cls.unet_name) for key in keys) and not only_text_encoder:
+        if not only_text_encoder:
             # Load the layers corresponding to UNet.
             logger.info(f"Loading {cls.unet_name}.")
             unet.load_attn_procs(
