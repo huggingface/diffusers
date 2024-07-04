0da4a17fb1632f22aa14d5f194911ab54eb10f10
PommesPeter <54879512+PommesPeter@users.noreply.github.com>
2024-07-04 14:25:41 +0800

    Update src/diffusers/models/attention_processor.py
    Co-authored-by: YiYi Xu <yixu310@gmail.com>

--- src/diffusers/models/attention_processor.py
+++ src/diffusers/models/attention_processor.py
@@ -168,15 +168,12 @@ class Attention(nn.Module):
             self.norm_q = None
             self.norm_k = None
         elif qk_norm == "layer_norm":
-            if kv_heads is None:
-                q_norm_dim = dim_head
-                k_norm_dim = dim_head
-            else:
+            self.norm_q = nn.LayerNorm(dim_head, eps=eps)
+            self.norm_k = nn.LayerNorm(dim_head, eps=eps)
+            elif qk_norm == "layer_norm_across_heads":
                 # Lumina applys qk norm across all heads
-                q_norm_dim = dim_head * heads
-                k_norm_dim = dim_head * kv_heads
-            self.norm_q = nn.LayerNorm(q_norm_dim, eps=eps)
-            self.norm_k = nn.LayerNorm(k_norm_dim, eps=eps)
+                self.norm_q = nn.LayerNorm(dim_head * heads, eps=eps)
+                self.norm_k = nn.LayerNorm(dim_head * kv_heads, eps=eps)
         else:
             raise ValueError(f"unknown qk_norm: {qk_norm}. Should be None or 'layer_norm'")
 
