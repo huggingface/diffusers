870c04fbf7abd2c23f079680c890ce2231f321d6
maxin-cn <maxin.cn@outlook.com>
2024-07-09 10:26:18 +1000

    modify for Latte pipeline

--- src/diffusers/pipelines/latte/pipeline_latte.py
+++ src/diffusers/pipelines/latte/pipeline_latte.py
@@ -62,9 +62,67 @@ EXAMPLE_DOC_STRING = """
         ```
 """
 
+def retrieve_timesteps(
+    scheduler,
+    num_inference_steps: Optional[int] = None,
+    device: Optional[Union[str, torch.device]] = None,
+    timesteps: Optional[List[int]] = None,
+    sigmas: Optional[List[float]] = None,
+    **kwargs,
+):
+    """
+    Calls the scheduler's `set_timesteps` method and retrieves timesteps from the scheduler after the call. Handles
+    custom timesteps. Any kwargs will be supplied to `scheduler.set_timesteps`.
+
+    Args:
+        scheduler (`SchedulerMixin`):
+            The scheduler to get timesteps from.
+        num_inference_steps (`int`):
+            The number of diffusion steps used when generating samples with a pre-trained model. If used, `timesteps`
+            must be `None`.
+        device (`str` or `torch.device`, *optional*):
+            The device to which the timesteps should be moved to. If `None`, the timesteps are not moved.
+        timesteps (`List[int]`, *optional*):
+            Custom timesteps used to override the timestep spacing strategy of the scheduler. If `timesteps` is passed,
+            `num_inference_steps` and `sigmas` must be `None`.
+        sigmas (`List[float]`, *optional*):
+            Custom sigmas used to override the timestep spacing strategy of the scheduler. If `sigmas` is passed,
+            `num_inference_steps` and `timesteps` must be `None`.
+
+    Returns:
+        `Tuple[torch.Tensor, int]`: A tuple where the first element is the timestep schedule from the scheduler and the
+        second element is the number of inference steps.
+    """
+    if timesteps is not None and sigmas is not None:
+        raise ValueError("Only one of `timesteps` or `sigmas` can be passed. Please choose one to set custom values")
+    if timesteps is not None:
+        accepts_timesteps = "timesteps" in set(inspect.signature(scheduler.set_timesteps).parameters.keys())
+        if not accepts_timesteps:
+            raise ValueError(
+                f"The current scheduler class {scheduler.__class__}'s `set_timesteps` does not support custom"
+                f" timestep schedules. Please check whether you are using the correct scheduler."
+            )
+        scheduler.set_timesteps(timesteps=timesteps, device=device, **kwargs)
+        timesteps = scheduler.timesteps
+        num_inference_steps = len(timesteps)
+    elif sigmas is not None:
+        accept_sigmas = "sigmas" in set(inspect.signature(scheduler.set_timesteps).parameters.keys())
+        if not accept_sigmas:
+            raise ValueError(
+                f"The current scheduler class {scheduler.__class__}'s `set_timesteps` does not support custom"
+                f" sigmas schedules. Please check whether you are using the correct scheduler."
+            )
+        scheduler.set_timesteps(sigmas=sigmas, device=device, **kwargs)
+        timesteps = scheduler.timesteps
+        num_inference_steps = len(timesteps)
+    else:
+        scheduler.set_timesteps(num_inference_steps, device=device, **kwargs)
+        timesteps = scheduler.timesteps
+    return timesteps, num_inference_steps
+
 @dataclass
 class LattePipelineOutput(BaseOutput):
-    video: torch.Tensor
+    frames: torch.Tensor
 
 
 class LattePipeline(DiffusionPipeline):
@@ -642,8 +700,7 @@ class LattePipeline(DiffusionPipeline):
             prompt_embeds = torch.cat([negative_prompt_embeds, prompt_embeds], dim=0)
 
         # 4. Prepare timesteps
-        self.scheduler.set_timesteps(num_inference_steps, device=device)
-        timesteps = self.scheduler.timesteps
+        timesteps, num_inference_steps = retrieve_timesteps(self.scheduler, num_inference_steps, device, timesteps)
 
         # 5. Prepare latents.
         latent_channels = self.transformer.config.in_channels
@@ -721,7 +778,7 @@ class LattePipeline(DiffusionPipeline):
             video = self.video_processor.postprocess_video(video=video, output_type=output_type)
         else:
             video = latents
-            return LattePipelineOutput(video=video)
+            return LattePipelineOutput(frames=video)
 
         # Offload all models
         self.maybe_free_model_hooks()
@@ -729,9 +786,9 @@ class LattePipeline(DiffusionPipeline):
         if not return_dict:
             return (video,)
 
-        return LattePipelineOutput(video=video)
+        return LattePipelineOutput(frames=video)
     
-
+    # Copied from diffusers/pipelines/stable_video_diffusion/pipeline_stable_video_diffusion.py
     def decode_latents(self, latents, video_length, decode_chunk_size=1):
         # [batch, channels, frames, height, width] -> [batch*frames, channels, height, width]
         latents = latents.permute(0, 2, 1, 3, 4).flatten(0, 1)
