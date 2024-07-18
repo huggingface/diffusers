3227bcf59b73da3c9019a141a1b4ed790e52d1c1
Aryan <aryan@huggingface.co>
2024-07-18 17:14:53 +0200

    dummy objects

--- src/diffusers/utils/dummy_pt_objects.py
+++ src/diffusers/utils/dummy_pt_objects.py
@@ -362,6 +362,21 @@ class SD3Transformer2DModel(metaclass=DummyObject):
         requires_backends(cls, ["torch"])
 
 
+class SparseControlNetModel(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+    @classmethod
+    def from_config(cls, *args, **kwargs):
+        requires_backends(cls, ["torch"])
+
+    @classmethod
+    def from_pretrained(cls, *args, **kwargs):
+        requires_backends(cls, ["torch"])
+
+
 class T2IAdapter(metaclass=DummyObject):
     _backends = ["torch"]
 
