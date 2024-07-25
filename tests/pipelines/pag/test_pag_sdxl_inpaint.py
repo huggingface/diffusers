6cf2b940a99fb16abcd944b712b9b1acd05e2943
sayakpaul <spsayakpaul@gmail.com>
2024-07-25 07:40:26 +0530

    style

--- tests/pipelines/pag/test_pag_sdxl_inpaint.py
+++ tests/pipelines/pag/test_pag_sdxl_inpaint.py
@@ -269,9 +269,7 @@ class StableDiffusionXLPAGInpaintPipelineFastTests(
             64,
             3,
         ), f"the shape of the output image should be (1, 64, 64, 3) but got {image.shape}"
-        expected_slice = np.array(
-            [0.8366, 0.5513, 0.6105, 0.6213, 0.6957, 0.7400, 0.6614, 0.6102, 0.5239]
-        )
+        expected_slice = np.array([0.8366, 0.5513, 0.6105, 0.6213, 0.6957, 0.7400, 0.6614, 0.6102, 0.5239])
 
         max_diff = np.abs(image_slice.flatten() - expected_slice).max()
         assert max_diff < 1e-3, f"output is different from expected, {image_slice.flatten()}"
