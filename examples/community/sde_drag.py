527430d0a4aee332dc354f2fc61e2f7df1eb4d59
Sayak Paul <spsayakpaul@gmail.com>
2024-07-25 21:40:58 +0530

    [LoRA] introduce LoraBaseMixin to promote reusability. (#8774)
    * introduce  to promote reusability.
    
    * up
    
    * add more tests
    
    * up
    
    * remove comments.
    
    * fix fuse_nan test
    
    * clarify the scope of fuse_lora and unfuse_lora
    
    * remove space
    
    * rewrite fuse_lora a bit.
    
    * feedback
    
    * copy over load_lora_into_text_encoder.
    
    * address dhruv's feedback.
    
    * fix-copies
    
    * fix issubclass.
    
    * num_fused_loras
    
    * fix
    
    * fix
    
    * remove mapping
    
    * up
    
    * fix
    
    * style
    
    * fix-copies
    
    * change to SD3TransformerLoRALoadersMixin
    
    * Apply suggestions from code review
    
    Co-authored-by: Dhruv Nair <dhruv.nair@gmail.com>
    
    * up
    
    * handle wuerstchen
    
    * up
    
    * move lora to lora_pipeline.py
    
    * up
    
    * fix-copies
    
    * fix documentation.
    
    * comment set_adapters().
    
    * fix-copies
    
    * fix set_adapters() at the model level.
    
    * fix?
    
    * fix
    
    ---------
    
    Co-authored-by: Dhruv Nair <dhruv.nair@gmail.com>

--- examples/community/sde_drag.py
+++ examples/community/sde_drag.py
@@ -11,7 +11,7 @@ from tqdm.auto import tqdm
 from transformers import CLIPTextModel, CLIPTokenizer
 
 from diffusers import AutoencoderKL, DiffusionPipeline, DPMSolverMultistepScheduler, UNet2DConditionModel
-from diffusers.loaders import AttnProcsLayers, LoraLoaderMixin
+from diffusers.loaders import AttnProcsLayers, StableDiffusionLoraLoaderMixin
 from diffusers.models.attention_processor import (
     AttnAddedKVProcessor,
     AttnAddedKVProcessor2_0,
@@ -321,7 +321,7 @@ class SdeDragPipeline(DiffusionPipeline):
             optimizer.zero_grad()
 
         with tempfile.TemporaryDirectory() as save_lora_dir:
-            LoraLoaderMixin.save_lora_weights(
+            StableDiffusionLoraLoaderMixin.save_lora_weights(
                 save_directory=save_lora_dir,
                 unet_lora_layers=unet_lora_layers,
                 text_encoder_lora_layers=None,
