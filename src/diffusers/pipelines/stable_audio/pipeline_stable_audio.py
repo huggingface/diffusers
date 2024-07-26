29dc552cb32b19fe6e464675e15d42c702473d57
Yoach Lacombe <52246514+ylacombe@users.noreply.github.com>
2024-07-26 10:47:21 +0200

    Update src/diffusers/pipelines/stable_audio/pipeline_stable_audio.py
    Co-authored-by: YiYi Xu <yixu310@gmail.com>

--- src/diffusers/pipelines/stable_audio/pipeline_stable_audio.py
+++ src/diffusers/pipelines/stable_audio/pipeline_stable_audio.py
@@ -750,6 +750,8 @@ class StableAudioPipeline(DiffusionPipeline):
 
         if output_type == "np":
             audio = audio.cpu().float().numpy()
+        
+        self.maybe_free_model_hooks()
 
         if not return_dict:
             return (audio,)
