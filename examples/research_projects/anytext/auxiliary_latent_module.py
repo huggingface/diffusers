6e8088f61e0296f9308599c6d9fc4762a878b9de
Tolga Cangöz <mtcangoz@gmail.com>
2024-07-28 21:19:36 +0300

    Add initial template

---
+++ examples/research_projects/anytext/auxiliary_latent_module.py
@@ -0,0 +1,3 @@
+# text -> glyph render -> glyph l_g -> glyph block ->
+                                                    # +> fuse layer
+# position l_p -> position block ->
---
+++ examples/research_projects/anytext/text_embedding_module.py
@@ -0,0 +1,3 @@
+# text -> glyph render -> glyph lines -> OCR -> linear ->
+                                                        # +> Token Replacement -> FrozenCLIPEmbedderT3
+# text -> tokenizer ->
