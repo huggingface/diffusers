05b706c003666709471c62bd44b8f40190506000
Aryan <aryan@huggingface.co>
2024-08-01 12:39:39 +0530

    PAG variant for AnimateDiff (#8789)
    * add animatediff pag pipeline
    
    * remove unnecessary print
    
    * make fix-copies
    
    * fix ip-adapter bug
    
    * update docs
    
    * add fast tests and fix bugs
    
    * update
    
    * update
    
    * address review comments
    
    * update ip adapter single test expected slice
    
    * implement test_from_pipe_consistent_config; fix expected slice values
    
    * LoraLoaderMixin->StableDiffusionLoraLoaderMixin; add latest freeinit test

--- src/diffusers/pipelines/__init__.py
+++ src/diffusers/pipelines/__init__.py
@@ -143,6 +143,7 @@ else:
     )
     _import_structure["pag"].extend(
         [
+            "AnimateDiffPAGPipeline",
             "StableDiffusionPAGPipeline",
             "StableDiffusionControlNetPAGPipeline",
             "StableDiffusionXLPAGPipeline",
@@ -527,6 +528,7 @@ if TYPE_CHECKING or DIFFUSERS_SLOW_IMPORT:
         )
         from .musicldm import MusicLDMPipeline
         from .pag import (
+            AnimateDiffPAGPipeline,
             StableDiffusionControlNetPAGPipeline,
             StableDiffusionPAGPipeline,
             StableDiffusionXLControlNetPAGPipeline,
