2607b6b88c03bda907b9578607b659811f8247e6
Tolga Cangöz <mtcangoz@gmail.com>
2024-08-01 17:16:09 +0300

    Up

--- examples/research_projects/anytext/text_controlnet.py
+++ examples/research_projects/anytext/text_controlnet.py
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# TODO: Figure out `hint_channels` enigma(?)
 from typing import Any, Dict, Optional, Tuple, Union
 
 import torch
