01c747f94705f66cb5d67bf9192d410d7c894e91
sayakpaul <spsayakpaul@gmail.com>
2024-08-02 08:26:27 +0530

    fix tests

--- tests/pipelines/flux/test_pipeline_flux.py
+++ tests/pipelines/flux/test_pipeline_flux.py
@@ -13,28 +13,14 @@ from diffusers.utils.testing_utils import (
     torch_device,
 )
 
-from ..test_pipelines_common import (
-    PipelineTesterMixin,
-    check_qkv_fusion_matches_attn_procs_length,
-    check_qkv_fusion_processors_exist,
-)
+from ..test_pipelines_common import PipelineTesterMixin
 
 
 @unittest.skip("Tests needs to be revisited.")
 class FluxPipelineFastTests(unittest.TestCase, PipelineTesterMixin):
     pipeline_class = FluxPipeline
-    params = frozenset(
-        [
-            "prompt",
-            "height",
-            "width",
-            "guidance_scale",
-            "negative_prompt",
-            "prompt_embeds",
-            "negative_prompt_embeds",
-        ]
-    )
-    batch_params = frozenset(["prompt", "negative_prompt"])
+    params = frozenset(["prompt", "height", "width", "guidance_scale", "prompt_embeds", "pooled_prompt_embeds"])
+    batch_params = frozenset(["prompt"])
 
     def get_dummy_components(self):
         torch.manual_seed(0)
@@ -130,21 +116,6 @@ class FluxPipelineFastTests(unittest.TestCase, PipelineTesterMixin):
         # Outputs should be different here
         assert max_diff > 1e-2
 
-    def test_flux_different_negative_prompts(self):
-        pipe = self.pipeline_class(**self.get_dummy_components()).to(torch_device)
-
-        inputs = self.get_dummy_inputs(torch_device)
-        output_same_prompt = pipe(**inputs).images[0]
-
-        inputs = self.get_dummy_inputs(torch_device)
-        inputs["negative_prompt_2"] = "deformed"
-        output_different_prompts = pipe(**inputs).images[0]
-
-        max_diff = np.abs(output_same_prompt - output_different_prompts).max()
-
-        # Outputs should be different here
-        assert max_diff > 1e-2
-
     def test_flux_prompt_embeds(self):
         pipe = self.pipeline_class(**self.get_dummy_components()).to(torch_device)
         inputs = self.get_dummy_inputs(torch_device)
@@ -154,7 +125,6 @@ class FluxPipelineFastTests(unittest.TestCase, PipelineTesterMixin):
         inputs = self.get_dummy_inputs(torch_device)
         prompt = inputs.pop("prompt")
 
-        do_classifier_free_guidance = inputs["guidance_scale"] > 1
         (
             prompt_embeds,
             negative_prompt_embeds,
@@ -163,9 +133,6 @@ class FluxPipelineFastTests(unittest.TestCase, PipelineTesterMixin):
             text_ids,
         ) = pipe.encode_prompt(
             prompt,
-            prompt_2=None,
-            prompt_3=None,
-            do_classifier_free_guidance=do_classifier_free_guidance,
             device=torch_device,
         )
         output_with_embeds = pipe(
@@ -178,46 +145,6 @@ class FluxPipelineFastTests(unittest.TestCase, PipelineTesterMixin):
 
         max_diff = np.abs(output_with_prompt - output_with_embeds).max()
         assert max_diff < 1e-4
-
-    def test_fused_qkv_projections(self):
-        device = "cpu"  # ensure determinism for the device-dependent torch.Generator
-        components = self.get_dummy_components()
-        pipe = self.pipeline_class(**components)
-        pipe = pipe.to(device)
-        pipe.set_progress_bar_config(disable=None)
-
-        inputs = self.get_dummy_inputs(device)
-        image = pipe(**inputs).images
-        original_image_slice = image[0, -3:, -3:, -1]
-
-        # TODO (sayakpaul): will refactor this once `fuse_qkv_projections()` has been added
-        # to the pipeline level.
-        pipe.transformer.fuse_qkv_projections()
-        assert check_qkv_fusion_processors_exist(
-            pipe.transformer
-        ), "Something wrong with the fused attention processors. Expected all the attention processors to be fused."
-        assert check_qkv_fusion_matches_attn_procs_length(
-            pipe.transformer, pipe.transformer.original_attn_processors
-        ), "Something wrong with the attention processors concerning the fused QKV projections."
-
-        inputs = self.get_dummy_inputs(device)
-        image = pipe(**inputs).images
-        image_slice_fused = image[0, -3:, -3:, -1]
-
-        pipe.transformer.unfuse_qkv_projections()
-        inputs = self.get_dummy_inputs(device)
-        image = pipe(**inputs).images
-        image_slice_disabled = image[0, -3:, -3:, -1]
-
-        assert np.allclose(
-            original_image_slice, image_slice_fused, atol=1e-3, rtol=1e-3
-        ), "Fusion of QKV projections shouldn't affect the outputs."
-        assert np.allclose(
-            image_slice_fused, image_slice_disabled, atol=1e-3, rtol=1e-3
-        ), "Outputs, with QKV projection fusion enabled, shouldn't change when fused QKV projections are disabled."
-        assert np.allclose(
-            original_image_slice, image_slice_disabled, atol=1e-2, rtol=1e-2
-        ), "Original outputs should match when fused QKV projections are disabled."
 
 
 @slow
