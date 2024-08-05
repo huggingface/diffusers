1cdbb55a3fe16fa8779f349cdf69e9815963a3a1
Tolga Cangöz <mtcangoz@gmail.com>
2024-08-05 18:31:04 +0300

    `make style`

--- examples/research_projects/anytext/frozen_clip_embedder_t3.py
+++ examples/research_projects/anytext/frozen_clip_embedder_t3.py
@@ -110,8 +110,9 @@ class FrozenCLIPEmbedderT3(AbstractEncoder):
             )
             # CLIP's text model uses causal mask, prepare it here.
             # https://github.com/openai/CLIP/blob/cfcffb90e69f37bf2ff1e988237a0fbe41f33c04/clip/model.py#L324
-            causal_attention_mask = _create_4d_causal_attention_mask(input_shape, hidden_states.dtype,
-                                                                     device=hidden_states.device)
+            causal_attention_mask = _create_4d_causal_attention_mask(
+                input_shape, hidden_states.dtype, device=hidden_states.device
+            )
             # expand attention_mask
             if attention_mask is not None:
                 # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
