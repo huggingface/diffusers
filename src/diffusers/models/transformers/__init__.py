2dad462d9bf9890df09bfb088bf0a446c6074bec
zR <2448370773@qq.com>
2024-08-07 15:23:57 +0800

    Add CogVideoX text-to-video generation model (#9082)
    * add CogVideoX
    
    ---------
    
    Co-authored-by: Aryan <aryan@huggingface.co>
    Co-authored-by: sayakpaul <spsayakpaul@gmail.com>
    Co-authored-by: Aryan <contact.aryanvs@gmail.com>
    Co-authored-by: yiyixuxu <yixu310@gmail.com>
    Co-authored-by: Steven Liu <59462357+stevhliu@users.noreply.github.com>

--- src/diffusers/models/transformers/__init__.py
+++ src/diffusers/models/transformers/__init__.py
@@ -3,6 +3,7 @@ from ...utils import is_torch_available
 
 if is_torch_available():
     from .auraflow_transformer_2d import AuraFlowTransformer2DModel
+    from .cogvideox_transformer_3d import CogVideoXTransformer3DModel
     from .dit_transformer_2d import DiTTransformer2DModel
     from .dual_transformer_2d import DualTransformer2DModel
     from .hunyuan_transformer_2d import HunyuanDiT2DModel
