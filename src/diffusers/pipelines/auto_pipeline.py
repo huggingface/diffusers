a5b6529c54e31eae2bdf02312842e0204b5e4d58
gumgood <gumgood@naver.com>
2024-08-08 18:48:32 +0900

    fix: class name in `AutoPipelineForInpainting`

--- src/diffusers/pipelines/auto_pipeline.py
+++ src/diffusers/pipelines/auto_pipeline.py
@@ -953,7 +953,7 @@ class AutoPipelineForInpainting(ConfigMixin):
         if "enable_pag" in kwargs:
             enable_pag = kwargs.pop("enable_pag")
             if enable_pag:
-                orig_class_name = config["_class_name"].replace("Pipeline", "PAGPipeline")
+                orig_class_name = config["_class_name"].replace("InpaintPipeline", "PAGInpaintPipeline")
 
         inpainting_cls = _get_task_class(AUTO_INPAINT_PIPELINES_MAPPING, orig_class_name)
 
