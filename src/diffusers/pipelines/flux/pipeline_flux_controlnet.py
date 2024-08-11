e6fdb8cad25125f62ba4ebb73f584fe46ebbcdb2
Frank (Haofan) Wang <haofanwang.ai@gmail.com>
2024-08-12 00:46:33 +0800

    Update pipeline_flux_controlnet.py

--- src/diffusers/pipelines/flux/pipeline_flux_controlnet.py
+++ src/diffusers/pipelines/flux/pipeline_flux_controlnet.py
@@ -61,19 +61,13 @@ EXAMPLE_DOC_STRING = """
         >>> from diffusers import FluxControlNetPipeline
         >>> from diffusers import FluxControlNetModel
 
-        >>> controlnet_model = 'InstantX/FLUX.1-dev-controlnet-canny-alpha'
+        >>> controlnet_model = "InstantX/FLUX.1-dev-controlnet-canny-alpha"
         >>> controlnet = FluxControlNetModel.from_pretrained(controlnet_model, torch_dtype=torch.bfloat16)
         >>> pipe = FluxControlNetPipeline.from_pretrained(base_model, controlnet=controlnet, torch_dtype=torch.bfloat16)
         >>> pipe.to("cuda")
         >>> control_image = load_image("https://huggingface.co/InstantX/SD3-Controlnet-Canny/resolve/main/canny.jpg")
         >>> prompt = "A girl in city, 25 years old, cool, futuristic"
-        >>> image = pipe(
-                prompt,
-                control_image=control_image,
-                controlnet_conditioning_scale=0.6,
-                num_inference_steps=28,
-                guidance_scale=3.5,
-            ).images[0]
+        >>> image = pipe(prompt, control_image=control_image, controlnet_conditioning_scale=0.6, num_inference_steps=28, guidance_scale=3.5).images[0]
         >>> image.save("flux.png")
         ```
 """
