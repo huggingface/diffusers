make style; fix autoencoder scaling factor

scripts/convert_cogvideox_to_diffusers.py
@@ -126,11 +126,17 @@ def update_state_dict_inplace(state_dict: Dict[str, Any], old_key: str, new_key:
     state_dict[new_key] = state_dict.pop(old_key)
 
 
-def convert_transformer(ckpt_path: str, num_layers: int, num_attention_heads: int, use_rotary_positional_embeddings: bool):
+def convert_transformer(
+    ckpt_path: str, num_layers: int, num_attention_heads: int, use_rotary_positional_embeddings: bool
+):
     PREFIX_KEY = "model.diffusion_model."
 
     original_state_dict = get_state_dict(torch.load(ckpt_path, map_location="cpu", mmap=True))
-    transformer = CogVideoXTransformer3DModel(num_layers=num_layers, num_attention_heads=num_attention_heads, use_rotary_positional_embeddings=use_rotary_positional_embeddings)
+    transformer = CogVideoXTransformer3DModel(
+        num_layers=num_layers,
+        num_attention_heads=num_attention_heads,
+        use_rotary_positional_embeddings=use_rotary_positional_embeddings,
+    )
 
     for key in list(original_state_dict.keys()):
         new_key = key[len(PREFIX_KEY) :]
@@ -148,9 +154,9 @@ def convert_transformer(ckpt_path: str, num_layers: int, num_attention_heads: in
     return transformer
 
 
-def convert_vae(ckpt_path: str):
+def convert_vae(ckpt_path: str, scaling_factor: float):
     original_state_dict = get_state_dict(torch.load(ckpt_path, map_location="cpu", mmap=True))
-    vae = AutoencoderKLCogVideoX()
+    vae = AutoencoderKLCogVideoX(scaling_factor=scaling_factor)
 
     for key in list(original_state_dict.keys()):
         new_key = key[:]
@@ -188,7 +194,11 @@ def get_args():
     # For CogVideoX-2B, num_attention_heads is 30. For 5B, it is 48
     parser.add_argument("--num_attention_heads", type=int, default=30, help="Number of attention heads")
     # For CogVideoX-2B, use_rotary_positional_embeddings is False. For 5B, it is True
-    parser.add_argument("--use_rotary_positional_embeddings", action="store_true", default=False, help="Whether to use RoPE or not")
+    parser.add_argument(
+        "--use_rotary_positional_embeddings", action="store_true", default=False, help="Whether to use RoPE or not"
+    )
+    # For CogVideoX-2B, scaling_factor is 1.15258426. For 5B, it is 0.7
+    parser.add_argument("--scaling_factor", type=float, default=1.15258426, help="Scaling factor in the VAE")
     return parser.parse_args()
 
 
@@ -201,9 +211,14 @@ if __name__ == "__main__":
     if args.fp16 and args.bf16:
         raise ValueError("You cannot pass both --fp16 and --bf16 at the same time.")
     if args.transformer_ckpt_path is not None:
-        transformer = convert_transformer(args.transformer_ckpt_path, args.num_layers, args.num_attention_heads, args.use_rotary_positional_embeddings)
+        transformer = convert_transformer(
+            args.transformer_ckpt_path,
+            args.num_layers,
+            args.num_attention_heads,
+            args.use_rotary_positional_embeddings,
+        )
     if args.vae_ckpt_path is not None:
-        vae = convert_vae(args.vae_ckpt_path)
+        vae = convert_vae(args.vae_ckpt_path, args.scaling_factor)
 
     text_encoder_id = "google/t5-v1_1-xxl"
     tokenizer = T5Tokenizer.from_pretrained(text_encoder_id, model_max_length=TOKENIZER_MAX_LENGTH)

src/diffusers/models/embeddings.py
@@ -374,7 +374,9 @@ class CogVideoXPatchEmbed(nn.Module):
         return embeds
 
 
-def get_3d_rotary_pos_embed(embed_dim, crops_coords, grid_size, temporal_size=13, theta=10000, use_real=True):
+def get_3d_rotary_pos_embed(
+    embed_dim, crops_coords, grid_size, temporal_size, theta: int = 10000, use_real: bool = True
+) -> Union[torch.Tensor, Tuple[torch.Tensor, torch.Tensor]]:
     """
     RoPE for video tokens with 3D structure.
 
@@ -395,6 +397,23 @@ def get_3d_rotary_pos_embed(embed_dim, crops_coords, grid_size, temporal_size=13
     Returns:
         `torch.Tensor`: positional embedding with shape `(temporal_size * grid_size[0] * grid_size[1], embed_dim/2)`.
     """
+    # start, stop = crops_coords
+    # grid_t = torch.arange(temporal_size, dtype=torch.float32)
+    # grid_h = torch.arange(stop[0] - start[0], dtype=torch.float32)
+    # grid_w = torch.arange(stop[1] - start[1], dtype=torch.float32)
+
+    # dim_t = embed_dim // 4
+    # dim_h = embed_dim // 8 * 3
+    # dim_w = embed_dim // 8 * 3
+
+    # freqs_t = 1.0 / (theta ** (torch.arange(0, dim_t, 2, dtype=torch.float32)[: dim_t // 2] / dim_t))
+    # freqs_h = 1.0 / (theta ** (torch.arange(0, dim_h, 2, dtype=torch.float32)[: dim_h // 2] / dim_h))
+    # freqs_w = 1.0 / (theta ** (torch.arange(0, dim_w, 2, dtype=torch.float32)[: dim_w // 2] / dim_w))
+
+    # freqs_t = torch.einsum("..., f -> ... f", grid_t, freqs_t).repeat_interleave(2, dim=-1)
+    # freqs_h = torch.einsum("..., f -> ... f", grid_h, freqs_h).repeat_interleave(2, dim=-1)
+    # freqs_w = torch.einsum("..., f -> ... f", grid_w, freqs_w).repeat_interleave(2, dim=-1)
+
     start, stop = crops_coords
     grid_h = np.linspace(start[0], stop[0], grid_size[0], endpoint=False, dtype=np.float32)
     grid_w = np.linspace(start[1], stop[1], grid_size[1], endpoint=False, dtype=np.float32)
@@ -422,7 +441,7 @@ def get_3d_rotary_pos_embed(embed_dim, crops_coords, grid_size, temporal_size=13
     freqs_w = freqs_w.repeat_interleave(2, dim=-1)
 
     # Broadcast and concatenate tensors along specified dimension
-    def broadcat(tensors, dim=-1):
+    def broadcast(tensors, dim=-1):
         num_tensors = len(tensors)
         shape_lens = {len(t.shape) for t in tensors}
         assert len(shape_lens) == 1, "tensors must all have the same number of dimensions"
@@ -440,9 +459,8 @@ def get_3d_rotary_pos_embed(embed_dim, crops_coords, grid_size, temporal_size=13
         tensors = [t[0].expand(*t[1]) for t in zip(tensors, expandable_shapes)]
         return torch.cat(tensors, dim=dim)
 
-    freqs = broadcat((freqs_t[:, None, None, :], freqs_h[None, :, None, :], freqs_w[None, None, :, :]), dim=-1)
+    freqs = broadcast((freqs_t[:, None, None, :], freqs_h[None, :, None, :], freqs_w[None, None, :, :]), dim=-1)
 
-    # Flatten and reorder dimensions manually without rearrange
     t, h, w, d = freqs.shape
     freqs = freqs.view(t * h * w, d)
 

src/diffusers/models/transformers/cogvideox_transformer_3d.py
@@ -22,7 +22,7 @@ from ...configuration_utils import ConfigMixin, register_to_config
 from ...utils import is_torch_version, logging
 from ...utils.torch_utils import maybe_allow_in_graph
 from ..attention import Attention, FeedForward
-from ..attention_processor import HunyuanAttnProcessor2_0, CogVideoXAttnProcessor2_0
+from ..attention_processor import CogVideoXAttnProcessor2_0
 from ..embeddings import CogVideoXPatchEmbed, TimestepEmbedding, Timesteps, get_3d_sincos_pos_embed
 from ..modeling_outputs import Transformer2DModelOutput
 from ..modeling_utils import ModelMixin
@@ -120,7 +120,6 @@ class CogVideoXBlock(nn.Module):
         temb: torch.Tensor,
         image_rotary_emb: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,
     ) -> torch.Tensor:
-        breakpoint()
         norm_hidden_states, norm_encoder_hidden_states, gate_msa, enc_gate_msa = self.norm1(
             hidden_states, encoder_hidden_states, temb
         )

src/diffusers/pipelines/cogvideo/pipeline_cogvideox.py
@@ -429,8 +429,14 @@ class CogVideoXPipeline(DiffusionPipeline):
                     f" {negative_prompt_embeds.shape}."
                 )
 
-    def _prepare_rotary_embeddings(
-        self, height: int, width: int, max_sequence_length: int
+    def _prepare_rotary_positional_embeddings(
+        self,
+        height: int,
+        width: int,
+        num_frames: int,
+        max_sequence_length: int,
+        device: torch.device,
+        dtype: torch.dtype,
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         grid_height = height // (self.vae_scale_factor_spatial * self.transformer.config.patch_size)
         grid_width = width // (self.vae_scale_factor_spatial * self.transformer.config.patch_size)
@@ -444,13 +450,14 @@ class CogVideoXPipeline(DiffusionPipeline):
             embed_dim=self.transformer.config.attention_head_dim,
             crops_coords=grid_crops_coords,
             grid_size=(grid_height, grid_width),
+            temporal_size=num_frames,
             use_real=True,
         )
 
         pad_tensor_cos = freqs_cos.new_zeros((max_sequence_length, freqs_cos.size(1)))
         pad_tensor_sin = freqs_sin.new_zeros((max_sequence_length, freqs_sin.size(1)))
-        freqs_cos = torch.cat([pad_tensor_cos, freqs_cos])
-        freqs_sin = torch.cat([pad_tensor_sin, freqs_sin])
+        freqs_cos = torch.cat([pad_tensor_cos, freqs_cos]).to(device=device, dtype=dtype)
+        freqs_sin = torch.cat([pad_tensor_sin, freqs_sin]).to(device=device, dtype=dtype)
 
         return freqs_cos, freqs_sin
 
@@ -646,7 +653,9 @@ class CogVideoXPipeline(DiffusionPipeline):
 
         # 7. Create rotary embeds if required
         image_rotary_emb = (
-            self._prepare_rotary_embeddings(height, width, max_sequence_length)
+            self._prepare_rotary_positional_embeddings(
+                height, width, latents.size(1), max_sequence_length, device, prompt_embeds.dtype
+            )
             if self.transformer.config.use_rotary_positional_embeddings
             else None
         )