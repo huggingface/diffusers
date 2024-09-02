2140924c150ece6620811027cc92bd340e70d3a5
Gothos <vishnujaddipal@gmail.com>
2024-09-02 22:29:56 +0000

    Change lora conversion for xlabs

--- src/diffusers/loaders/lora_conversion_utils.py
+++ src/diffusers/loaders/lora_conversion_utils.py
@@ -562,7 +562,7 @@ def _convert_xlabs_flux_lora_to_diffusers(old_state_dict):
                 new_key += ".attn.to_out.0"
             elif "processor.proj_lora2" in old_key:
                 new_key += ".attn.to_add_out"
-            elif "processor.qkv_lora1" in old_key and "up" not in old_key:
+            elif "processor.qkv_lora2" in old_key and "up" not in old_key:
                 handle_qkv(
                     old_state_dict,
                     new_state_dict,
@@ -574,7 +574,7 @@ def _convert_xlabs_flux_lora_to_diffusers(old_state_dict):
                     ],
                 )
                 # continue
-            elif "processor.qkv_lora2" in old_key and "up" not in old_key:
+            elif "processor.qkv_lora1" in old_key and "up" not in old_key:
                 handle_qkv(
                     old_state_dict,
                     new_state_dict,
