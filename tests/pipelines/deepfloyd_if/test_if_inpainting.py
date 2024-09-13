314e76df641c9c09a5194dbfa87fb892483afb16
Fanli Lin <fanli.lin@intel.com>
2024-09-12 23:06:28 -0700

    add more

--- tests/pipelines/deepfloyd_if/test_if_inpainting.py
+++ tests/pipelines/deepfloyd_if/test_if_inpainting.py
@@ -72,7 +72,6 @@ class IFInpaintingPipelineFastTests(PipelineTesterMixin, IFPipelineTesterMixin, 
     def test_save_load_optional_components(self):
         self._test_save_load_optional_components()
 
-    @unittest.skipIf(torch_device != "cuda", reason="float16 requires CUDA")
     def test_save_load_float16(self):
         # Due to non-determinism in save load of the hf-internal-testing/tiny-random-t5 text encoder
         super().test_save_load_float16(expected_max_diff=1e-1)
--- tests/pipelines/deepfloyd_if/test_if_inpainting_superresolution.py
+++ tests/pipelines/deepfloyd_if/test_if_inpainting_superresolution.py
@@ -74,7 +74,6 @@ class IFInpaintingSuperResolutionPipelineFastTests(PipelineTesterMixin, IFPipeli
     def test_save_load_optional_components(self):
         self._test_save_load_optional_components()
 
-    @unittest.skipIf(torch_device != "cuda", reason="float16 requires CUDA")
     def test_save_load_float16(self):
         # Due to non-determinism in save load of the hf-internal-testing/tiny-random-t5 text encoder
         super().test_save_load_float16(expected_max_diff=1e-1)
