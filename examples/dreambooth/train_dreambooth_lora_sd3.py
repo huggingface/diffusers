747dc42d144e3580de00f728e3906b7933955653
sayakpaul <spsayakpaul@gmail.com>
2024-09-28 08:03:52 +0530

    quality

--- examples/dreambooth/train_dreambooth_lora_sd3.py
+++ examples/dreambooth/train_dreambooth_lora_sd3.py
@@ -1455,7 +1455,7 @@ def main(args):
     # Clear the memory here
     if not args.train_text_encoder and not train_dataset.custom_instance_prompts:
         # Explicitly delete the objects as well, otherwise only the lists are deleted and the original references remain, preventing garbage collection
-        del tokenizers, text_encoders 
+        del tokenizers, text_encoders
         del text_encoder_one, text_encoder_two, text_encoder_three
         retain_memory()
 
