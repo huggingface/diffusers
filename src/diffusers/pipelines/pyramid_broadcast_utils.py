b3547c647ecae96024c555c43b220a6614d076c6
Aryan <aryan@huggingface.co>
2024-10-04 08:16:24 +0200

    add docs

--- src/diffusers/pipelines/pyramid_broadcast_utils.py
+++ src/diffusers/pipelines/pyramid_broadcast_utils.py
@@ -26,6 +26,35 @@ logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
 
 
 class PyramidAttentionBroadcastAttentionProcessorWrapper:
+    r"""
+    Helper attention processor that wraps logic required for Pyramid Attention Broadcast to function.
+
+    PAB works by caching and re-using attention computations from past inference steps. This is due to the realization
+    that the attention states do not differ too much numerically between successive inference steps. The difference is
+    most significant/prominent in the spatial attention blocks, lesser so in the temporal attention blocks, and least
+    in cross attention blocks.
+
+    Currently, only spatial and cross attention block skipping is supported in Diffusers due to not having any models
+    tested with temporal attention blocks. Feel free to open a PR adding support for this in case there's a model that
+    you would like to use PAB with.
+
+    Args:
+        pipeline ([`~diffusers.DiffusionPipeline`]):
+            The underlying DiffusionPipeline object that inherits from the PAB Mixin and utilized this attention
+            processor.
+        processor ([`~diffusers.models.attention_processor.AttentionProcessor`]):
+            The underlying attention processor that will be wrapped to cache the intermediate attention computation.
+        skip_range (`int`):
+            The attention block to execute after skipping intermediate attention blocks. If set to the value `N`, `N -
+            1` attention blocks are skipped and every N'th block is executed. Different models have different
+            tolerances to how much attention computation can be reused based on the differences between successive
+            blocks. So, this parameter must be adjusted per model after performing experimentation. Setting this value
+            to `2` is recommended for different models PAB has been experimented with.
+        timestep_range (`Tuple[int, int]`):
+            The timestep range between which PAB will remain activated in attention blocks. While activated, PAB will
+            re-use attention computations between inference steps.
+    """
+
     def __init__(
         self, pipeline, processor: AttentionProcessor, skip_range: int, timestep_range: Tuple[int, int]
     ) -> None:
@@ -59,14 +88,18 @@ class PyramidAttentionBroadcastAttentionProcessorWrapper:
         *args,
         **kwargs,
     ):
+        r"""Method that wraps the underlying call to compute attention and cache states for re-use."""
+
         if (
             hasattr(self.pipeline, "_current_timestep")
             and self.pipeline._current_timestep is not None
             and self._iteration % self._skip_range != 0
             and (self._timestep_range[0] < self.pipeline._current_timestep < self._timestep_range[1])
         ):
+            # Skip attention computation by re-using past attention states
             hidden_states = self._prev_hidden_states
         else:
+            # Perform attention computation
             call_kwargs = {}
             for param in self._attn_processor_params:
                 call_kwargs.update({param: locals()[param]})
@@ -122,6 +155,33 @@ class PyramidAttentionBroadcastMixin:
         spatial_attn_timestep_range: Optional[Tuple[int, int]] = None,
         cross_attn_timestep_range: Optional[Tuple[int, int]] = None,
     ) -> None:
+        r"""
+        Enable pyramid attention broadcast to speedup inference by re-using attention states and skipping computation
+        systematically as described in the paper: [Pyramid Attention Broadcast](https://www.arxiv.org/abs/2408.12588).
+
+        Args:
+            spatial_attn_skip_range (`int`, *optional*):
+                The attention block to execute after skipping intermediate spatial attention blocks. If set to the
+                value `N`, `N - 1` attention blocks are skipped and every N'th block is executed. Different models have
+                different tolerances to how much attention computation can be reused based on the differences between
+                successive blocks. So, this parameter must be adjusted per model after performing experimentation.
+                Setting this value to `2` is recommended for different models PAB has been experimented with.
+            cross_attn_skip_range (`int`, *optional*):
+                The attention block to execute after skipping intermediate cross attention blocks. If set to the value
+                `N`, `N - 1` attention blocks are skipped and every N'th block is executed. Different models have
+                different tolerances to how much attention computation can be reused based on the differences between
+                successive blocks. So, this parameter must be adjusted per model after performing experimentation.
+                Setting this value to `6` is recommended for different models PAB has been experimented with.
+            spatial_attn_timestep_range (`Tuple[int, int]`, *optional*):
+                The timestep range between which PAB will remain activated in spatial attention blocks. While
+                activated, PAB will re-use attention computations between inference steps. Setting this to `(100, 850)`
+                is recommended for different models PAB has been experimented with.
+            cross_attn_timestep_range (`Tuple[int, int]`, *optional*):
+                The timestep range between which PAB will remain activated in cross attention blocks. While activated,
+                PAB will re-use attention computations between inference steps. Setting this to `(100, 800)` is
+                recommended for different models PAB has been experimented with.
+        """
+
         if spatial_attn_timestep_range is None:
             spatial_attn_timestep_range = (100, 800)
         if cross_attn_skip_range is None:
@@ -145,6 +205,8 @@ class PyramidAttentionBroadcastMixin:
         self._enable_pyramid_attention_broadcast()
 
     def disable_pyramid_attention_broadcast(self) -> None:
+        r"""Disables the pyramid attention broadcast sampling mechanism."""
+
         self._pab_spatial_attn_skip_range = None
         self._pab_cross_attn_skip_range = None
         self._pab_spatial_attn_timestep_range = None
