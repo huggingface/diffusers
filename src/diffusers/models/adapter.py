7868ba556b7e243484ce6244e1aafa4b322b482d
Ahnjj_DEV <ahnjj.dev@gmail.com>
2024-10-07 13:21:52 +0900

    Update src/diffusers/models/adapter.py

--- src/diffusers/models/adapter.py
+++ src/diffusers/models/adapter.py
@@ -161,7 +161,7 @@ class MultiAdapter(ModelMixin):
             pretrained_model_path (`os.PathLike`):
                 A path to a *directory* containing model weights saved using
                 [`~diffusers.models.adapter.MultiAdapter.save_pretrained`], e.g., `./my_model_directory/adapter`.
-            torch_dtype (`str` or `torch.dtype`, optional):
+            torch_dtype (`str` or `torch.dtype`, *optional*):
                 Override the default `torch.dtype` and load the model under this dtype. If `"auto"` is passed the dtype
                 will be automatically derived from the model's weights.
             output_loading_info(`bool`, optional, defaults=False):
