bc073fcf13768778513c1212421cbabfc77d0690
Tolga Cangöz <mtcangoz@gmail.com>
2024-10-07 11:28:50 +0300

    Refactor `matryoshka.py` to include proper licensing and attribution

--- examples/community/matryoshka.py
+++ examples/community/matryoshka.py
@@ -1,4 +1,23 @@
-# #TODO Licensed under the Apache License, Version 2.0 or MIT?
+# Copyright 2024 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Based on [🪆Matryoshka Diffusion Models](https://huggingface.co/papers/2310.15111).
+# Authors: Jiatao Gu, Shuangfei Zhai, Yizhe Zhang, Josh Susskind, Navdeep Jaitly
+# Code: https://github.com/apple/ml-mdm with MIT license
+#
+# Adapted to Diffusers by [M. Tolga Cangöz](https://github.com/tolgacangoz).
+
 
 import inspect
 import math
