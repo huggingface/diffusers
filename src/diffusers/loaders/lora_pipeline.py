1c253e2a40f27909b4d39935d339d22c87745568
sayakpaul <spsayakpaul@gmail.com>
2024-10-08 08:40:10 +0530

    fix-copies

--- src/diffusers/loaders/lora_pipeline.py
+++ src/diffusers/loaders/lora_pipeline.py
@@ -2394,6 +2394,7 @@ class CogVideoXLoraLoaderMixin(LoraBaseMixin):
             user_agent=user_agent,
             allow_pickle=allow_pickle,
         )
+
         is_dora_scale_present = any("dora_scale" in k for k in state_dict)
         if is_dora_scale_present:
             warn_msg = "It seems like you are using a DoRA checkpoint that is not compatible in Diffusers at the moment. So, we are going to filter out the keys associated to 'dora_scale` from the state dict. If you think this is a mistake please open an issue https://github.com/huggingface/diffusers/issues/new."
