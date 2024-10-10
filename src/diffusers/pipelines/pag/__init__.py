2019d56b54d1a5ba68eb3c79313b8723f04c06d6
Darshil Jariwala <jariwala.darshil2002@gmail.com>
2024-10-10 08:04:25 +0530

    adding imports

--- src/diffusers/pipelines/pag/__init__.py
+++ src/diffusers/pipelines/pag/__init__.py
@@ -54,6 +54,8 @@ if TYPE_CHECKING or DIFFUSERS_SLOW_IMPORT:
         from .pipeline_pag_hunyuandit import HunyuanDiTPAGPipeline
         from .pipeline_pag_kolors import KolorsPAGPipeline
         from .pipeline_pag_pixart_sigma import PixArtSigmaPAGPipeline
+        from .pipeline_pag_sd_img2img import StableDiffusionPAGImg2ImgPipeline
+        from .pipeline_pag_sd_inpaint import StableDiffusionPAGInpaintPipeline
         from .pipeline_pag_sd import StableDiffusionPAGPipeline
         from .pipeline_pag_sd_3 import StableDiffusion3PAGPipeline
         from .pipeline_pag_sd_animatediff import AnimateDiffPAGPipeline
