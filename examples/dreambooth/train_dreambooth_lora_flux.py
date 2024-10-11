323cd83aaa6a117a7c13f32b97725f15e32ca9c2
Aviral Kamal Jain <aviralkamaljain@gmail.com>
2024-10-11 16:03:33 +0530

    fixed issue #9350, Tensor is deprecated

--- examples/dreambooth/train_dreambooth_lora_flux.py
+++ examples/dreambooth/train_dreambooth_lora_flux.py
@@ -1007,8 +1007,7 @@ def encode_prompt(
         text_input_ids=text_input_ids_list[1] if text_input_ids_list else None,
     )
 
-    text_ids = torch.zeros(batch_size, prompt_embeds.shape[1], 3).to(device=device, dtype=dtype)
-    text_ids = text_ids.repeat(num_images_per_prompt, 1, 1)
+    text_ids = torch.zeros(prompt_embeds.shape[1], 3).to(device=device, dtype=dtype)
 
     return prompt_embeds, pooled_prompt_embeds, text_ids
 
