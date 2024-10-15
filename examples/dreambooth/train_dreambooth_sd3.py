0c7355189e3bd06ca5aaeb8f13d97a1ee6a2c449
linoytsaban <linoy@huggingface.co>
2024-10-15 11:54:04 +0300

    update license

--- examples/dreambooth/train_dreambooth_sd3.py
+++ examples/dreambooth/train_dreambooth_sd3.py
@@ -119,7 +119,7 @@ Please adhere to the licensing terms as described `[here](https://huggingface.co
     model_card = load_or_create_model_card(
         repo_id_or_path=repo_id,
         from_training=True,
-        license="openrail++",
+        license="other",
         base_model=base_model,
         prompt=instance_prompt,
         model_description=model_description,
