220e27feb78551319466c09009f56878053e5ede
Roger <rogersinghchugh@gmail.com>
2024-10-22 07:34:24 +0530

    Changes after running make style.

--- src/diffusers/training_utils.py
+++ src/diffusers/training_utils.py
@@ -59,7 +59,7 @@ def set_seed(seed: int):
 
 def compute_snr(noise_scheduler, timesteps):
     """
-    Computes SNR as per https://github.com/TiankaiHang/Min-SNR-Diffusion-Training/blob/521b624bd70c67cee4bdf49225915f5945a872e3/guided_diffusion/gaussian_diffusion.py#L847-L849 
+    Computes SNR as per https://github.com/TiankaiHang/Min-SNR-Diffusion-Training/blob/521b624bd70c67cee4bdf49225915f5945a872e3/guided_diffusion/gaussian_diffusion.py#L847-L849
     for the given timesteps using the provided noise scheduler.
 
     Args:
@@ -67,10 +67,10 @@ def compute_snr(noise_scheduler, timesteps):
             An object containing the noise schedule parameters, specifically `alphas_cumprod`, which is used
             to compute the SNR values.
         timesteps (`torch.Tensor`):
-            A tensor of timesteps for which the SNR is computed. 
+            A tensor of timesteps for which the SNR is computed.
 
     Returns:
-        `torch.Tensor`: A tensor containing the computed SNR values for each timestep.     
+        `torch.Tensor`: A tensor containing the computed SNR values for each timestep.
     """
     alphas_cumprod = noise_scheduler.alphas_cumprod
     sqrt_alphas_cumprod = alphas_cumprod**0.5
