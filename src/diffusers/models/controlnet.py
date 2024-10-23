e7e7adf9c13d13cff517e5fd2dafa9bb07fef0ac
sayakpaul <spsayakpaul@gmail.com>
2024-10-23 17:45:45 +0530

    fix

--- src/diffusers/models/controlnet.py
+++ src/diffusers/models/controlnet.py
@@ -12,10 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from ..utils import deprecate
-from .controlnets.controlnet import (
+from .controlnets.controlnet import (  # noqa
+    BaseOutput,
     ControlNetConditioningEmbedding,
     ControlNetModel,
     ControlNetOutput,
+    zero_module,
 )
 
 
