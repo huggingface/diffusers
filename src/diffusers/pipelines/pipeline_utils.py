b062bd9b9161230f5655a9928125da85425791fa
sayakpaul <spsayakpaul@gmail.com>
2024-11-02 15:04:00 +0530

    print.

--- src/diffusers/pipelines/pipeline_utils.py
+++ src/diffusers/pipelines/pipeline_utils.py
@@ -983,6 +983,7 @@ class DiffusionPipeline(ConfigMixin, PushToHubMixin):
         r"""
         Removes all hooks that were added when using `enable_sequential_cpu_offload` or `enable_model_cpu_offload`.
         """
+        print("Within remove_all_hooks().")
         for _, model in self.components.items():
             if isinstance(model, torch.nn.Module) and hasattr(model, "_hf_hook"):
                 accelerate.hooks.remove_hook_from_module(model, recurse=True)
