aad05e5af94533912804251f49f8afce83ecf5bd
JB-Bai <1355864570@qq.com>
2024-11-06 19:38:33 +0800

    add support to Meissonic

--- src/diffusers/models/__init__.py
+++ src/diffusers/models/__init__.py
@@ -62,6 +62,7 @@ if is_torch_available():
     _import_structure["transformers.transformer_mochi"] = ["MochiTransformer3DModel"]
     _import_structure["transformers.transformer_sd3"] = ["SD3Transformer2DModel"]
     _import_structure["transformers.transformer_temporal"] = ["TransformerTemporalModel"]
+    _import_structure["transformers.transformer_meissonic"] = ["MeissonicTransformer2DModel"]
     _import_structure["unets.unet_1d"] = ["UNet1DModel"]
     _import_structure["unets.unet_2d"] = ["UNet2DModel"]
     _import_structure["unets.unet_2d_condition"] = ["UNet2DConditionModel"]
@@ -121,6 +122,7 @@ if TYPE_CHECKING or DIFFUSERS_SLOW_IMPORT:
             T5FilmDecoder,
             Transformer2DModel,
             TransformerTemporalModel,
+            MeissonicTransformer2DModel,
         )
         from .unets import (
             I2VGenXLUNet,
