7ef5dabbe816050e6b506030aee943edaf960f71
Fanli Lin <fanli0116@gmail.com>
2024-11-11 09:43:07 +0800

    Update tests/pipelines/deepfloyd_if/test_if.py
    Co-authored-by: Dhruv Nair <dhruv.nair@gmail.com>

--- tests/pipelines/deepfloyd_if/test_if.py
+++ tests/pipelines/deepfloyd_if/test_if.py
@@ -58,7 +58,8 @@ class IFPipelineFastTests(PipelineTesterMixin, IFPipelineTesterMixin, unittest.T
     def test_save_load_optional_components(self):
         self._test_save_load_optional_components()
 
-    @require_non_cpu
+    @unittest.skipIf(torch_device not in ["cuda", "xpu"], reason="float16 requires CUDA or XPU")
+    @require_accelerator
     def test_save_load_float16(self):
         # Due to non-determinism in save load of the hf-internal-testing/tiny-random-t5 text encoder
         super().test_save_load_float16(expected_max_diff=1e-1)
