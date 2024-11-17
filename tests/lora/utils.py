83bb65500384751350ef99c039276089fe42932e
sayakpaul <spsayakpaul@gmail.com>
2024-11-17 16:03:50 +0530

    remove space.

--- tests/lora/utils.py
+++ tests/lora/utils.py
@@ -348,7 +348,6 @@ class PeftLoraLoaderMixinTests:
 
             _, _, inputs = self.get_dummy_inputs()
             output_lora = pipe(**inputs)[0]
-
             self.assertTrue(output_lora.shape == self.output_shape)
 
     @require_peft_version_greater("0.13.1")
