b317d0a7a273ef8ef5053819fdc19192acb47a42
yiyixuxu <yixu310@gmail.com>
2024-11-20 04:05:21 +0100

    fix

--- src/diffusers/pipelines/controlnet/pipeline_controlnet_inpaint_sd_xl.py
+++ src/diffusers/pipelines/controlnet/pipeline_controlnet_inpaint_sd_xl.py
@@ -35,7 +35,7 @@ from ...loaders import (
     StableDiffusionXLLoraLoaderMixin,
     TextualInversionLoaderMixin,
 )
-from ...models import AutoencoderKL, ControlNetModel, ImageProjection, UNet2DConditionModel
+from ...models import AutoencoderKL, ControlNetModel, ImageProjection, MultiControlNetModel, UNet2DConditionModel
 from ...models.attention_processor import (
     AttnProcessor2_0,
     XFormersAttnProcessor,
@@ -54,7 +54,6 @@ from ...utils import (
 from ...utils.torch_utils import is_compiled_module, randn_tensor
 from ..pipeline_utils import DiffusionPipeline, StableDiffusionMixin
 from ..stable_diffusion_xl.pipeline_output import StableDiffusionXLPipelineOutput
-from .multicontrolnet import MultiControlNetModel
 
 
 if is_invisible_watermark_available():
