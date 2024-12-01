35085ea9dcc0db90befd0e20898266ca91e3e5b3
Pedro Cuenca <pedro@huggingface.co>
2024-12-01 13:01:43 +0100

    Workaround for upscale with large output tensors.
    Fixes #10040.

--- src/diffusers/models/upsampling.py
+++ src/diffusers/models/upsampling.py
@@ -165,6 +165,12 @@ class Upsample2D(nn.Module):
         # if `output_size` is passed we force the interpolation output
         # size and do not make use of `scale_factor=2`
         if self.interpolate:
+            # upsample_nearest_nhwc also fails when the number of output elements is large
+            # https://github.com/pytorch/pytorch/issues/141831
+            scale_factor = 2 if output_size is None else max(output_size)
+            if hidden_states.numel() * scale_factor > pow(2, 31):
+                hidden_states = hidden_states.contiguous()
+
             if output_size is None:
                 hidden_states = F.interpolate(hidden_states, scale_factor=2.0, mode="nearest")
             else:
