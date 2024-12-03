a98b946a39b9c6327d2468241a2670adffd6c656
hlky <hlky@hlky.ac>
2024-12-03 15:16:27 +0000

    make

--- examples/controlnet/train_controlnet_sdxl.py
+++ examples/controlnet/train_controlnet_sdxl.py
@@ -598,7 +598,6 @@ def parse_args(input_args=None):
     if args.dataset_name is None and args.train_data_dir is None:
         raise ValueError("Specify either `--dataset_name` or `--train_data_dir`")
 
-
     if args.proportion_empty_prompts < 0 or args.proportion_empty_prompts > 1:
         raise ValueError("`--proportion_empty_prompts` must be in the range [0, 1].")
 
