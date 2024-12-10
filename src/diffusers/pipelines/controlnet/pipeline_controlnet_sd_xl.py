abddcd68ed89224b8e54bd44a510ec6d8e6236bc
LinchuanXUTheSEAAI <unilin54092@gmail.com>
2024-12-10 17:34:58 +0900

    fix bug

--- src/diffusers/pipelines/controlnet/pipeline_controlnet_sd_xl.py
+++ src/diffusers/pipelines/controlnet/pipeline_controlnet_sd_xl.py
@@ -631,6 +631,7 @@ class StableDiffusionXLControlNetPipeline(
         return ip_adapter_image_embeds
 
     def run_safety_checker(self, image, device, dtype):
+        image_original = copy.deepcopy(image)
         if self.safety_checker is None:
             has_nsfw_concept = None
         else:
@@ -639,7 +640,7 @@ class StableDiffusionXLControlNetPipeline(
             else:
                 feature_extractor_input = self.image_processor.numpy_to_pil(image)
             safety_checker_input = self.feature_extractor(feature_extractor_input, return_tensors="pt").to(device)
-            image_original = copy.deepcopy(image)
+            
             image, has_nsfw_concept = self.safety_checker(
                 images=image, clip_input=safety_checker_input.pixel_values.to(dtype)
             )
