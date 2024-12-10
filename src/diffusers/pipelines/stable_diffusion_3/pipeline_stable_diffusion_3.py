7333bfc352e1c2123577d82aebc93025db50021d
Daniel Regado <danielregado@gmail.com>
2024-12-10 12:36:33 +0000

    Minor change correcting checking for ip_adapter embeds

--- src/diffusers/pipelines/stable_diffusion_3/pipeline_stable_diffusion_3.py
+++ src/diffusers/pipelines/stable_diffusion_3/pipeline_stable_diffusion_3.py
@@ -953,7 +953,7 @@ class StableDiffusion3Pipeline(DiffusionPipeline, SD3LoraLoaderMixin, FromSingle
         # Either image is passed and ip_adapter is active
         # Or image_embeds are passed directly
         if (ip_adapter_image is not None and self.is_ip_adapter_active()) or ip_adapter_image_embeds is not None:
-            image_embeds = self.prepare_ip_adapter_image_embeds(
+            ip_adapter_image_embeds = self.prepare_ip_adapter_image_embeds(
                 ip_adapter_image,
                 ip_adapter_image_embeds,
                 device,
@@ -972,9 +972,9 @@ class StableDiffusion3Pipeline(DiffusionPipeline, SD3LoraLoaderMixin, FromSingle
                 # broadcast to batch dimension in a way that's compatible with ONNX/Core ML
                 timestep = t.expand(latent_model_input.shape[0])
 
-                if image_embeds is not None:
+                if ip_adapter_image_embeds is not None:
                     ip_hidden_states, temb = self.transformer.image_proj(
-                        image_embeds,
+                        ip_adapter_image_embeds,
                         timestep.to(dtype=latents.dtype),
                         need_temb=True,
                     )
