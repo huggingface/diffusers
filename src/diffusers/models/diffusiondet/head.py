508aa4181c02777ecb5b35434d9aa380f02dcf39
HichTala <98521878+HichTala@users.noreply.github.com>
2024-12-16 15:06:33 +0100

    Add DynamicConv

--- src/diffusers/models/diffusiondet/head.py
+++ src/diffusers/models/diffusiondet/head.py
@@ -42,8 +42,52 @@ class DynamicHead(nn.Module):
         pass
 
 
+class DynamicConv(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+
+        self.hidden_dim = config.hidden_dim
+        self.dim_dynamic = config.dim_dynamic
+        self.num_dynamic = config.num_dynamic
+        self.num_params = self.hidden_dim * self.dim_dynamic
+        self.dynamic_layer = nn.Linear(self.hidden_dim, self.num_dynamic * self.num_params)
+
+        self.norm1 = nn.LayerNorm(self.dim_dynamic)
+        self.norm2 = nn.LayerNorm(self.hidden_dim)
+
+        self.activation = nn.ReLU(inplace=True)
+
+        pooler_resolution = config.pooler_resolution
+        num_output = self.hidden_dim * pooler_resolution ** 2
+        self.out_layer = nn.Linear(num_output, self.hidden_dim)
+        self.norm3 = nn.LayerNorm(self.hidden_dim)
+
+
+    def forward(self, pro_features, roi_features):
+        features = roi_features.permute(1, 0, 2)
+        parameters = self.dynamic_layer(pro_features).permute(1, 0, 2)
+
+        param1 = parameters[:, :, :self.num_params].view(-1, self.hidden_dim, self.dim_dynamic)
+        param2 = parameters[:, :, self.num_params:].view(-1, self.dim_dynamic, self.hidden_dim)
+
+        features = torch.bmm(features, param1)
+        features = self.norm1(features)
+        features = self.activation(features)
+
+        features = torch.bmm(features, param2)
+        features = self.norm2(features)
+        features = self.activation(features)
+
+        features = features.flatten(1)
+        features = self.out_layer(features)
+        features = self.norm3(features)
+        features = self.activation(features)
+
+        return features
+
+
 class DiffusionDetHead(nn.Module):
-    def __init__(self, config, roi_input_shape):
+    def __init__(self, config, roi_input_shape, d_model, num_classes, dim_feedforward=2048, nhead=8, dropout=0.1):
         super().__init__()
 
         in_features = config.roi_head_in_features
@@ -57,25 +101,40 @@ class DiffusionDetHead(nn.Module):
             sampling_ratio=sampling_ratio,
         )
 
+        # dynamic.
+        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
+        self.inst_interact = DynamicConv(cfg)
+
+        self.linear1 = nn.Linear(d_model, dim_feedforward)
+        self.dropout = nn.Dropout(dropout)
+        self.linear2 = nn.Linear(dim_feedforward, d_model)
+
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+        self.norm3 = nn.LayerNorm(d_model)
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+        self.dropout3 = nn.Dropout(dropout)
+
     def forward(self, features, bboxes, time_emb):
-        N, nr_boxes = bboxes.shape[:2]
+        B, N = bboxes.shape[:2]
 
         # roi_feature.
         roi_features = self.pooler(features, bboxes)
 
-        pro_features = roi_features.view(N, nr_boxes, self.d_model, -1).mean(-1)
+        pro_features = roi_features.view(B, N, self.d_model, -1).mean(-1)
 
-        roi_features = roi_features.view(N * nr_boxes, self.d_model, -1).permute(2, 0, 1)
+        roi_features = roi_features.view(B * N, self.d_model, -1).permute(2, 0, 1)
 
         # self_att.
-        pro_features = pro_features.view(N, nr_boxes, self.d_model).permute(1, 0, 2)
+        pro_features = pro_features.view(B, N, self.d_model).permute(1, 0, 2)
         pro_features2 = self.self_attn(pro_features, pro_features, value=pro_features)[0]
         pro_features = pro_features + self.dropout1(pro_features2)
         pro_features = self.norm1(pro_features)
 
         # inst_interact.
-        pro_features = pro_features.view(nr_boxes, N, self.d_model).permute(1, 0, 2).reshape(1, N * nr_boxes,
-                                                                                             self.d_model)
+        pro_features = pro_features.view(N, B, self.d_model).permute(1, 0, 2).reshape(1, B * N,
+                                                                                      self.d_model)
         pro_features2 = self.inst_interact(pro_features, roi_features)
         pro_features = pro_features + self.dropout2(pro_features2)
         obj_features = self.norm2(pro_features)
@@ -85,10 +144,10 @@ class DiffusionDetHead(nn.Module):
         obj_features = obj_features + self.dropout3(obj_features2)
         obj_features = self.norm3(obj_features)
 
-        fc_feature = obj_features.transpose(0, 1).reshape(N * nr_boxes, -1)
+        fc_feature = obj_features.transpose(0, 1).reshape(B * N, -1)
 
         scale_shift = self.block_time_mlp(time_emb)
-        scale_shift = torch.repeat_interleave(scale_shift, nr_boxes, dim=0)
+        scale_shift = torch.repeat_interleave(scale_shift, N, dim=0)
         scale, shift = scale_shift.chunk(2, dim=1)
         fc_feature = fc_feature * (scale + 1) + shift
 
@@ -102,7 +161,7 @@ class DiffusionDetHead(nn.Module):
         bboxes_deltas = self.bboxes_delta(reg_feature)
         pred_bboxes = self.apply_deltas(bboxes_deltas, bboxes.view(-1, 4))
 
-        return class_logits.view(N, nr_boxes, -1), pred_bboxes.view(N, nr_boxes, -1), obj_features
+        return class_logits.view(B, N, -1), pred_bboxes.view(B, N, -1), obj_features
 
 
 class ROIPooler(nn.Module):
