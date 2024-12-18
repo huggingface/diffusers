8601da9c81f63fef1da941830f8ade019d8e9ee1
Aryan <aryan@huggingface.co>
2024-12-18 03:14:02 +0100

    remove nullop imports

--- tests/lora/test_lora_layers_sd3.py
+++ tests/lora/test_lora_layers_sd3.py
@@ -29,16 +29,12 @@ from diffusers import (
 from diffusers.utils import load_image
 from diffusers.utils.import_utils import is_accelerate_available
 from diffusers.utils.testing_utils import (
-    is_peft_available,
     numpy_cosine_similarity_distance,
     require_peft_backend,
     require_torch_gpu,
     torch_device,
 )
 
-
-if is_peft_available():
-    pass
 
 sys.path.append(".")
 
