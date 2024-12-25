19f48c55a7ed0fcf778bdc38a50919e49d0b1b2d
chaowenguo <chaowen.guo1@gmail.com>
2024-12-24 22:43:33 -0800

    Update pipeline_text_to_video_zero.py

--- src/diffusers/pipelines/text_to_video_synthesis/pipeline_text_to_video_zero.py
+++ src/diffusers/pipelines/text_to_video_synthesis/pipeline_text_to_video_zero.py
@@ -20,6 +20,12 @@ from ...utils.torch_utils import randn_tensor
 from ..pipeline_utils import DiffusionPipeline, StableDiffusionMixin
 from ..stable_diffusion import StableDiffusionSafetyChecker
 
+if is_torch_xla_available():
+    import torch_xla.core.xla_model as xm
+
+    XLA_AVAILABLE = True
+else:
+    XLA_AVAILABLE = False
 
 logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
 
