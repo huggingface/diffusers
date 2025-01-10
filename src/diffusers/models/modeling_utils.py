ceae0591ad5e7fc348c103c73679eb516fd9d82f
suzukimain <taka222e@gmail.com>
2025-01-10 13:54:45 +0900

    update

--- src/diffusers/models/modeling_utils.py
+++ src/diffusers/models/modeling_utils.py
@@ -1207,7 +1207,7 @@ class ModelMixin(torch.nn.Module, PushToHubMixin):
         parameters = inspect.signature(obj.__init__).parameters
         required_parameters = {k: v for k, v in parameters.items() if v.default == inspect._empty}
         optional_parameters = set({k for k, v in parameters.items() if v.default != inspect._empty})
-        expected_modules = set(required_parameters.keys()) - {"self"}
+        expected_modules = set(required_parameters.keys()) - {"self","kwargs","args"}
 
         return expected_modules, optional_parameters
 
