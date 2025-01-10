a5604da5739dbba7e0fb5dcf9e5ccdd98d1b834a
suzukimain <taka222e@gmail.com>
2025-01-10 15:15:03 +0900

    update

--- src/diffusers/pipelines/pipeline_flax_utils.py
+++ src/diffusers/pipelines/pipeline_flax_utils.py
@@ -539,7 +539,7 @@ class FlaxDiffusionPipeline(ConfigMixin, PushToHubMixin):
         parameters = inspect.signature(obj.__init__).parameters
         required_parameters = {k: v for k, v in parameters.items() if v.default == inspect._empty}
         optional_parameters = set({k for k, v in parameters.items() if v.default != inspect._empty})
-        expected_modules = set(required_parameters.keys()) - {"self","kwargs","args"}
+        expected_modules = set(required_parameters.keys()) - {"self"}
 
         return expected_modules, optional_parameters
 
