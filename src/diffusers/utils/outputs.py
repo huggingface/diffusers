dc48631da44f38e44420cd5ae5663aec983240cf
Aamir Nazir <aamir.nazir@intel.com>
2025-01-14 11:24:49 +0400

    Update outputs.py

--- src/diffusers/utils/outputs.py
+++ src/diffusers/utils/outputs.py
@@ -71,7 +71,7 @@ class BaseOutput(OrderedDict):
                     cls,
                     torch.utils._pytree._dict_flatten,
                     lambda values, context: cls(**torch.utils._pytree._dict_unflatten(values, context)),
-                    serialized_type_name=f"{cls.__module__}.{cls.__name__}"
+                    serialized_type_name=f"{cls.__module__}.{cls.__name__}",
                 )
 
     def __post_init__(self) -> None:
