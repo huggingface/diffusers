[fix] fix bug in mixed precision
the transformer before accelerator.prepare should not convert to fp16 in mix precision;
it will broken the grad precision and result in worse training result

examples/research_projects/sd3_lora_colab/train_dreambooth_lora_sd3_miniature.py
@@ -714,7 +714,7 @@ def main(args):
         )
 
     vae.to(accelerator.device, dtype=torch.float32)
-    transformer.to(accelerator.device, dtype=weight_dtype)
+    # transformer.to(accelerator.device, dtype=weight_dtype)
 
     if args.gradient_checkpointing:
         transformer.enable_gradient_checkpointing()