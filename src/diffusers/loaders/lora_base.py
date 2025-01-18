6e2fe26bfd46a65e3044b44b75df24f489f03c39
yiyixuxu <yixu310@gmail.com>
2025-01-18 08:04:12 +0100

    fix more for lora

--- src/diffusers/loaders/lora_base.py
+++ src/diffusers/loaders/lora_base.py
@@ -557,8 +557,10 @@ class LoraBaseMixin:
         # Decompose weights into weights for denoiser and text encoders.
         _component_adapter_weights = {}
         for component in self._lora_loadable_modules:
-            model = getattr(self, component)
-
+            model = getattr(self, component, None)
+            if model is None:
+                logger.warning(f"Model {component} not found in pipeline.")
+                continue
             for adapter_name, weights in zip(adapter_names, adapter_weights):
                 if isinstance(weights, dict):
                     component_adapter_weights = weights.pop(component, None)
