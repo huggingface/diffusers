83e0825a74d8cc089e277321a34a36e423168f24
Hameer Abbasi <2190658+hameerabbasi@users.noreply.github.com>
2025-01-19 15:09:12 +0100

    Sync with upstream changes.

--- src/diffusers/loaders/lora_pipeline.py
+++ src/diffusers/loaders/lora_pipeline.py
@@ -1990,6 +1990,29 @@ class AuraFlowLoraLoaderMixin(LoraBaseMixin):
 
         super().unfuse_lora(components=components)
 
+    @staticmethod
+    # Copied from diffusers.loaders.lora_pipeline.FluxLoraLoaderMixin._calculate_module_shape
+    def _calculate_module_shape(
+        model: "torch.nn.Module",
+        base_module: "torch.nn.Linear" = None,
+        base_weight_param_name: str = None,
+    ) -> "torch.Size":
+        def _get_weight_shape(weight: torch.Tensor):
+            return weight.quant_state.shape if weight.__class__.__name__ == "Params4bit" else weight.shape
+
+        if base_module is not None:
+            return _get_weight_shape(base_module.weight)
+        elif base_weight_param_name is not None:
+            if not base_weight_param_name.endswith(".weight"):
+                raise ValueError(
+                    f"Invalid `base_weight_param_name` passed as it does not end with '.weight' {base_weight_param_name=}."
+                )
+            module_path = base_weight_param_name.rsplit(".weight", 1)[0]
+            submodule = get_submodule_by_name(model, module_path)
+            return _get_weight_shape(submodule.weight)
+
+        raise ValueError("Either `base_module` or `base_weight_param_name` must be provided.")
+
 
 class FluxLoraLoaderMixin(LoraBaseMixin):
     r"""
