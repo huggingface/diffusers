d175d93d6832da67ed442f943876e7ded72b7280
Aryan <aryan@huggingface.co>
2025-01-21 16:29:59 +0100

    add layerwise fp8 pipeline test

--- tests/pipelines/cogview3/test_cogview3plus.py
+++ tests/pipelines/cogview3/test_cogview3plus.py
@@ -56,6 +56,7 @@ class CogView3PlusPipelineFastTests(PipelineTesterMixin, unittest.TestCase):
         ]
     )
     test_xformers_attention = False
+    test_layerwise_casting = True
 
     def get_dummy_components(self):
         torch.manual_seed(0)
