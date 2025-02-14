9a147b82f72e5df4553cb0f845bb957be3aa6028
Aryan <aryan@huggingface.co>
2025-02-14 12:59:45 +0530

    Module Group Offloading (#10503)
    * update
    
    * fix
    
    * non_blocking; handle parameters and buffers
    
    * update
    
    * Group offloading with cuda stream prefetching (#10516)
    
    * cuda stream prefetch
    
    * remove breakpoints
    
    * update
    
    * copy model hook implementation from pab
    
    * update; ~very workaround based implementation but it seems to work as expected; needs cleanup and rewrite
    
    * more workarounds to make it actually work
    
    * cleanup
    
    * rewrite
    
    * update
    
    * make sure to sync current stream before overwriting with pinned params
    
    not doing so will lead to erroneous computations on the GPU and cause bad results
    
    * better check
    
    * update
    
    * remove hook implementation to not deal with merge conflict
    
    * re-add hook changes
    
    * why use more memory when less memory do trick
    
    * why still use slightly more memory when less memory do trick
    
    * optimise
    
    * add model tests
    
    * add pipeline tests
    
    * update docs
    
    * add layernorm and groupnorm
    
    * address review comments
    
    * improve tests; add docs
    
    * improve docs
    
    * Apply suggestions from code review
    
    Co-authored-by: Steven Liu <59462357+stevhliu@users.noreply.github.com>
    
    * apply suggestions from code review
    
    * update tests
    
    * apply suggestions from review
    
    * enable_group_offloading -> enable_group_offload for naming consistency
    
    * raise errors if multiple offloading strategies used; add relevant tests
    
    * handle .to() when group offload applied
    
    * refactor some repeated code
    
    * remove unintentional change from merge conflict
    
    * handle .cuda()
    
    ---------
    
    Co-authored-by: Steven Liu <59462357+stevhliu@users.noreply.github.com>

--- tests/pipelines/lumina/test_lumina_nextdit.py
+++ tests/pipelines/lumina/test_lumina_nextdit.py
@@ -33,6 +33,7 @@ class LuminaText2ImgPipelinePipelineFastTests(unittest.TestCase, PipelineTesterM
 
     supports_dduf = False
     test_layerwise_casting = True
+    test_group_offloading = True
 
     def get_dummy_components(self):
         torch.manual_seed(0)
