69dda9a78d1f4c945060574cd88b0caf6681c942
Marc Sun <marc@huggingface.co>
2025-02-18 11:33:56 +0100

    add log

--- src/diffusers/models/model_loading_utils.py
+++ src/diffusers/models/model_loading_utils.py
@@ -326,6 +326,8 @@ def _load_state_dict_into_model(
     def load(module: torch.nn.Module, prefix: str = "", assign_to_params_buffers: bool = False):
         local_metadata = {}
         local_metadata["assign_to_params_buffers"] = assign_to_params_buffers
+        if assign_to_params_buffers and not is_torch_version(">=", "2.1"):
+            logger.info("You need to have torch>=2.1 in order to load the model with assign_to_params_buffers=True")
         args = (state_dict, prefix, local_metadata, True, [], [], error_msgs)
         module._load_from_state_dict(*args)
 
