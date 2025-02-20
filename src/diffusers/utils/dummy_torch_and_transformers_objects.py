e0078634ca3fc72b6da0a1d0aeabb7095068bef0
Anton Obukhov <anton.obukhov@gmail.com>
2025-02-21 00:41:25 +0100

    integrate iid

--- src/diffusers/utils/dummy_torch_and_transformers_objects.py
+++ src/diffusers/utils/dummy_torch_and_transformers_objects.py
@@ -1202,6 +1202,21 @@ class MarigoldDepthPipeline(metaclass=DummyObject):
         requires_backends(cls, ["torch", "transformers"])
 
 
+class MarigoldIntrinsicsPipeline(metaclass=DummyObject):
+    _backends = ["torch", "transformers"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch", "transformers"])
+
+    @classmethod
+    def from_config(cls, *args, **kwargs):
+        requires_backends(cls, ["torch", "transformers"])
+
+    @classmethod
+    def from_pretrained(cls, *args, **kwargs):
+        requires_backends(cls, ["torch", "transformers"])
+
+
 class MarigoldNormalsPipeline(metaclass=DummyObject):
     _backends = ["torch", "transformers"]
 
