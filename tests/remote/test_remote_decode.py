f03a1055cae83aa120839d27e221819e417015b5
hlky <hlky@hlky.ac>
2025-02-28 17:42:07 +0000

    more test

--- tests/remote/test_remote_decode.py
+++ tests/remote/test_remote_decode.py
@@ -36,7 +36,7 @@ class RemoteAutoencoderKLMixin:
     out_hw: Tuple[int, int] = None
     endpoint: str = None
     dtype: torch.dtype = None
-    scale_factor: float = None
+    scaling_factor: float = None
     shift_factor: float = None
     processor_cls: Union[VaeImageProcessor, VideoProcessor] = None
 
@@ -44,6 +44,8 @@ class RemoteAutoencoderKLMixin:
         inputs = {
             "endpoint": self.endpoint,
             "tensor": torch.randn(self.shape, device=torch_device, dtype=self.dtype),
+            "scaling_factor": self.scaling_factor,
+            "shift_factor": self.shift_factor,
         }
         return inputs
 
@@ -53,21 +55,21 @@ class RemoteAutoencoderKLMixin:
         output = remote_decode(output_type="pt", processor=processor, **inputs)
         self.assertTrue(isinstance(output, PIL.Image.Image), f"Expected `PIL.Image.Image` output, got {type(output)}")
         self.assertEqual(output.height, self.out_hw[0], f"Expected image height {self.out_hw[0]}, got {output.height}")
-        self.assertEqual(output.width, self.out_hw[1], f"Expected image width {self.out_hw[0]}, got {output.height}")
+        self.assertEqual(output.width, self.out_hw[1], f"Expected image width {self.out_hw[0]}, got {output.width}")
 
     def test_output_type_pil(self):
         inputs = self.get_dummy_inputs()
         output = remote_decode(output_type="pil", **inputs)
         self.assertTrue(isinstance(output, PIL.Image.Image), f"Expected `PIL.Image.Image` output, got {type(output)}")
         self.assertEqual(output.height, self.out_hw[0], f"Expected image height {self.out_hw[0]}, got {output.height}")
-        self.assertEqual(output.width, self.out_hw[1], f"Expected image width {self.out_hw[0]}, got {output.height}")
+        self.assertEqual(output.width, self.out_hw[1], f"Expected image width {self.out_hw[0]}, got {output.width}")
 
     def test_output_type_pil_image_format(self):
         inputs = self.get_dummy_inputs()
         output = remote_decode(output_type="pil", image_format="png", **inputs)
         self.assertTrue(isinstance(output, PIL.Image.Image), f"Expected `PIL.Image.Image` output, got {type(output)}")
         self.assertEqual(output.height, self.out_hw[0], f"Expected image height {self.out_hw[0]}, got {output.height}")
-        self.assertEqual(output.width, self.out_hw[1], f"Expected image width {self.out_hw[0]}, got {output.height}")
+        self.assertEqual(output.width, self.out_hw[1], f"Expected image width {self.out_hw[0]}, got {output.width}")
         self.assertEqual(output.format, "png", f"Expected image format `png`, got {output.format}")
 
     def test_output_type_pt_partial_postprocess(self):
@@ -75,7 +77,41 @@ class RemoteAutoencoderKLMixin:
         output = remote_decode(output_type="pt", partial_postprocess=True, **inputs)
         self.assertTrue(isinstance(output, PIL.Image.Image), f"Expected `PIL.Image.Image` output, got {type(output)}")
         self.assertEqual(output.height, self.out_hw[0], f"Expected image height {self.out_hw[0]}, got {output.height}")
-        self.assertEqual(output.width, self.out_hw[1], f"Expected image width {self.out_hw[0]}, got {output.height}")
+        self.assertEqual(output.width, self.out_hw[1], f"Expected image width {self.out_hw[0]}, got {output.width}")
+
+    def test_output_type_pt_return_type_pt(self):
+        inputs = self.get_dummy_inputs()
+        output = remote_decode(output_type="pt", return_type="pt", **inputs)
+        self.assertTrue(isinstance(output, torch.Tensor), f"Expected `torch.Tensor` output, got {type(output)}")
+        self.assertEqual(
+            output.shape[2], self.out_hw[0], f"Expected image height {self.out_hw[0]}, got {output.shape[2]}"
+        )
+        self.assertEqual(
+            output.shape[3], self.out_hw[1], f"Expected image width {self.out_hw[0]}, got {output.shape[3]}"
+        )
+
+    def test_output_type_pt_partial_postprocess_return_type_pt(self):
+        inputs = self.get_dummy_inputs()
+        output = remote_decode(output_type="pt", partial_postprocess=True, return_type="pt", **inputs)
+        self.assertTrue(isinstance(output, torch.Tensor), f"Expected `torch.Tensor` output, got {type(output)}")
+        self.assertEqual(
+            output.shape[1], self.out_hw[0], f"Expected image height {self.out_hw[0]}, got {output.shape[1]}"
+        )
+        self.assertEqual(
+            output.shape[2], self.out_hw[1], f"Expected image width {self.out_hw[0]}, got {output.shape[2]}"
+        )
+
+    def test_do_scaling_deprecation(self):
+        inputs = self.get_dummy_inputs()
+        inputs.pop("scaling_factor", None)
+        inputs.pop("shift_factor", None)
+        with self.assertWarns(FutureWarning) as warning:
+            _ = remote_decode(output_type="pt", partial_postprocess=True, **inputs)
+            self.assertEqual(
+                str(warning.warnings[0].message),
+                "`do_scaling` is deprecated, pass `scaling_factor` and `shift_factor` if required.",
+                str(warning.warnings[0].message),
+            )
 
 
 class RemoteAutoencoderKLSDv1Tests(
@@ -94,6 +130,6 @@ class RemoteAutoencoderKLSDv1Tests(
     )
     endpoint = "https://bz0b3zkoojf30bhx.us-east-1.aws.endpoints.huggingface.cloud/"
     dtype = torch.float16
-    scale_factor = 0.18215
+    scaling_factor = 0.18215
     shift_factor = None
     processor_cls = VaeImageProcessor
