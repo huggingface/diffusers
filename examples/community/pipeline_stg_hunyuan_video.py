b08766b4f1b7f5ce8a3cbad6b1b18919330f8fd9
Kinam Kim <63842546+kinam0252@users.noreply.github.com>
2025-03-07 01:18:48 +0900

    Update pipeline_stg_hunyuan_video.py

--- examples/community/pipeline_stg_hunyuan_video.py
+++ examples/community/pipeline_stg_hunyuan_video.py
@@ -58,7 +58,7 @@ EXAMPLE_DOC_STRING = """
 
         >>> # Configure STG mode options
         >>> stg_applied_layers_idx = [2]  # Layer indices from 0 to 41
-        >>> stg_scale = 1.0
+        >>> stg_scale = 1.0 # Set 0.0 for CFG
         >>> do_rescaling = False
 
         >>> output = pipe(
