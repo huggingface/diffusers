fa273fd17925a4694b7b2bcea102754dcdd45515
DN6 <dhruv.nair@gmail.com>
2025-03-10 16:47:18 +0530

    update

--- src/diffusers/models/model_loading_utils.py
+++ src/diffusers/models/model_loading_utils.py
@@ -41,8 +41,6 @@ from ..utils import (
     is_gguf_available,
     is_torch_available,
     is_torch_version,
-    is_torchao_available,
-    is_torchao_version,
     logging,
 )
 
@@ -59,38 +57,6 @@ _CLASS_REMAPPING_DICT = {
 if is_accelerate_available():
     from accelerate import infer_auto_device_map
     from accelerate.utils import get_balanced_memory, get_max_memory, offload_weight, set_module_tensor_to_device
-
-
-def _update_torch_safe_globals():
-    safe_globals = [
-        (torch.uint1, "torch.uint1"),
-        (torch.uint2, "torch.uint2"),
-        (torch.uint3, "torch.uint3"),
-        (torch.uint4, "torch.uint4"),
-        (torch.uint5, "torch.uint5"),
-        (torch.uint6, "torch.uint6"),
-        (torch.uint7, "torch.uint7"),
-    ]
-    try:
-        from torchao.dtypes import NF4Tensor
-        from torchao.dtypes.floatx.float8_layout import Float8AQTTensorImpl
-        from torchao.dtypes.uintx.uint4_layout import UInt4Tensor
-        from torchao.dtypes.uintx.uintx_layout import UintxAQTTensorImpl, UintxTensor
-
-        safe_globals.extend([UintxTensor, UInt4Tensor, UintxAQTTensorImpl, Float8AQTTensorImpl, NF4Tensor])
-
-    except (ImportError, ModuleNotFoundError) as e:
-        logger.warning(
-            "Unable to import `torchao` Tensor objects. This may affect loading checkpoints serialized with `torchao`"
-        )
-        logger.debug(e)
-
-    finally:
-        torch.serialization.add_safe_globals(safe_globals=safe_globals)
-
-
-if is_torch_version(">=", "2.6") and is_torchao_available() and is_torchao_version(">=", "0.7.0"):
-    _update_torch_safe_globals()
 
 
 # Adapted from `transformers` (see modeling_utils.py)
