c4879ffd7d794389124699cc442087c4a9839897
lawrence-cj <cjs1020440147@icloud.com>
2025-03-21 14:27:01 +0800

    change sample prompt;

--- src/diffusers/pipelines/sana/pipeline_sana_sprint.py
+++ src/diffusers/pipelines/sana/pipeline_sana_sprint.py
@@ -118,7 +118,7 @@ EXAMPLE_DOC_STRING = """
         ... )
         >>> pipe.to("cuda")
 
-        >>> image = pipe(prompt='a cyberpunk cat with a neon sign that says "Sana"')[0]
+        >>> image = pipe(prompt='a tiny astronaut hatching from an egg on the moon')[0]
         >>> image[0].save("output.png")
         ```
 """
