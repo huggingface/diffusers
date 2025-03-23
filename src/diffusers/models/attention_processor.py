999174537da3b84af7082eb832336501a358ec4c
LinchuanXUTheSEAAI <unilin54092@gmail.com>
2025-03-23 13:07:00 +0900

    fix bug

--- src/diffusers/models/attention_processor.py
+++ src/diffusers/models/attention_processor.py
@@ -2496,7 +2496,6 @@ class FluxAttnProcessor2_0:
                 ) 
                 txt_mask_downsample = txt_mask_downsample.to(dtype=query.dtype, device=query.device)
                 txt_mask_downsample[txt_mask_downsample>=0.5] *= (2.0 - img_ratio)
-
                 masked_txt_hidden_states = txt_hidden_states[:,512:,:] * txt_mask_downsample
 
                 
@@ -2507,7 +2506,7 @@ class FluxAttnProcessor2_0:
                     hidden_states.shape[2],
                 )
                 img_mask_downsample = img_mask_downsample.to(dtype=query.dtype, device=query.device)
-                img_mask_downsample[img_mask_downsample<=0.5] *= img_ratio
+                img_mask_downsample[img_mask_downsample<0.5] *= img_ratio
                 masked_img_hidden_states = img_hidden_states[:,729:,:] * img_mask_downsample
                 
                 hidden_states = torch.cat([txt_hidden_states[:,:-hidden_states.shape[1],:], img_hidden_states[:,:-hidden_states.shape[1],:], masked_txt_hidden_states + masked_img_hidden_states],dim=1)
