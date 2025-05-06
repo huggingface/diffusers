8cd92b5ad17d56580c38e78ac88c28afde4503a0
sayakpaul <spsayakpaul@gmail.com>
2025-05-06 15:33:36 +0530

    small.

--- tests/pipelines/test_pipelines.py
+++ tests/pipelines/test_pipelines.py
@@ -1997,7 +1997,9 @@ class PipelineSlowTests(unittest.TestCase):
         reason="Torch Dynamo isn't yet supported for Python 3.12.",
     )
     def test_from_save_pretrained_dynamo(self):
-        run_test_in_subprocess(test_case=self, target_func=_test_from_save_pretrained_dynamo, inputs=None)
+        torch.compiler.rest()
+        with torch._inductor.utils.fresh_inductor_cache():
+            run_test_in_subprocess(test_case=self, target_func=_test_from_save_pretrained_dynamo, inputs=None)
 
     def test_from_pretrained_hub(self):
         model_path = "google/ddpm-cifar10-32"
