24a46cc3c6e7e158f9f58043c32b7562154a0658
sayakpaul <spsayakpaul@gmail.com>
2025-05-15 18:05:05 +0530

    start overhauling the benchmarking suite.

---
+++ benchmarks/benchmarking_flux.py
@@ -0,0 +1,40 @@
+import torch
+
+from diffusers import FluxTransformer2DModel
+from diffusers.utils.testing_utils import torch_device
+
+from .benchmarking_utils import BenchmarkMixin
+
+
+class BenchmarkFlux(BenchmarkMixin):
+    model_class = FluxTransformer2DModel
+    compile_kwargs = {"fullgraph": True, "mode": "max-autotune"}
+
+    def get_model_init_dict(self):
+        return {"ckpt_id": "black-forest-labs/FLUX.1-dev", "subfolder": "transformer", "torch_dtype": torch.bfloat16}
+
+    def initialize_model(self):
+        model = self.model_class.from_pretrained(**self.get_model_init_dict())
+        model = model.to(torch_device).eval()
+        return model
+
+    def get_input_dict(self):
+        # resolution: 1024x1024
+        # maximum sequence length 512
+        hidden_states = torch.randn(1, 4096, 64, device=torch_device, dtype=torch.bfloat16)
+        encoder_hidden_states = torch.randn(1, 512, 4096, device=torch_device, dtype=torch.bfloat16)
+        pooled_prompt_embeds = torch.randn(1, 768, device=torch_device, dtype=torch.bfloat16)
+        image_ids = torch.ones(512, 3, device=torch_device, dtype=torch.bfloat16)
+        text_ids = torch.ones(4096, 3, device=torch_device, dtype=torch.bfloat16)
+        timestep = torch.tensor([1.0], device=torch_device)
+        guidance = torch.tensor([1.0], device=torch_device)
+
+        return {
+            "hidden_states": hidden_states,
+            "encoder_hidden_states": encoder_hidden_states,
+            "img_ids": image_ids,
+            "txt_ids": text_ids,
+            "pooled_projections": pooled_prompt_embeds,
+            "timestep": timestep,
+            "guidance": guidance,
+        }
---
+++ benchmarks/benchmarking_utils.py
@@ -0,0 +1,81 @@
+import gc
+
+import torch
+from torch.utils.benchmark import benchmark
+
+from diffusers.models.modeling_utils import ModelMixin
+from diffusers.utils.testing_utils import require_torch_gpu
+
+
+def benchmark_fn(f, *args, **kwargs):
+    t0 = benchmark.Timer(
+        stmt="f(*args, **kwargs)",
+        globals={"args": args, "kwargs": kwargs, "f": f},
+        num_threads=torch.get_num_threads(),
+    )
+    return f"{(t0.blocked_autorange().mean):.3f}"
+
+
+def flush():
+    gc.collect()
+    torch.cuda.empty_cache()
+    torch.cuda.reset_max_memory_allocated()
+    torch.cuda.reset_peak_memory_stats()
+
+
+@require_torch_gpu
+class BenchmarkMixin:
+    model_class: ModelMixin = None
+    compile_kwargs: dict = None
+
+    def get_model_init_dict(self):
+        raise NotImplementedError
+
+    def initialize_model(self):
+        raise NotImplementedError
+
+    def get_input_dict(self):
+        raise NotImplementedError
+
+    def pre_benchmark(self):
+        flush()
+        torch.compiler.reset()
+
+    def post_benchmark(self, model):
+        model.cpu()
+        flush()
+        torch.compiler.reset()
+
+    @torch.no_grad()
+    def run_benchmark(self):
+        self.pre_benchmark()
+
+        model = self.initialize_model()  # Takes care of device placement.
+        input_dict = self.get_input_dict()  # Takes care of device placement.
+
+        # warmup
+        for _ in range(5):
+            _ = model(**input_dict)
+
+        time = benchmark_fn(lambda model, input_dict: model(**input_dict), model, input_dict)
+        memory = torch.cuda.max_memory_allocated() / (1024**3)
+        memory = float(f"{memory:.2f}")
+        non_compile_stats = {"time": time, "memory": memory}
+
+        self.post_benchmark(model)
+        del model
+        self.pre_benchmark()
+
+        compile_stats = None
+        if self.compile_kwargs is not None:
+            model = self.initialize_model()
+            with torch._inductor.utils.fresh_inductor_cache():
+                model.compile(**self.compile_kwargs)
+                time = benchmark_fn(lambda model, input_dict: model(**input_dict), model, input_dict)
+            memory = torch.cuda.max_memory_allocated() / (1024**3)
+            memory = float(f"{memory:.2f}")
+            compile_stats = {"time": time, "memory": memory}
+
+        self.post_benchmark(model)
+        del model
+        return non_compile_stats, compile_stats
