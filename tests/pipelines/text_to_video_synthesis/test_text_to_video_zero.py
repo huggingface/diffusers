89eed1f07cd841d72cdb36a807ce6bbdd5190c4e
Matrix Yao <matrix.yao@intel.com>
2025-05-19 07:11:28 +0000

    update per comments
    Signed-off-by: Matrix Yao <matrix.yao@intel.com>

--- tests/pipelines/text_to_video_synthesis/test_text_to_video_zero.py
+++ tests/pipelines/text_to_video_synthesis/test_text_to_video_zero.py
@@ -55,7 +55,8 @@ class TextToVideoZeroPipelineSlowTests(unittest.TestCase):
         result = pipe(prompt=prompt, generator=generator).images
 
         expected_result = load_pt(
-            "https://huggingface.co/datasets/hf-internal-testing/diffusers-images/resolve/main/text-to-video/A bear is playing a guitar on Times Square.pt"
+            "https://huggingface.co/datasets/hf-internal-testing/diffusers-images/resolve/main/text-to-video/A bear is playing a guitar on Times Square.pt",
+            weights_only=False,
         )
 
         assert_mean_pixel_difference(result, expected_result)
