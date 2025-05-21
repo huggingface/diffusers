f7e5895044077585b45c6874de874038c57968da
Dhruv Nair <dhruv.nair@gmail.com>
2025-05-21 16:21:06 +0200

    update

--- src/diffusers/pipelines/pipeline_utils.py
+++ src/diffusers/pipelines/pipeline_utils.py
@@ -1663,8 +1663,10 @@ class DiffusionPipeline(ConfigMixin, PushToHubMixin):
         for k, v in inspect.signature(cls.__init__).parameters.items():
             if inspect.isclass(v.annotation):
                 signature_types[k] = (v.annotation,)
-            elif get_origin(v.annotation) == Union:
+            elif get_origin(v.annotation) in [Union]:
                 signature_types[k] = get_args(v.annotation)
+            elif get_origin(v.annotation) in [List, Dict, list, dict]:
+                signature_types[k] = (v.annotation,)
             else:
                 logger.warning(f"cannot get type annotation for Parameter {k} of {cls}.")
         return signature_types
