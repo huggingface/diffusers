09586472d01d4f1e55180bec640d035bd2bb99a7
Tolga Cangöz <mtcangoz@gmail.com>
2025-05-30 09:01:51 +0300

    add statistics

--- src/diffusers/pipelines/skyreels_v2/pipeline_skyreels_v2_diffusion_forcing.py
+++ src/diffusers/pipelines/skyreels_v2/pipeline_skyreels_v2_diffusion_forcing.py
@@ -946,6 +946,14 @@ class SkyReelsV2DiffusionForcingPipeline(DiffusionPipeline, WanLoraLoaderMixin):
 
                 if not output_type == "latent":
                     latents = latents.to(self.vae.dtype)
+                    latents_mean = (
+                        torch.tensor(self.vae.config.latents_mean)
+                        .view(1, self.vae.config.z_dim, 1, 1, 1)
+                        .to(latents.device, latents.dtype)
+                    )
+                    latents_std = 1.0 / torch.tensor(self.vae.config.latents_std).view(
+                        1, self.vae.config.z_dim, 1, 1, 1
+                    ).to(latents.device, latents.dtype)
                     latents = latents / latents_std + latents_mean
                     videos = self.vae.decode(latents, return_dict=False)[0]
                     if video is None:
