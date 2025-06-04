dc669ef506277f1b807ffa87a70f4591fc73d18c
LinchuanXUTheSEAAI <unilin54092@gmail.com>
2025-06-04 21:41:12 +0800

    fix bug

--- src/diffusers/pipelines/flux/pipeline_flux_controlnet.py
+++ src/diffusers/pipelines/flux/pipeline_flux_controlnet.py
@@ -477,6 +477,18 @@ class FluxControlNetPipeline(DiffusionPipeline, FluxLoraLoaderMixin, FromSingleF
 
         return ip_adapter_image_embeds
 
+    # Copied from diffusers.pipelines.stable_diffusion_3.pipeline_stable_diffusion_3_img2img.StableDiffusion3Img2ImgPipeline.get_timesteps
+    def get_timesteps(self, num_inference_steps, strength, device):
+        # get the original timestep using init_timestep
+        init_timestep = min(num_inference_steps * strength, num_inference_steps)
+
+        t_start = int(max(num_inference_steps - init_timestep, 0))
+        timesteps = self.scheduler.timesteps[t_start * self.scheduler.order :]
+        if hasattr(self.scheduler, "set_begin_index"):
+            self.scheduler.set_begin_index(t_start * self.scheduler.order)
+
+        return timesteps, num_inference_steps - t_start
+    
     def check_inputs(
         self,
         prompt,
