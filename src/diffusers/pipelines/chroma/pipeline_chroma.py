1bd8fdfcb6e43622a04e9477afd7cd7cfae4e441
Edna <88869424+Ednaordinary@users.noreply.github.com>
2025-06-11 20:56:27 -0600

    don't return length

--- src/diffusers/pipelines/chroma/pipeline_chroma.py
+++ src/diffusers/pipelines/chroma/pipeline_chroma.py
@@ -220,14 +220,12 @@ class ChromaPipeline(
             padding="max_length",
             max_length=max_sequence_length,
             truncation=True,
-            return_length=True,
+            return_length=False,
             return_overflowing_tokens=False,
             return_tensors="pt",
         )
         text_input_ids = text_inputs.input_ids
         untruncated_ids = self.tokenizer(prompt, padding="longest", return_tensors="pt").input_ids
-
-        #text_inputs.attention_mask[:, : text_inputs.length + 1] = 1.0
         
         prompt_embeds = self.text_encoder(
             text_input_ids.to(device),
