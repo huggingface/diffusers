9a449d434f271899d617f5427fa1c35bd7a387d1
Aryan <aryan@huggingface.co>
2025-06-19 04:04:57 +0200

    update

--- examples/community/llm_grounded_diffusion.py
+++ examples/community/llm_grounded_diffusion.py
@@ -1,4 +1,4 @@
-# Copyright 2024 Long Lian, the GLIGEN Authors, and The HuggingFace Team. All rights reserved.
+# Copyright 2025 Long Lian, the GLIGEN Authors, and The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
