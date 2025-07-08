e8c124d917e5f4d98ec37af19df7b67d4a58d1b9
sayakpaul <spsayakpaul@gmail.com>
2025-07-08 10:20:27 +0530

    update

--- src/diffusers/utils/testing_utils.py
+++ src/diffusers/utils/testing_utils.py
@@ -991,13 +991,18 @@ def pytest_terminal_summary_main(tr, id):
 
 
 # Adapted from https://github.com/huggingface/transformers/blob/000e52aec8850d3fe2f360adc6fd256e5b47fe4c/src/transformers/testing_utils.py#L1905
-def is_flaky(
-    max_attempts: int = 5,
-    wait_before_retry: Optional[float] = None,
-    description: Optional[str] = None,
-):
+def is_flaky(max_attempts: int = 5, wait_before_retry: Optional[float] = None, description: Optional[str] = None):
     """
     To decorate flaky tests (methods or entire classes). They will be retried on failures.
+
+    Args:
+        max_attempts (`int`, *optional*, defaults to 5):
+            The maximum number of attempts to retry the flaky test.
+        wait_before_retry (`float`, *optional*):
+            If provided, will wait that number of seconds before retrying the test.
+        description (`str`, *optional*):
+            A string to describe the situation (what / where / why is flaky, link to GH issue/PR comments, errors,
+            etc.)
     """
 
     def decorator(obj):
@@ -1026,7 +1031,6 @@ def is_flaky(
                         time.sleep(wait_before_retry)
                     retry_count += 1
 
-            # final attempt, let any exception bubble up
             return obj(*args, **kwargs)
 
         return wrapper
