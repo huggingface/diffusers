d66b92ddaf7a119680410c320060eea7d1cb8744
onkarsus13 <onkarsus13@gmail.com>
2025-08-02 03:43:45 -0500

    added fe chnages

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 from diffusers import AutoencoderKLMMQuant
 import torch
 
-model = AutoencoderKLMMQuant.from_pretrained("Wan-AI/Wan2.2-T2V-A14B-Diffusers", subfolder="vae", torch_dtype=torch.float32, cache_dir="/data2/onkar/video_diffusion_weights/VAE")
+model = AutoencoderKLMMQuant.from_pretrained("onkarsus13/MMVQVae", subfolder="vae", torch_dtype=torch.float32)
 
-print(model)
