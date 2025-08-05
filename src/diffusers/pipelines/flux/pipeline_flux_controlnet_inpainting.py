bd86502ec0875741d355c6108179c5d2ef6624af
DN6 <dhruv.nair@gmail.com>
2025-08-05 16:23:09 +0530

    update

--- src/diffusers/pipelines/flux/pipeline_flux_controlnet_inpainting.py
+++ src/diffusers/pipelines/flux/pipeline_flux_controlnet_inpainting.py
@@ -346,7 +346,7 @@ class FluxControlNetInpaintPipeline(DiffusionPipeline, FluxLoraLoaderMixin, From
     def encode_prompt(
         self,
         prompt: Union[str, List[str]],
-        prompt_2: Union[str, List[str]],
+        prompt_2: Optional[Union[str, List[str]]] = None,
         device: Optional[torch.device] = None,
         num_images_per_prompt: int = 1,
         prompt_embeds: Optional[torch.FloatTensor] = None,
