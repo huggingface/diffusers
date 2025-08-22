0605c836f7504565ddc3053f561abf6433811cfc
Sayak Paul <spsayakpaul@gmail.com>
2025-08-22 22:00:26 +0530

    Update src/diffusers/models/attention_flax.py
    Co-authored-by: Dhruv Nair <dhruv.nair@gmail.com>

--- src/diffusers/models/attention_flax.py
+++ src/diffusers/models/attention_flax.py
@@ -157,7 +157,7 @@ class FlaxAttention(nn.Module):
 
     def setup(self):
         logger.warning(
-            "Flax classes are deprecated and will be removed in Diffusers v1. We "
+            "Flax classes are deprecated and will be removed in Diffusers v1.0.0 We "
             "recommend migrating to PyTorch classes or pinning your version of Diffusers."
         )
 
