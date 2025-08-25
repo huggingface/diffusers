0d26c143fe66cfde87191751a6f778b54922913a
stevhliu <steven.liu@huggingface.co>
2025-08-25 10:15:59 -0700

    feedback

--- src/diffusers/__init__.py
+++ src/diffusers/__init__.py
@@ -160,15 +160,6 @@ else:
             "apply_pyramid_attention_broadcast",
         ]
     )
-    _import_structure["image_processor"] = [
-        "IPAdapterMaskProcessor",
-        "PixArtImageProcessor",
-        "VaeImageProcessor",
-        "VaeImageProcessorLDM3D",
-    ]
-    _import_structure["video_processor"] = [
-        "VideoProcessor",
-    ]
     _import_structure["models"].extend(
         [
             "AllegroTransformer3DModel",
@@ -840,12 +831,6 @@ if TYPE_CHECKING or DIFFUSERS_SLOW_IMPORT:
             apply_layer_skip,
             apply_pyramid_attention_broadcast,
         )
-        from .image_processor import (
-            IPAdapterMaskProcessor,
-            PixArtImageProcessor,
-            VaeImageProcessor,
-            VaeImageProcessorLDM3D,
-        )
         from .models import (
             AllegroTransformer3DModel,
             AsymmetricAutoencoderKL,
@@ -1005,8 +990,6 @@ if TYPE_CHECKING or DIFFUSERS_SLOW_IMPORT:
             VQDiffusionScheduler,
         )
         from .training_utils import EMAModel
-        from .video_processor import VideoProcessor
-
     try:
         if not (is_torch_available() and is_scipy_available()):
             raise OptionalDependencyNotAvailable()
