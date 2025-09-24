062c21cd94239edc5354ef4d1281d6c83a03aefd
yiyixuxu <yixu310@gmail.com>
2025-09-24 02:30:00 +0200

    fix imports

--- src/diffusers/pipelines/hunyuan_image/__init__.py
+++ src/diffusers/pipelines/hunyuan_image/__init__.py
@@ -22,7 +22,8 @@ except OptionalDependencyNotAvailable:
 
     _dummy_objects.update(get_objects_from_module(dummy_torch_and_transformers_objects))
 else:
-    _import_structure["pipeline_hunyuan_image"] = ["HunyuanImagePipeline"]
+    _import_structure["pipeline_hunyuanimage"] = ["HunyuanImagePipeline"]
+    _import_structure["pipeline_hunyuanimage_refiner"] = ["HunyuanImageRefinerPipeline"]
 
 if TYPE_CHECKING or DIFFUSERS_SLOW_IMPORT:
     try:
@@ -33,6 +34,7 @@ if TYPE_CHECKING or DIFFUSERS_SLOW_IMPORT:
         from ...utils.dummy_torch_and_transformers_objects import *
     else:
         from .pipeline_hunyuanimage import HunyuanImagePipeline
+        from .pipeline_hunyuanimage_refiner import HunyuanImageRefinerPipeline
 
 else:
     import sys
