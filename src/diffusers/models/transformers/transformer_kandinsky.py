d3b959750bc3e39e44bcd6910504a9e1b23260bd
Lev Novitskiy <57654885+leffff@users.noreply.github.com>
2025-10-17 22:46:34 +0300

    Update src/diffusers/models/transformers/transformer_kandinsky.py
    Co-authored-by: YiYi Xu <yixu310@gmail.com>

--- src/diffusers/models/transformers/transformer_kandinsky.py
+++ src/diffusers/models/transformers/transformer_kandinsky.py
@@ -616,7 +616,7 @@ class Kandinsky5Transformer3DModel(
         self,
         hidden_states: torch.Tensor,  # x
         encoder_hidden_states: torch.Tensor,  # text_embed
-        timestep: Union[torch.Tensor, float, int],  # time
+        timestep: torch.Tensor,  # time
         pooled_projections: torch.FloatTensor,  # pooled_text_embed
         visual_rope_pos: Tuple[int, int, int],
         text_rope_pos: torch.LongTensor,
