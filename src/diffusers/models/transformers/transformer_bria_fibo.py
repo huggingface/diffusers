dc77b316c5c7f4119d2b0dc0c3a60c048143137b
galbria <gal@bria.ai>
2025-10-28 10:11:46 +0000

    change Inspired by to Based on

--- src/diffusers/models/transformers/transformer_bria_fibo.py
+++ src/diffusers/models/transformers/transformer_bria_fibo.py
@@ -66,7 +66,7 @@ def _get_qkv_projections(attn: "BriaFiboAttention", hidden_states, encoder_hidde
     return _get_projections(attn, hidden_states, encoder_hidden_states)
 
 
-# Inspired by from diffusers.models.transformers.transformer_flux.FluxAttnProcessor FluxAttnProcessor->BriaFiboAttnProcessor, FluxAttention-> BriaFiboAttention
+# Based on from diffusers.models.transformers.transformer_flux.FluxAttnProcessor
 class BriaFiboAttnProcessor:
     _attention_backend = None
     _parallel_config = None
@@ -134,7 +134,7 @@ class BriaFiboAttnProcessor:
             return hidden_states
 
 
-# Inspired by from diffusers.models.transformers.transformer_flux.FluxAttention -> BriaFiboAttention
+# Based on from diffusers.models.transformers.transformer_flux.FluxAttention
 class BriaFiboAttention(torch.nn.Module, AttentionModuleMixin):
     _default_processor_cls = BriaFiboAttnProcessor
     _available_processors = [
@@ -308,7 +308,7 @@ class BriaFiboTextProjection(nn.Module):
 
 
 @maybe_allow_in_graph
-# Copied from diffusers.models.transformers.transformer_flux.FluxTransformerBlock
+# Based on from diffusers.models.transformers.transformer_flux.FluxTransformerBlock
 class BriaFiboTransformerBlock(nn.Module):
     def __init__(
         self, dim: int, num_attention_heads: int, attention_head_dim: int, qk_norm: str = "rms_norm", eps: float = 1e-6
