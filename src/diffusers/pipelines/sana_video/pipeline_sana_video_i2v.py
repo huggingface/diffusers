4d3e16549919dac415c4fe13f15c8a1fec477802
Junsong Chen <cjs1020440147@icloud.com>
2025-11-17 14:26:44 +0800

    Update src/diffusers/pipelines/sana_video/pipeline_sana_video_i2v.py
    Co-authored-by: dg845 <58458699+dg845@users.noreply.github.com>

--- src/diffusers/pipelines/sana_video/pipeline_sana_video_i2v.py
+++ src/diffusers/pipelines/sana_video/pipeline_sana_video_i2v.py
@@ -403,7 +403,7 @@ class SanaImageToVideoPipeline(DiffusionPipeline, SanaLoraLoaderMixin):
             negative_prompt_embeds = negative_prompt_embeds.view(batch_size * num_images_per_prompt, seq_len, -1)
 
             negative_prompt_attention_mask = negative_prompt_attention_mask.view(bs_embed, -1)
-            negative_prompt_attention_mask = negative_prompt_attention_mask.repeat(num_images_per_prompt, 1)
+            negative_prompt_attention_mask = negative_prompt_attention_mask.repeat(num_videos_per_prompt, 1)
         else:
             negative_prompt_embeds = None
             negative_prompt_attention_mask = None
