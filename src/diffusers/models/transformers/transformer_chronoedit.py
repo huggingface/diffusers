e5fed0133c0b8780d21104ae844e5f27959467aa
DefTruth <qiustudent_r@163.com>
2025-11-22 10:46:46 +0800

    fix style

--- src/diffusers/models/transformers/transformer_chronoedit.py
+++ src/diffusers/models/transformers/transformer_chronoedit.py
@@ -571,9 +571,9 @@ class ChronoEditTransformer3DModel(
             "hidden_states": ContextParallelInput(split_dim=1, expected_dims=3, split_output=False),
         },
         # Reference: https://github.com/huggingface/diffusers/pull/12660
-        # We need to disable the splitting of encoder_hidden_states because 
-        # the image_encoder consistently generates 257 tokens for image_embed. This causes 
-        # the shape of encoder_hidden_states—whose token count is always 769 (512 + 257) 
+        # We need to disable the splitting of encoder_hidden_states because
+        # the image_encoder consistently generates 257 tokens for image_embed. This causes
+        # the shape of encoder_hidden_states—whose token count is always 769 (512 + 257)
         # after concatenation—to be indivisible by the number of devices in the CP.
         "proj_out": ContextParallelOutput(gather_dim=1, expected_dims=3),
     }
