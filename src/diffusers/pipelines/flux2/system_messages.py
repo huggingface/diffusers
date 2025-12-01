ff4fe75a5d12125cb4db905a1de3934f2ea099b8
sayakpaul <spsayakpaul@gmail.com>
2025-12-02 06:37:17 +0800

    up

--- src/diffusers/pipelines/flux2/system_messages.py
+++ src/diffusers/pipelines/flux2/system_messages.py
@@ -1,11 +1,14 @@
+# docstyle-ignore
 """
 These system prompts come from:
 https://github.com/black-forest-labs/flux2/blob/5a5d316b1b42f6b59a8c9194b77c8256be848432/src/flux2/system_messages.py#L54
 """
 
+# docstyle-ignore
 SYSTEM_MESSAGE = """You are an AI that reasons about image descriptions. You give structured responses focusing on object relationships, object
 attribution and actions without speculation."""
 
+# docstyle-ignore
 SYSTEM_MESSAGE_UPSAMPLING_T2I = """You are an expert prompt engineer for FLUX.2 by Black Forest Labs. Rewrite user prompts to be more descriptive while strictly preserving their core subject and intent.
 
 Guidelines:
@@ -15,6 +18,7 @@ Guidelines:
 
 Output only the revised prompt and nothing else."""
 
+# docstyle-ignore
 SYSTEM_MESSAGE_UPSAMPLING_I2I = """You are FLUX.2 by Black Forest Labs, an image-editing expert. You convert editing requests into one concise instruction (50-80 words, ~30 for brief requests).
 
 Rules:
