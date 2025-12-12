7bc847ac389166c79efaaf64378e4b1d0590248b
hlky <hlky@hlky.ac>
2025-12-12 18:33:13 +0000

    check_dummies

--- src/diffusers/utils/dummy_pt_objects.py
+++ src/diffusers/utils/dummy_pt_objects.py
@@ -1762,6 +1762,21 @@ class WanVACETransformer3DModel(metaclass=DummyObject):
         requires_backends(cls, ["torch"])
 
 
+class ZImageControlNetModel(metaclass=DummyObject):
+    _backends = ["torch"]
+
+    def __init__(self, *args, **kwargs):
+        requires_backends(self, ["torch"])
+
+    @classmethod
+    def from_config(cls, *args, **kwargs):
+        requires_backends(cls, ["torch"])
+
+    @classmethod
+    def from_pretrained(cls, *args, **kwargs):
+        requires_backends(cls, ["torch"])
+
+
 class ZImageTransformer2DModel(metaclass=DummyObject):
     _backends = ["torch"]
 
