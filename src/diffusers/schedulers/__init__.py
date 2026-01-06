b7f6a5e39f3b7c093f857d96fe3b3dc2bfda78a7
yiyixuxu <yixu310@gmail.com>
2026-01-07 00:57:42 +0100

    import ltx scheduler

--- src/diffusers/schedulers/__init__.py
+++ src/diffusers/schedulers/__init__.py
@@ -169,6 +169,7 @@ if TYPE_CHECKING or DIFFUSERS_SLOW_IMPORT:
         from .scheduling_k_dpm_2_ancestral_discrete import KDPM2AncestralDiscreteScheduler
         from .scheduling_k_dpm_2_discrete import KDPM2DiscreteScheduler
         from .scheduling_lcm import LCMScheduler
+        from .scheduling_ltx_euler_ancestral_rf import LTXEulerAncestralRFScheduler
         from .scheduling_pndm import PNDMScheduler
         from .scheduling_repaint import RePaintScheduler
         from .scheduling_sasolver import SASolverScheduler
