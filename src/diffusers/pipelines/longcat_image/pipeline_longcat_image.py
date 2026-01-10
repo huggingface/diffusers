652cf26eb241ae3b1c1778d7b3b86335a70e2978
Yahweasel <git-rpo@yahweasel.com>
2026-01-10 09:32:03 -0500

    Don't attempt to move the text_encoder. Just move the generated_ids.

--- src/diffusers/pipelines/longcat_image/pipeline_longcat_image.py
+++ src/diffusers/pipelines/longcat_image/pipeline_longcat_image.py
@@ -262,8 +262,8 @@ class LongCatImagePipeline(DiffusionPipeline, FromSingleFileMixin):
 
         inputs = self.text_processor(text=all_text, padding=True, return_tensors="pt").to(device)
 
-        self.text_encoder.to(device)
         generated_ids = self.text_encoder.generate(**inputs, max_new_tokens=self.tokenizer_max_length)
+        generated_ids.to(device)
         generated_ids_trimmed = [out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)]
         output_text = self.text_processor.batch_decode(
             generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
