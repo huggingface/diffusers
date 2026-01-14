c22eed5a8ef7e471e3607fc20417eee8dc39826e
Pham Hong Vinh <phamvinh257@gmail.com>
2026-01-14 22:51:35 +0700

    run make style and quality

--- src/diffusers/pipelines/ltx2/utils.py
+++ src/diffusers/pipelines/ltx2/utils.py
@@ -1,4 +1,4 @@
-# Pre-trained sigma values for distilled model are taken from 
+# Pre-trained sigma values for distilled model are taken from
 # https://github.com/Lightricks/LTX-2/blob/main/packages/ltx-pipelines/src/ltx_pipelines/utils/constants.py
 DISTILLED_SIGMA_VALUES = [1.0, 0.99375, 0.9875, 0.98125, 0.975, 0.909375, 0.725, 0.421875]
 
