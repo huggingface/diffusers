f93ac2c7f6ed4e4b68df6b34dcdb416a9762c00e
David El Malih <david@billiv.fr>
2026-01-16 13:37:48 +0100

    fix

--- src/diffusers/schedulers/scheduling_cosine_dpmsolver_multistep.py
+++ src/diffusers/schedulers/scheduling_cosine_dpmsolver_multistep.py
@@ -44,8 +44,8 @@ class CosineDPMSolverMultistepScheduler(SchedulerMixin, ConfigMixin):
             The standard deviation of the data distribution. This is set to 1.0 in Stable Audio Open [1].
         sigma_schedule (`str`, defaults to `"exponential"`):
             Sigma schedule to compute the `sigmas`. Must be one of `"exponential"` or `"karras"`. The exponential
-            schedule was incorporated in [stabilityai/cosxl](https://huggingface.co/stabilityai/cosxl). The Karras schedule is
-            introduced in the [EDM](https://huggingface.co/papers/2206.00364) paper.
+            schedule was incorporated in [stabilityai/cosxl](https://huggingface.co/stabilityai/cosxl). The Karras
+            schedule is introduced in the [EDM](https://huggingface.co/papers/2206.00364) paper.
         num_train_timesteps (`int`, defaults to `1000`):
             The number of diffusion steps to train the model.
         solver_order (`int`, defaults to `2`):
