a232cd9d305442f6eb44f5b8dd94b33bceea8218
yiyixuxu <yixu310@gmail.com>
2026-01-21 12:29:12 +0100

    up

--- src/diffusers/modular_pipelines/flux2/modular_blocks_flux2.py
+++ src/diffusers/modular_pipelines/flux2/modular_blocks_flux2.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import PIL.Image
 from typing import List
+
+import PIL.Image
 import torch
 
 from ...utils import logging
