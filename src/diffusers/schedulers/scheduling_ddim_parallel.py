9e3cc02ceb4d70171766ba6150aa9e90f4750955
David El Malih <david@billiv.fr>
2026-01-23 18:15:22 +0100

    fix style

--- src/diffusers/schedulers/scheduling_ddim_parallel.py
+++ src/diffusers/schedulers/scheduling_ddim_parallel.py
@@ -407,10 +407,10 @@ class DDIMParallelScheduler(SchedulerMixin, ConfigMixin):
                 current instance of sample being created by diffusion process.
             eta (`float`): weight of noise for added noise in diffusion step.
             use_clipped_model_output (`bool`, defaults to `False`):
-                If `True`, compute "corrected" `model_output` from the clipped predicted original sample. This correction is necessary
-                because the predicted original sample is clipped to [-1, 1] when `self.config.clip_sample` is `True`. If no
-                clipping occurred, the "corrected" `model_output` matches the input and
-                `use_clipped_model_output` has no effect.
+                If `True`, compute "corrected" `model_output` from the clipped predicted original sample. This
+                correction is necessary because the predicted original sample is clipped to [-1, 1] when
+                `self.config.clip_sample` is `True`. If no clipping occurred, the "corrected" `model_output` matches
+                the input and `use_clipped_model_output` has no effect.
             generator (`torch.Generator`, *optional*):
                 Random number generator.
             variance_noise (`torch.Tensor`): instead of generating noise for the variance using `generator`, we
