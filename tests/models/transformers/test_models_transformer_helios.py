95a04089ff60bcb6fae0a606b97c4ccdf0756587
Daniel Gu <dgu8957@gmail.com>
2026-03-04 09:03:06 +0100

    Make get_dummy_inputs deterministic using self.generator

--- tests/models/transformers/test_models_transformer_helios.py
+++ tests/models/transformers/test_models_transformer_helios.py
@@ -92,16 +92,30 @@ class HeliosTransformer3DTesterConfig(BaseModelTesterConfig):
             generator=self.generator,
             device=torch_device,
         )
-        timestep = torch.randint(0, 1000, size=(batch_size,)).to(torch_device)
-        encoder_hidden_states = torch.randn((batch_size, sequence_length, text_encoder_embedding_dim)).to(torch_device)
+        timestep = torch.randint(0, 1000, size=(batch_size,), generator=self.generator).to(torch_device)
+        encoder_hidden_states = randn_tensor(
+            (batch_size, sequence_length, text_encoder_embedding_dim),
+            generator=self.generator,
+            device=torch_device,
+        )
         indices_hidden_states = torch.ones((batch_size, num_frames)).to(torch_device)
         indices_latents_history_short = torch.ones((batch_size, num_frames - 1)).to(torch_device)
         indices_latents_history_mid = torch.ones((batch_size, num_frames - 1)).to(torch_device)
         indices_latents_history_long = torch.ones((batch_size, (num_frames - 1) * 4)).to(torch_device)
-        latents_history_short = torch.randn((batch_size, num_channels, num_frames - 1, height, width)).to(torch_device)
-        latents_history_mid = torch.randn((batch_size, num_channels, num_frames - 1, height, width)).to(torch_device)
-        latents_history_long = torch.randn((batch_size, num_channels, (num_frames - 1) * 4, height, width)).to(
-            torch_device
+        latents_history_short = randn_tensor(
+            (batch_size, num_channels, num_frames - 1, height, width),
+            generator=self.generator,
+            device=torch_device,
+        )
+        latents_history_mid = randn_tensor(
+            (batch_size, num_channels, num_frames - 1, height, width),
+            generator=self.generator,
+            device=torch_device,
+        )
+        latents_history_long = randn_tensor(
+            (batch_size, num_channels, (num_frames - 1) * 4, height, width),
+            generator=self.generator,
+            device=torch_device,
         )
 
         return {
