3143b56017ccb82c926ceb9aef583c1eaf07d678
terarachang <qqq09289@gmail.com>
2026-03-12 08:32:54 +0000

    checkpoint lora weights during training

--- examples/cosmos/train_lora.sh
+++ examples/cosmos/train_lora.sh
@@ -1,16 +1,17 @@
 export MODEL_NAME="nvidia/Cosmos-Predict2.5-2B"
-export DATA_DIR="datasets/cosmos_nemo_assets"
-#export DATASET_NAME=""
+export DATA_DIR="datasets/benchmark_train/gr1"
 export TOKENIZERS_PARALLELISM=false
+revision='post-trained'
+echo $revision
 
 accelerate launch --mixed_precision="bf16" train_cosmos_predict25_lora.py \
-  --pretrained_model_name_or_path=$MODEL_NAME --revision pre-trained \
+  --pretrained_model_name_or_path=$MODEL_NAME --revision $revision \
   --dataset_name=$DATASET_NAME --train_data_dir=$DATA_DIR \
   --train_batch_size=1 \
-  --num_train_epochs=100 --checkpointing_steps=5000 \
-  --learning_rate=1e-04 --lr_scheduler="constant" --lr_warmup_steps=0 \
-  --seed=42 \
-  --output_dir="outputs/mydiffusers" \
+  --num_train_epochs=100 --checkpointing_epochs=10 \
+  --seed=0 \
+  --output_dir="outputs/gr1_ep100" \
   --validation_prompt="" \
   --report_to=wandb \
+  --height 432 --width 768 \
   --allow_tf32 --gradient_checkpointing \
