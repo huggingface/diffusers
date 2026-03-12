a0a68ee2b7e7caee88e3a1cad1955957c4d57723
Ting-Yun Chang <tingyunc@nvidia.com>
2026-03-12 14:45:33 +0000

    refactor

--- examples/cosmos/train_lora.sh
+++ examples/cosmos/train_lora.sh
@@ -1,16 +1,16 @@
 export MODEL_NAME="nvidia/Cosmos-Predict2.5-2B"
 export DATA_DIR="datasets/benchmark_train/gr1"
-export TOKENIZERS_PARALLELISM=false
 revision='post-trained'
 echo $revision
 
+export TOKENIZERS_PARALLELISM=false
 accelerate launch --mixed_precision="bf16" train_cosmos_predict25_lora.py \
   --pretrained_model_name_or_path=$MODEL_NAME --revision $revision \
-  --dataset_name=$DATASET_NAME --train_data_dir=$DATA_DIR \
+  --train_data_dir=$DATA_DIR \
   --train_batch_size=1 \
   --num_train_epochs=100 --checkpointing_epochs=10 \
   --seed=0 \
-  --output_dir="outputs/gr1_ep100" \
+  --output_dir="outputs/gr1" \
   --report_to=wandb \
   --height 432 --width 768 \
   --allow_tf32 --gradient_checkpointing \
