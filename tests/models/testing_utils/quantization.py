8a9013d52c9490db0cff8813a46530a9594cf537
jiqing-feng <jiqing.feng@intel.com>
2026-04-21 13:11:43 +0800

    fix format
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>

--- tests/models/testing_utils/quantization.py
+++ tests/models/testing_utils/quantization.py
@@ -825,7 +825,8 @@ class TorchAoConfigMixin:
         config_cls = getattr(_torchao_quantization, config_name)
         # TorchAO int4 quantization requires plain_int32 packing format on Intel XPU
         if torch_device == "xpu":
-            config_cls.int4_packing_format = "plain_int32"
+            return TorchAoConfig(config_cls(int4_packing_format="plain_int32"))
+
         return TorchAoConfig(config_cls())
 
     def _create_quantized_model(self, config_name, **extra_kwargs):
