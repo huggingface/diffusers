3049d4559e42105ed3f1aa72702efbad194dd7c5
Ting-Yun <root@az-sat-lepton-002-7-000002.o5dz02pxb1retmy3ybsofhqjle.jx.internal.cloudapp.net>
2026-04-24 06:04:06 +0000

    edit data preprocess script to avoid downloading cosmos2.5 repo

--- examples/cosmos/download_and_preprocess_datasets.sh
+++ examples/cosmos/download_and_preprocess_datasets.sh
@@ -3,15 +3,12 @@ train_dir=$dataset_dir/train
 test_dir=$dataset_dir/test
 
 # Download and Preprocess Training Dataset
-git clone https://github.com/nvidia-cosmos/cosmos-predict2.5.git
-cd cosmos-predict2.5
-
 hf download nvidia/GR1-100 --repo-type dataset --local-dir datasets/benchmark_train/hf_gr1/ && \
 mkdir -p datasets/benchmark_train/gr1/videos && \
 mv datasets/benchmark_train/hf_gr1/gr1/*mp4 datasets/benchmark_train/gr1/videos && \
 mv datasets/benchmark_train/hf_gr1/metadata.csv datasets/benchmark_train/gr1/
 
-python -m scripts.create_prompts_for_gr1_dataset --dataset_path datasets/benchmark_train/gr1
+python create_prompts_for_gr1_dataset.py --dataset_path datasets/benchmark_train/gr1
 
 # Download Eval Dataset
 hf download nvidia/EVAL-175 --repo-type dataset --local-dir dream_gen_benchmark
@@ -23,5 +20,3 @@ mv datasets/benchmark_train/gr1 $train_dir
 mv dream_gen_benchmark/gr1_object $test_dir
 echo Download training data to $train_dir
 echo Download test data to $test_dir
-mv $dataset_dir ..
-cd ..
