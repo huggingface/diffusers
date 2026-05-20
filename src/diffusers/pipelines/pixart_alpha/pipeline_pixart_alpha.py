45a881246788a1dd3097f5aff736bac9e1848aff
Jingya HUANG <44135271+JingyaHuang@users.noreply.github.com>
2026-05-20 17:31:49 +0200

    Apply suggestions from code review
    Co-authored-by: YiYi Xu <yixu310@gmail.com>

--- src/diffusers/pipelines/pixart_alpha/pipeline_pixart_alpha.py
+++ src/diffusers/pipelines/pixart_alpha/pipeline_pixart_alpha.py
@@ -862,7 +862,7 @@ class PixArtAlphaPipeline(DiffusionPipeline):
             prompt_attention_mask = torch.cat([negative_prompt_attention_mask, prompt_attention_mask], dim=0)
 
         # 4. Prepare timesteps
-        is_neuron_device = hasattr(device, "type") and device.type == "neuron"
+        is_neuron_device = device.type == "neuron"
         if XLA_AVAILABLE or is_neuron_device:
             timestep_device = "cpu"
         else:
