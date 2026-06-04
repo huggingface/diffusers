68749ff2e39cc3460d35e4c451de7dbe0a178956
DN6 <dhruv.nair@gmail.com>
2026-06-04 15:52:12 +0530

    update

--- tests/models/transformers/test_models_transformer_chroma.py
+++ tests/models/transformers/test_models_transformer_chroma.py
@@ -13,19 +13,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any
-
 import torch
 
 from diffusers import ChromaTransformer2DModel
-from diffusers.models.attention_processor import FluxIPAdapterJointAttnProcessor2_0
-from diffusers.models.embeddings import ImageProjection
 from diffusers.utils.torch_utils import randn_tensor
 
 from ...testing_utils import enable_full_determinism, torch_device
 from ..testing_utils import (
     BaseModelTesterConfig,
-    IPAdapterTesterMixin,
     LoraHotSwappingForModelTesterMixin,
     LoraTesterMixin,
     ModelTesterMixin,
@@ -34,50 +29,6 @@ from ..testing_utils import (
 
 
 enable_full_determinism()
-
-
-def create_chroma_ip_adapter_state_dict(model) -> dict[str, dict[str, Any]]:
-    ip_cross_attn_state_dict = {}
-    key_id = 0
-
-    for name in model.attn_processors.keys():
-        if name.startswith("single_transformer_blocks"):
-            continue
-
-        joint_attention_dim = model.config["joint_attention_dim"]
-        hidden_size = model.config["num_attention_heads"] * model.config["attention_head_dim"]
-        sd = FluxIPAdapterJointAttnProcessor2_0(
-            hidden_size=hidden_size, cross_attention_dim=joint_attention_dim, scale=1.0
-        ).state_dict()
-        ip_cross_attn_state_dict.update(
-            {
-                f"{key_id}.to_k_ip.weight": sd["to_k_ip.0.weight"],
-                f"{key_id}.to_v_ip.weight": sd["to_v_ip.0.weight"],
-                f"{key_id}.to_k_ip.bias": sd["to_k_ip.0.bias"],
-                f"{key_id}.to_v_ip.bias": sd["to_v_ip.0.bias"],
-            }
-        )
-        key_id += 1
-
-    image_projection = ImageProjection(
-        cross_attention_dim=model.config["joint_attention_dim"],
-        image_embed_dim=model.config["pooled_projection_dim"],
-        num_image_text_embeds=4,
-    )
-
-    ip_image_projection_state_dict = {}
-    sd = image_projection.state_dict()
-    ip_image_projection_state_dict.update(
-        {
-            "proj.weight": sd["image_embeds.weight"],
-            "proj.bias": sd["image_embeds.bias"],
-            "norm.weight": sd["norm.weight"],
-            "norm.bias": sd["norm.bias"],
-        }
-    )
-
-    del sd
-    return {"image_proj": ip_image_projection_state_dict, "ip_adapter": ip_cross_attn_state_dict}
 
 
 class ChromaTransformerTesterConfig(BaseModelTesterConfig):
@@ -179,22 +130,6 @@ class TestChromaTransformerTraining(ChromaTransformerTesterConfig, TrainingTeste
     def test_gradient_checkpointing_is_applied(self):
         expected_set = {"ChromaTransformer2DModel"}
         super().test_gradient_checkpointing_is_applied(expected_set=expected_set)
-
-
-class TestChromaTransformerIPAdapter(ChromaTransformerTesterConfig, IPAdapterTesterMixin):
-    @property
-    def ip_adapter_processor_cls(self):
-        return FluxIPAdapterJointAttnProcessor2_0
-
-    def modify_inputs_for_ip_adapter(self, model, inputs_dict):
-        torch.manual_seed(0)
-        cross_attention_dim = getattr(model.config, "joint_attention_dim", 32)
-        image_embeds = torch.randn(1, 1, cross_attention_dim).to(torch_device)
-        inputs_dict.update({"joint_attention_kwargs": {"ip_adapter_image_embeds": image_embeds}})
-        return inputs_dict
-
-    def create_ip_adapter_state_dict(self, model: Any) -> dict[str, dict[str, Any]]:
-        return create_chroma_ip_adapter_state_dict(model)
 
 
 class TestChromaTransformerLoRA(ChromaTransformerTesterConfig, LoraTesterMixin):
