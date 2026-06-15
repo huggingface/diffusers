e5c142210b61bfb464a5c8862954efb5f22be8e5
Sayak Paul <spsayakpaul@gmail.com>
2026-06-15 05:56:30 +0000

    component manager tests

--- tests/modular_pipelines/flux/test_modular_pipeline_flux.py
+++ tests/modular_pipelines/flux/test_modular_pipeline_flux.py
@@ -29,6 +29,7 @@ from diffusers.modular_pipelines import (
 )
 
 from ...testing_utils import floats_tensor, torch_device
+from ..test_components_manager import ComponentsManagerTesterMixin, ModularPipelineOffloadTesterMixin
 from ..test_modular_pipelines_common import ModularPipelineTesterMixin
 
 
@@ -45,7 +46,11 @@ FLUX_TEXT2IMAGE_WORKFLOWS = {
 }
 
 
-class TestFluxModularPipelineFast(ModularPipelineTesterMixin):
+class TestFluxModularPipelineFast(
+    ComponentsManagerTesterMixin,
+    ModularPipelineOffloadTesterMixin,
+    ModularPipelineTesterMixin,
+):
     pipeline_class = FluxModularPipeline
     pipeline_blocks_class = FluxAutoBlocks
     pretrained_model_name_or_path = "hf-internal-testing/tiny-flux-modular"
